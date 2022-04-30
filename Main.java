0162f876498f629e725b50a3a35f7aae0a3ad238
Hyunggi <phg9587@naver.com>
2022-04-30 16:51:54 +0900

    백준 소수 구하기 문제 [1929번]

--- Main.java
+++ Main.java
@@ -3,11 +3,49 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
+    static final boolean IS_NOT_PRIME = true;
+    static final boolean IS_PRIME = false;
+
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int stNum = Integer.parseInt(st.nextToken());
+        int fiNum = Integer.parseInt(st.nextToken());
+
+        //에라스토테네스의 체 방식을 사용하자. (https://ko.wikipedia.org/wiki/%EC%97%90%EB%9D%BC%ED%86%A0%EC%8A%A4%ED%85%8C%EB%84%A4%EC%8A%A4%EC%9D%98_%EC%B2%B4)
+        int MAX_NUM_LIMIT = 1000_000;
+        boolean[] eratosArray = new boolean[MAX_NUM_LIMIT+1]; //배열 과 사용할 Index를 맞추기 위해서 +1 해줌 , default는 모두 IS_PRIME이다.
+
+        eratosArray[0] = IS_NOT_PRIME; // 0 소수 아님
+        eratosArray[1] = IS_NOT_PRIME; // 1 소수 아님
+
+        //소수값을 미리 배열에 정의
+        for (int i = 2; i <= fiNum ; i++) {
+            if(eratosArray[i] == IS_NOT_PRIME)
+                continue;
+            
+            for (int j = 2; i*j <= fiNum; j++) {
+                eratosArray[i * j] = IS_NOT_PRIME;
+            }
+        }
+
+        for (int i = stNum; i <= fiNum; i++) {
+            if(eratosArray[i] == IS_PRIME)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+
+//안쓰는 코드 모음
+
 //        Scanner scanner = new Scanner(System.in);
 //        String testCaseNumStr = scanner.nextLine();
 //        int testCaseNum = Integer.parseInt(testCaseNumStr);
@@ -20,50 +58,11 @@ public class Main {
 //        Scanner scanner = new Scanner(System.in);
 //        int testValue = scanner.nextInt();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testValue = Integer.parseInt(br.readLine());
+//        int testValue = Integer.parseInt(br.readLine());
 
-        System.out.println(solveProblem(testValue));
-        }
-    //1 .. x   /   6  .. 2   /   11  .. 3
-    //2 .. x   /   7  .. x   /   12  .. 4
-    //3 .. 1   /   8  .. 2   /   13  .. 3
-    //4 .. X   /   9  .. 3   /   14  .. 4
-    //5 .. 1   /   10 .. 2   /   15  .. 3
 
-    private static int solveProblem(int testValue) {
-        int rtCount = 0;
-
-        if (testValue < 11) {
-            switch (testValue) {
-                case 1:
-                case 2:
-                case 4:
-                case 7:
-                    return -1;
-                case 3:
-                case 5:
-                    return 1;
-                case 6:
-                case 8:
-                case 10:
-                    return 2;
-                case 9:
-                    return 3;
-            }
-        } else {
-            rtCount = testValue / 5;
-            switch (testValue % 5) {
-                case 0:
-                    return rtCount;
-                case 1:
-                case 3:
-                    return rtCount + 1;
-                case 2:
-                case 4:
-                    return rtCount + 2;
-            }
-        }
-        return rtCount;
-    }
-}
+//    private static void solveProblem(int testValue) {
+//        if((testValue) == 0){
+//            System.out.println(testValue);
+//        }
+//    }
