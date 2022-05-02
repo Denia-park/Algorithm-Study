4cfc447e362d6b835e6666cad594d0f05971314b
Hyunggi <phg9587@naver.com>
2022-05-02 16:56:24 +0900

    백준 괄호 문제 [9012번]

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,33 +13,52 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        int stNum = Integer.parseInt(st.nextToken());
-        int fiNum = Integer.parseInt(st.nextToken());
+        int testCaseNum = Integer.parseInt(br.readLine());
+        String[] testCase = new String[testCaseNum];
+        for (int i = 0; i < testCaseNum; i++) {
+            testCase[i] = br.readLine();
+        }
 
-        //에라스토테네스의 체 방식을 사용하자. (https://ko.wikipedia.org/wiki/%EC%97%90%EB%9D%BC%ED%86%A0%EC%8A%A4%ED%85%8C%EB%84%A4%EC%8A%A4%EC%9D%98_%EC%B2%B4)
-        int MAX_NUM_LIMIT = 1000_000;
-        boolean[] eratosArray = new boolean[MAX_NUM_LIMIT+1]; //배열 과 사용할 Index를 맞추기 위해서 +1 해줌 , default는 모두 IS_PRIME이다.
+        //저장한 데이터를 for문으로 확인해보자.
+        for (int i = 0; i < testCaseNum; i++) {
+            solveProblem(testCase[i]);
+        }
+    }
 
-        eratosArray[0] = IS_NOT_PRIME; // 0 소수 아님
-        eratosArray[1] = IS_NOT_PRIME; // 1 소수 아님
+    static private void solveProblem(String inputString) {
+        //Stack 생성
+        Stack<Character> stack = new Stack<>();
 
-        //소수값을 미리 배열에 정의
-        for (int i = 2; i <= fiNum ; i++) {
-            if(eratosArray[i] == IS_NOT_PRIME)
-                continue;
-            
-            for (int j = 2; i*j <= fiNum; j++) {
-                eratosArray[i * j] = IS_NOT_PRIME;
+        char[] inputChars = inputString.toCharArray();
+
+        //처음부터 ) 이 들어오면 잘못된 문자열 => "NO"
+        if(inputChars[0] == ')'){
+            System.out.println("NO");
+            return;
+        }
+
+        for(int i = 0; i < inputChars.length; i++) {
+            // ( 이 들어오면 무조건 push
+            if(inputChars[i] == '('){
+                stack.push(inputChars[i]);
+            }
+            // ) 이 들어오면 무조건 pop한다. empty 라면 잘못된 문자열 => "NO"
+            else{
+                if(stack.empty()){
+                    System.out.println("NO");
+                    return;
+                }
+                else{
+                    stack.pop();
+                }
             }
         }
 
-        for (int i = stNum; i <= fiNum; i++) {
-            if(eratosArray[i] == IS_PRIME)
-            {
-                System.out.println(i);
-            }
+        //문자열을 한바퀴 돌렸는데 남는게 없으면 정상 , 남아있으면 잘못된 문자열 => "NO"
+        if(stack.empty()){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
         }
     }
 }
@@ -46,6 +66,7 @@ public class Main {
 
 //안쓰는 코드 모음
 
+//        *Sacnner*
 //        Scanner scanner = new Scanner(System.in);
 //        String testCaseNumStr = scanner.nextLine();
 //        int testCaseNum = Integer.parseInt(testCaseNumStr);
@@ -55,14 +76,13 @@ public class Main {
 //            testCaseArray[i] = scanner.nextLine();
 //        }
 
-//        Scanner scanner = new Scanner(System.in);
-//        int testValue = scanner.nextInt();
-
+//        *StringTokenizer*
+//        StringTokenizer st;
+//        st = new StringTokenizer(br.readLine());
+//        int stNum = Integer.parseInt(st.nextToken());
 //        int testValue = Integer.parseInt(br.readLine());
 
+//        *BufferedReader*
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int fiNum = Integer.parseInt(br.readLine());
 
-//    private static void solveProblem(int testValue) {
-//        if((testValue) == 0){
-//            System.out.println(testValue);
-//        }
-//    }
