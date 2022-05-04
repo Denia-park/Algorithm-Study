5827d103e8be754171cf6e0231465f7f87cc4c6b
Hyunggi <phg9587@naver.com>
2022-05-04 20:20:54 +0900

    컬렉션 스트림 생성

--- StreamExample.java
+++ StreamExample.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 //Stream
@@ -9,5 +10,10 @@ public class StreamExample {
         String[] arr = new String[]{"a", "b", "c"};
         Stream<String> stream = Arrays.stream(arr);
         Stream<String> streamOfArrayPart = Arrays.stream(arr, 1, 3);
+
+        List<String> list = Arrays.asList("a", "b", "c");
+        Stream<String> stream2 = list.stream();
+        Stream<String> parallelStream = list.parallelStream(); // 병렬 처리 스트림
+    }
     }
 }
