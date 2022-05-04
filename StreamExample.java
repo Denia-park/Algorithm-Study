eb418c9ea3134e72d92f13b5ff5eb8db7adc72fc
Hyunggi <phg9587@naver.com>
2022-05-04 21:01:37 +0900

    Stream.generate() 생성
    generate 메소드를 이용하면 Supplier<T> 에 해당하는 람다로 값을 넣을 수 있습니다.
    Supplier<T> 는 인자는 없고 리턴값만 있는 함수형 인터페이스죠. 람다에서 리턴하는 값이 들어갑니다.
    특정사이즈로 최대 크기를 제한해야함

--- StreamExample.java
+++ StreamExample.java
@@ -21,6 +21,13 @@ public class StreamExample {
                 .add("Elena")
                 .add("Java")
                 .build(); // [Eric, Elena, Java]
+
+        //generate 메소드를 이용하면 supplier<T> 에 해당하는 람다로 값을 넣을 수 있다.
+        //Supplier<T>는 인자는 없고 리턴값만 있는 함수형 인터페이스.
+        //람다에서 리턴하는 값이 들어간다.
+        //이때 생성되는 스트림은 크기가 정해짐 X [무한] -> 특정 사이즈로 최대 크기를 제한해야 한다.
+        Stream<String> generateStream =
+                Stream.generate(() -> "gen").limit(5); // 5개의 "gen"이 들어간 스트림 생성
     }
 
     public Stream<String> streamOf(List<String> list){
