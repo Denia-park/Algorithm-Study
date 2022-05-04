aeeea19959ccf16f7cd74def29c09c85c9c7d6b2
Hyunggi <phg9587@naver.com>
2022-05-04 18:03:05 +0900

    백준 균형잡힌 세상 문제 [4949번]

--- Main.java
+++ Main.java
@@ -8,15 +8,14 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCaseNum = Integer.parseInt(br.readLine());
-        String[] testCase = new String[testCaseNum];
-        for (int i = 0; i < testCaseNum; i++) {
-            testCase[i] = br.readLine();
-        }
 
-        //저장한 데이터를 for문으로 확인해보자.
-        for (int i = 0; i < testCaseNum; i++) {
-            solveProblem(testCase[i]);
+        // "." 이 나올때까지 돌아야함
+        while(true) {
+            String inputStr = br.readLine();
+            if(inputStr.equals(".")){
+                break;
+            }
+            solveProblem(inputStr);
         }
     }
 
@@ -27,36 +26,50 @@ public class Main {
         //String을 비교하기 좋게 CharArray로 변경
         char[] inputChars = inputString.toCharArray();
 
-        //처음부터 ) 이 들어오면 잘못된 문자열 => "NO"
-        if(inputChars[0] == ')'){
-            System.out.println("NO");
-            return;
-        }
-
         //CharArray를 for문으로 비교하자
         for(int i = 0; i < inputChars.length; i++) {
-            // ( 이 들어오면 무조건 push
-            if(inputChars[i] == '('){
+            if(isNotBraket(inputChars[i])){
+                continue;
+            }
+            // (  , [이 들어오면 무조건 push
+            if(inputChars[i] == '(' || inputChars[i] == '['){
                 stack.push(inputChars[i]);
             }
-            // ) 이 들어오면 무조건 pop한다. empty 라면 잘못된 문자열 => "NO"
+            // ) 이 들어오면 무조건 pop한다. empty 라면 잘못된 문자열 => "no"
             else{
                 if(stack.empty()){
-                    System.out.println("NO");
+                    System.out.println("no");
                     return;
                 }
                 else{
-                    stack.pop();
+                    if(inputChars[i] == ')'){
+                        if(stack.pop() != '('){
+                            System.out.println("no");
+                            return;
+                        }
+                    }else if(inputChars[i] == ']'){
+                        if(stack.pop() != '['){
+                            System.out.println("no");
+                            return;
+                        }
+                    }
                 }
             }
         }
 
-        //문자열을 한바퀴 돌렸는데 남는게 없으면 정상 , 남아있으면 잘못된 문자열 => "NO"
+        //문자열을 한바퀴 돌렸는데 남는게 없으면 정상 , 남아있으면 잘못된 문자열 => "no"
         if(stack.empty()){
-            System.out.println("YES");
+            System.out.println("yes");
         }else{
-            System.out.println("NO");
+            System.out.println("no");
         }
+    }
+
+    private static boolean isNotBraket(char inputChar) {
+        return inputChar != '(' &&
+                inputChar != ')' &&
+                inputChar != '[' &&
+                inputChar != ']';
     }
 }
 
@@ -81,5 +94,10 @@ public class Main {
 
 //        *BufferedReader*
 //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        int fiNum = Integer.parseInt(br.readLine());
+//
+//        int testCaseNum = Integer.parseInt(br.readLine());
+//        String[] testCase = new String[testCaseNum];
+//        for (int i = 0; i < testCaseNum; i++) {
+//            testCase[i] = br.readLine();
+//        }
 
