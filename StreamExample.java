b97d485fa7a33d090d32746f9b3c2c558ff33cda
Hyunggi <phg9587@naver.com>
2022-05-04 21:04:54 +0900

    Stream.iterate()
    초기값과 해당 값을 다루는 람다를 이용해서 스트림에 들어갈 요소를 만든다.

--- StreamExample.java
+++ StreamExample.java
@@ -28,6 +28,13 @@ public class StreamExample {
         //이때 생성되는 스트림은 크기가 정해짐 X [무한] -> 특정 사이즈로 최대 크기를 제한해야 한다.
         Stream<String> generateStream =
                 Stream.generate(() -> "gen").limit(5); // 5개의 "gen"이 들어간 스트림 생성
+
+        //iterate 메소드를 이용하면 초기값과 해당ㄱ밧을 다루는 람다를 이용하여 스트림에 들어갈 요소를 만든다.
+        //예제는 30이 초기값이고 값이 2씩 증가하는 값들이 들어간다.
+        //요소가 다음 요소의 인풋으로 들어간다.
+        //이 방법도 스트림의 사이즈가 무한하기 때문에 특정 사이즈로 제한해야함
+        Stream<Integer> iterateStream =
+                Stream.iterate(30, n -> n + 2).limit(5);//[30,32,34,36,38]
     }
 
     public Stream<String> streamOf(List<String> list){
