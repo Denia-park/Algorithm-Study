4cbe73269dddb1d46eb1fabfea4ad10993928be7
Hyunggi <phg9587@naver.com>
2022-05-15 20:55:50 +0900

    백준 잃어버린 괄호 문제 [1541번]

--- Main.java
+++ Main.java
@@ -4,37 +4,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // N의 진짜 약수의 개수
-        int testCase = Integer.parseInt(br.readLine());
-        int[] inputArray = new int[testCase];
+        String calculation = br.readLine();
 
-        // N의 진짜 약수
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < testCase; i++) {
-            inputArray[i] = Integer.parseInt(st.nextToken());
-        }
-
-        solveProblem(inputArray);
+        solveProblem(calculation);
     }
 
-    static private void solveProblem(int[] inputArray) {
-        int arrSize = inputArray.length;
-        //arrSzie == 1
-        //제곱값 이므로 그냥 제곱 하고 끝
+    static private void solveProblem(String intputString) {
+        char[] chars = intputString.toCharArray();
+        int[] numArr = new int[50];
+        int numIndex = 0;
+        int numArrIndex = 0;
+        int symbol = 1; // plus 1 , minus -1
 
-        //arrSize != 1
-        //약수들을 정렬하고 약수중에 제일 작은 값 * 제일 큰 값을 하면 어떤 수 N이 나옴
-        //12의 약수 => [1, 2, 3, 4, 6, 12]
-        Arrays.sort(inputArray);
+        for (int i = 0; i < chars.length; i++) {
+            if(chars[i] == '+' || chars[i] == '-'){
+                String temp = intputString.substring(numIndex, i);
+                numArr[numArrIndex++] = symbol * Integer.parseInt(temp);
+                numIndex = i + 1;
+                if(chars[i] == '-')
+                    symbol = -1;
+            }
+        }
+        String temp = intputString.substring(numIndex, chars.length);
+        numArr[numArrIndex++] = symbol * Integer.parseInt(temp);
 
-        System.out.println(inputArray[0] * inputArray[arrSize - 1]);
+        int totalSum = 0;
+        for (int i = 0; i < 50 ; i ++){
+            totalSum += (numArr[i]);
+        }
+
+        System.out.println(totalSum);
     }
 }
 //안쓰는 코드 모음
