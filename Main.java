ca04691fa104f80bb4637c96634a284f537350e5
Hyunggi <phg9587@naver.com>
2022-05-27 01:00:32 +0900

    백준 나무 자르기 문제 [2805번] - 시간 초과 걸림

--- Main.java
+++ Main.java
@@ -3,49 +3,48 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main{
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        int f[] = new int[T+1];
-        int cache[] = new int[T+1];
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int treeNum = Integer.parseInt(st.nextToken());
+        int needLength = Integer.parseInt(st.nextToken());
 
-        for(int i = 1; i<=T; i++){
-            f[i] = Integer.parseInt(br.readLine());
+        int[] trees = new int[treeNum];
+
+        st = new StringTokenizer(br.readLine());
+
+        int curMaxTreeHeight = 0;
+        int curCutterHeight = 0;
+        int curTotalLenghth = 0;
+        int curMinusValue = 1;
+
+        for(int i = 0; i<trees.length; i++){
+            trees[i] = Integer.parseInt(st.nextToken());
+            if(curMaxTreeHeight < trees[i])
+                curMaxTreeHeight = trees[i];
         }
 
-        cache[1] = f[1];
+        while(curTotalLenghth != needLength){
+            curTotalLenghth = 0;
+            curCutterHeight = curMaxTreeHeight - curMinusValue;
+            curMinusValue++;
 
-        if(T == 1){
-            System.out.println(cache[1]);
-            return;
+            for (int i = 0; i < treeNum; i++) {
+                if(trees[i] > curCutterHeight)
+                    curTotalLenghth += trees[i] - curCutterHeight;
+            }
+
         }
 
-        cache[2] = f[2]+f[1];
-
-        if(T == 2){
-            System.out.println(cache[2]);
-            return;
-        }
-
-        cache[3]= Math.max(f[1]+f[3], f[2]+f[3]);
-
-        if(T == 3){
-            System.out.println(cache[3]);
-            return;
-        }
-
-        for(int i=4; i<=T; i++){
-            cache[i] = Math.max(cache[i-3]+f[i]+f[i-1], cache[i-2]+f[i]);
-        }
-        System.out.println(cache[T]);
+        System.out.println(curCutterHeight);
     }
 }
-
-// 출처: https://sundries-in-myidea.tistory.com/22 [얇고 넓은 개발 블로그]
 
 //안쓰는 코드 모음
 
