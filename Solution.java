a0e9f18b2a99c980332010588c054b79b5358788
Hyunggi <phg9587@naver.com>
2022-07-09 21:22:15 +0900

    프로그래머스 정렬 - K번째수 [선택정렬 구현 - 시간복잡도 : O(N^2)]

--- Solution.java
+++ Solution.java
@@ -26,7 +26,9 @@ class Solution {
             for (int i = (arrCutStart - 1) , j = 0; i < arrCutFinal; i++,j++) {
                 notSortingArr[j] = numbers[i];
             }
-            Arrays.sort(notSortingArr);
+//            Arrays.sort(notSortingArr); // 기존 라이브러리 사용
+            sort(notSortingArr,true);
+            System.out.println(Arrays.toString(notSortingArr));
 
             answer[index] = notSortingArr[answerIndex - 1];
             index++;
@@ -34,4 +36,30 @@ class Solution {
 
         return answer;
     }
+
+    //선택정렬
+    //선택 정렬은 N-1 만큼 가장 작은 수를 찾아서 맨 앞으로 보내야 한다.
+    //또한 매번 가장 작은 수를 찾기 위해 비교 연산이 필요
+    //빅오 표기법 O(N^2)
+    static private void sort(int[] arr , boolean ascending){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if(ascending) {
+                    if(arr[i] > arr[j]){
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+                else{
+                    if (arr[i] < arr[j]) {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+    }
 }
+
