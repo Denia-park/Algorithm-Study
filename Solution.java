e3035a694be093a2ea919a659eb1aadee99840bd
Hyunggi <phg9587@naver.com>
2022-07-23 13:05:07 +0900

    프로그래머스 월간 코드 챌린지 시즌2 약수의 개수와 덧셈
    쉬운 문제

--- Solution.java
+++ Solution.java
@@ -1,34 +1,42 @@
 package com.company;
 
-import java.util.Arrays;
-
 class Solution {
     static public void main(String[] args) {
-        System.out.println((solution(new int[]{1,3,2,5,4} , 9))==3);
-        System.out.println((solution(new int[]{2,2,3,3} , 10))==4);
+        System.out.println((solution(13, 17)) == 43);
+        System.out.println((solution(24, 27)) == 52);
     }
 
-    static public int solution(int[] d, int budgetLimit) {
+    static public int solution(int left, int right) {
         int answer = 0;
-        int sumValue = 0;
 
-        //오름차순 크기로 정렬
-        Arrays.sort(d);
-
-        //부서에서 요청한 금액을 처음부터 1개씩 더하면서 예산을 넘나 안넘나 확인
-        for (int wantBudget : d) {
-            //sumValue에 부서에서 요청한 값을 1개씩 더함
-            sumValue = sumValue + wantBudget;
-            //더한 값이 예산기준을 넘지 않았으면 answer를 +1개
-            //넘었으면 break 걸어서 for문을 나가고 answer를 return
-            if (sumValue <= budgetLimit) {
-                answer++;
-            } else {
-                break;
+        for (int i = left; i <= right; i++) {
+            int commonDivisor = getCommonDivisorNum(i);
+            int tempVal = i;
+            //짝수이면 더하기
+            if( commonDivisor % 2 == 0){
+            }else { //홀수이면 빼기
+                tempVal = -1 * tempVal;
             }
+
+            answer = answer + tempVal;
         }
 
         return answer;
+    }
+
+    private static int getCommonDivisorNum(int num) {
+        int rtVal = 0;
+
+        //num에 해당하는 값으로는 나누지 않고 밑에서 그냥 +1을 따로 해주자.
+        for (int i = 1; i < num; i++) {
+            if(num % i ==0 ){
+                rtVal++;
+            }
+        }
+
+        rtVal++; // num에 해당하는 값을 나누면 무조건 공약수가 되므로, +1 해준다. [위에서 num을 포함 안하고 for문을 돌리기 때문]
+
+        return rtVal;
     }
 }
 
