80397d2404b39dc557e7a439305d197bf574c040
DESKTOP-PPNQK13\com2022 <phg9587@naver.com>
2022-08-01 14:44:30 +0900

    프로그래머스 깊이/너비 우선 탐색(DFS/BFS) 타겟 넘버
    문제 풀이 진행중

--- Solution.java
+++ Solution.java
@@ -1,55 +1,55 @@
-package com.company;
-
-import java.util.HashMap;
-
 public class Solution {
     static public void main(String[] args) {
-        System.out.println(solution(1).equals("1") );
-        System.out.println(solution(2).equals("2") );
-        System.out.println(solution(3).equals("4") );
-        System.out.println(solution(4).equals("11") );
-        System.out.println(solution(5).equals("12") );
-        System.out.println(solution(6).equals("14") );
-        System.out.println(solution(7).equals("21") );
-        System.out.println(solution(8).equals("22") );
-        System.out.println(solution(9).equals("24") );
-        System.out.println(solution(10).equals("41") );
+        int[] quiz1 = { 1, 1, 1, 1, 1 };
+        int quizTarget1 = 3;
+        int[] quiz2 = { 4, 1, 2, 1 };
+        int quizTarget2 = 4;
+
+        System.out.println(solution(quiz1, quizTarget1) == 5);
+        System.out.println(solution(quiz2, quizTarget2) == 2);
     }
 
-    //124의 나라는 숫자 3개만 쓰므로 3진법이라 생각을 하고 DIV_VALUE 를 3으로 둔다.
-    static int DIV_VALUE = 3;
+    static int answer;
+    static int target;
+    static char[] operators = {'+','-'};
+    static int[] numbers;
 
-    static public String solution(int n) {
-        String answer = "";
+    static public int solution(int[] quizNumbers, int quizTarget) {
+        answer = 0;
+        target = quizTarget;
+        numbers = quizNumbers;
+        int index = 0;
 
-        answer = solveWithRecursive(n);
-
+        for (char operator : operators) {
+            answer += dfs(operator,index,target);
+        }
+        
         return answer;
     }
 
-    static public String solveWithRecursive(int n) {
-        // n이 0인 경우 나눌게 없으므로 ""을 return 한다.
-        if(n == 0)
-            return "";
+    private static int dfs(char op, int index, int quizTarget) {
+        int tempAnswer = 0;
 
-        // 1 2 4 의 나라에 맞게 Array 를 정렬 , 3으로 나눴을때 0이 나오면 숫자 4가 들어가므로 0번쨰 인자가 "4"이다
-        int[] strangeCounturyNumberArray = {4,1,2};
-
-        //몫을 구한다.
-        int div = n / DIV_VALUE;
-        //나머지를 구한다.
-        int mod = n % DIV_VALUE;
-
-        //몫이 없는 경우 앞에 붙일 숫자가 없으므로 바로 1 2 4 중 하나의 값을 String 으로 만들어 Return
-        if (div == 0) {
-            return String.valueOf(strangeCounturyNumberArray[mod]);
+        if(numbers.length == index && quizTarget == 0){
+            return 1;
+        }else if (numbers.length == index && quizTarget != 0){
+            return 0;
         }
-        //몫이 있는 경우 앞에 붙여야 하는 숫자가 있으므로 재귀함수를 한번 더 탄다.
-        else {
-            //3의 배수인 경우 몫을 1만큼 빼줘야 정상적으로 값이 나온다.
-            if(mod == 0)
-                div -= 1;
-            return solveWithRecursive(div) + strangeCounturyNumberArray[mod];
+
+        if(op == '+'){
+            quizTarget = quizTarget - numbers[index];
         }
+        //op == '-'
+        else{
+            quizTarget = quizTarget + numbers[index];
+        }
+
+        //재귀함수 타기
+        index++;
+        for (char operator : operators) {
+            tempAnswer += dfs(operator,index,quizTarget);
+        }
+
+        return tempAnswer;
     }
 }
