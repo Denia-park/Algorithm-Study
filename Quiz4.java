38ad4c6601916d7bfb09ccd81b5b83f9ebdef3e6
Hyunggi <phg9587@naver.com>
2022-08-02 16:35:37 +0900

    프로그래머스 1차 모의고사 4번 문제 - 풀이 중 , 테스트케이스 3 6 실패
    무조건 행을 먼저 뒤집고 열을 뒤집는 방법을 썼었는데, 행 보다 열을 먼저 뒤집을 경우 더 빨리 Targe t이 되는 경우도 있음을 발견하고 (예외 케이스 발견)
    행 우선 뒤집기 vs 열 먼저 뒤집기 비교해서 더 작은 값을 return으로 사용함 (2,9번 해결)

--- Quiz4.java
+++ Quiz4.java
@@ -30,46 +30,86 @@ public class Quiz4 {
                 {0, 0, 0}
         };
 
-        System.out.println(solution(quiz1, target1) == 5);
-        System.out.println(solution(quiz2, target2) == -1);
+        int[][] quiz3 = {
+                {0,0,0,0,1,0},
+                {0,0,0,0,1,0},
+                {0,0,0,0,1,0}
+        };
+        int[][] target3 = {
+                {1,0,0,0,0,1},
+                {1,0,0,0,0,1},
+                {1,0,0,0,0,1}
+        };
+//        int[][] quiz3 = {
+//                {0,0,0},
+//                {0,0,0},
+//                {0,0,0}
+//        };
+//        int[][] target3 = {
+//                {0,0,0},
+//                {0,0,0},
+//                {0,0,0}
+//        };
+
+
+//        System.out.println(solution(quiz1, target1) == 5);
+//        System.out.println(solution(quiz2, target2) == -1);
+        System.out.println(solution(quiz3, target3));
     }
 
     static int[][] startBoard;
 
     static public int solution ( int[][] beginning, int[][] target){
-        int answer = 0;
-        startBoard = beginning;
+        int[] answer = new int[2];
 
         int whileCount = 0;
 
-        while(!Arrays.deepEquals(startBoard, target)){
-            //row 비교
-            for (int i = 0; i < beginning.length; i++) {
-                if(beginning[i][0] != target[i][0]){
-                    reverseRow(i);
-                    answer++;
-                }
+        int i = 0;
+
+        int odd = 0;
+
+        for (int j = 0; j < answer.length; j++) {
+
+            odd = j;
+            whileCount = 0;
+
+            startBoard = new int[beginning.length][beginning[0].length];
+            for(int jj=0; jj<startBoard.length; jj++){
+                System.arraycopy(beginning[jj], 0, startBoard[jj], 0, beginning[0].length);
             }
 
-            //column 비교
-            for (int i = 0; i < beginning[0].length; i++) {
-                if(beginning[0][i] != target[0][i]){
-                    reverseColumn(i);
-                    answer++;
+            while(!Arrays.deepEquals(startBoard, target)){
+                if(odd == 0){
+                    //row 비교
+                    for (i = 0; i < beginning.length; i++) {
+                        if(startBoard[i][0] != target[i][0]){
+                            reverseRow(i);
+                            answer[j]++;
+                        }
+                    }
+                    odd = 1;
+                }else{
+                    //column 비교
+                    for (i = 0; i < beginning[0].length; i++) {
+                        if(startBoard[0][i] != target[0][i]){
+                            reverseColumn(i);
+                            answer[j]++;
+                        }
+                    }
+                    odd = 0;
                 }
-            }
 
-            whileCount ++;
+                whileCount ++;
 
-            if(whileCount > 10){
-                System.out.println("해당 알고리즘은 실패 ㅠㅠ");
-                return -1;
+                if(whileCount > 30){
+                    System.out.println("해당 알고리즘은 실패 ㅠㅠ");
+                    answer[j] = -1;
+                    break;
+                }
             }
         }
 
-        System.out.println(Arrays.deepToString(startBoard));
-
-        return answer;
+        return Math.min(answer[0], answer[1]);
     }
 
     //가로줄 뒤집기 메서드
