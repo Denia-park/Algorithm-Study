acfda91b75da2b48428ff6422c882a59988d0ca8
Hyunggi <phg9587@naver.com>
2022-08-02 17:17:12 +0900

    프로그래머스 1차 모의고사 4번 문제 - 해결 (완전순회 문제 , 사각형의 모든면을 순회하면 된다.)

--- Quiz4.java
+++ Quiz4.java
@@ -31,26 +31,11 @@ public class Quiz4 {
         };
 
         int[][] quiz3 = {
-                {0,0,0,0,1,0},
-                {0,0,0,0,1,0},
-                {0,0,0,0,1,0}
+                {0,0,0,0,0,0,1,1,1,0},
         };
         int[][] target3 = {
-                {1,0,0,0,0,1},
-                {1,0,0,0,0,1},
-                {1,0,0,0,0,1}
+                {1,0,1,0,1,0,0,0,0,0},
         };
-//        int[][] quiz3 = {
-//                {0,0,0},
-//                {0,0,0},
-//                {0,0,0}
-//        };
-//        int[][] target3 = {
-//                {0,0,0},
-//                {0,0,0},
-//                {0,0,0}
-//        };
-
 
 //        System.out.println(solution(quiz1, target1) == 5);
 //        System.out.println(solution(quiz2, target2) == -1);
@@ -61,15 +46,15 @@ public class Quiz4 {
 
     static public int solution ( int[][] beginning, int[][] target){
         int[] answer = new int[2];
+        int[] answer1 = new int[2];
 
         int whileCount = 0;
-
         int i = 0;
-
+        //행 , 열 중에 어떤 것을 먼저 순회할지 정할 변수
         int odd = 0;
 
+        //사각형의 왼쪽면 , 윗쪽면 순회하는 Loop
         for (int j = 0; j < answer.length; j++) {
-
             odd = j;
             whileCount = 0;
 
@@ -78,6 +63,7 @@ public class Quiz4 {
                 System.arraycopy(beginning[jj], 0, startBoard[jj], 0, beginning[0].length);
             }
 
+            //2개의 사각형이 일치할떄까지 While문 순회
             while(!Arrays.deepEquals(startBoard, target)){
                 if(odd == 0){
                     //row 비교
@@ -101,7 +87,7 @@ public class Quiz4 {
 
                 whileCount ++;
 
-                if(whileCount > 30){
+                if(whileCount > 20){
                     System.out.println("해당 알고리즘은 실패 ㅠㅠ");
                     answer[j] = -1;
                     break;
@@ -109,7 +95,56 @@ public class Quiz4 {
             }
         }
 
-        return Math.min(answer[0], answer[1]);
+        //사각형의 오른쪽면 , 아래쪽면 순회하는 Loop
+        for (int j = 0; j < answer1.length; j++) {
+            odd = j;
+            whileCount = 0;
+
+            startBoard = new int[beginning.length][beginning[0].length];
+            for(int jj=0; jj<startBoard.length; jj++){
+                System.arraycopy(beginning[jj], 0, startBoard[jj], 0, beginning[0].length);
+            }
+
+            //2개의 사각형이 일치할떄까지 While문 순회
+            while(!Arrays.deepEquals(startBoard, target)){
+                if(odd == 0){
+                    //row 비교
+                    for (i = 0; i < beginning.length; i++) {
+                        if(startBoard[i][beginning[0].length - 1] != target[i][beginning[0].length - 1]){
+                            reverseRow(i);
+                            answer1[j]++;
+                        }
+                    }
+                    odd = 1;
+                }else{
+                    //column 비교
+                    for (i = 0; i < beginning[0].length; i++) {
+                        if(startBoard[beginning.length - 1][i] != target[beginning.length - 1][i]){
+                            reverseColumn(i);
+                            answer1[j]++;
+                        }
+                    }
+                    odd = 0;
+                }
+
+                whileCount ++;
+
+                if(whileCount > 20){
+                    System.out.println("해당 알고리즘은 실패 ㅠㅠ");
+                    answer1[j] = -1;
+                    break;
+                }
+            }
+        }
+
+        //사각형을 완전 순회 하면서 최솟값을 찾아야함
+        //1. 왼쪽 면 순회 + 윗쪽 면 순회 + 오른쪽 면 순회 + 아래쪽 면 순회
+        //2. 윗쪽 면 순회 + 왼쪽 면 순회 + 오른쪽 면 순회 + 아래쪽 면 순회
+        //3. 왼쪽 면 순회 + 윗쪽 면 순회 + 아래쪽 면 순회 + 오른쪽 면 순회
+        //4. 윗쪽 면 순회 + 왼쪽 면 순회 + 아래쪽 면 순회 + 오른쪽 면 순회
+
+        //위에서 나온 4가지 케이스 중에서 가장 최솟값을 return 해주면 된다.
+        return Math.min(Math.min(answer[0], answer[1]), Math.min(answer1[0], answer1[1]));
     }
 
     //가로줄 뒤집기 메서드
