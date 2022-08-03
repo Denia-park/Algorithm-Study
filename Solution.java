7773793c0f10cd1532d2cb0b75570c8004d1b147
Hyunggi <phg9587@naver.com>
2022-08-03 20:57:15 +0900

    2017 카카오코드 예선 카카오프렌즈 컬러링북
    DFS로 풀이 진행

--- Solution.java
+++ Solution.java
@@ -1,80 +1,73 @@
 package com.company;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 
 public class Solution {
     static public void main(String[] args) {
-        int[] quiz1 = { 1, 1, 1, 1, 1 };
-        int quizTarget1 = 3;
-        int[] quiz2 = { 4, 1, 2, 1 };
-        int quizTarget2 = 4;
 
-        System.out.println(solution(quiz1, quizTarget1) == 5);
-        System.out.println(solution(quiz2, quizTarget2) == 2);
+        int m = 6;
+        int n = 4;
+        int[][] picture = {{1, 1, 1, 0},{1, 2, 2, 0},{1, 0, 0, 1},{0, 0, 0, 1},{0, 0, 0, 3},{0, 0, 0, 3} };
+
+        System.out.println(Arrays.toString(solution(m, n, picture)));
     }
 
-    //bfs 에서 값을 계산할떄 사용이 되어야 하므로 전역변수로 설정
-    static int[] numbers;
+    static int numberOfArea;
+    static int maxSizeOfOneArea;
+    static int pictureHeight;
+    static int pictureWidth;
+    static int[][] pictureGraph;
+    static int standardValue;
+    static int tempWidthValue;
 
-    static public int solution(int[] quizNumbers, int target) {
-        int answer = 0;
-        numbers = quizNumbers;
-        //너비 우선 탐색으로 진행을 할때 visited를 확인해야 하는데 index를 사용해서 대신 확인 , 처음에는 0으로 설정
-        int index = 0;
+    static public int[] solution(int height, int width, int[][] picture) {
+        numberOfArea = 0;
+        maxSizeOfOneArea = 0;
+        pictureGraph = new int[picture.length][picture[0].length];
+        for (int i = 0; i < picture.length; i++) {
+            System.arraycopy(picture[i], 0, pictureGraph[i], 0, picture[i].length);
+        }
+        System.out.println(Arrays.deepToString(pictureGraph));
+        pictureHeight = height;
+        pictureWidth = width;
+        tempWidthValue = 0; // 이름 변경하기 임시로 너비의 값을 가지고 있다는 것으로
+        int[] answer = new int[2];
 
-        answer = bfs(index, target);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if(pictureGraph[y][x] != 0 ){
+                    numberOfArea++; // 해당 if문에 들어올때마다 영역의 수 ++ , 연결된 영역은 한번에 다 visited 처리를 할 것이므로 또 방문하지 않기 때문에
+                    tempWidthValue = 0; // 해당 영역의 너비를 구할때 사용할 임시 변수
+                    standardValue = pictureGraph[y][x]; // 같은 영역인지 판단을 해줄 변수 , 내가 처음에 진입하는 그 좌표의 값을 할당한다. , 좌표의 값과 같은 부분들만 영역으로 인정한다.
+                    dfs(y, x);
+                    maxSizeOfOneArea = Math.max(maxSizeOfOneArea, tempWidthValue); //위에서 정의했던 checkValue를 사용하여 지금까지 최대 영역을 구해서 대입
+                }
+            }
+        }
 
+        answer[0] = numberOfArea;
+        answer[1] = maxSizeOfOneArea;
         return answer;
     }
 
-    private static int bfs(int index, int target) {
-        //answer 에 값을 넘기기 전에 임시로 값을 가지고 있을 임시 변수
-        int tempAnswer = 0;
-        //너비 우선 탐색시에 사용되는 계산을 마무리 지을 최종 인덱스
-        final int FINAL_INDEX = numbers.length - 1;
-
-        //BFS를 수행할때 필요한 Queue를 생성
-        Queue<Number> queue = new LinkedList<Number>();
-
-        //index가 0인 (=너비가 제일 안쪽인 ) 노드들을 Queue에 추가
-        queue.add(new Number(numbers[index], index)); // + 기호일때
-        queue.add(new Number(-numbers[index], index)); // - 기호일때
-
-        // Queue가 빌때까지 while문을 수행
-        while (!queue.isEmpty()) {
-            //Queue에 있는 첫번째 노드를 꺼냄
-            Number tempNumber = queue.poll();
-
-            //해당 노드의 Index를 확인
-            //Final Index 면 제일 바깥까지 너비를 확인한 경우이므로 Node의 Value가 Target가 일치하는지 확인
-            if(tempNumber.index == FINAL_INDEX){
-                //Node의 Value가 Target가 일치하는지 확인 => 일치한다면 옳은 경우 이므로 tempAnswer를 ++ 한다.
-                if (tempNumber.value == target) {
-                    tempAnswer++;
-                }
-            }else{
-                //해당 노드의 Index가 Final Index가 아니면 아직 마지막까지 가지 못했으므로 index를 ++ 해주고 Visted 처리
-                tempNumber.index += 1;
-                //index를 올려주면서 numbers에 해당하는 값을 더해주거나 빼준다.
-                queue.add(new Number(tempNumber.value + numbers[tempNumber.index], tempNumber.index)); // + 기호일때
-                queue.add(new Number(tempNumber.value - numbers[tempNumber.index], tempNumber.index)); // - 기호일때
-            }
+    private static void dfs(int y, int x) {
+        if (y < 0 || x < 0 || x >= pictureWidth || y >= pictureHeight || pictureGraph[y][x] == 0) {
+            return;
         }
-        //while문을 돌면서 다 더해진 tempAnswer를 Return
-        return tempAnswer;
+
+        if(pictureGraph[y][x] == standardValue){
+            tempWidthValue++;
+        }else{
+            return;
+        }
+
+        //Visited 처리
+        pictureGraph[y][x] = 0;
+
+        dfs(y-1, x);
+        dfs(y, x+1);
+        dfs(y+1, x);
+        dfs(y, x-1);
     }
 }
 
-//BFS 에서 사용할 Number Class를 만들어 준다.
-class Number {
-    //numbers에 값들을 더하거나 빼서 가지고 있을 value
-    int value;
-    //해당 Node의 index가 몇인지 확인해줄 index 변수
-    int index;
-
-    public Number(int value, int index) {
-        this.value = value;
-        this.index = index;
-    }
-}
