e89990eb20cbfd730c7726fa081155fbdf941140
Hyunggi <phg9587@naver.com>
2022-08-05 00:07:18 +0900

    프로그래머스 2차 모의고사 3번 문제 - 완료 (BFS 사용 , 목적지를 기준으로 BFS 구하기)
    발상의 전환

--- Quiz3.java
+++ Quiz3.java
@@ -8,7 +8,7 @@ import java.util.Queue;
 public class Quiz3 {
     static public void main(String[] args) {
 
-//        System.out.println(Arrays.toString(solution(3, new int[][]{{1, 2}, {2, 3}}, new int[]{2, 3}, 1)));
+        System.out.println(Arrays.toString(solution(3, new int[][]{{1, 2}, {2, 3}}, new int[]{2, 3}, 1)));
         System.out.println(Arrays.toString(solution(5, new int[][]{{1, 2}, {1, 4}, {2, 4}, {2, 5}, {4, 5}}, new int[]{1, 3, 5}, 5)));
 //        System.out.println(Arrays.toString(solution(6, new int[][]{{1, 2}, {1, 3}, {2, 5}, {3, 4},{3,6}, {4, 5},{4,6}}, new int[]{2,6,1,4,5}, 5)));
     }
@@ -16,20 +16,18 @@ public class Quiz3 {
     static ArrayList<ArrayList<Integer>> graph;
     static int answerArrIndex;
     static int dest;
-    static boolean[] visited;
+    static int[] visited;
     static int[] answer;
-    static int[][] disArr;
     static public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        //초기값을 -1로 해두고, 업데이트를 안하면 개는 도달하지 못한 아이.
         answer = new int[sources.length];
- 
+
         dest = destination;
 
         graph = new ArrayList<ArrayList<Integer>>(n);
 
         //그래프 초기화 (node랑 Index랑 맞추기 위해서 + 1 해줌)
         for (int i = 0; i < n+1; i++) {
-            graph.add(new ArrayList<Integer>());
+            graph.add(new ArrayList<Integer>(2000));
         }
         //그래프 내용 업데이트, 서로 연결이기 때문에 양쪽으로 연결해준다.
         for (int[] road : roads) {
@@ -37,24 +35,17 @@ public class Quiz3 {
             graph.get(road[1]).add(road[0]);
         }
 
-        //거리를 적어둔 2차원 배열
-        disArr = new int[n+1][n+1];
-        for (int i = 1; i < n+1; i++) {
-            for (int j = 1; j < n+1; j++) {
-                disArr[i][j] = -1;
-            }
+        visited = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            visited[i] = -1;
         }
 
-        for (int source = 1; source <= n; source++) {
-            visited = new boolean[n+1];
-            bfs(source, 0);
-        }
-
-//        System.out.println(Arrays.deepToString(disArr));
+        bfs(dest, 0);
 
         answerArrIndex = 0;
         for (int source : sources) {
-            answer[answerArrIndex] = disArr[source][dest];
+            answer[answerArrIndex] = visited[source];
+
             answerArrIndex++;
         }
 
@@ -67,20 +58,17 @@ public class Quiz3 {
         //집어넣기
         queue.add(new Node(source, distance));
         // visited
-        disArr[source][source] = distance;
-        visited[source] = true;
+        visited[source] = distance;
 
         while (!queue.isEmpty()) {
             Node tempNode = queue.poll();
 
-            disArr[source][tempNode.source] = tempNode.distance;
-
             ArrayList<Integer> graphOfEachSource = graph.get(tempNode.source);
 
             for (Integer eachSource : graphOfEachSource) {
-                if (!visited[eachSource]) {
+                if (visited[eachSource] == -1) {
                     queue.add(new Node(eachSource, tempNode.distance + 1));
-                    visited[eachSource] = true;
+                    visited[eachSource] = tempNode.distance + 1;
                 }
             }
         }
