92ea3cbe55aae1bf9ba3b2dfaae40722b2666400
Hyunggi <phg9587@naver.com>
2022-08-07 19:10:15 +0900

    프로그래머스 힙(Heap) 더 맵게 - PriorityQueue : 주석 추가 Ver

--- Solution.java
+++ Solution.java
@@ -13,30 +13,42 @@ public class Solution {
         System.out.println(solution(scoville2, K));
     }
 
+    // 참고용 블로그 추천
+    // 1. https://coding-factory.tistory.com/603 [[Java] PriorityQueue(우선순위 큐) 클래스 사용법 & 예제 총정리]
+    // 2. https://st-lab.tistory.com/205 [배열을 이용한 Heap (힙) 구현하기]
+
     static public int solution(int[] scoville, int K) {
         int answer = 0;
 
-        //PriorityQueue 생성 : minValue를 제일 위로 보낸다.
+        //PriorityQueue 생성 : minValue를 제일 위로 보낸다. , maxHeap을 하려면 Collections.reverseOrder() 를 넣자.
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
+        //PriorityQueue의 내용을 채운다.
         for (int sco : scoville) {
             pq.add(sco);
         }
 
+        //사이즈가 1 초과 일때 계속해서 비교를 한다.
         while ( pq.size() > 1 ) {
+            //PriorityQueue의 제일 Min 값이 K보다 높으면 루프를 멈춘다.
             if(pq.peek() >= K)
                 return answer;
 
+            //합치기 위해서 2개의 값을 꺼낸다.
             int scoville0 = pq.poll();
             int scoville1 = pq.poll();
 
+            //2개의 스코빌을 합친다.
             int newScoville = scoville0 + scoville1 * 2;
 
+            //합친 스코빌을 다시 PriorityQueue에 넣는다.
             pq.add(newScoville);
 
+            //섞었기 때문에 answer을 1개 올린다.
             answer++;
         }
 
+        //PriorityQueue의 사이즈가 1인데 해당 원소가 K보다 작을 경우 -1을 리턴한다.
         if(pq.peek() >= K)
             return answer;
 
