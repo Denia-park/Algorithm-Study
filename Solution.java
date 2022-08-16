e3c8675e5d3e84622090e735d482d7a8f1e444bf
Hyunggi <phg9587@naver.com>
2022-08-16 15:06:48 +0900

    프로그래머스 스택/큐 프린터

--- Solution.java
+++ Solution.java
@@ -1,40 +1,89 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
     static public void main(String[] args) {
         Solution testSolution = new Solution();
 
-        System.out.println(testSolution.solution(8,1,2));
-        System.out.println(testSolution.solution(8,3,2));
-        System.out.println(testSolution.solution(8,4,7));
+        int[] quizArr1 = {2, 1, 3, 2};
+        int quizInt1 = 2;
+        int[] quizArr2 = {1, 1, 9, 1, 1, 1};
+        int quizInt2 = 0;
+
+        System.out.println(testSolution.solution(quizArr1, quizInt1));
+        System.out.println(testSolution.solution(quizArr2, quizInt2));
 
     }
 
-    public int solution(int n, int a, int b)
-    {
-        //처음에 바로 만나면 1라운드만에 만난 것
-        int answer = 1;
+    public int solution(int[] priorities, int target) {
+        int answer = 0;
 
-        // while 문에서 조건을 비교하고 값을 변경할 변수, 처음에는 할당 받은 번호를 사용함
-        int afterA = a;
-        int afterB = b;
+        //프린터 인쇄 대기열을 저장해둘 Queue 생성
+        Queue<Paper> queue = new LinkedList<Paper>();
 
-        //둘이서 승부를 하기 위해서는 바로 인접한 번호이여야 함 => Math.abs 를 사용하여 1 차이가 나는지 확인
-        //그리고 두개가 1차이는 나지만 옆 조 인지 승부하는 번호인지 확인이 필요함
-            //예를 들어) 2 , 3 번은 승부하는 번호가 아니고 서로 옆 조이다.
-            // (1, 2) , (3, 4) 같은 케이스가 승부하는 번호
-            // 승부하는 번호들로 확인해보면 두개가 2로 나눴을때 반드시 달라야 한다.
-        while (!(Math.abs(afterA-afterB) == 1 && (afterA/2 != afterB/2))){
-            // 승리하면 다음 라운드의 번호 지정 룰 => (현재 번호 + 1) / 2
-                // 예를 들어 3번이 이기면 2번이 된다.
-                // 그러므로 +1 하고 2로 정수 나누기를 하면 승리시 받을 번호가 됨.
-            afterA = (afterA + 1) / 2;
-            afterB = (afterB + 1) / 2;
+        //현재 대기열을 Queue 에 다 집어넣는다.
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(new Paper(priorities[i], i));
+        }
 
-            //다음 라운드 진출했으므로 결과에 +1를 해준다.
-            answer++;
+        //우선 순위대로 뽑기 위해서 정렬을 수행
+        Integer[] sortedDescPriorities = new Integer[priorities.length]; //Integer 배열 생성
+        Arrays.setAll(sortedDescPriorities, index -> priorities[index]); //priorities int[] 를 Integer[]로 변경
+        Arrays.sort(sortedDescPriorities, Collections.reverseOrder()); //Integer[] 을 reverseOrder 로 정렬
+
+        //현재 우선순위 몇번째까지 처리되었는지 확인할 Index
+        int priorIndex = 0;
+
+        //target에 해당할때 가지 while Loop 를 돈다.
+        while(true){
+            //Queue에서 poll 해서 값을 꺼낼때 필요한 변수
+            Paper headValue = null;
+
+            //queue 가 비어있지 않으면 꺼낸다.
+            if(!queue.isEmpty())
+                headValue = queue.poll();
+
+            //Queue에서 꺼낸 값이 현재 우선순위에 맞는 인쇄물인지 확인
+                //다르면 queue 맨 뒤에 추가
+            if(headValue.getPriority() != sortedDescPriorities[priorIndex] ){
+                queue.add(headValue);
+            }
+            //우선순위가 맞으면 우선순위 Index를 1 올린다.
+            //인쇄물이 뽑혔으므로 answer를 1 올린다.
+            //현재 뽑은 인쇄물의 index가 target가 동일하면 break문을 통해 while을 빠져나온다.
+            else{
+                priorIndex++;
+                answer++;
+                if(headValue.getIndex() == target)
+                    break;
+            }
         }
 
         return answer;
+    }
+}
+
+//인쇄물을 나타낼 class
+class Paper{
+    int priority; //해당 인쇄물 우선순위
+    int index; //해당 인쇄물의 Index
+
+    //생성자
+    public Paper(int priority, int index) {
+        this.priority = priority;
+        this.index = index;
+    }
+
+    //Getter
+    public int getPriority() {
+        return priority;
+    }
+
+    public int getIndex() {
+        return index;
     }
 }
