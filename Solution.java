7881fe7d6298c177d470c3b5cd407144e39a4ff6
Hyunggi <phg9587@naver.com>
2022-08-17 23:22:27 +0900

    프로그래머스 해시 전화번호 목록 - 풀이 완료 , 자료구조 사용 X , 아이디어 구현
    사전순 정렬을 하고 for문을 돌면서 뒤에 있는 문자열만 비교해서 값을 내는 방식을 사용함

--- Solution.java
+++ Solution.java
@@ -1,7 +1,6 @@
 package com.company;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class Solution {
     static public void main(String[] args) {
@@ -10,38 +9,29 @@ public class Solution {
         String[] quizArr1 = {"119", "97674223", "1195524421"};
         String[] quizArr2 = {"123", "456", "789"};
         String[] quizArr3 = {"12", "123", "1235", "567", "88"};
+        String[] quizArr4 = {"115524421", "119", "1197674223", "1234", "1235", "456", "567", "789", "88"};
 
         System.out.println(testSolution.solution(quizArr1));
         System.out.println(testSolution.solution(quizArr2));
         System.out.println(testSolution.solution(quizArr3));
+        System.out.println(testSolution.solution(quizArr4));
 
     }
 
     public boolean solution(String[] phone_book) {
         boolean answer = true;
 
-        //정렬
-        Arrays.sort(phone_book,new MyComparator());
+        //정렬 - 문자열 정렬이므로 사전순으로 정렬됨
+        Arrays.sort(phone_book);
 
-//        System.out.println(Arrays.toString(phone_book));
-
-        for (int i = 0; i < phone_book.length; i++) {
-            for (int j = i+1; j < phone_book.length; j++) {
-                if(phone_book[j].contains(phone_book[i])) return false;
-            }
+        // 사전순 정렬이면 비슷한 단어끼리 정렬이 되고
+        // 길이가 짧은 문자열이 무조건 앞에 오므로
+        // 앞에 있는 문자열이 뒤에 있는 문자열의 시작문자 인지 확인만 하면 된다.
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if(i + 1 < phone_book.length && phone_book[i + 1].startsWith(phone_book[i]))
+                return false;
         }
 
         return answer;
-    }
-}
-
-class MyComparator implements Comparator<String> {
-
-    @Override
-    public int compare(String o1, String o2) {
-        if (o1.length() < o2.length()) {
-            return -1;
-        }
-        return 1;
     }
 }
