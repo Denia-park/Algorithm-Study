0ef5cf3430ed332ff129f097ace0c90641b9afa7
Hyunggi <phg9587@naver.com>
2022-08-21 15:17:00 +0900

    프로그래머스 완전탐색 소수 찾기 - 풀이 완료 - DFS , 에라스토테네스의 채 사용
    DFS 를 통해 순열을 , 에라스토테네스의 채를 통해 소수를 구해서 풀이 완료

--- Main.java
+++ Main.java
@@ -5,19 +5,13 @@ public class Main {
         Solution testSolution = new Solution();
 
 //        String[] quizArr1_a = {"AN", "CF", "MJ", "RT", "NA"};
-        int[] quizArr1_b = {6, 10, 2};
+//        int[] quizArr1_b = {6, 10, 2};
 //        String[] quizArr2_a = {"TR", "RT", "TR"};
-        int[] quizArr2_b = {3, 30, 34, 5, 9};
-        int[] quizArr3_b = {3, 30, 34};
+//        int[] quizArr2_b = {3, 30, 34, 5, 9};
 
-//        System.out.println(testSolution.solution(quizArr1_b).equals("6210"));
-//        System.out.println(testSolution.solution(quizArr2_b).equals("9534330"));
-//        System.out.println(testSolution.solution(quizArr1_b));
-//        System.out.println(testSolution.solution(quizArr2_b));
-//        System.out.println(testSolution.solution(new int[]{1,10 ,11, 110, 1110,0}));
-//        System.out.println(testSolution.solution(new int[]{101, 10, 232, 23}));
-//        System.out.println(testSolution.solution(new int[]{978, 97}));
-        System.out.println(testSolution.solution(new int[]{98, 991, 990, 989, 988, 987}));
+
+        System.out.println(testSolution.solution("17"));
+        System.out.println(testSolution.solution("011"));
 
     }
 }
--- Solution.java
+++ Solution.java
@@ -1,55 +1,112 @@
 package com.company;
 
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
-    public String solution(int[] numbers) {
-        //문자열을 합치기 위해서 StringBuilder 생성
-        StringBuilder answer = new StringBuilder();
+    //순열을 모아두기 위한 Set 생성
+    Set<Integer> permuSet;
+    //순열조합시 나올수있는 길이를 저장
+    int length;
+    //에라스토테네스의 채를 구할때 사용할 maxValue
+    int maxValue;
+    //받아온 numbers를 char[] 로 저장
+    char[] numbersCharArr;
+    //DFS 사용시에 방문처리르 확인
+    boolean[] isVisited;
+    public int solution(String numbers) {
+        int answer = 0;
+        //charArr 로 변환
+        numbersCharArr = numbers.toCharArray();
+        //set 생성
+        permuSet = new HashSet<Integer>();
+        //방문처리를 확인할 booleanArr 생성
+        isVisited = new boolean[numbers.length()];
 
-        //정렬을 위해서 Integer Array 만듬
-        Integer[] numbersInt = new Integer[numbers.length];
+        //maxValue 이므로 0으로 초기화
+        maxValue = 0;
 
-        //numbers 원소들을 numbersInt 배열로 옮김
-        Arrays.setAll(numbersInt , index -> numbers[index]);
-
-        //정렬
-        Arrays.sort(numbersInt, new MyComparator());
-
-        //정렬된 값들을 StringBuilder를 통해서 합침
-        for (Integer i : numbersInt) {
-            answer.append(i);
+        // 모든 순열 찾기
+        for (int i = 1; i <= numbers.length(); i++) {
+            //길이를 지정
+            length = i;
+            //DFS를 통해 모든 순열을 찾는다.
+            findAllPermutationByDFS("");
         }
 
-        //"0000" 이 나온 경우 "0" 으로 return ,앞자리가 0이 아니면 그냥 숫자 return
-        return answer.toString().charAt(0) == '0' ? "0" : answer.toString() ;
+        //에라스토테네스의 채 이용해서 소수 배열 구하기.
+        boolean[] primeNumArray = getPrimeNumCount(maxValue);
+
+        //순열 Set을 돌면서 해당 숫자가 소수인지 판별
+        for (Integer integer : permuSet) {
+            //소수가 맞으면 answer 에 + 1
+            if(primeNumArray[integer])
+                answer++;
+        }
+
+        return answer;
     }
 
-    class MyComparator implements Comparator<Integer>{
-        @Override
-        public int compare(Integer a, Integer b) {
-            //a , b, 문자열로 만듬
-            String aStr = a.toString();
-            String bStr = b.toString();
+    private boolean[] getPrimeNumCount(int maxValue) {
+        boolean[] tempPrimeNumArray = new boolean[maxValue + 1]; // maxValue 까지 포함해야 하므로 + 1
+        Arrays.fill(tempPrimeNumArray, true);
+        tempPrimeNumArray[0] = false;
+        tempPrimeNumArray[1] = false;
 
-            //둘 중에 길이가 긴 문자열의 길이를 구하고 2를 곱한다 (2를 곱하는 이유 : 비교할 때 사용하기 위해서)
-            int maxLength = Math.max(aStr.length(), bStr.length()) * 2;
-            //maxLength 길이가 될 수 있도록 문자열을 늘린다.
-            while ((aStr.length() < maxLength) || (bStr.length() < maxLength)) {
-                aStr += aStr;
-                bStr += bStr;
+        for (int i = 0; i < Math.sqrt(maxValue + 1); i++) {
+            if(tempPrimeNumArray[i]){
+                if(isPrimeNum(i)){
+                    for (int j = i*2; j < maxValue + 1; j += i) {
+                        tempPrimeNumArray[j] = false;
+                    }
+                }else{
+                    tempPrimeNumArray[i] = false;
+                }
             }
-            char[] aChars = aStr.toCharArray();
-            char[] bChars = bStr.toCharArray();
+        }
 
-            //maxLength 길이만큼 for문을 돌면서 문자열의 각 숫자들을 비교한다.
-            for (int i = 0; i < maxLength; i++) {
-                if(aChars[i] > bChars[i]) return -1;
-                else if(aChars[i] < bChars[i])return 1;
+        return tempPrimeNumArray;
+    }
+
+    //소수인지 판단
+    private boolean isPrimeNum(int num) {
+        // 0 1은 소수가 아님
+        if(num == 0 || num == 1 ){
+            return false;
+        }
+
+        //2부터 체크한다.
+        for (int i = 2; i < num; i++) {
+            if(num % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    //DFS를 통해 모든 순열을 구한다.
+    //savedStr 를 통해 재귀함수에서 현재까지 완성된 String 을 확인한다.
+    private void findAllPermutationByDFS(String savedStr) {
+        //전역변수 length 를 통해서 길이에 맞으면 set에 저장
+        if(savedStr.length() == length){
+            //set 저장시에 maxValue 를 구하기 위해서 tempValue 사용
+            int tempValue = Integer.parseInt(savedStr);
+            maxValue = Math.max(maxValue, tempValue);
+            permuSet.add(tempValue);
+        }
+
+        //아직 방문처리 되지 않은 숫자들을 돌면서 순열을 생성 , 재귀함수 사용
+        for (int i = 0; i < numbersCharArr.length; i++) {
+            //방문처리 안되었으면 방문
+            if(!isVisited[i]){
+                //재귀함수 타기전 방문 처리
+                isVisited[i] = true;
+                //재귀함수 돌입
+                findAllPermutationByDFS(savedStr + numbersCharArr[i]);
+                //재귀함수에서 빠져 나왔으면 방문 처리 취소
+                isVisited[i] = false;
             }
-
-            return 0;
         }
     }
 }
