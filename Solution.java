25db44fe6e8b55abb65720a5bb1e49ffb9f858bc
Hyunggi <phg9587@naver.com>
2022-08-25 13:45:29 +0900

    2021 카카오 채용연계형 인턴십 거리두기 확인하기 - 풀이 완료
    원래는 BFS 를 사용하는 문제인데, 나는 감을 못잡아서 그냥 구현으로 풀었다.

--- Solution.java
+++ Solution.java
@@ -1,112 +1,106 @@
 package com.company;
 
-import java.util.Stack;
-
-/*
-// 2020 KAKAO BLIND RECRUITMENT
-// 괄호 변환
-- p는 '(' 와 ')' 로만 이루어진 문자열이며 길이는 2 이상 1,000 이하인 짝수입니다.
-- 문자열 p를 이루는 '(' 와 ')' 의 개수는 항상 같습니다.
-- 만약 p가 이미 "올바른 괄호 문자열"이라면 그대로 return 하면 됩니다.
-*/
-
 class Solution {
-    public String solution(String p) {
-        // 만약 p가 이미 "올바른 괄호 문자열"이라면 그대로 return 하면 됩니다.
-        if(isPerfectBracket(p))
-            return p;
+    public int[] solution(String[][] places) {
+        //answer 를 return 하기 위해서 배열 만듬
+        int[] answer = new int[places.length];
 
-        // "균형잡힌 괄호 문자열" p가 매개변수로 주어질 때,
-        // 주어진 알고리즘을 수행해 "올바른 괄호 문자열"로 변환한 결과를 return
-        return makeRightBracket(p);
+        //places 를 하나씩 확인하기 위해서 index를 생성하고 , foreach 문을 사용
+        //isKeepingDistanceInPlace 에서 주어진 place 가 거리두기를 제대로 하는지 확인
+        int index = 0;
+        for (String[] place : places){
+            answer[index++] = isKeepingDistanceInPlace(place);
+        }
+        return answer;
     }
 
-    private String  makeRightBracket(String str) {
-        // 1. 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다.
-        if(str.equals("")) return "";
+    private int isKeepingDistanceInPlace(String[] place) {
+        //place 가 주어졌을 때 모든 자리를 확인하면서 거리두기를 제대로 하는지 확인
+        //모든 자리를 확인하기 위해서 2중 for문 을 사용
+        for (int i = 0; i < place.length; i++) {
+            for (int j = 0; j < place[0].length(); j++) {
+                //사람이 있는 자리만 검사를 수행함
+                // isKeepingDistanceAtSeat 은 해당 자리에 있는 사람 주위로 거리두기가 제대로 되는지 확인
+                // 사람이 있는 자리인데 거리두기가 제대로 되지 않았으면 0을 return한다.
+                // 사람이 앉지 않은 자리이면 확인할 필요가 없다
+                if( place[i].charAt(j) == 'P' && !isKeepingDistanceAtSeat(i,j,place))
+                    return 0;
+            }
+        }
+        //거리두기가 모두 제대로 된 place 라면 1을 return한다
+        return 1;
+    }
 
-        // u는 "균형잡힌 괄호 문자열"로 더 이상 분리할 수 없어야 한다.
-        // 시작 괄호를 1 , 닫는 괄호를 -1 로 지정해서 합이 0 이면 균형잡힌 괄호 문자열
-        // for 문을 돌면서 균형잡힌 괄호 문자열 인지 확인
-            // for문을 돌기전에 맨 첫번째 글자를 미리 넣어서 2번째에 바로 균형잡힌 괄호 문자열이 나올 경우를 체크한다.
-        int bracketSum = checkBracketSum(str.charAt(0));
+    //해당 좌석 주위로 거리두기가 제대로 되었는지 확인
+    private boolean isKeepingDistanceAtSeat(int row, int column, String[] place) {
+        //1칸씩 상하좌우 계산
+        int[][] dxy = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}; // 상 우 하 좌
+        //2칸씩 상하좌우 계산
+        int[][] dxy2 = {{-2, 0}, {0, 2}, {2, 0}, {0, -2}}; // 상 우 하 좌
+        //대각선 계산
+        int[][] ddiagonal = {{-1, 1}, {1, 1}, {1, -1}, {-1, -1}}; // 1 5 7 11
+        // 1.상 하 좌 우 에 사람이 있는지 확인하기. (1칸 ,2칸 모두 계산)
+        for (int i = 0; i < dxy.length; i++) {
+            //Row1 , Column1은 1칸 떨어져있는 좌표
+            int tempRow1 = row + dxy[i][0];
+            int tempColumn1 = column + dxy[i][1];
 
-        int index;
-        for (index = 1; index < str.length(); index++) {
-            bracketSum += checkBracketSum(str.charAt(index));
+            //범위를 벗어나지 않는지 확인 , 벗어나면 continue
+            if (isOutOfMatrix(tempRow1, tempColumn1, place))
+                continue;
 
-            if(bracketSum == 0){
-                index++;
-                break;
+            //1칸씩 떨어진 자리에 사람이 있으면 false 를 반환
+            if (place[tempRow1].charAt(tempColumn1) == 'P') {
+                return false;
+            }
+
+            //Row2 , Column2은 2칸 떨어져있는 좌표
+            int tempRow2 = row + dxy2[i][0];
+            int tempColumn2 = column + dxy2[i][1];
+
+            //범위를 벗어나지 않는지 확인 , 벗어나면 continue
+            if (isOutOfMatrix(tempRow2, tempColumn2, place))
+                continue;
+
+            //2칸 떨어진 자리에 사람이 있는데 사이에 벽이 없으면 false 를 반환
+            if (place[tempRow2].charAt(tempColumn2) == 'P' && place[tempRow1].charAt(tempColumn1) != 'X') {
+                return false;
             }
         }
 
-        // 2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다.
-        // 단, u는 "균형잡힌 괄호 문자열"로 더 이상 분리할 수 없어야 하며, v는 빈 문자열이 될 수 있습니다.
-        String u = str.substring(0, index);
-        String v = str.substring(index);
+        // 2. 대각선을 확인
+        for (int i = 0; i < ddiagonal.length; i++) {
+            //RowD, ColumnD은 대각선에 위치한 좌표
+            int tempRowD = row + ddiagonal[i][0];
+            int tempColumnD = column + ddiagonal[i][1];
 
-        // 3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다.
-        //   3-1. 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다.
-        if(isPerfectBracket(u)){
-            return u + makeRightBracket(v);
-        }
-        // 4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다.
-        else{
-            String tempStr;
+            //범위를 벗어나지 않는지 확인 , 벗어나면 continue
+            if (isOutOfMatrix(tempRowD, tempColumnD, place))
+                continue;
 
-            //   4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다.
-            //   4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다.
-            //   4-3. ')'를 다시 붙입니다.
-            tempStr = "(" + makeRightBracket(v) + ")";
-
-            //   4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다.
-            //   4-5. 생성된 문자열을 반환합니다.
-            return tempStr + changeUString(u);
-        }
-    }
-
-
-    // 4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다.
-    private String changeUString(String u) {
-        String rtStr = "";
-        // u의 첫 번째와 마지막 문자를 제거
-        String tempStr = u.substring(1, u.length() - 1);
-
-        // 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다.
-        for (int i = 0; i < tempStr.length(); i++) {
-            char c = tempStr.charAt(i);
-
-            if (c == '(') {
-                rtStr = rtStr + ")";
-            }else{
-                rtStr = rtStr + "(";
+            //대각선에 사람이 존재할 경우
+            //그 사람과 나 사이의 1의 거리만큼에 해당하는 좌표에는 벽이 존재해야함
+            // 예를 들어 1시에 사람이 존재할 경우 12시 와 3시에는 벽이 존재해야한다.
+                //그래서 대각선에 위치한 사람과 나 사이의 벽을 확인하기 위해서
+                //for문을 사용하여 2개의 벽이 존재하는지 확인 - 이때 다시 dxy 배열을 사용함
+                //2군데중에 한곳이라도 벽이 없으면 false를 return
+            if (place[tempRowD].charAt(tempColumnD) == 'P') {
+                for (int j = 0; j < 2; j++) {
+                    int selectDxy = (i + j) % ddiagonal.length;
+                    int tempRow1 = row + dxy[selectDxy][0];
+                    int tempColumn1 = column + dxy[selectDxy][1];
+                    if(place[tempRow1].charAt(tempColumn1) != 'X')
+                        return false;
+                }
             }
         }
 
-        return rtStr;
+        //거리를 다 잘 지키면 true;
+        return true;
     }
 
-    // 시작 괄호를 1 , 닫는 괄호를 -1 로 지정해서 합이 0 이면 균형잡힌 괄호 문자열
-    private int checkBracketSum(char strChar) {
-        return strChar == '(' ? 1 : -1;
-    }
-
-    //올바른 괄호 문자열인지 확인하는 메서드
-    private boolean isPerfectBracket(String str) {
-        Stack<Character> stack = new Stack<>();
-
-        //str 길이 만큼 for 문을 돌면서 스택을 이용하여 올바른 괄호 문자열인지 체크
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            
-            if(c == ')' && !stack.isEmpty() && stack.peek() == '('){
-                stack.pop();
-            }else{
-                stack.push(c);
-            }
-        }
-
-        return stack.isEmpty();
+    //거리가 벗어났는지 확인하기 위한 메서드
+    private  boolean isOutOfMatrix(int rowIndex , int columnIndex , String[] matrix ){
+        return rowIndex < 0 || columnIndex < 0 || rowIndex >= matrix.length || columnIndex >= matrix[0].length();
     }
 }
