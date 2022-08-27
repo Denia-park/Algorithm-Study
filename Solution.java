dd10663afec7b4392e5fb0dc866347b203de3679
Hyunggi <phg9587@naver.com>
2022-08-27 21:16:15 +0900

    프로그래머스 2021 KAKAO BLIND RECRUITMENT 메뉴 리뉴얼 - 풀이 중
    구조를 변경했는데 동일함.. 뭐가 크게 문제인데 모르겠다

--- Solution.java
+++ Solution.java
@@ -6,49 +6,31 @@ class Solution {
     String dfsString;
     Set<String> tempGlobalSet;
     boolean[] isVisited;
-    int minCourseValue;
-    int maxCourseValue;
 
     public String[] solution(String[] orders, int[] course) {
         List<String> answer = new ArrayList<String>();
 
-        minCourseValue = course[0];
-        maxCourseValue = course[course.length - 1];
+        for (int courseNum : course) {
+            Map<String, Integer> answerMap = new HashMap<String, Integer>();
+            Map<Integer, Integer> maxValueMap = new HashMap<Integer, Integer>();
 
-        Map<String, Integer> answerMap = new HashMap<String, Integer>();
-        Map<Integer, Integer> maxValueMap = new HashMap<Integer, Integer>();
+            for (String order : orders) {
+                tempGlobalSet = new HashSet<>();
+                dfsString = order;
+                isVisited = new boolean[dfsString.length()];
 
-        for (String order : orders) {
+                dfs("",courseNum);
 
-            tempGlobalSet = new HashSet<>();
-
-            dfsString = order;
-            isVisited = new boolean[dfsString.length()];
-
-            dfs("");
-
-            for (String element : tempGlobalSet) {
-                answerMap.put(element, answerMap.getOrDefault(element, 0) + 1);
-                if(maxValueMap.getOrDefault(element.length(),0) < answerMap.get(element)){
-                    maxValueMap.put(element.length(),answerMap.get(element));
+                for (String element : tempGlobalSet) {
+                    answerMap.put(element, answerMap.getOrDefault(element, 0) + 1);
+                    if(maxValueMap.getOrDefault(element.length(),0) < answerMap.get(element)){
+                        maxValueMap.put(element.length(),answerMap.get(element));
+                    }
                 }
             }
-        }
 
-        for (String str : answerMap.keySet()) {
-            if(answerMap.get(str) == 1)
-                answer.add(str);
-        }
-
-        for (String str : answer) {
-            answerMap.remove(str);
-        }
-
-        answer.clear();
-
-        for (int i = 0; i < course.length; i++) {
             for (String str : answerMap.keySet()) {
-                if (str.length() == course[i] && Objects.equals(answerMap.get(str), maxValueMap.get(str.length()))) {
+                if (answerMap.get(str) != 1&& answerMap.get(str) >= maxValueMap.get(str.length())) {
                     answer.add(str);
                 }
             }
@@ -59,18 +41,18 @@ class Solution {
         return answer.toArray(new String[0]);
     }
 
-    private void dfs(String str) {
-        if (str.length() >= minCourseValue && maxCourseValue >= str.length()){
+    private void dfs(String str, int courseNum) {
+        if (str.length() == courseNum ){
             char[] chars = str.toCharArray();
             Arrays.sort(chars);
             tempGlobalSet.add(String.valueOf(chars));
+            return;
         }
-        if(str.length() == dfsString.length()) return;
 
         for (int i = 0; i < dfsString.length(); i++) {
             if (!isVisited[i]) {
                 isVisited[i] = true;
-                dfs(str + dfsString.charAt(i));
+                dfs(str + dfsString.charAt(i), courseNum);
                 isVisited[i] = false;
             }
         }
