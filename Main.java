c6dad000bd339a524869fa08a59a40328ebcc3c7
Hyunggi <phg9587@naver.com>
2022-08-28 16:00:19 +0900

    프로그래머스 해시 위장 - 풀이 완료 , 주석 추가
    알고리즘 문제 보다는 수학 문제에 가까웠다. String 과 Integer를 묶기 위해서 HashMap을 사용함

--- Main.java
+++ Main.java
@@ -6,16 +6,15 @@ public class Main {
     static public void main(String[] args) {
         Solution testSolution = new Solution();
 
-        String[] quizArr1_a = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
-        int[] quizArr1_b = {2,3,4};
-        String[] quizArr2_a = {"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"};
-        int[] quizArr2_b = {2,3,5};
-        String[] quizArr3_a = {"XYZ", "XWY", "WXA"};
-        int[] quizArr3_b = {2, 3, 4};
+//        String[] quizArr1_a = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
+//        int[] quizArr1_b = {2,3,4};
+        String[][] quizArr2_a = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        String[][] quizArr3_a = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
+
 
 //        System.out.println(Arrays.toString(testSolution.solution(quizArr1_a, quizArr1_b)));
-        System.out.println(Arrays.toString(testSolution.solution(quizArr2_a, quizArr2_b)));
-//        System.out.println(Arrays.toString(testSolution.solution(quizArr3_a, quizArr3_b)));
+        System.out.println(testSolution.solution(quizArr2_a));
+        System.out.println(testSolution.solution(quizArr3_a));
 
     }
 }
--- Solution.java
+++ Solution.java
@@ -1,92 +1,47 @@
 package com.company;
 
-import java.util.*;
-
-// https://fbtmdwhd33.tistory.com/249 참고
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
-    //dfs 에서 사용할 String을 전역변수로 설정
-    String dfsString;
+    public int solution(String[][] clothes) {
+        //옷의 종류 [String] 와 옷의 개수 [Integer] 를 묶어서 저장하기 위해서 HashMap을 사용
+        Map<String, Integer> map = new HashMap<String, Integer>();
 
-    //dfs에서 사용할 Map 설정
-    Map<String, Integer> answerMap;
-
-    public String[] solution(String[] orders, int[] course) {
-        //필요한 만큼만 추가해야 하므로 answer를 List로 생성
-        List<String> answer = new ArrayList<String>();
-
-        // 1. 각 문자열을 오름차순 정렬.
-        for(int i =0;i<orders.length;i++){
-            // 2. 각 문자열을 문자형 배열로 변환.
-            char[] charArr = orders[i].toCharArray();
-            // 3. 해당 문자형 배열을 정렬.
-            Arrays.sort(charArr);
-            // 4. 정렬된 문자형 배열을 문자열로 변환해 저장.
-            orders[i] = String.valueOf(charArr);
+        //clothes를 for-each 사용해서 String[] 로 나눠서 코드 적용
+        //1번째 요소는 옷의 종류
+        //0번째 요소는 옷의 이름 [※같은 이름을 가진 의상은 존재하지 않습니다.]
+        for (String[] clothe : clothes) {
+            //옷의 종류를 type으로 지정
+            String type = clothe[1];
+            //type을 map에 넣고 getOrDefault 를 사용해서 갯수를 세서 집어넣는다.
+            map.put(type, map.getOrDefault(type, 0) + 1);
         }
 
-        // 5. course의 길이만큼 반복하여 필요한 조합을 구함.
-        for (int courseNum : course) {
-            // 6. HashMap으로 조합의 수를 카운팅.
-            answerMap = new HashMap<String, Integer>();
+        //스파이는 옷을 서로 다르게 조합으로 입어야 하므로
+        //(옷의 종류에 따른 의상의 수 + 1(안입는 경우)) 를 옷의 종류만큼 서로 곱해주고,
+        //무조건 한개의 의상은 입어야 하므로 마지막에 모두 벗은 경우를 고려하여 -1 해주면 답이 나온다.
 
-            // 7. course의 경우에 따라 구한 조합들 중 가장 많이 주문된 횟수를 저장.
-            int max = Integer.MIN_VALUE;
+        // 예를 들면 다음과 같다.
+        // 옷이 다음과 같이 있을때
+        // [얼굴: 2개,  상의: 1개, 하의 : 1개,  겉옷 : 1개]
+            //얼굴 기준 : 안 입는다 / 1번 의류 입는다 / 2번 의류 입는다. => 경우의수 : 3개
+            //상의 기준 : 안 입는다 / 1번 의류 입는다  => 경우의수 : 2개
+            //하의 기준 : 안 입는다 / 1번 의류 입는다  => 경우의수 : 2개
+            //겉옷 기준 : 안 입는다 / 1번 의류 입는다  => 경우의수 : 2개
+        // 3개 * 2개 * 2개 * 2개 => 24개
+        // 근데 여기서 모두 안입는 경우는 빼줘야 하므로 -1을 뺀다
+        // 정답은 23개
 
-            // 8. 각 사람들의 조합을 구하기 위해 탐색.
-            for (String order : orders) {
-                //order가 courseNum 보다 작을 경우 계산하지 않음
-                if(order.length() < courseNum) continue;
-                //dfsString은 고객 1명이 주문한 order로 설정
-                dfsString = order;
-                //빠른 속도를 위해서 StringBuilder 를 설정
-                StringBuilder sb = new StringBuilder();
-                //StringBuilder 와 조합시에 계산을 도와줄 index , 메뉴 개수 제한을 알려줄 courseNum을 dfs의 매개변수로 할당
-                dfs(sb,0,courseNum);
-            }
+        //곱셈에 필요한 초기값은 1
+        int tempMultiply = 1;
 
-            //Math.max 를 이용하여 기존의 max 와 answerMap Value 중에 제일 큰 값을 비교해서 max 를 구한다.
-            //max는 아래에서 answer List에 추가할때 기준이 된다.
-            for (Map.Entry<String, Integer> entry : answerMap.entrySet()) {
-                max = Math.max(max, entry.getValue());
-            }
-
-            //고객이 1명만 먹은 메뉴는 추가하지 않는다.
-            if(max == 1) continue;
-
-            //entry를 돌면서 max보다 큰 값을 가진 메뉴는 answer List에 추가한다.
-            for (Map.Entry<String, Integer> entry : answerMap.entrySet()) {
-                if (entry.getValue() == max) {
-                    answer.add(entry.getKey());
-                }
-            }
+        //map의 values를 탐색하면서 (num + 1) 을 tempMultiply에 계속해서 곱해준다.
+        for (Integer num : map.values()) {
+             tempMultiply *= (num+1);
         }
 
-        //course 배열을 돌면서 구한 answer List를 오름차순으로 정렬
-        answer.sort(null);
-
-        //List 를 Array 로 변환
-        return answer.toArray(new String[0]);
-    }
-
-    // 13. 조합을 구하는 메소드 (curStr : 현재 메뉴, 조합을 계산하기 위한 index, 코스요리 개수에 따른 종료조건을 위한 strLenLimit)
-    public void dfs(StringBuilder curStr, int idx, int strLenLimit) {
-        // 14. 코스요리의 조합의 개수가 종료조건에 해당하면
-        // map에 추가하고 종료 (map에 추가할때 기존의 값에 + 1 을 적용)
-        if (curStr.length() == strLenLimit) {
-            // 15. map에 해당 조합을 삽입 및 카운팅.
-            answerMap.put(curStr.toString(), answerMap.getOrDefault(curStr.toString(), 0) + 1);
-            return;
-        }
-
-        // 16. idx부터 시작함으로써 조합을 구할 수 있다.
-        for (int i = idx; i < dfsString.length(); i++) {
-            //StringBuilder 를 사용했으므로 1글자를 추가
-            curStr.append(dfsString.charAt(i));
-            // 18. 재귀호출.
-            dfs(curStr, i + 1, strLenLimit);
-            //StringBuilder 를 사용했으므로 1글자를 제거 (마지막 글자를 제거해야 하므로 curStr.length() -1 을 적용함)
-            curStr.deleteCharAt(curStr.length() - 1);
-        }
+        //tempMultiply에서 모두 벗은 경우를 1개 빼면 답이 된다.
+        return (tempMultiply - 1);
     }
 }
