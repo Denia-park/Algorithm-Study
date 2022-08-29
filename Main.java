7fc7d6899770b8ec11f244a714d76cd20cf5d36d
Hyunggi <phg9587@naver.com>
2022-08-29 12:06:56 +0900

    프로그래머스 스택/큐 다리를 지나는 트럭 - 풀이 완료 - 큐를 사용 , 주석 추가
    문제를 제대로 이해하고, 이해한대로 코드를 작성하면 풀이가 가능 (큐를 사용함)

--- Main.java
+++ Main.java
@@ -8,13 +8,14 @@ public class Main {
 
 //        String[] quizArr1_a = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
 //        int[] quizArr1_b = {2,3,4};
-        String[][] quizArr2_a = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
-        String[][] quizArr3_a = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
+//        String[][] quizArr2_a = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+//        String[][] quizArr3_a = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
 
 
 //        System.out.println(Arrays.toString(testSolution.solution(quizArr1_a, quizArr1_b)));
-        System.out.println(testSolution.solution(quizArr2_a));
-        System.out.println(testSolution.solution(quizArr3_a));
+        System.out.println(testSolution.solution(2, 10, new int[]{7, 4, 5, 6}));
+        System.out.println(testSolution.solution(100, 100, new int[]{10}));
+        System.out.println(testSolution.solution(100, 100, new int[]{10, 10, 10, 10, 10, 10, 10, 10, 10, 10}));
 
     }
 }
--- Solution.java
+++ Solution.java
@@ -1,47 +1,88 @@
 package com.company;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
-    public int solution(String[][] clothes) {
-        //옷의 종류 [String] 와 옷의 개수 [Integer] 를 묶어서 저장하기 위해서 HashMap을 사용
-        Map<String, Integer> map = new HashMap<String, Integer>();
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        //초를 계산할 answer
+        int answer = 0;
+        //총 몇대의 트럭이 지나가야하는지 변수 설정
+        int truckTotalNum = truck_weights.length;
+        //다리 위에 얼마만큼의 무게가 실려있는지 변수 설정
+        int weightOnBridge = 0;
+        // 대기 트럭 , 다리를 건너는 트럭 , 다리를 지난 트럭 모두 Queue를 구현한 LinkedList를 사용하여 변수 설정
+        Queue<Truck> waitQueue = new LinkedList<>();
+        Queue<Truck> bridgeQueue = new LinkedList<>();
+        Queue<Truck> completeQueue = new LinkedList<>();
 
-        //clothes를 for-each 사용해서 String[] 로 나눠서 코드 적용
-        //1번째 요소는 옷의 종류
-        //0번째 요소는 옷의 이름 [※같은 이름을 가진 의상은 존재하지 않습니다.]
-        for (String[] clothe : clothes) {
-            //옷의 종류를 type으로 지정
-            String type = clothe[1];
-            //type을 map에 넣고 getOrDefault 를 사용해서 갯수를 세서 집어넣는다.
-            map.put(type, map.getOrDefault(type, 0) + 1);
+        //truck_weights 의 원소들을 waitQueue에 모두 삽입
+        for (int truckWeight : truck_weights) {
+            waitQueue.add(new Truck(truckWeight));
         }
 
-        //스파이는 옷을 서로 다르게 조합으로 입어야 하므로
-        //(옷의 종류에 따른 의상의 수 + 1(안입는 경우)) 를 옷의 종류만큼 서로 곱해주고,
-        //무조건 한개의 의상은 입어야 하므로 마지막에 모두 벗은 경우를 고려하여 -1 해주면 답이 나온다.
+        //completeQueue 가 truckTotalNum 의 수에 도달하면 모든 트럭이 지나갔으므로 while문 종료
+        while (completeQueue.size() < truckTotalNum) {
+            //1. 도로에서 빠져나가기
+                //bridgeQueue에 원소가 존재하고 , bridgeQueue의 맨 앞의 원소가 다리를 건넌 시점일때 [맨 앞의 차부터 차례차례 건너야 하므로 peek()를 사용]
+                //bridgeQueue에서 completeQueue 로 이동
+                //다리 위의 무게에서도 트럭의 무게 만큼 빼준다.
+            if(!bridgeQueue.isEmpty() && bridgeQueue.peek().getDistance() >= bridge_length){
+                //bridgeQueue 에서 completeQueue 로 원소를 이동
+                Truck completedTruck = bridgeQueue.poll();
+                completeQueue.add(completedTruck);
+                //다리 위의 무게에서도 트럭의 무게 만큼 빼준다.
+                weightOnBridge -= completedTruck.getWeight();
+            }
 
-        // 예를 들면 다음과 같다.
-        // 옷이 다음과 같이 있을때
-        // [얼굴: 2개,  상의: 1개, 하의 : 1개,  겉옷 : 1개]
-            //얼굴 기준 : 안 입는다 / 1번 의류 입는다 / 2번 의류 입는다. => 경우의수 : 3개
-            //상의 기준 : 안 입는다 / 1번 의류 입는다  => 경우의수 : 2개
-            //하의 기준 : 안 입는다 / 1번 의류 입는다  => 경우의수 : 2개
-            //겉옷 기준 : 안 입는다 / 1번 의류 입는다  => 경우의수 : 2개
-        // 3개 * 2개 * 2개 * 2개 => 24개
-        // 근데 여기서 모두 안입는 경우는 빼줘야 하므로 -1을 뺀다
-        // 정답은 23개
+            //2. 도로에 진입하기
+                //한번에 한대씩만 들어갈 수 있으므로 bridgeQueue.size() + 1 <= bridgelength 로 계산을 했다.
+                //waitQueue가 비어있지 않고 weightOnBridge에 이번에 들어올 원소의 무게를 더했을때 weight를 넘지 않는 경우에만
+                //트럭을 waitQueue 에서 bridgeQueue 로 이동
+            if ((bridgeQueue.size() + 1 <= bridge_length) && !waitQueue.isEmpty() && (weightOnBridge + waitQueue.peek().getWeight() <= weight)) {
+                //waitQueue 에서 bridgeQueue 로 원소를 이동
+                Truck completedTruck = waitQueue.poll();
+                bridgeQueue.add(completedTruck);
+                //다리 위의 무게에 트럭의 무게 만큼 더한다.
+                weightOnBridge += completedTruck.getWeight();
+            }
 
-        //곱셈에 필요한 초기값은 1
-        int tempMultiply = 1;
+            //3. 도로에 존재하는 트럭들 모두 1칸씩 이동하기.
+            if(!bridgeQueue.isEmpty()){
+                for (Truck truck : bridgeQueue) {
+                    truck.moving();
+                }
+            }
 
-        //map의 values를 탐색하면서 (num + 1) 을 tempMultiply에 계속해서 곱해준다.
-        for (Integer num : map.values()) {
-             tempMultiply *= (num+1);
+            //while문이 1번 돌때마다 answer를 증가시켜 1초가 지났음을 표시
+            answer++;
         }
 
-        //tempMultiply에서 모두 벗은 경우를 1개 빼면 답이 된다.
-        return (tempMultiply - 1);
+        //모든 트럭이 길을 지났으므로 answer를 return
+        return answer;
+    }
+}
+
+class Truck {
+    int weight; // 트럭 무게
+    int distance; // 트럭 거리
+
+    // 트럭이 이동함을 표시
+    public void moving() {
+        this.distance++;
+    }
+
+    //트럭 생성자
+    public Truck(int weight) {
+        this.weight = weight;
+        this.distance = 0;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getDistance() {
+        return distance;
     }
 }
