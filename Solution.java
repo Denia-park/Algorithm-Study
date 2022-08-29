590036653383d20c05cbca2fab52f3baa6ba1f72
Hyunggi <phg9587@naver.com>
2022-08-29 18:00:20 +0900

    프로그래머스 정렬 H-Index - 풀이 완료 - 주석 추가
    쉽게 푸는 방법이 있는데 힘들게 빙글 빙글 돈 것 같다.

--- Solution.java
+++ Solution.java
@@ -6,41 +6,47 @@ class Solution {
     public int solution(int[] citations) {
         int answer = 0;
 
+        //일단 정렬 - 오름차순
         Arrays.sort(citations);
 
+        //이진탐색으로 필요한 답을 찾아보자.
         answer = binarySearch(citations);
 
         return answer;
     }
 
     private int binarySearch(int[] citations) {
+        //return 할 변수
         int answer = -1;
+        //이진탐색에 필요한 left , right
         int left = 0;
-        int right = citations.length - 1;
+        int right = citations.length;
+        //전체 길이
         int totalLength = citations.length;
 
+        //left가 right 범위를 넘어서는 순간까지 while
         while(left <= right) {
+            //mid를 구한다.
             int mid = (left + right) / 2;
-            int quoteNum = citations[mid];
 
-            int firstIndex = getFirstNumIndex(citations, quoteNum, left, right);
+            //mid에 해당하는 값 에 해당하는 index를 구한다.
+            int firstIndex = getFirstNumIndex(citations, mid);
 
-            if((totalLength - firstIndex) >= quoteNum) {
-                answer = quoteNum;
-                left = getNextNumIndex(citations, quoteNum, left, right);
+            //규칙에 해당하는 값을 찾으면 mid 값을 answer에 할당 , 아니면 right 를 mid - 1 로 변경하고 다시 이진탐색
+            if((totalLength - firstIndex) >= mid && firstIndex <= mid) {
+                answer = mid;
+                left = mid + 1;
             }else{
-                if(answer != -1)
-                    break;
-                else{
-                    right = mid - 1;
-                }
+                right = mid - 1;
             }
         }
 
         return answer;
     }
 
-    public int getFirstNumIndex(int[] arr, int num, int left, int right) {
+    public int getFirstNumIndex(int[] arr, int num) {
+        int left = 0;
+        int right = arr.length-1;
         while (left < right) {
             int mid = (left + right) / 2;
             if (arr[mid] >= num) {
@@ -54,10 +60,6 @@ class Solution {
     }
 
     public int getNextNumIndex(int[] arr, int num, int left, int right) {
-        if(left == right){
-            return right + 1;
-        }
-
         while (left < right) {
             int mid = (left + right) / 2;
             if (arr[mid] > num) {
