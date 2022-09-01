4952b77c388b26659d62c99e45320431fd191aa6
Hyunggi <phg9587@naver.com>
2022-09-01 17:00:43 +0900

    프로그래머스 완전탐색 카펫 - 풀이 완료
    풀이 자체는 간단함 , 주석 추가

--- Main.java
+++ Main.java
@@ -10,23 +10,11 @@ public class Main {
 //        int[] quizArr1_b = {2,3,4};
 //        String[][] quizArr2_a = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
 
-        String number1 = "1924";
-        int k1 = 2;
-        String number2 = "1231234";
-        int k2 = 3;
-        String number3 = "4177252841";
-        int k3 = 3;
-        String number4 = "192345678";
-        int k4 = 3;
-
-
 //        System.out.println(Arrays.toString(testSolution.solution(quizArr1_a, quizArr1_b)));
 
 //        System.out.println(testSolution.solution(quizArr3_a));
-        System.out.println(testSolution.solution(number1,k1));
-        System.out.println(testSolution.solution("1921",k1));
-        System.out.println(testSolution.solution(number2,k2));
-        System.out.println(testSolution.solution(number3,k3));
-        System.out.println(testSolution.solution(number4,k4));
+//        System.out.println(Arrays.toString(testSolution.solution(10, 2)));
+//        System.out.println(Arrays.toString(testSolution.solution(8, 1)));
+        System.out.println(Arrays.toString(testSolution.solution(24, 24)));
     }
 }
