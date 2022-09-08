98a45cb18f09eba2023f8efd56c8844c7175d87d
Hyunggi <phg9587@naver.com>
2022-09-08 14:20:56 +0900

    프로그래머스 스택/큐 주식가격 - 풀이 완료 , 정답 참고
    스택/큐 를 사용하는 문제인데 도저히 답이 안나와서 에라 모르겠다 하고
    2중 for문 으로 풀었는데 효울성에서도 통과해서 당황스러웠다.
    
    해당 문제를 스택으로 풀 수 있다는 사실에 놀랐다. 아직 갈길이 멀다.

--- Solution.java
+++ Solution.java
@@ -1,37 +1,27 @@
 package com.company;
 
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    public int[] solution(int peopleNum, String[] words) {
-        //중복을 체크할 HashMap 사용
-        Map<String, Integer> answerMap = new HashMap<>();
-        //현재가 몇번째인지 확인할 변수
-        int forIndex = 0;
-        //마지막 글자를 저장할 변수
-        char lastChar = '\0';
+    public int[] solution(int[] prices) {
+        //answer 선언
+        int[] answer = new int[prices.length];
 
-        //for문을 돌면서 words 를 확인
-        for (forIndex = 0; forIndex < words.length; forIndex++) {
-            //lasgChar 초기화를 \0 으로 했기 때문에
-            // \0이 아닌 경우 + lastChar 가 현재 단어의 맨 앞 글자와 다른 경우 for문 종료
-            if(lastChar != '\0' && words[forIndex].charAt(0) != lastChar) break;
-
-            // HashMap 에 저장된 해당 단어의 수가 2번째로 나오는 경우
-            // 해당 단어는 중복되었으므로 for문 종료
-            int wordCount = answerMap.getOrDefault(words[forIndex], 0) + 1;
-            if(wordCount == 2) break;
-
-            // 위의 2개의 조건에 모두 해당하지 않는 경우 나온 단어를 HashMap 에 저장 하고 단어 수를 1 올린다.
-            answerMap.put(words[forIndex], wordCount);
-            //단어에 대한 확인이 끌날때마다 lastChar 을 업데이트 해준다.
-            lastChar = words[forIndex].charAt(words[forIndex].length() - 1);
+        //모든 요소에 대해서 완전 탐색 수행 - 2중 for문
+        //시작점을 선언
+        for (int i = 0; i < prices.length; i++) {
+            //시작하는 값
+            int startValue = prices[i];
+            //count 용 변수
+            int count = 0;
+            //시작하는 값 다음부터 시작하는 값 과 비교하여 count를 올리고 작은 값이 나오면 break
+            for (int j = i + 1; j < prices.length; j++) {
+                count++;
+                if(prices[j] < startValue)
+                    break;
+            }
+            //answer에 count 삽입
+            answer[i] = count;
         }
 
-        // 정상적으로 for문이 종료되었으면 forIndex 가 words.length 가 나오므로
-            // {0,0} 을 리턴
-            // 정상적으로 for문이 종료되지 않았다면 forIndex 값을 이용하여 순서와 몇번째 턴인지를 확인한다.
-        return forIndex == words.length ? new int[]{0, 0} : new int[]{forIndex % peopleNum + 1, forIndex / peopleNum + 1};
+        return answer;
     }
 }
