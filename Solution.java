93f2c5fd40fe199050cfff54e70242bd11bc72c8
Hyunggi <phg9587@naver.com>
2022-09-10 19:46:01 +0900

    프로그래머스 월간 코드 챌린지 시즌1 삼각 달팽이 - 풀이 완료 , 답지 참고
    삼각형으로 값을 계산해야 하는 것들은 2차원 배열을 이용하여 좌표계를 구현하여 사용하면 쉽게 해결이 된다.
    물론 나는 정답을 찾지 못해서 답지를 참고하여 2차원 배열을 사용해야 하는 것을 알았다.

--- Solution.java
+++ Solution.java
@@ -1,22 +1,64 @@
 package com.company;
 
-// 참조 : https://school.programmers.co.kr/questions/10368
-// 참조 : https://deveric.tistory.com/61
+// 참조 : https://hyojun.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%82%BC%EA%B0%81-%EB%8B%AC%ED%8C%BD%EC%9D%B4-Java
 
 class Solution {
-    //Combination nCr = n! / (r! * (n-r)!) 으로 처음에 풀었으나 overFlow 때문에 계산이 되지 않음
-    //피보나치 수열로 접근을 해야한다.
-    //DP 문제 , 메모이제이션 접근
-    public int solution(int quizNum) {
-        long[] memo = new long[quizNum + 1];
+    int[][] coordinates;
+    int maxIndex;
+    public int[] solution(int n) {
+        coordinates = new int[n][n];
+        maxIndex = n;
+        int maxValue = 0;
+        int direction = 1; // 1 : 아래 , 2: 오른쪽 , 3: 위로
+        for (int i = 1; i <= n; i++) {
+            maxValue += i;
+        }
+        int[] answer = new int[maxValue];
 
-        memo[1] = 1;
-        memo[2] = 2;
 
-        for (int i = 3; i <= quizNum; i++) {
-            memo[i] = (memo[i-1] + memo[i-2])% 1000000007;
+        int index = 1;
+        int row = -1 , col = 0;
+        while (index <= maxValue) {
+            int tempRow = row;
+            int tempCol = col;
+            if(direction == 1) {
+                tempRow++;
+            } else if (direction == 2) {
+                tempCol++;
+            }else if (direction == 3) {
+                tempRow--;
+                tempCol--;
+            }
+
+            if(!isRightCoordinates(tempRow, tempCol)){
+                if(direction < 3) direction++;
+                else direction = 1;
+
+                continue;
+            }
+
+            row = tempRow;
+            col = tempCol;
+
+            coordinates[row][col] = index;
+            index++;
         }
 
-        return (int) (memo[quizNum]);
+        int answerIndex = 0;
+        for (int tempRow = 0; tempRow < n; tempRow++) {
+            for (int tempCol = 0; tempCol < n; tempCol++) {
+                int tempAnswerValue = coordinates[tempRow][tempCol];
+                if(tempAnswerValue != 0){
+                    answer[answerIndex] = tempAnswerValue;
+                    answerIndex++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean isRightCoordinates(int row , int col) {
+        return row >= 0 && row < maxIndex && col >= 0 && col < maxIndex && coordinates[row][col] == 0;
     }
 }
