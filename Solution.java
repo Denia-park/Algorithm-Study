7f8dac6a9accdf3f8e53bb5f2fe97ddcb17b2982
Hyunggi <phg9587@naver.com>
2022-09-13 14:20:29 +0900

    프로그래머스 완전탐색 전력망을 둘로 나누기 - 풀이 중
    완전탐색 , 모든 노드의 수 라서 그냥 count 만 잘 세면 된다. dfs, bfs 구분 없이

--- Solution.java
+++ Solution.java
@@ -1,86 +1,49 @@
 package com.company;
 
-// 참고 : https://school.programmers.co.kr/questions/19184
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-    public int solution(int[] people, int limit) {
-        //총 이동 수를 저장할 변수 선언
-        int moveCount = 0;
+    List<List<Integer>> graph;
+    boolean[] isVisited;
+    int answer;
+    int dfsDeepth;
+    public int solution(int nodeNum, int[][] wires) {
+        graph = new ArrayList<>();
+        answer = -1;
 
-        //people 배열을 정렬
-        Arrays.sort(people);
-
-        // 정렬한 최소값을 기준으로 하여 limit 에서 해당 값을 뺀 값보다 큰 값들은 어떻게 해도 혼자 배를 타야 하므로
-        // 미리 정렬에서 빼준다. 그리고 구한 Index 를 이용하여 이동 수도 미리 구한다.
-        //bisectRight ,bisectLeft 는 파이썬에서 사용하는 라이브러리를 참고하여 직접 구현하였다.
-        int limitValIndex = bisectRight(people, limit - people[0]);
-
-        //limitValIndex 를 이용하여 moveCount 를 구함
-        moveCount += people.length - limitValIndex;
-
-        //최소값 과 최대값을 구하고 빼줘야 하므로 Deque 를 이용한다.
-        Deque<Integer> deque = new LinkedList<>();
-
-        //아까 구한 limitValIndex 이용하여 확인해야하는 people 배열만을 구한다.
-        int[] newPeople = Arrays.copyOfRange(people, 0, limitValIndex);
-
-        //deque 에 필요한 값들을 추가한다.
-        for (int newPerson : newPeople) {
-            deque.add(newPerson);
+        for (int i = 0; i < nodeNum + 1; i++) {
+            graph.add(new ArrayList<>());
         }
 
-        //deque 가 빌 때까지 해당 로직을 수행한다.
-        while(!deque.isEmpty()){
-            //마지막 값 = 제일 무거운 사람
-            int lastVal = deque.pollLast();
-
-            //제일 무거운 사람 과 제일 가벼운 사람을 더했을때 limit 보다 작으면 가벼운 사람도 같이 보트를 태워 보낸다.
-            //제일 가벼운 사람이 보트를 타지 못하는 경우에는 제일 무거운 사람만 태워 보낸다.
-            //이렇게 로직이 가능한 이유는 보트가 작아서 최대 2명씩만 탈 수 있기 때문이다.
-            if(!deque.isEmpty() && lastVal + deque.peekFirst() <= limit){
-                deque.pollFirst();
-            }
-
-            moveCount++;
+        for (int[] wire : wires) {
+            int start = wire[0];
+            int end = wire[1];
+            graph.get(start).add(end);
+            graph.get(end).add(start);
         }
 
-        return moveCount;
+        int targetNode = 1;
+
+        dfsDeepth = 1;
+        isVisited = new boolean[nodeNum + 1];
+        dfs(targetNode, dfsDeepth);
+
+        System.out.println(dfsDeepth);
+
+        return answer;
     }
 
-    public int bisectLeft(int[] people, int targetValue) {
-        int start = 0;
-        int end = people.length;
+    private void dfs(int targetNode, int deepth) {
+        dfsDeepth = Math.max(dfsDeepth, deepth);
 
-        while (start < end) {
-            int mid = (start + end) / 2;
-
-            if(targetValue <= people[mid]) {
-                end = mid;
-            }else{
-                start = mid + 1;
+        for (int i = 0; i < graph.get(targetNode).size(); i++){
+            int newNode = graph.get(targetNode).get(i);
+            if(!isVisited[newNode]){
+                isVisited[newNode] = true;
+                int newDeepth = deepth + 1;
+                dfs(newNode, newDeepth);
             }
         }
-
-        return end;
-    }
-
-    public int bisectRight(int[] people, int targetValue) {
-        int start = 0;
-        int end = people.length;
-
-        while (start < end) {
-            int mid = (start + end) / 2;
-
-            if(targetValue < people[mid]) {
-                end = mid;
-            }else{
-                start = mid + 1;
-            }
-        }
-
-        return end;
     }
 }
-
