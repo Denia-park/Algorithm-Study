1096519a17535aad8eb5307d41fbfc5dbbf489d4
Hyunggi <phg9587@naver.com>
2022-09-14 17:31:53 +0900

    프로그래머스 Summer/Winter Coding(~2018) 점프와 순간 이동 - 풀이 완료
    시간 복잡도를  logN 으로 변경 및 메모리 사용량도 줄임.

--- Solution.java
+++ Solution.java
@@ -1,20 +1,15 @@
 package com.company;
 
 public class Solution {
-
     public int solution(int n) {
-        int[] memo = new int[n/2 + 1];
-        if(n == 1 || n == 2 || n == 3) {
-            return 1;
+        int answer = 0;
+
+        while(n != 0) {
+            if(n % 2 == 1) answer++;
+
+            n /= 2;
         }
 
-        memo[1] = 1;
-        memo[2] = 1;
-
-        for(int i = 3; i <= n/2; i++) {
-            memo[i] = i % 2 == 0 ? memo[i / 2] : memo[i / 2] + 1;
-        }
-
-        return n % 2 == 0 ? memo[n / 2] : memo[n / 2] + 1;
+        return answer;
     }
 }
