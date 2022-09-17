273185de6ecac6b3f5093c0ac25aa5b47034fc7c
Hyunggi <phg9587@naver.com>
2022-09-17 16:15:11 +0900

    프로그래머스 월간 코드 챌린지 시즌2 2개 이하로 다른 비트 - 풀이 중
    생각나는 대로 풀었는데 시간초과 걸림

--- Solution.java
+++ Solution.java
@@ -1,38 +1,27 @@
 package com.company;
+
 class Solution {
-    public int[] solution(String s) {
-        //answer 배열 정의
-        int[] answer = new int[2];
+    public long[] solution(long[] numbers) {
+        long[] answer = new long[numbers.length];
 
-        //변환 횟수를 저장할 변수 생성
-        int conversionCount = 0;
-        //없앤 0의 수를 저장할 변수 생성
-        int zeroCount = 0;
-
-        //주어진 s 가 "1"이 될 때까지 루프를 반복
-        while(!s.equals("1")) {
-            //현재의 길이를 저장
-            int curStrLength = s.length();
-            //0을 공백으로 변환
-            s = s.replace("0", "");
-            //공백으로 변환 후 길이를 저장
-            int afterStrLength = s.length();
-
-            //2개의 길이 차이를 구하면 변환된 0의 갯수를 알 수 있음
-            zeroCount += (afterStrLength - curStrLength);
-
-            //현재 길이를 다시 2진수로 변환
-            s = Integer.toBinaryString(afterStrLength);
-
-            //변환을 할때마다 conversionCount 업데이트
-            conversionCount++;
+        int index = 0;
+        for (long number : numbers) {
+            answer[index] = findNewNum(number);
+            index++;
         }
 
-        //루프가 끝나면 answer 배열을 업데이트.
-        //zeroCount 는 음수가 나오기 때문에 마지막에 - 을 다시 붙여서 양수로 만든다.
-        answer[0] = conversionCount;
-        answer[1] = -zeroCount;
-
         return answer;
+    }
+
+    private long findNewNum(long number) {
+        String numberBinaryString = Long.toBinaryString(number);
+        long newNumber = number + 1;
+        while (true) {
+            long XorValue = number ^ newNumber;
+            String newNumberBinaryString = Long.toBinaryString(XorValue).replace("0", "");
+            if(newNumberBinaryString.length() <= 2)
+                return newNumber;
+            newNumber++;
+        }
     }
 }
