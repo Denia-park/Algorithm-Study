39cda66aec50d6d3c328b41010c9848ec5baf5f1
Hyunggi <phg9587@naver.com>
2022-09-21 21:12:25 +0900

    프로그래머스 Summer/Winter Coding(~2018) 방문 길이 - 풀이 중
    테스트 케이스 여러개 실패함 다시 확인 중

--- Solution.java
+++ Solution.java
@@ -4,18 +4,59 @@ import java.util.Set;
 class Solution {
     int mapSizeLimit;
     Set<String> pathMemory;
+    int[] startPoint;
     public int solution(String dirs) {
         mapSizeLimit = 5;
         pathMemory = new HashSet<>();
+        startPoint = new int[]{0, 0};
 
         for (int i = 0; i < dirs.length(); i++) {
-            movePath(dirs.substring(i));
+            char command = dirs.charAt(i);
+            movePath(command);
         }
 
         return pathMemory.size();
     }
 
-    private void movePath(String substring) {
+    private void movePath(char command) {
+        int originX = startPoint[0];
+        int originY = startPoint[1];
 
+        StringBuilder tempPathMemory = new StringBuilder();
+        tempPathMemory.append(originX).append(originY);
+
+        switch (command) {
+            case 'U':
+                if (originY + 1 <= 5){
+                    originY++;
+                    break;
+                }
+                return;
+            case 'D':
+                if (originY - 1 >= -5) {
+                    originY--;
+                    break;
+                }
+                return;
+            case 'R':
+                if (originX + 1 <= 5) {
+                    originX++;
+                    break;
+                }
+                return;
+            case 'L':
+                if (originX - 1 >= -5) {
+                    originX--;
+                    break;
+                }
+                return;
+        }
+
+        tempPathMemory.append(originX).append(originY);
+
+        startPoint[0] = originX;
+        startPoint[1] = originY;
+
+        pathMemory.add(tempPathMemory.toString());
     }
 }
