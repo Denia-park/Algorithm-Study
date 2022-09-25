cc3661e8038584b6f5e66c87f6b03506b413cfe0
Hyunggi <phg9587@naver.com>
2022-09-25 22:39:52 +0900

    Union-Find (합집합 찾기) 알고리즘 공부

---
+++ UnionFind/UnionFind.java
@@ -0,0 +1,47 @@
+package UnionFind;
+
+public class UnionFind {
+    public static void main(String[] args) {
+        int[] parents = new int[11];
+        for (int i = 1; i < 11; i++) {
+            parents[i] = i;
+        }
+
+        unionParent(parents, 1, 2);
+        unionParent(parents, 2, 3);
+        unionParent(parents, 3, 4);
+        unionParent(parents, 5, 6);
+
+        System.out.printf("1 과 4는 연결되어 있나요? %s \n", findParent(parents,1,4));
+        System.out.printf("1 과 6는 연결되어 있나요? %s \n", findParent(parents,1,6));
+
+        unionParent(parents,1,6);
+
+        System.out.printf("1 과 6는 연결되어 있나요? %s \n", findParent(parents,1,6));
+    }
+
+    private static void unionParent(int[] parents, int element1, int element2) {
+        int parentOfElem1 = getParent(parents, element1);
+        int parentOfElem2 = getParent(parents, element2);
+
+        if(parentOfElem1 == parentOfElem2) return;
+
+        if(parentOfElem1 < parentOfElem2) parents[parentOfElem2] = parentOfElem1;
+        else parents[parentOfElem1] = parentOfElem2;
+    }
+
+    private static int getParent(int[] parents, int element) {
+        int myParent = parents[element];
+
+        if(myParent == element) return element;
+
+        return parents[element] = getParent(parents, myParent);
+    }
+
+    private static boolean findParent(int[] parents, int element1, int element2) {
+        int parentOfElem1 = getParent(parents, element1);
+        int parentOfElem2 = getParent(parents, element2);
+
+        return parentOfElem1 == parentOfElem2;
+    }
+}
