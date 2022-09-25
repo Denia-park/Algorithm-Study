334f2359de4173354aa960796983e7468c75e296
Hyunggi <phg9587@naver.com>
2022-09-25 22:47:52 +0900

    주석 추가

--- UnionFind/UnionFind.java
+++ UnionFind/UnionFind.java
@@ -20,6 +20,7 @@ public class UnionFind {
         System.out.printf("1 과 6는 연결되어 있나요? %s \n", findParent(parents,1,6));
     }
 
+    //두 부모 노드를 합치는 함수
     private static void unionParent(int[] parents, int element1, int element2) {
         int parentOfElem1 = getParent(parents, element1);
         int parentOfElem2 = getParent(parents, element2);
@@ -30,6 +31,7 @@ public class UnionFind {
         else parents[parentOfElem1] = parentOfElem2;
     }
 
+    //부모 노드를 찾는 함수
     private static int getParent(int[] parents, int element) {
         int myParent = parents[element];
 
@@ -37,7 +39,7 @@ public class UnionFind {
 
         return parents[element] = getParent(parents, myParent);
     }
-
+    //같은 부모를 가지는지 확인
     private static boolean findParent(int[] parents, int element1, int element2) {
         int parentOfElem1 = getParent(parents, element1);
         int parentOfElem2 = getParent(parents, element2);
