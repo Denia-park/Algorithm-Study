b5095f0d393bf9c70135191006b7152285d28ef3
Hyunggi <phg9587@naver.com>
2022-09-27 23:36:10 +0900

    프로그래머스 2018 KAKAO BLIND RECRUITMENT [3차] n진수 게임 - 풀이 완료 , 다른 풀이 적용 , 주석 추가
    쓸모없는 과정을 생략하여 시간을 단축시킴 , 주석 추가

--- Solution.java
+++ Solution.java
@@ -5,11 +5,13 @@ class Solution {
         int number = 0;
         StringBuilder allNumberString = new StringBuilder();
 
+        //우리가 필요한 길이만큼 String을 합친다.
         while (allNumberString.length() < t * m){
             allNumberString.append(Integer.toString(number, n));
             number++;
         }
 
+        //내 차례에 맞는 char을 더하면서 answer를 만든다.
         for (int round = 0; round < t; round++) {
             answer.append(allNumberString.charAt(p - 1 + (round * m)));
         }
