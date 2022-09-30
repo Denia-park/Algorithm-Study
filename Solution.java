1215fdee468b5e70f06681eab630492ad5e80c58
Hyunggi <phg9587@naver.com>
2022-09-30 20:29:55 +0900

    프로그래머스 코딩테스트 입문 옹알이 - 풀이 완료
    String.replace 사용

--- Solution.java
+++ Solution.java
@@ -1,28 +1,21 @@
-//정답 코드 참조 : https://shanepark.tistory.com/183
+class Solution {
+    String[] banList = {"ayaaya", "yeye", "woowoo", "mama"};
+    String[] speakingList = {"aya", "ye", "woo", "ma"};
+    public int solution(String[] babbling) {
+        int answer = 0;
 
-public class Solution {
-    int solution(int[][] land) {
-        final int length = land.length;
-
-        for (int i = 1; i < length; i++) {
-            land[i][0] += max(land[i - 1][1], land[i - 1][2], land[i - 1][3]);
-            land[i][1] += max(land[i - 1][0], land[i - 1][2], land[i - 1][3]);
-            land[i][2] += max(land[i - 1][1], land[i - 1][3], land[i - 1][0]);
-            land[i][3] += max(land[i - 1][1], land[i - 1][2], land[i - 1][0]);
+        for (String str : babbling) {
+            String tempStr = str;
+            for (String ban : banList) {
+                tempStr = tempStr.replace(ban, "z");
+            }
+            for (String speak : speakingList) {
+                tempStr = tempStr.replace(speak, "");
+            }
+            if (tempStr.length() == 0) {
+                answer++;
+            }
         }
-
-        return max(land[length - 1]);
-    }
-
-    public int max(int a, int b, int c) {
-        return Math.max(Math.max(a, b), c);
-    }
-
-    public int max(int[] arr) {
-        int max = 0;
-        for(int number : arr) {
-            max = Math.max(max, number);
-        }
-        return max;
+        return answer;
     }
 }
