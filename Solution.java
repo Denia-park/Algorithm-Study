4758abc5a11ba0066a7112f4c241a9b11bdd6137
Hyunggi <phg9587@naver.com>
2022-10-05 21:29:24 +0900

    프로그래머스 월간 코드 챌린지 시즌1 쿼드압축 후 개수 세기 - 풀이 진행중 , 구현 문제
    풀이 진행중

--- Solution.java
+++ Solution.java
@@ -1,11 +1,60 @@
 class Solution {
-    public int solution(int[] sides) {
-        int bigValue = Math.max(sides[0], sides[1]);
-        int smallValue = Math.min(sides[0], sides[1]);
+    int zeroCount;
+    int oneCount;
+    public int[] solution(int[][] arr) {
+        int[] answer = new int[2];
 
-        int lowLimit = bigValue - smallValue;
-        int highLimit = bigValue + smallValue;
+        zeroCount = 0;
+        oneCount = 0;
 
-        return highLimit - lowLimit - 1;
+        for (int[] ints : arr) {
+            for (int anInt : ints) {
+                if(anInt == 0){
+                    zeroCount++;
+                }else{
+                    oneCount++;
+                }
+            }
+        }
+
+        checkAllElements(arr, 0, arr.length);
+
+        answer[0] += zeroCount;
+        answer[1] += oneCount;
+
+        return answer;
+    }
+
+    private void checkAllElements(int[][] arr, int start ,int end) {
+        if(start == end){
+            return;
+        }
+
+        boolean flag = true;
+        int startValue = arr[start][start];
+
+        out : for (int i = start; i < end; i++) {
+            for (int j = start; j < end; j++) {
+                if(startValue != arr[i][j]){
+                    flag = false;
+                    break out;
+                }
+            }
+        }
+
+        if(flag){
+            int tempValue = (int) Math.pow((end - start + 1), 2);
+
+            if(startValue == 0){
+                zeroCount += tempValue;
+            }else{
+                oneCount += tempValue;
+            }
+        }
+
+        checkAllElements(arr, start, end / 2);
+        checkAllElements(arr, end / 2, end);
+        checkAllElements(arr, start, end / 2);
+        checkAllElements(arr, start, end / 2);
     }
 }
