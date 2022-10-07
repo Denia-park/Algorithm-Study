603aeba7e9a55a52a09785768a51715436bd1f1d
Hyunggi <phg9587@naver.com>
2022-10-07 13:09:04 +0900

    프로그래머스 코딩테스트 입문 외계어 사전 - 풀이 완료, 다른 답 참고
    제한사항에서 제공되는 제한이 너무 널널해서 완전 탐색으로 풀이를 진행하는게 100배 빠르다.

--- Solution.java
+++ Solution.java
@@ -19,3 +19,30 @@ class Solution {
         return 2;
     }
 }
+
+/*
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        int answer = 2;
+
+
+        for(String dicS : dic) {
+
+            boolean isRight = true;
+            for(String spellS : spell) {
+                if(dicS.indexOf(spellS) == -1) {
+                    isRight = false;
+                    break;
+                }
+            }
+
+            if(isRight) {
+                answer = 1;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
+ */
