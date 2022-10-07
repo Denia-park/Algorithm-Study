121c7b76dad236ecc05271a1aaca011f1505d7b5
Hyunggi <phg9587@naver.com>
2022-10-07 13:06:24 +0900

    프로그래머스 코딩테스트 입문 외계어 사전 - 풀이 완료
    완전탐색으로 쉽게 풀수도 있었지만 , 그냥 조금 더 효율적인 코드를 찾다보니 시간이 오래 걸렸다.

--- Solution.java
+++ Solution.java
@@ -1,50 +1,21 @@
-class Solution{
-    //따로 makeNewArr 를 써서 배열을 만들게 되면 메모리 초과가 남
-    //구현 문제로 이해하고 그냥 바로 return할 arr를 만들어야 함
-    public int[] solution (int n, long left, long right){
-        //구해야하는 개수 초기화
-        int arrNum = (int) (right - left + 1L);
-        //answer 초기화
-        int[] answer = new int[arrNum];
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-        //필요한 row, col 초기화
-        int startRow = (int) (left / n);
-        int startCol = (int) (left %  n);
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        List<Character> words = Arrays.stream(spell).map(s -> s.charAt(0)).collect(Collectors.toList());
 
-        //지금까지 arr에 저장한 원소 개수를 셀 변수
-        int myCount = 0;
+        for (String str : dic) {
+            if(str.length() < spell.length) continue;
 
-        //처음에 내가 원하는 startRow , startCol 로 시작을 하고 startFlag를 초기화 시켜서
-        //다음 row 부터는 col을 0부터 시작하게 설정
-        boolean startFlag = true;
+            Set<Character> spellSet = str.chars().mapToObj(c -> (char) c).collect(Collectors.toSet());
 
-        out :
-        for(int row = startRow; row < n; row++){
-            for(int col = startFlag ? startCol : 0; col < n; col++){
-                answer[myCount++] = Math.max(row+1,col+1);
-                
-                //arrNum이 myCount 와 같으면 return할 배열을 제대로 채웠으므로 for문을 종료
-                if(myCount == arrNum){
-                    break out;
-                }
-            }
-
-            startFlag = false;
+            if (spellSet.containsAll(words))
+                return 1;
         }
 
-        return answer;
-    }
-    
-    //처음에 만들어서 사용을 했는데 필요없어서 사용하지 않음
-    public int[][] makeNewArr (int size){
-        int[][] rtArr = new int[size][size];
-
-        for (int row = 0; row < size; row++){
-            for (int col = 0; col < size; col++){
-                rtArr[row][col] = Math.max(row+1,col+1);
-            }
-        }
-
-        return rtArr;
+        return 2;
     }
 }
