05badcc857fa2e2b31fd9697e157992d8dc65864
Hyunggi <phg9587@naver.com>
2022-10-11 22:16:52 +0900

    프로그래머스 코딩테스트 입문 겹치는 선분의 길이 - 풀이 중
    여전히 한개가 틀리는데 뭔지 모르겠음

--- Solution.java
+++ Solution.java
@@ -1,33 +1,22 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int solution(int[][] lines) {
         int answer = 0;
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
 
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] lineArr = new int[200];
 
         for (int[] line : lines) {
             int tempMin = Math.min(line[0], line[1]);
             int tempMax = Math.max(line[0], line[1]);
-            min = Math.min(min, tempMin);
-            max = Math.max(max, tempMax);
-
             for (int i = tempMin; i < tempMax + 1; i++) {
-                map.put(i, map.getOrDefault(i, 0) + 1);
+                lineArr[i + 100]++;
             }
         }
 
-        for (int i = min + 1; i < max + 1; i++) {
-            if (!map.containsKey(i) || !map.containsKey(i - 1)) continue;
-
-            if (map.get(i - 1) >= 2 && map.get(i) >= 2) {
+        for (int i = 1; i < lineArr.length; i++) {
+            if (lineArr[i - 1] > 1 && lineArr[i] > 1) {
                 answer++;
             }
         }
-
 
         return answer;
     }
