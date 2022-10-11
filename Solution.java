64a45f8e5ae2c63a49bd60bbd99f2e03e566b9db
Hyunggi <phg9587@naver.com>
2022-10-11 22:33:54 +0900

    프로그래머스 코딩테스트 입문 겹치는 선분의 길이 - 풀이 완료
    Map 사용해서 모든 값을 기록함. 시간이 오래 걸린다.

--- Solution.java
+++ Solution.java
@@ -1,21 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int solution(int[][] lines) {
         int answer = 0;
 
-        int[] lineArr = new int[200];
+        Map<String, Integer> map = new HashMap<String, Integer>();
 
         for (int[] line : lines) {
             int tempMin = Math.min(line[0], line[1]);
             int tempMax = Math.max(line[0], line[1]);
-            for (int i = tempMin; i < tempMax + 1; i++) {
-                lineArr[i + 100]++;
+            for (int i = tempMin + 1; i < tempMax + 1; i++) {
+                String str = (i - 1) + "/" + i;
+                map.put(str, map.getOrDefault(str, 0) + 1);
             }
         }
 
-        for (int i = 1; i < lineArr.length; i++) {
-            if (lineArr[i - 1] > 1 && lineArr[i] > 1) {
-                answer++;
-            }
+        for (Integer value : map.values()) {
+            if (value > 1) answer++;
         }
 
         return answer;
