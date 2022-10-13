70cd42ea902b2646f7370608bbee0f9a8616d2a9
Hyunggi <phg9587@naver.com>
2022-10-14 01:20:08 +0900

    프로그래머스 코딩테스트 입문 특이한 정렬 - 풀이 완료
    Stream 의 체인 메서드로 해결

--- Solution.java
+++ Solution.java
@@ -1,29 +1,20 @@
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Solution {
     public Integer[] solution(int[] numlist, int n) {
-        List<Integer> solution = IntStream.of(numlist).boxed().collect(Collectors.toList());
 
-        solution.sort(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                int o1Abs = Math.abs(o1 - n);
-                int o2Abs = Math.abs(o2 - n);
+        return IntStream.of(numlist).boxed().sorted((o1, o2) -> {
+            int o1Abs = Math.abs(o1 - n);
+            int o2Abs = Math.abs(o2 - n);
 
-                if (o1Abs == o2Abs) {
-                    if (o1 > o2) return -1;
-                    else if (o1 < o2) return 1;
-                } else {
-                    return o1Abs - o2Abs;
-                }
-
-                return 0;
+            if (o1Abs == o2Abs) {
+                if (o1 > o2) return -1;
+                else if (o1 < o2) return 1;
+            } else {
+                return o1Abs - o2Abs;
             }
-        });
 
-        return solution.toArray(new Integer[0]);
+            return 0;
+        }).toArray(Integer[]::new);
     }
 }
