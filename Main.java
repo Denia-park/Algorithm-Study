c5393f0b8b7ed35aa27d0728b02a3a096ad53d90
Hyunggi <phg9587@naver.com>
2022-10-15 02:36:32 +0900

    프로그래머스 코딩테스트 입문 등수 매기기 - 풀이 완료
    간단한 방법이 생각나지 않아서 객체지향적으로 코드를 작성함

--- Main.java
+++ Main.java
@@ -4,7 +4,8 @@ public class Main {
     static public void main(String[] args) {
         Solution testSolution = new Solution();
 
-        System.out.println(Arrays.toString(testSolution.solution(new int[]{1, 2, 3, 4, 5, 6}, 4)));
+        System.out.println(Arrays.toString(testSolution.solution(new int[][]{{80, 70}, {90, 50}, {40, 70}, {50, 80}})));
+        System.out.println(Arrays.toString(testSolution.solution(new int[][]{{80, 70}, {70, 80}, {30, 50}, {90, 100}, {100, 90}, {100, 100}, {10, 30}})));
     }
 }
 
--- Solution.java
+++ Solution.java
@@ -1,20 +1,67 @@
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-    public Integer[] solution(int[] numlist, int n) {
+    public int[] solution(int[][] score) {
+        int[] answer = new int[score.length];
 
-        return IntStream.of(numlist).boxed().sorted((o1, o2) -> {
-            int o1Abs = Math.abs(o1 - n);
-            int o2Abs = Math.abs(o2 - n);
+        List<StudentScore> list = new ArrayList<StudentScore>();
 
-            if (o1Abs == o2Abs) {
-                if (o1 > o2) return -1;
-                else if (o1 < o2) return 1;
-            } else {
-                return o1Abs - o2Abs;
+        for (int i = 0; i < score.length; i++) {
+            int[] ints = score[i];
+            list.add(new StudentScore(ints[0], ints[1], i));
+        }
+
+        list.sort(null);
+
+        double tempScore = list.get(0).getAvg();
+        int tempRank = 1;
+        int tempCount = 1;
+
+        for (StudentScore studentScore : list) {
+            if (tempScore > studentScore.getAvg()) {
+                tempScore = studentScore.getAvg();
+                tempRank = tempCount;
             }
 
-            return 0;
-        }).toArray(Integer[]::new);
+            int tempIndex = studentScore.getIndex();
+            answer[tempIndex] = tempRank;
+
+            tempCount++;
+        }
+
+        return answer;
+    }
+}
+
+class StudentScore implements Comparable<StudentScore> {
+    private final int engScore, mathScore, index;
+    private final double avg;
+
+    public StudentScore(int engScore, int mathScore, int index) {
+        this.engScore = engScore;
+        this.mathScore = mathScore;
+        this.index = index;
+
+        this.avg = (engScore + mathScore) / 2.0;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public double getAvg() {
+        return avg;
+    }
+
+    @Override
+    public int compareTo(StudentScore o1) {
+        double avg1 = this.getAvg();
+        double avg2 = o1.getAvg();
+
+        if (avg1 > avg2) return -1;
+        else if (avg1 < avg2) return 1;
+
+        return 0;
     }
 }
