d18c9aa96f94766f7644748672ddf7720c43e041
Hyunggi <phg9587@naver.com>
2022-10-16 16:07:31 +0900

    프로그래머스 코딩테스트 입문 로그인 성공? - 풀이 완료
    Map 사용

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
-import java.util.Arrays;
-
 public class Main {
     static public void main(String[] args) {
         Solution testSolution = new Solution();
 
-        System.out.println(Arrays.toString(testSolution.solution(new int[][]{{80, 70}, {90, 50}, {40, 70}, {50, 80}})));
-        System.out.println(Arrays.toString(testSolution.solution(new int[][]{{80, 70}, {70, 80}, {30, 50}, {90, 100}, {100, 90}, {100, 100}, {10, 30}})));
+        System.out.println((testSolution.solution(new String[]{"meosseugi", "1234"}, new String[][]{{"rardss", "123"}, {"yyoom", "1234"}, {"meosseugi", "1234"}})));
     }
 }
 
--- Solution.java
+++ Solution.java
@@ -1,67 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
-    public int[] solution(int[][] score) {
-        int[] answer = new int[score.length];
+    public String solution(String[] id_pw, String[][] db) {
+        Map<String, String> dbMap = new HashMap<String, String>();
 
-        List<StudentScore> list = new ArrayList<StudentScore>();
-
-        for (int i = 0; i < score.length; i++) {
-            int[] ints = score[i];
-            list.add(new StudentScore(ints[0], ints[1], i));
+        for (String[] strings : db) {
+            dbMap.put(strings[0], strings[1]);
         }
 
-        list.sort(null);
-
-        double tempScore = list.get(0).getAvg();
-        int tempRank = 1;
-        int tempCount = 1;
-
-        for (StudentScore studentScore : list) {
-            if (tempScore > studentScore.getAvg()) {
-                tempScore = studentScore.getAvg();
-                tempRank = tempCount;
+        if (dbMap.containsKey(id_pw[0])) {
+            if (dbMap.get(id_pw[0]).equals(id_pw[1])) {
+                return "login";
+            } else {
+                return "wrong pw";
             }
-
-            int tempIndex = studentScore.getIndex();
-            answer[tempIndex] = tempRank;
-
-            tempCount++;
+        } else {
+            return "fail";
         }
-
-        return answer;
-    }
-}
-
-class StudentScore implements Comparable<StudentScore> {
-    private final int engScore, mathScore, index;
-    private final double avg;
-
-    public StudentScore(int engScore, int mathScore, int index) {
-        this.engScore = engScore;
-        this.mathScore = mathScore;
-        this.index = index;
-
-        this.avg = (engScore + mathScore) / 2.0;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
-    public double getAvg() {
-        return avg;
-    }
-
-    @Override
-    public int compareTo(StudentScore o1) {
-        double avg1 = this.getAvg();
-        double avg2 = o1.getAvg();
-
-        if (avg1 > avg2) return -1;
-        else if (avg1 < avg2) return 1;
-
-        return 0;
     }
 }
