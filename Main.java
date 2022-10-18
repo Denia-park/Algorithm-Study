4d7aaa044446c3ca35f7b9815fb8a4d4d22615ab
Hyunggi <phg9587@naver.com>
2022-10-18 18:39:33 +0900

    프로그래머스 코딩테스트 입문 이진수 더하기 - 풀이 완료
    풀이 완

--- Main.java
+++ Main.java
@@ -2,10 +2,7 @@ public class Main {
     static public void main(String[] args) {
         Solution testSolution = new Solution();
 
-        System.out.println((testSolution.solution(0)));
-        System.out.println((testSolution.solution(100)));
-//        System.out.println((testSolution.solution(1081)));
-//        System.out.println((testSolution.solution(1000000)));
+        System.out.println((testSolution.solution("10", "11")));
     }
 }
 
--- Solution.java
+++ Solution.java
@@ -1,17 +1,7 @@
 class Solution {
-    final int BONUS_NUM = 10;
+    public String solution(String bin1, String bin2) {
+        long newValue = Long.parseLong(bin1, 2) + Long.parseLong(bin2, 2);
 
-    public int solution(int chicken) {
-        int answer = 0;
-
-        while (chicken >= BONUS_NUM) {
-            int newChick = chicken / BONUS_NUM;
-            int restChick = chicken % BONUS_NUM;
-            chicken = newChick + restChick;
-
-            answer += newChick;
-        }
-
-        return answer;
+        return Long.toBinaryString(newValue);
     }
 }
