1d3983dbc0f083035135140ce676f6ffe3c1a182
Hyunggi <phg9587@naver.com>
2022-10-23 00:41:32 +0900

    프로그래머스 연습문제 할인 행사 - 풀이 중
    Map을 2개 써서 풀이 진행중

--- Solution.java
+++ Solution.java
@@ -1,14 +1,54 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-    public int solution(int submitNum, int receiveNum, int initCokeNum) {
+    final int USER_QUALIFICATION_DAY = 10;
+
+    public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
 
-        while (initCokeNum >= submitNum) {
-            int tempReceive = (initCokeNum / submitNum) * receiveNum;
-            initCokeNum = initCokeNum % submitNum + tempReceive;
+        Map<String, Integer> wantMap = new HashMap<String, Integer>();
+        for (int i = 0; i < want.length; i++) {
+            String s = want[i];
+            int count = number[i];
 
-            answer += tempReceive;
+            wantMap.put(s, count);
         }
 
+        Map<String, Integer> discountMap = new HashMap<String, Integer>();
+        for (int i = 0; i < USER_QUALIFICATION_DAY; i++) {
+            String s = discount[i];
+
+            discountMap.put(s, discountMap.getOrDefault(s, 0) + 1);
+        }
+
+        if (compare(wantMap, discountMap)) {
+            answer++;
+        }
+
+        for (int i = 1; i <= discount.length - USER_QUALIFICATION_DAY; i++) {
+            String deleteItem = discount[i - 1];
+            discountMap.put(deleteItem, discountMap.get(deleteItem) - 1);
+
+            String addItem = discount[i + USER_QUALIFICATION_DAY - 1];
+            discountMap.put(addItem, discountMap.getOrDefault(addItem, 0) + 1);
+
+            if (compare(wantMap, discountMap)) {
+                answer++;
+            }
+        }
+
+
         return answer;
+    }
+
+    public boolean compare(Map<String, Integer> wantMap, Map<String, Integer> compareMap) {
+        for (String key : wantMap.keySet()) {
+            if (!compareMap.containsKey(key) || wantMap.get(key) != compareMap.get(key)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
