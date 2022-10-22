07bbd7634967b439d8eea5692ddaa174a391613d
Hyunggi <phg9587@naver.com>
2022-10-22 23:43:49 +0900

    프로그래머스 연습문제 콜라 문제 - 풀이 완료

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     static public void main(String[] args) {
         Solution testSolution = new Solution();
 
-        System.out.println((testSolution.solution(3)));
+        System.out.println((testSolution.solution(2, 1, 20)));
     }
 }
 
