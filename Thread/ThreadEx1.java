af895b234ac13a997f7964abe878e0a8197a7486
HyunggiPark <phg9587@naver.com>
2022-11-02 20:20:22 +0900

    Thread 기본 사용 코드 구현

---
+++ Thread/ThreadEx1.java
@@ -0,0 +1,32 @@
+package Thread;
+
+public class ThreadEx1 {
+    public static void main(String[] args) {
+        ThreadEx1_1 t1 = new ThreadEx1_1();
+
+        Runnable r = new ThreadEx1_2();
+        Thread t2 = new Thread(r);
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class ThreadEx1_1 extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(getName()); // 조상인 Thread 의 getName () 을 호출
+        }
+    }
+}
+
+class ThreadEx1_2 implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            // Thread.currentThread() - 현재 실행중인 Thread를 반환한다.
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+}
+
