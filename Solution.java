f25ae741951cdc94fc61e964838bff7f2982ff3c
HyunggiPark <phg9587@naver.com>
2022-11-02 10:44:06 +0900

    프로그래머스 연습문제 야간 전술보행 - 풀이 진행중
    생각보다 정렬을 할 게 많음

--- Solution.java
+++ Solution.java
@@ -1,11 +1,36 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
-    public int solution(int[] sides) {
-        Arrays.sort(sides);
-        int max = sides[sides.length - 1];
+    public int solution(int distance, int[][] scope, int[][] times) {
+        int answer = 0;
+        Map<Integer, Boolean> workStatusMap = new HashMap<Integer, Boolean>();
 
-        int restLength = sides[0] + sides[1];
-        return restLength <= max ? 2 : 1;
+        int scopeIndex = 0;
+
+        out:
+        for (int[] eachScope : scope) {
+            int startTime = Math.min(eachScope[0], eachScope[1]);
+            int endTime = Math.max(eachScope[0], eachScope[1]);
+            for (int curTime = startTime; curTime <= endTime; curTime++) {
+                if (isWorkingTime(curTime, times[scopeIndex])) {
+                    break out;
+                }
+            }
+
+            scopeIndex++;
+        }
+
+        return Math.min(answer, distance);
+    }
+
+    private boolean isWorkingTime(int curTime, int[] time) {
+        int workTime = time[0];
+        int restTime = time[1];
+
+        int totalTime = workTime + restTime;
+
+        curTime
+
     }
 }
