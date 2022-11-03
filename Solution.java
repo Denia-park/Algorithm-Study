2fbe40910f3af7f04cfebfa5797c062c6939d0e2
HyunggiPark <phg9587@naver.com>
2022-11-03 20:32:26 +0900

    프로그래머스 연습문제 숫자블록 - 풀이 중
    테케는 통과하지만 효율성에서 다 탈락

--- Solution.java
+++ Solution.java
@@ -2,75 +2,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public int solution(int distance, int[][] scope, int[][] times) {
-        List<Enemy> enemies = new ArrayList<Enemy>();
+    public int[] solution(long begin, long end) {
+        int[] answer = new int[(int) (end - begin) + 1];
+        List<Integer> primeList = new ArrayList<Integer>();
 
-        for (int i = 0; i < scope.length; i++) {
-            int[] distanceArr = scope[i];
-            int[] timeArr = times[i];
+        long myNum = begin;
 
-            int workStartDistance = Math.min(distanceArr[0], distanceArr[1]);
-            int workEndDistance = Math.max(distanceArr[0], distanceArr[1]);
+        primeList.add(2);
 
-            int workTime = timeArr[0];
-            int restTime = timeArr[1];
+        for (int val = 3; val <= begin; val += 2) {
+            boolean addFlag = true;
 
-            enemies.add(new Enemy(workStartDistance, workEndDistance, workTime, restTime));
-        }
-
-        enemies.sort(null);
-
-        for (Enemy enemy : enemies) {
-            for (int curTime = enemy.getWorkStartDistance(); curTime <= enemy.getWorkEndDistance(); curTime++) {
-                if (isWorkingTime(curTime, enemy.getWorkTime(), enemy.getRestTime())) {
-                    return curTime;
+            for (Integer integer : primeList) {
+                if (val % integer == 0) {
+                    addFlag = false;
+                    break;
                 }
+            }
+
+            if (addFlag) {
+                primeList.add((int) val);
             }
         }
 
-        return distance;
-    }
+        for (int idx = 0; idx <= (end - begin); idx++) {
+            if (myNum == 1) {
+                answer[idx] = 0;
+            } else if (myNum == 2 || myNum == 3) {
+                answer[idx] = 1;
+                primeList.add((int) myNum);
+            } else {
+                boolean addFlag = true;
+                for (Integer integer : primeList) {
+                    if (myNum % integer == 0) {
+                        answer[idx] = (int) (myNum / integer);
+                        addFlag = false;
+                        break;
+                    }
+                }
+                if (addFlag) {
+                    answer[idx] = 1;
+                    primeList.add((int) myNum);
+                }
+            }
 
-    private boolean isWorkingTime(int curTime, int workTime, int restTime) {
-        int totalTime = workTime + restTime;
-        int curStatus = curTime % totalTime;
+            myNum++;
+        }
 
-        return 0 < curStatus && curStatus <= workTime;
-    }
-}
-
-class Enemy implements Comparable<Enemy> {
-    private final int workStartDistance;
-    private final int workEndDistance;
-
-    private final int workTime;
-    private final int restTime;
-
-    public Enemy(int workStartDistance, int workEndDistance, int workTime, int restTime) {
-        this.workStartDistance = workStartDistance;
-        this.workEndDistance = workEndDistance;
-        this.workTime = workTime;
-        this.restTime = restTime;
-    }
-
-    public int getWorkStartDistance() {
-        return workStartDistance;
-    }
-
-    public int getWorkEndDistance() {
-        return workEndDistance;
-    }
-
-    public int getWorkTime() {
-        return workTime;
-    }
-
-    public int getRestTime() {
-        return restTime;
-    }
-
-    @Override
-    public int compareTo(Enemy another) {
-        return this.workStartDistance - another.workStartDistance;
+        return answer;
     }
 }
