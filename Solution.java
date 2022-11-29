8013dcbd3db32d6384db553410d0ffb5d76efb15
HyunggiPark <phg9587@naver.com>
2022-11-30 00:28:01 +0900

    2019 KAKAO BLIND RECRUITMENT 무지의 먹방 라이브 - 3차 풀이 - 답변 확인 후 혼자서 풀었는데 효율성 테스트에서 탈락함

--- Solution.java
+++ Solution.java
@@ -1,42 +1,54 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
 
 class Solution {
-    public int solution(int[] food_times, long k) {
+    public int solution(int[] food_times, long timeLimit) {
+        PriorityQueue<Food> pq = new PriorityQueue<>();
+
+        int totalFoodEatTime = 0;
         int idxCheck = 1;
 
-        Queue<Food> queue = new LinkedList<>();
-
         for (int food_time : food_times) {
-            queue.add(new Food(idxCheck, food_time));
+            pq.add(new Food(idxCheck, food_time));
+            totalFoodEatTime += food_time;
             idxCheck++;
         }
+
+        if (totalFoodEatTime <= timeLimit) {
+            return -1;
+        }
+
         long time = 0;
+        long timeWeight = 0;
 
-        while (time < k) {
-            if (!queue.isEmpty()) {
-                Food food = queue.poll();
+        while (true) {
+            int size = pq.size();
+            Food minQuantityFood = pq.peek();
 
-                food.eat();
+            long curSpandTime = (minQuantityFood.getFoodQuantity() - timeWeight) * size;
 
-                if (food.getFoodQuantity() > 0) {
-                    queue.add(food);
-                }
+            if (time + curSpandTime > timeLimit) {
+                break;
             }
 
-            time++;
+            pq.poll();
+
+            time += curSpandTime;
+
+            timeWeight = minQuantityFood.getFoodQuantity();
         }
 
-        if (queue.isEmpty()) {
-            return -1;
-        } else {
-            return queue.peek().getNumber();
-        }
+        List<Food> restFoods = pq.stream().sorted(Comparator.comparingInt(Food::getNumber)).collect(Collectors.toList());
+        int size = restFoods.size();
+
+        return restFoods.get((int) (timeLimit - time) % size).getNumber();
     }
 
 }
 
-class Food {
+class Food implements Comparable<Food> {
     int number;
     int foodQuantity;
 
@@ -45,15 +57,16 @@ class Food {
         this.foodQuantity = foodQuantity;
     }
 
-    void eat() {
-        this.foodQuantity--;
-    }
-
     public int getNumber() {
         return number;
     }
 
     public int getFoodQuantity() {
         return foodQuantity;
+    }
+
+    @Override
+    public int compareTo(Food o) {
+        return this.getFoodQuantity() - o.foodQuantity;
     }
 }
