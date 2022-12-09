fbd4c78dae4a82a6ec0ce150b8debe720e701f62
HyunggiPark <phg9587@naver.com>
2022-12-09 20:48:50 +0900

    프로그래머스 연습문제 디펜스 게임 - 2차 풀이 실패 - 틀린 테케 많음

--- Solution.java
+++ Solution.java
@@ -1,38 +1,25 @@
 class Solution {
-    int answer;
-    int[] gEnemy;
+    public int solution(int mySoldierNum, int superPass, int[] enemys) {
+        int roundCheck = 0;
 
-    public int solution(int mySoldierNum, int superPass, int[] enemy) {
-        answer = Integer.MIN_VALUE;
-        gEnemy = enemy;
-
-        int round = 0;
-
-        defeatEnemy(mySoldierNum, superPass, round);
-
-
-        return answer;
-    }
-
-    private void defeatEnemy(int mySoldierNum, int superPass, int round) {
-        if (round >= gEnemy.length) {
-            answer = round;
-            return;
-        } else if (mySoldierNum < gEnemy[round] && superPass == 0) {
-            answer = Math.max(answer, round);
-            return;
-        }
-
-        for (int i = 0; i < 2; i++) {
-            if (i == 0) {
-                if (mySoldierNum >= gEnemy[round]) {
-                    defeatEnemy(mySoldierNum - gEnemy[round], superPass, round + 1);
-                }
-            } else {
-                if (superPass > 0) {
-                    defeatEnemy(mySoldierNum, superPass - 1, round + 1);
-                }
+        for (int enemy : enemys) {
+            if (mySoldierNum < enemy && superPass == 0) {
+                break;
             }
+
+            if (mySoldierNum - enemy > enemy) {
+                mySoldierNum -= enemy;
+            } else if (superPass > 0) {
+                superPass--;
+            } else if (mySoldierNum >= enemy) {
+                mySoldierNum -= enemy;
+            } else {
+                break;
+            }
+
+            roundCheck++;
         }
+
+        return roundCheck;
     }
 }
