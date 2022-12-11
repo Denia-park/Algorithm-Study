7676dbe0b2059d42fabe02696330b65fbad8adb9
HyunggiPark <phg9587@naver.com>
2022-12-11 16:24:48 +0900

    16장 네트워킹 - URL - NetworkEx2 구현

---
+++ Network/NetworkEx2.java
@@ -0,0 +1,24 @@
+package Network;
+
+import java.net.URL;
+
+public class NetworkEx2 {
+    public static void main(String[] args) throws Exception {
+        // 동작이 되지 않는 사이트
+        URL url = new URL("http://www.codechobo.com:80/sample/" + "hello.html?referer=codechobo#index1");
+
+        System.out.println("url.getAuthority() : " + url.getAuthority());
+        System.out.println("url.getContent()  : " + url.getContent());
+        System.out.println("url.getDefaultPort() : " + url.getDefaultPort());
+        System.out.println("url.getPort()  : " + url.getPort());
+        System.out.println("url.getFile()  : " + url.getFile());
+        System.out.println("url.getHost()  : " + url.getHost());
+        System.out.println("url.getPath()  : " + url.getPath());
+        System.out.println("url.getProtocol()  : " + url.getProtocol());
+        System.out.println("url.getQuery()  : " + url.getQuery());
+        System.out.println("url.getRef()  : " + url.getRef());
+        System.out.println("url.getUserInfo() : " + url.getUserInfo());
+        System.out.println("url.toExternalForm()  : " + url.toExternalForm());
+        System.out.println("url.toURI()  : " + url.toURI());
+    }
+}
