524c114bb55f2b23fd6606d5b3657e05c679f60c
HyunggiPark <phg9587@naver.com>
2022-12-12 20:36:21 +0900

    프로그래머스 2021 Dev-Matching: 웹 백엔드 개발자(상반기) 다단계 칫솔 판매 - 객체지향적 접근

--- Solution.java
+++ Solution.java
@@ -1,42 +1,77 @@
-import java.util.PriorityQueue;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
-    public int solution(int mySoldierNum, int superPass, int[] enemys) {
-        if (superPass >= enemys.length) {
-            return enemys.length;
+    final int UNIT_PRICE = 100;
+
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int[] answer = new int[enroll.length];
+        Map<String, Member> members = new HashMap<>();
+
+        for (String name : enroll) {
+            members.put(name, new Member(name));
         }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < referral.length; i++) {
+            if (referral[i].equals("-")) continue;
 
-        for (int i = 0; i < superPass; i++) {
-            pq.offer(enemys[i]);
+            Member curMember = members.get(enroll[i]);
+            Member referralMember = members.get(referral[i]);
+
+            curMember.setReferral(referralMember);
         }
 
-        int roundCheck = superPass;
+        for (int i = 0; i < seller.length; i++) {
+            Member tempSeller = members.get(seller[i]);
+            int tempSellAmount = amount[i] * UNIT_PRICE;
 
-        for (int i = superPass; i < enemys.length; i++) {
-            int superPassMinValue = pq.peek();
-
-            if (enemys[i] > superPassMinValue) {
-                pq.poll();
-                pq.offer(enemys[i]);
-
-                if (superPassMinValue > mySoldierNum) {
-                    break;
-                } else {
-                    mySoldierNum -= superPassMinValue;
-                }
-            } else {
-                if (enemys[i] > mySoldierNum) {
-                    break;
-                } else {
-                    mySoldierNum -= enemys[i];
-                }
-            }
-
-            roundCheck++;
+            tempSeller.calculateCredit(tempSellAmount);
         }
 
-        return roundCheck;
+        for (int i = 0; i < enroll.length; i++) {
+            int memberCredit = members.get(enroll[i]).getCredit();
+            answer[i] = memberCredit;
+        }
+        return answer;
+    }
+}
+
+class Member {
+    String name;
+    Member referral;
+    int credit;
+
+    public Member(String name) {
+        this.name = name;
+        this.referral = null;
+        this.credit = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public void setReferral(Member referral) {
+        this.referral = referral;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void addCredit(int amount) {
+        this.credit += amount;
+    }
+
+    public void calculateCredit(int tempSellAmount) {
+        int restCredit = (int) (tempSellAmount * 0.1f);
+        int myCredit = tempSellAmount - restCredit;
+        this.addCredit(myCredit);
+
+        if (referral == null) {
+            return;
+        }
+
+        this.referral.calculateCredit(restCredit);
     }
 }
