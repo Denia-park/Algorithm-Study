be8d3dcc4347f02293cccfab454da04934e7c77a
HyunggiPark <phg9587@naver.com>
2022-12-14 18:37:39 +0900

    백준 Java vs C++[3613] - 정답 코드 참조해서 스스로 풀었는데 틀림 - 이유 못찾는중

--- Solution.java
+++ Solution.java
@@ -1,68 +1,107 @@
 // 백준 - 3613 - Java vs C++
 
-import java.util.Stack;
+// 정답 참고
+// https://codejin.tistory.com/127
 
 class Solution {
     public String solution(String quizString) {
-        boolean errFlag = false;
+        String answer = quizString;
+
+        if (isError(quizString)) {
+            return "Error!";
+        } else if (isJava(quizString)) {
+            answer = makeJavaToCpp(quizString);
+        } else if (isCpp(quizString)) {
+            answer = makeCppToJava(quizString);
+        }
+
+        return answer;
+    }
+
+    String makeJavaToCpp(String quizString) {
+        StringBuilder sb = new StringBuilder(quizString);
+
+        for (int i = 0; i < sb.length(); i++) {
+            if (Character.isUpperCase(sb.charAt(i))) {
+                sb.replace(i, i + 1, "_" + Character.toLowerCase(sb.charAt(i)));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    String makeCppToJava(String quizString) {
+        StringBuilder sb = new StringBuilder(quizString);
         boolean changeFlag = false;
 
-        Stack<Character> stack = new Stack<>();
-
-        char startCh = quizString.charAt(0);
-
-        if (startCh == '_' || Character.isUpperCase(startCh) ||
-                quizString.charAt(quizString.length() - 1) == '_') {
-            return "Error!";
-        }
-
-        for (int i = 0; i < quizString.length(); i++) {
-            char ch = quizString.charAt(i);
-
-            if (ch == '_') {
-                errFlag = true;
-            } else {
-                if (errFlag && Character.isUpperCase(ch)) {
-                    return "Error!";
-                }
-            }
-
-            if (stack.isEmpty()) {
-                stack.push(ch);
-                continue;
-            }
-
-            char topChar = stack.peek();
-
-            if (changeFlag) {
-                stack.push(ch);
-                changeFlag = false;
-                continue;
-            }
-
-            if (topChar == '_') {
-                if (ch == '_') {
-                    return "Error!";
-                }
-                stack.pop();
-                stack.push(Character.toUpperCase(ch));
-                changeFlag = true;
-            } else if (Character.isUpperCase(topChar)) {
-                stack.pop();
-                stack.push('_');
-                stack.push(Character.toLowerCase(topChar));
-                stack.push(ch);
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == '_') {
                 changeFlag = true;
             } else {
-                stack.push(ch);
+                if (changeFlag) {
+                    sb.replace(i - 1, i + 1, String.valueOf(Character.toUpperCase(sb.charAt(i))));
+                    changeFlag = false;
+                }
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-
-        for (Character tempCh : stack) {
-            sb.append(tempCh);
-        }
         return sb.toString();
+    }
+
+    boolean isJava(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isUpperCase(str.charAt(i))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    boolean isCpp(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '_') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    boolean isError(String str) {
+        boolean hasUnderBar = false;
+        boolean hasUpper = false;
+
+        char saveCh = ' ';
+
+        for (int i = 0; i < str.length(); i++) {
+            char curCh = str.charAt(i);
+            if (curCh == '_') {
+                hasUnderBar = true;
+                if (saveCh == '_') {
+                    return true;
+                }
+            } else if (Character.isUpperCase(curCh)) {
+                hasUpper = true;
+            }
+
+            saveCh = curCh;
+        }
+
+        if (hasUnderBar && hasUpper) {
+            return true;
+        }
+
+        char startCh = str.charAt(0);
+        if (startCh == '_' || Character.isUpperCase(startCh)) {
+            return true;
+        }
+
+        char finalCh = str.charAt(str.length() - 1);
+        if (finalCh == '_') {
+            return true;
+        }
+
+        return false;
     }
 }
