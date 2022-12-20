26feac4b2178368b66088c63afe62bd1f03fbb2c
HyunggiPark <phg9587@naver.com>
2022-12-20 10:17:43 +0900

    백준 숫자 고르기 [2668] - 2차 문제 풀이 - visited 구현하고 사이클 확인했으나 틀림

--- Solution.java
+++ Solution.java
@@ -1,20 +1,38 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
+    Set<Integer> answer;
+    int[] gTable;
+    boolean[] visited;
 
     public void solution(int size, int[] table) {
-        List<Integer> answerList = new ArrayList<>();
+        answer = new HashSet<>();
+        gTable = table;
 
         for (int i = 1; i <= size; i++) {
-            if (i == table[table[i]]) {
-                answerList.add(i);
-            }
+            visited = new boolean[size + 1];
+            getParent(i, i);
         }
-        
-        System.out.println(answerList.size());
-        for (Integer integer : answerList) {
+
+        System.out.println(answer.size());
+        for (Integer integer : answer) {
             System.out.println(integer);
         }
+    }
+
+    private void getParent(int origin, int idx) {
+        if (visited[idx]) {
+            if (origin == idx) {
+                answer.add(origin);
+            }
+
+            return;
+        }
+
+        visited[idx] = true;
+
+        int parent = gTable[idx];
+        getParent(origin, parent);
     }
 }
