62fc561ea6a9e105a9b8a30f5efa0d1ef4d9963f
HyunggiPark <phg9587@naver.com>
2022-12-26 18:50:15 +0900

    백준 오르막 수 [11057] - 정답 참고

--- Solution.java
+++ Solution.java
@@ -1,28 +1,30 @@
+//정답 참고
+//https://minhamina.tistory.com/147
+
+//반례
+//Input : 856 -> Output : 100
+
 class Solution {
     final int DIVIDE_VALUE = 10007;
     long[][] dp;
 
     public void solution(int size) {
-        dp = new long[1001][11];
+        dp = new long[1002][10];
 
         //1자리 수 초기화
         long sum = 0;
         for (int i = 0; i < 10; i++) {
             dp[1][i] = 1;
-            sum += dp[1][i];
         }
-        dp[1][10] = sum;
 
-        for (int i = 2; i <= size; i++) {
-            dp[i][0] = dp[i - 1][10];
-            sum = dp[i][0];
-            for (int j = 1; j < 10; j++) {
-                dp[i][j] = dp[i][j - 1] - dp[i - 1][j - 1];
-                sum += dp[i][j];
+        for (int i = 2; i <= size + 1; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = j; k < 10; k++) {
+                    dp[i][j] += (dp[i - 1][k] % DIVIDE_VALUE);
+                }
             }
-            dp[i][10] = sum;
         }
 
-        System.out.println(dp[size][10] % DIVIDE_VALUE);
+        System.out.println(dp[size + 1][0] % DIVIDE_VALUE);
     }
 }
