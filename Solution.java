4d28bcb6ec8dac12958014177321ea59440fd44e
HyunggiPark <phg9587@naver.com>
2022-12-27 14:36:17 +0900

    백준 녹색 옷 입은 애가 젤다지? [4485] - 2차 풀이 실패 - 테케 못구함

--- Solution.java
+++ Solution.java
@@ -1,10 +1,11 @@
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 
 class Solution {
     int gSize;
     int[][] gTable;
-    int[][] directions = {{0, 1}, {1, 0}};
+    int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     int answer;
 
     public String solution(int size, int[][] table) {
@@ -12,14 +13,18 @@ class Solution {
         gTable = table;
         answer = Integer.MAX_VALUE;
 
+        int[] minTable = new int[size * size];
+        Arrays.fill(minTable, Integer.MAX_VALUE);
+
         Deque<Point> dq = new ArrayDeque<>();
-        dq.push(new Point(0, 0, table[0][0]));
+        dq.push(new Point(0, 0, table[0][0], 0));
 
         while (!dq.isEmpty()) {
             Point p = dq.pop();
             int r = p.r;
             int c = p.c;
             int v = p.value;
+            int d = p.depth;
 
             if (r == size - 1 && c == size - 1) {
                 answer = Math.min(answer, v);
@@ -31,7 +36,10 @@ class Solution {
                 int nc = c + direction[1];
                 if (isOutOfTable(nr, nc)) continue;
 
-                dq.push(new Point(nr, nc, v + table[nr][nc]));
+                if (d < (size * size) && minTable[d] > v + table[nr][nc]) {
+                    minTable[d] = v + table[nr][nc];
+                    dq.push(new Point(nr, nc, v + table[nr][nc], d + 1));
+                }
             }
         }
 
@@ -47,10 +55,12 @@ class Point {
     int r;
     int c;
     int value;
+    int depth;
 
-    public Point(int r, int c, int value) {
+    public Point(int r, int c, int value, int depth) {
         this.r = r;
         this.c = c;
         this.value = value;
+        this.depth = depth;
     }
 }
