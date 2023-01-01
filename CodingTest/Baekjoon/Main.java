7cff56041f2bf119ab6b9d8b413ce350b29747a6
HyunggiPark <phg9587@naver.com>
2023-01-01 23:02:58 +0900

    백준 로봇 청소기 [14053] - 1차 풀이 실패 - 2번째 예제 틀림

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -4,65 +4,255 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//로봇 청소기
+//현재 위치 청소
+//현재 방향을 기준으로 왼쪽방향부터 차례대로 탐색 진행
+// 왼쪽이 더러우면 왼쪽회전 후 한칸 전진 -> 현재 위치 청소
+// 왼쪽 방향 깨끗하면, 왼쪽 회전 후 다시 왼쪽방향부터 차례대로 탐색 진행
+// 네방향이 모두 깨끗 or 벽 이면, 바라보는 방향 유지한 채로 한칸 후진 후 왼쪽 탐색
+// 네방향이 모두 깨끗 or 벽 이면, 바라보는 방향 유지한 채로 한칸 후진이 불가능하면 작동 멈춤
+
+// 로봇 청소기는 이미 청소된 칸 청소 X , 벽 통과 X
+
+// 로봇 청소기가 있는 칸의 상태는 빈칸
+
+// 로봇 청소기가 청소하는 칸의 개수를 출력
+
+enum View {
+    NORTH(0), EAST(1), SOUTH(2), WEST(3);
+    final int value;
+
+    View(int value) {
+        this.value = value;
+    }
+
+    public static View valueOf(int value) {
+        switch (value) {
+            case 0:
+                return View.NORTH;
+            case 1:
+                return View.EAST;
+            case 2:
+                return View.SOUTH;
+            case 3:
+                return View.WEST;
+            default:
+                return null;
+        }
+    }
+}
+
+enum Environment {
+    EMPTY(0), WALL(1), CLEAN(2);
+
+    final int value;
+
+    Environment(int value) {
+        this.value = value;
+    }
+
+    public static Environment valueOf(int value) {
+        switch (value) {
+            case 0:
+                return EMPTY;
+            case 1:
+                return WALL;
+            case 2:
+                return CLEAN;
+            default:
+                return null;
+        }
+    }
+}
+
 public class Main {
     static public void main(String[] args) throws IOException {
         BjSolution sol = new BjSolution();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] line = br.readLine().split(" ");
-        int rows = Integer.parseInt(line[0]);
-        int cols = Integer.parseInt(line[1]);
-        int specialNum = Integer.parseInt(line[2]);
+        String[] fl = br.readLine().split(" ");
+        int rowNum = Integer.parseInt(fl[0]);
+        int colNum = Integer.parseInt(fl[1]);
 
-        sol.solution(rows, cols, specialNum);
+        String[] sl = br.readLine().split(" ");
+
+        int[][] table = new int[rowNum][colNum];
+
+        for (int r = 0; r < rowNum; r++) {
+            String[] tl = br.readLine().split(" ");
+            for (int c = 0; c < colNum; c++) {
+                table[r][c] = Integer.parseInt(tl[c]);
+            }
+        }
+
+        sol.solution(sl, table);
     }
 }
 
 class BjSolution {
-    int answer, gRows, gCols, gSpecialNum;
-    int[][] directions = {{0, 1}, {1, 0}};
-    int[][] grid;
+    public void solution(String[] machineCoordi, int[][] table) {
+        int row = Integer.parseInt(machineCoordi[0]);
+        int col = Integer.parseInt(machineCoordi[1]);
+        int view = Integer.parseInt(machineCoordi[2]);
 
-    public void solution(int rows, int cols, int specialNum) {
-        answer = 0;
-        gRows = rows;
-        gCols = cols;
-        gSpecialNum = specialNum;
+        Machine machine = new Machine(row, col, View.valueOf(view), table);
 
-        grid = new int[rows][cols];
-
-        int tempVal = 1;
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                grid[r][c] = tempVal;
-                tempVal++;
-            }
-        }
-
-        dfs(0, 0, false);
-
-        System.out.println(answer);
+        System.out.println(machine.simulation());
     }
 
-    private void dfs(int r, int c, boolean checkFlag) {
-        if (r == gRows - 1 && c == gCols - 1) {
-            if (gSpecialNum != 0) {
-                if (checkFlag)
-                    answer++;
+}
+
+class Coordi {
+    int row, col;
+
+    Coordi(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
+
+class Machine {
+    int row, col;
+    View view;
+    Environment[][] table;
+    int turnCount, cleanCount;
+
+    public Machine(int row, int col, View view, int[][] table) {
+        this.row = row;
+        this.col = col;
+        this.view = view;
+        this.table = initIsCleaned(table);
+    }
+
+    public int simulation() {
+
+        while (true) {
+            clean();
+
+            if (getTurnCount() == 4) {
+                if (isAbleToBackUp()) {
+                    backUp();
+                    resetTurnCount();
+                } else {
+                    break;
+                }
             } else {
-                answer++;
+                if (isAbleToCleanOnLeftSide()) {
+                    changeView();
+                    go();
+                    resetTurnCount();
+                } else {
+                    changeView();
+                    upTurnCount();
+                }
             }
-            return;
         }
 
-        for (int[] direction : directions) {
-            int nr = r + direction[0];
-            int nc = c + direction[1];
-            if (nr >= 0 && nr < gRows && nc >= 0 && nc < gCols) {
-                boolean tempCheckFlag = checkFlag || grid[nr][nc] == gSpecialNum;
-                dfs(nr, nc, tempCheckFlag);
+        return cleanCount;
+    }
+
+    private Environment[][] initIsCleaned(int[][] table) {
+        Environment[][] returnTable = new Environment[table.length][table[0].length];
+        for (int r = 0; r < table.length; r++) {
+            for (int c = 0; c < table[r].length; c++) {
+                returnTable[r][c] = Environment.valueOf(table[r][c]);
             }
         }
+
+        return returnTable;
+    }
+
+    public void clean() {
+        if (this.table[this.row][this.col] != Environment.EMPTY)
+            return;
+
+        this.table[this.row][this.col] = Environment.CLEAN;
+        cleanCount++;
+    }
+
+    public int getTurnCount() {
+        return this.turnCount;
+    }
+
+    public void upTurnCount() {
+        turnCount++;
+    }
+
+    public void resetTurnCount() {
+        turnCount = 0;
+    }
+
+    public boolean isAbleToCleanOnLeftSide() {
+        Coordi turnCoordi = getTurnCoordi(this.row, this.col);
+
+        return this.table[turnCoordi.row][turnCoordi.col] == Environment.EMPTY;
+    }
+
+    public boolean isAbleToBackUp() {
+        Coordi backUpCoordi = getTurnCoordi(this.row, this.col);
+
+        return this.table[backUpCoordi.row][backUpCoordi.col] != Environment.WALL;
+    }
+
+    public Coordi getBackCoordi(int curRow, int curCol) {
+        if (this.view == View.NORTH) {
+            curRow++;
+        } else if (this.view == View.EAST) {
+            curCol--;
+        } else if (this.view == View.SOUTH) {
+            curRow--;
+        } else { //West
+            curCol++;
+        }
+
+        return new Coordi(curRow, curCol);
+    }
+
+    public Coordi getTurnCoordi(int curRow, int curCol) {
+        if (this.view == View.NORTH) {
+            curCol--;
+        } else if (this.view == View.EAST) {
+            curRow--;
+        } else if (this.view == View.SOUTH) {
+            curCol++;
+        } else { //West
+            curRow++;
+        }
+
+        return new Coordi(curRow, curCol);
+    }
+
+    public void go() {
+        if (this.view == View.NORTH) {
+            this.row--;
+        } else if (this.view == View.EAST) {
+            this.col++;
+        } else if (this.view == View.SOUTH) {
+            this.row++;
+        } else { //West
+            this.col--;
+        }
+    }
+
+    public void backUp() {
+        if (this.view == View.NORTH) {
+            this.row++;
+        } else if (this.view == View.EAST) {
+            this.col--;
+        } else if (this.view == View.SOUTH) {
+            this.row--;
+        } else { //West
+            this.col++;
+        }
+    }
+
+    private void changeView() {
+        int tempViewValue = this.view.value;
+
+        tempViewValue += 3;
+        tempViewValue %= 4;
+
+        this.view = View.valueOf(tempViewValue);
     }
 }
 
