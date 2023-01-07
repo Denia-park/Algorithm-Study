b2e104fa6718177eb0f712db267d5c488850fdac
HyunggiPark <phg9587@naver.com>
2023-01-07 10:42:34 +0900

    백준 소수 [2581] - 1차 풀이 완료 - 틀림 . 뭐가 틀린거지 ?

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -3,35 +3,51 @@ package CodingTest.Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     static public void main(String[] args) throws IOException {
         BjSolution sol = new BjSolution();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCaseNum = Integer.parseInt(br.readLine());
-        for (int i = 0; i < testCaseNum; i++) {
-            int row = Integer.parseInt(br.readLine());
-            int col = Integer.parseInt(br.readLine()); // 1호 부터 산다.
-            sol.solution(row, col);
-        }
+        int start = Integer.parseInt(br.readLine());
+        int end = Integer.parseInt(br.readLine());
+        sol.solution(start, end);
     }
 }
 
 class BjSolution {
-    public void solution(int row, int col) {
-        int[][] dp = new int[row + 1][col + 1];
-        for (int i = 1; i < col + 1; i++) {
-            dp[0][i] = i;
-        }
-
-        for (int r = 1; r < row + 1; r++) {
-            for (int c = 1; c < col + 1; c++) {
-                dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
+    public void solution(int start, int end) {
+        List<Integer> primes = new ArrayList<>();
+        long sum = 0;
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                primes.add(i);
+                sum += i;
             }
         }
 
-        System.out.println(dp[row][col]);
+        if (primes.isEmpty()) {
+            System.out.println(-1);
+            return;
+        }
+
+        primes.sort(null);
+        System.out.println(sum);
+        System.out.println(primes.get(0));
+    }
+
+    private boolean isPrime(int i) {
+        boolean flag = true;
+        for (int j = 2; j <= Math.sqrt(i); j++) {
+            if (i % j == 0) {
+                flag = false;
+                break;
+            }
+        }
+
+        return flag;
     }
 }
 
