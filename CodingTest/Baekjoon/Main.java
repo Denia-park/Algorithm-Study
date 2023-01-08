411c83d1da4be40b833d29f9402d00e7272296a2
HyunggiPark <phg9587@naver.com>
2023-01-09 01:12:12 +0900

    백준 문자열 폭발 [9935] - 2차 풀이 - 정답 참고

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -3,8 +3,10 @@ package CodingTest.Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Stack;
+
+//정답 참고 - 문자열 폭발 [9935]
+//https://loosie.tistory.com/317
 
 public class Main {
     static public void main(String[] args) throws IOException {
@@ -19,30 +21,42 @@ public class Main {
 
 class BjSolution {
     public void solution(String fl, String sl) {
-        Deque<Character> answer = new ArrayDeque<>();
-        Deque<Character> temp = new ArrayDeque<>();
+        Stack<Character> st = new Stack<>();
+        int slLen = sl.length();
 
-        int slIdx = 0;
         for (int i = 0; i < fl.length(); i++) {
-            char fc = fl.charAt(i);
-            char sc = sl.charAt(slIdx);
+            st.push(fl.charAt(i));
 
-            if (fc != sc) {
-                while (!temp.isEmpty()) {
-                    answer.addLast(temp.removeFirst());
+            //터지는 문자열보다 길이가 길어지면 비교를 시작
+            if (st.size() >= slLen) {
+                boolean flag = true;
+                for (int j = 0; j < slLen; j++) {
+                    char stChar = st.get(st.size() - slLen + j);
+                    char slChar = sl.charAt(j);
+                    if (stChar != slChar) {
+                        flag = false;
+                        break;
+                    }
                 }
-                slIdx = 0;
-                answer.addLast(sc);
-                continue;
-            }
 
-            temp.addLast(sc);
-            slIdx++;
-            if (slIdx == sl.length()) {
-                temp.clear();
-                slIdx = 0;
+                if (flag) {
+                    for (int j = 0; j < slLen; j++) {
+                        st.pop();
+                    }
+                }
             }
         }
+
+        if (st.isEmpty()) {
+            System.out.println("FRULA");
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Character character : st) {
+            sb.append(character);
+        }
+        System.out.println(sb);
     }
 }
 
