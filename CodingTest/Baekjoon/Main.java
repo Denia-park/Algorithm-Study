0852541da570560a42d5fc2b75da49cd9dcb6e84
HyunggiPark <phg9587@naver.com>
2023-01-09 13:21:09 +0900

    백준 센서 [2212] - 정답 참고

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -3,60 +3,58 @@ package CodingTest.Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.Arrays;
 
-//정답 참고 - 문자열 폭발 [9935]
-//https://loosie.tistory.com/317
+//정답 참고 - 센서 [2212]
 
 public class Main {
     static public void main(String[] args) throws IOException {
         BjSolution sol = new BjSolution();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String fl = br.readLine();
-        String sl = br.readLine();
-        sol.solution(fl, sl);
+        int sensorNum = Integer.parseInt(br.readLine());
+        int companyNum = Integer.parseInt(br.readLine());
+        String[] sensorDistances = br.readLine().split(" ");
+        int[] sensors = new int[sensorNum];
+        for (int i = 0; i < sensorNum; i++) {
+            sensors[i] = Integer.parseInt(sensorDistances[i]);
+        }
+        sol.solution(sensorNum, companyNum, sensors);
     }
 }
 
 class BjSolution {
-    public void solution(String fl, String sl) {
-        Stack<Character> st = new Stack<>();
-        int slLen = sl.length();
-
-        for (int i = 0; i < fl.length(); i++) {
-            st.push(fl.charAt(i));
-
-            //터지는 문자열보다 길이가 길어지면 비교를 시작
-            if (st.size() >= slLen) {
-                boolean flag = true;
-                for (int j = 0; j < slLen; j++) {
-                    char stChar = st.get(st.size() - slLen + j);
-                    char slChar = sl.charAt(j);
-                    if (stChar != slChar) {
-                        flag = false;
-                        break;
-                    }
-                }
-
-                if (flag) {
-                    for (int j = 0; j < slLen; j++) {
-                        st.pop();
-                    }
-                }
-            }
-        }
-
-        if (st.isEmpty()) {
-            System.out.println("FRULA");
+    public void solution(int sensorNum, int companyNum, int[] sensors) {
+        if (companyNum >= sensorNum) {
+            System.out.println(0);
             return;
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (Character character : st) {
-            sb.append(character);
+        //sensor X 거리 기준으로 정렬
+        Arrays.sort(sensors);
+
+        int[] sensorDistanceDiffs = new int[sensorNum - 1];
+
+        for (int i = 0; i < sensorNum - 1; i++) {
+            sensorDistanceDiffs[i] = sensors[i + 1] - sensors[i];
         }
-        System.out.println(sb);
+
+        int jump = companyNum - 1;
+
+        //정렬
+        Arrays.sort(sensorDistanceDiffs);
+
+        int answer = 0;
+        for (int i = sensorDistanceDiffs.length - 1; i >= 0; i--) {
+            if (jump > 0) {
+                jump--;
+                continue;
+            }
+
+            answer += sensorDistanceDiffs[i];
+        }
+
+        System.out.println(answer);
     }
 }
 
