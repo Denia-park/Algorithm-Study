6dbb72df41ea31931c16819216cd8da0794f64a6
HyunggiPark <phg9587@naver.com>
2023-01-13 18:54:22 +0900

    백준 게임 개발 [1516] - 1차 풀이 완료 - 예제는 맞으나 틀림

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -3,7 +3,10 @@ package CodingTest.Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
 public class Main {
     static public void main(String[] args) throws IOException {
@@ -11,34 +14,63 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCase = Integer.parseInt(br.readLine());
-        int[] quizArr = new int[testCase];
+        List<String[]> quizArr = new ArrayList<>();
         for (int i = 0; i < testCase; i++) {
-            quizArr[i] = Integer.parseInt(br.readLine());
+            String[] arr = br.readLine().split(" ");
+            quizArr.add(arr);
         }
+
         sol.solution(testCase, quizArr);
     }
 }
 
 class BjSolution {
-    public void solution(int testCase, int[] quizArr) {
-        int answer = 0;
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i : quizArr) {
-            pq.add(i);
+    public void solution(int testCase, List<String[]> quizArr) {
+        int[] answer = new int[testCase + 1];
+        int[] time = new int[testCase + 1];
+        int[] indgree = new int[testCase + 1];
+        List<List<Integer>> waitList = new ArrayList<>();
+        for (int i = 0; i < testCase + 1; i++) {
+            waitList.add(new ArrayList<>());
         }
 
-        while (pq.size() > 1) {
-            int val1 = pq.poll();
-            int val2 = pq.poll();
-
-            int sumVal = val1 + val2;
-            answer += sumVal;
-
-            pq.add(sumVal);
+        for (int i = 0; i < quizArr.size(); i++) {
+            String[] arr = quizArr.get(i);
+            int curBuildingNum = i + 1;
+            time[curBuildingNum] = Integer.parseInt(arr[0]);
+            for (int j = 1; j < arr.length; j++) {
+                int preBuildingNum = Integer.parseInt(arr[j]);
+                if (preBuildingNum == -1) {
+                    break;
+                }
+                waitList.get(preBuildingNum).add(curBuildingNum);
+                indgree[curBuildingNum]++;
+            }
         }
 
-        System.out.println(answer);
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        for (int i = 1; i < testCase + 1; i++) {
+            if (indgree[i] == 0) {
+                queue.addLast(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int cur = queue.removeFirst();
+            answer[cur] += time[cur];
+            for (int i : waitList.get(cur)) {
+                indgree[i]--;
+                if (indgree[i] == 0) {
+                    answer[i] += answer[cur];
+                    queue.addLast(i);
+                }
+            }
+        }
+
+        for (int i = 1; i < answer.length; i++) {
+            System.out.println(answer[i]);
+        }
     }
 }
 
