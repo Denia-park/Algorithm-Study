ae61e1a54b9b3c6ae3ec719f84be4ba6dba77896
HyunggiPark <phg9587@naver.com>
2023-01-19 11:46:53 +0900

    프로그래머스 연습문제 롤케이크 자르기 - 2차 풀이 완료 - 다른 사람 풀이 참고 - 시간이 15배 가량 줄어듬
    - 나도 처음에 생각을 한 방법인데 그때는 가지수를 저장할 방법이 떠오르지 않아서 사용하지 않은 방법인데 array 로도 map처럼 사용이 가능하다. -> key의 size는 array 값을 변경시킬때마다 control 해주면 된다.

--- CodingTest/Programers/Solution.java
+++ CodingTest/Programers/Solution.java
@@ -1,44 +1,21 @@
 package CodingTest.Programers;
 
-import java.util.HashMap;
-import java.util.Map;
-
-//3분 -> 21분 = 18분 걸림
-//처음에 set을 이용하여 풀이하려고 했으나 set은 이전의 값들을 기억하지 못하므로 효율적이지 못함
-
-//그래서 토핑의 가지수가 다르다 => hash의 key가 다르다에서 착안하여 map을 사용함
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
-
-        if (topping.length == 1) {
-            return 0;
+        int[] left = new int[10001], right = new int[10001];
+        int ls = 0, rs = 0;
+        for (int i : topping) {
+            if (right[i] == 0) rs++;
+            right[i]++;
         }
-
-        Map<Integer, Integer> leftSet = new HashMap<>();
-        Map<Integer, Integer> rightSet = new HashMap<>();
-
-        for (int topp : topping) {
-            rightSet.put(topp, rightSet.getOrDefault(topp, 0) + 1);
+        for (int i : topping) {
+            right[i]--;
+            if (right[i] == 0) rs--;
+            if (left[i] == 0) ls++;
+            left[i]++;
+            if (rs == ls) answer++;
         }
-
-        for (int topp : topping) {
-            leftSet.put(topp, leftSet.getOrDefault(topp, 0) + 1);
-
-            int moveVal = rightSet.getOrDefault(topp, 0);
-            if (moveVal - 1 == 0) {
-                //이번에 삭제
-                rightSet.remove(topp);
-            } else {
-                //수정값 적용
-                rightSet.put(topp, moveVal - 1);
-            }
-
-            if (leftSet.size() == rightSet.size()) {
-                answer++;
-            }
-        }
-
         return answer;
     }
 }
