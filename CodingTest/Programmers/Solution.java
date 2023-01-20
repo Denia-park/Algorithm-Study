7f01d54b037cf95bb7d9e032ec7729fc62490a43
HyunggiPark <phg9587@naver.com>
2023-01-20 17:15:22 +0900

    프로그래머스 연습문제 시소짝꿍 - 2차 풀이 완료 - 약간만 다르게 풀어서 그런지 여전히 실패

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,26 +1,25 @@
 package CodingTest.Programmers;
 
 class Solution {
-    int[] multiple = {2, 3, 4};
-
     public long solution(int[] weights) {
         long answer = 0;
 
         for (int one = 0; one < weights.length; one++) {
-            out:
             for (int two = one + 1; two < weights.length; two++) {
                 if (weights[one] == weights[two]) {
                     answer++;
-                    continue;
-                }
-
-                for (int i : multiple) {
-                    for (int j : multiple) {
-                        if (weights[one] * i == weights[two] * j) {
-                            answer++;
-                            continue out;
-                        }
-                    }
+                } else if (weights[one] * 2 == weights[two] * 3) {
+                    answer++;
+                } else if (weights[one] * 2 == weights[two] * 4) {
+                    answer++;
+                } else if (weights[one] * 3 == weights[two] * 2) {
+                    answer++;
+                } else if (weights[one] * 3 == weights[two] * 4) {
+                    answer++;
+                } else if (weights[one] * 4 == weights[two] * 2) {
+                    answer++;
+                } else if (weights[one] * 4 == weights[two] * 3) {
+                    answer++;
                 }
             }
         }
