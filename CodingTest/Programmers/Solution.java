ef77bb64e3b8bce4e3e8e154133121b7848bf5fa
HyunggiPark <phg9587@naver.com>
2023-02-02 12:52:37 +0900

    프로그래머스 연습문제 옹알이2 - 1차 풀이 - 연속되면 안되는 조건을 빼먹음

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,39 +1,22 @@
 package CodingTest.Programmers;
 
-import java.util.Stack;
-
 class Solution {
-    final int[] hamberger = {1, 2, 3, 1};
+    String[] babyBabbling = {"aya", "ye", "woo", "ma"};
 
-    public int solution(int[] ingredient) {
+    public int solution(String[] babbling) {
         int answer = 0;
 
-        Stack<Integer> stack = new Stack<>();
 
-        for (int val : ingredient) {
-            stack.push(val);
+        for (String str : babbling) {
+            String newStr = str;
+            for (String babble : babyBabbling) {
+                newStr = newStr.replace(babble, "");
+            }
 
-            if (stack.size() >= 4 && stack.peek() == 1) {
-                int changeIdx = stack.size() - 4;
-                boolean makeFlag = true;
-
-                for (int tmpIdx = 0; tmpIdx < 4; tmpIdx++) {
-                    if (stack.get(changeIdx + tmpIdx) != hamberger[tmpIdx]) {
-                        makeFlag = false;
-                        break;
-                    }
-                }
-
-                if (makeFlag) {
-                    for (int j = 0; j < 4; j++) {
-                        stack.pop();
-                    }
-
-                    answer++;
-                }
+            if (newStr.equals("")) {
+                answer++;
             }
         }
-
         return answer;
     }
 }
