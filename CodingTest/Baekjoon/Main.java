c5133fc40bd1a32b3b2115360d9d5c8c2eaa34fe
HyunggiPark <phg9587@naver.com>
2023-02-04 13:33:00 +0900

    백준 문서 검색 [1543] - 1차 풀이 완료 - 17%에서 틀림!

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -3,61 +3,25 @@ package CodingTest.Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
     static public void main(String[] args) throws IOException {
         BjSolution sol = new BjSolution();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int inputCount = Integer.parseInt(br.readLine());
-        String input = br.readLine();
+        String quizString = br.readLine();
+        String quiz = br.readLine();
 
-        int[] nums = new int[inputCount];
-        for (int i = 0; i < inputCount; i++) {
-            nums[i] = Integer.parseInt(br.readLine());
-        }
-
-        sol.solution(input, nums);
+        sol.solution(quizString, quiz);
     }
 }
 
 class BjSolution {
 
-    public void solution(String originInput, int[] nums) {
-        Stack<Double> stack = new Stack<>();
+    public void solution(String quizString, String quiz) {
+        String[] splitStr = quizString.split(quiz);
 
-        for (int i = 0; i < originInput.length(); i++) {
-            char ch = originInput.charAt(i);
-
-            //영문자인 경우 값으로 바꿔서 Stack에 push
-            if (isCapitalLetter(ch)) {
-                stack.push((double) nums[ch - 'A']);
-            } else {
-                //계산식인 경우
-                double rearVal = stack.pop();
-                double frontVal = stack.pop();
-                double finalVal = 0;
-
-                if (ch == '+') {
-                    finalVal = frontVal + rearVal;
-                } else if (ch == '-') {
-                    finalVal = frontVal - rearVal;
-                } else if (ch == '*') {
-                    finalVal = frontVal * rearVal;
-                } else if (ch == '/') {
-                    finalVal = frontVal / rearVal;
-                }
-
-                stack.push(finalVal);
-            }
-        }
-
-        System.out.printf("%.2f", stack.peek());
-    }
-
-    private boolean isCapitalLetter(char c) {
-        return c >= 'A' && c <= 'Z';
+        System.out.println(splitStr.length - 1);
     }
 }
 
