93111193276f4103110e35a5bf9dbaedc13a4ee5
HyunggiPark <phg9587@naver.com>
2023-02-05 17:26:06 +0900

    백준 주식 [11501] - 1차 풀이 완료 - 4%에서 틀림

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -3,34 +3,52 @@ package CodingTest.Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.PriorityQueue;
 
 public class Main {
     static public void main(String[] args) throws IOException {
         BjSolution sol = new BjSolution();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String quizString = br.readLine();
-        String quiz = br.readLine();
+        int testCase = Integer.parseInt(br.readLine());
+        for (int i = 0; i < testCase; i++) {
+            int day = Integer.parseInt(br.readLine());
+            String[] stockPrices = br.readLine().split(" ");
 
-        sol.solution(quizString, quiz);
+            sol.solution(day, stockPrices);
+        }
+
     }
 }
 
 class BjSolution {
 
-    public void solution(String quizString, String quiz) {
-        String splitStr = quizString.replace(quiz, "*");
+    public void solution(int day, String[] stockStrPrices) {
+        long answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
 
-        int idx = -1;
-        int answer = 0;
+        int[] stockIntPrices = new int[stockStrPrices.length];
+        for (int idx = 0; idx < stockStrPrices.length; idx++) {
+            int i1 = Integer.parseInt(stockStrPrices[idx]);
+            stockIntPrices[idx] = i1;
+            pq.offer(i1);
+        }
 
-        while (true) {
-            idx = splitStr.indexOf("*", idx + 1);
-            if (idx == -1) {
-                break;
+        Deque<Integer> list = new ArrayDeque<>();
+
+        for (int idx = 0; idx < day; idx++) {
+            int curVal = stockIntPrices[idx];
+            if (curVal == pq.peek()) {
+                while (!list.isEmpty()) {
+                    answer += (curVal - list.poll());
+                }
+                pq.poll();
+            } else {
+                list.addLast(curVal);
             }
-
-            answer++;
         }
 
         System.out.println(answer);
