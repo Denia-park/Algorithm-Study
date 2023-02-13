35f03910421264ac3a0527182c5544606b6827fa
HyunggiPark <phg9587@naver.com>
2023-02-13 13:28:40 +0900

    프로그래머스 연습문제 크기가 작은 부분문자열 - 1차 풀이 완료 - 런타임 에러 때문에 문제를 못찾아서 조금 고생함.

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,13 +1,12 @@
 package CodingTest.Programmers;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 public class ProgrammersMain {
     static public void main(String[] args) throws IOException {
         Solution ts = new Solution();
 
-        System.out.println(Arrays.toString(ts.solution(new int[]{2, 3, 3, 5})));
+        System.out.println((ts.solution("3141592", "9000000")));
     }
 }
 
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,27 +1,16 @@
 package CodingTest.Programmers;
 
-//자신보다 크면서 , 가장 가까이 있는 수 => 뒷 큰수
-//뒷 큰수의 배열
-//존재하지 않으면 -1을 담는다.
-
-import java.util.Arrays;
-import java.util.Stack;
-
 class Solution {
-    public int[] solution(int[] numbers) {
-        int[] answer = new int[numbers.length];
+    public int solution(String main, String sub) {
+        int answer = 0;
+        long subVal = Long.parseLong(sub);
 
-        Arrays.fill(answer, -1);
+        for (int i = 0; i < main.length() - sub.length() + 1; i++) {
+            long mainVal = Long.parseLong(main.substring(i, i + sub.length()));
 
-        Stack<Integer> stack = new Stack<>();
-
-        for (int arrIdx = 0; arrIdx < numbers.length; arrIdx++) {
-            while (!stack.isEmpty() && numbers[arrIdx] > numbers[stack.peek()]) {
-                int stkIdx = stack.pop();
-                answer[stkIdx] = numbers[arrIdx];
+            if (mainVal <= subVal) {
+                answer++;
             }
-
-            stack.push(arrIdx);
         }
 
         return answer;
