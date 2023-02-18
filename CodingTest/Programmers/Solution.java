c1f19cab667aaa740126ba7bbf112e08f7e802f2
HyunggiPark <phg9587@naver.com>
2023-02-18 11:16:26 +0900

    프로그래머스 연습문제 최솟값 만들기 - 1차 풀이 완료

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,23 +1,18 @@
 package CodingTest.Programmers;
 
-class Solution {
-    public String solution(String[] cards1, String[] cards2, String[] goal) {
-        int cardIdx1 = 0;
-        int cardIdx1Max = cards1.length;
-        int cardIdx2 = 0;
-        int cardIdx2Max = cards2.length;
+import java.util.Arrays;
 
-        for (int i = 0; i < goal.length; i++) {
-            String curStr = goal[i];
-            if (cardIdx1 < cardIdx1Max && curStr.equals(cards1[cardIdx1])) {
-                cardIdx1++;
-            } else if (cardIdx2 < cardIdx2Max && curStr.equals(cards2[cardIdx2])) {
-                cardIdx2++;
-            } else {
-                return "No";
-            }
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for (int i = 0, j = A.length - 1; i < A.length; i++, j--) {
+            answer += (A[i] * B[j]);
         }
 
-        return "Yes";
+        return answer;
     }
 }
