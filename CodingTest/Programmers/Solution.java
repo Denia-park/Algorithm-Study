eb30249c3d68b2534d37a0eb71de0456e3f62123
HyunggiPark <phg9587@naver.com>
2023-02-21 10:36:25 +0900

    프로그래머스 입문 두수의 차 - 1차 풀이 완료

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,38 +1,7 @@
 package CodingTest.Programmers;
 
 class Solution {
-    public int solution(String s) {
-        int answer = 0;
-
-        char firstCh = '0';
-        int firstChNum = 0;
-        int otherChNum = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (firstCh == '0') {
-                firstCh = s.charAt(i);
-                firstChNum++;
-                continue;
-            }
-
-            if (firstCh == s.charAt(i)) {
-                firstChNum++;
-            } else {
-                otherChNum++;
-            }
-
-            if (firstChNum == otherChNum) {
-                answer++;
-                firstCh = '0';
-                firstChNum = 0;
-                otherChNum = 0;
-            }
-        }
-
-        if (firstChNum != 0) {
-            answer++;
-        }
-
-        return answer;
+    public int solution(int num1, int num2) {
+        return num1 - num2;
     }
 }
