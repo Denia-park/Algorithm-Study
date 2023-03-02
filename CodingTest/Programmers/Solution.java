9893b9e6bd75f747d83cf2bff945522ff415115a
HyunggiPark <phg9587@naver.com>
2023-03-02 22:03:36 +0900

    프로그래머스 연습문제 줄 서는 방법 - 정답 참고 후 풀이 - ArrayList 와 LinkedList의 차이 때문에 시간 차이가 남, LinkedList 를 ArrayList 로 바꿨더니 통과함 -- 이유가 멀까 ?

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,15 +1,12 @@
 package CodingTest.Programmers;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
-// 문제
+// 문제 - 1번부터 n번까지 줄 서기
 //https://school.programmers.co.kr/learn/courses/30/lessons/12936
-
 // 정답
-// https://hongcoding.tistory.com/55
-
-//1번부터 n번까지 줄 서기
+// https://tosuccess.tistory.com/75
 
 class Solution {
     List<Integer> list;
@@ -22,7 +19,7 @@ class Solution {
             factorial[i] = factorial[i - 1] * i;
         }
 
-        list = new LinkedList<>();
+        list = new ArrayList<>(21);
         for (int i = 1; i < n + 1; i++) {
             list.add(i);
         }
@@ -37,8 +34,7 @@ class Solution {
 
             int digitIdx = (int) (k / duplicationNum);
 
-            answer[idx] = list.get(digitIdx);
-            list.remove(digitIdx);
+            answer[idx] = list.remove(digitIdx);
 
             k %= duplicationNum;
 
