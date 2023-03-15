d3e60e7cf5913de9ad1d013dbcf062ee4c7ae137
HyunggiPark <phg9587@naver.com>
2023-03-16 05:15:07 +0900

    프로그래머스 연습문제 미로 탈출 - 5차 풀이 완료 - 한가지 특이 케이스를 생각못했다가 생각 나서 추가하고 해결
    - S에서 시작해서 L은 찾았으나 L에서 E를 못찾은 경우에 대한 예외도 추가 해줌.
    - 기존 int[] 더 보기 좋은거 같아서 수정함

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,17 +1,19 @@
 package CodingTest.Programmers;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
 
     private char[][] map;
     private boolean[][] visited;
-    private int answer;
     private Map<Character, int[]> mapInfo;
     private int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
     public int solution(String[] maps) {
-        answer = 0;
+        int answer = 0;
         map = new char[maps.length][maps[0].length()];
 
         mapInfo = new HashMap<>();
@@ -33,39 +35,38 @@ class Solution {
             }
         }
 
-        visited = new boolean[maps.length][maps[0].length()];
         //S에서 L을 찾는 과정
-        bfs('S', 'L');
+        visited = new boolean[maps.length][maps[0].length()];
+        int rtval = bfs('S', 'L');
 
         //L을 못 찾았으면 그대로 종료
-        if (answer == 0) {
-            return -1;
-        }
-
-        for (boolean[] booleans : visited) {
-            Arrays.fill(booleans, false);
-        }
+        if (rtval == 0) return -1;
+        answer += rtval;
 
         //L에서 E를 찾는 과정
-        bfs('L', 'E');
+        visited = new boolean[maps.length][maps[0].length()];
+        rtval = bfs('L', 'E');
 
-        return answer == 0 ? -1 : answer;
+        //E를 못 찾았으면 그대로 종료
+        if (rtval == 0) return -1;
+        answer += rtval;
+
+        return answer;
     }
 
-    private void bfs(char startChar, char targetChar) {
-        Deque<Info> deque = new ArrayDeque<>();
+    private int bfs(char startChar, char targetChar) {
+        Deque<int[]> deque = new ArrayDeque<>();
         int[] info = mapInfo.get(startChar);
-        deque.add(new Info(info[0], info[1], 0));
+        deque.add(new int[]{info[0], info[1], 0});
 
         while (!deque.isEmpty()) {
-            Info tempInfo = deque.poll();
-            int r = tempInfo.r;
-            int c = tempInfo.c;
-            int moveCount = tempInfo.moveCount;
+            int[] tempInfo = deque.poll();
+            int r = tempInfo[0];
+            int c = tempInfo[1];
+            int moveCount = tempInfo[2];
 
             if (map[r][c] == targetChar) {
-                answer += moveCount;
-                return;
+                return moveCount;
             }
 
             visited[r][c] = true;
@@ -80,26 +81,15 @@ class Solution {
 
                 if (!visited[newR][newC]) {
                     visited[newR][newC] = true;
-                    deque.add(new Info(newR, newC, moveCount + 1));
+                    deque.add(new int[]{newR, newC, moveCount + 1});
                 }
             }
         }
+
+        return 0;
     }
 
     private boolean isOutOfBound(int r, int c) {
         return r < 0 || r >= map.length || c < 0 || c >= map[0].length;
-    }
-
-}
-
-class Info {
-    int r;
-    int c;
-    int moveCount;
-
-    public Info(int r, int c, int moveCount) {
-        this.r = r;
-        this.c = c;
-        this.moveCount = moveCount;
     }
 }
