0ab97dfee60c8a51e5f46593cc67d46fbfcf0308
HyunggiPark <phg9587@naver.com>
2023-03-21 14:22:24 +0900

    크래프톤 코딩테스트 프로그래머스 JS 연습 - Lv1 - 나머지가 1이 되는 수 찾기

--- JavaScript/Programmers/test.js
+++ JavaScript/Programmers/test.js
@@ -1,15 +1,17 @@
 function solution(n) {
     var answer = 0;
-    let sqrt = Math.floor(Math.sqrt(n));
-    if (Math.pow(sqrt, 2) === n) {
-        answer = (sqrt + 1) ** 2;
-    } else {
-        answer = -1;
-    }
 
-    return answer;
+    let newN = n - 1;
+
+    return getDividers(newN)[1];
 }
 
-console.log(solution(1))
-console.log(solution(25))
-console.log(solution(121))
+function getDividers(number) {
+    let arr = [];
+    for (let i = 1; i <= number; i++) {
+        if (number % i === 0) {
+            arr.push(i);
+        }
+    }
+    return arr
+}
