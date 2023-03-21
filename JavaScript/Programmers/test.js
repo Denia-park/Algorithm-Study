a2f3a83a02d58bd2186284aa7af902eb5db7ce1f
HyunggiPark <phg9587@naver.com>
2023-03-21 13:33:27 +0900

    크래프톤 코딩테스트 프로그래머스 JS 연습 - Lv1 - 자릿수 더하기

--- JavaScript/Programmers/test.js
+++ JavaScript/Programmers/test.js
@@ -1,4 +1,12 @@
-function solution(arr) {
-    let length = arr.length;
-    return arr.reduce((a, b) => a + b, 0) / length;
+function solution(n) {
+    const arr = Array.from(String(n));
+
+    let sum = 0;
+    for (const arrElement of arr) {
+        sum += parseInt(arrElement);
+    }
+
+    return sum;
 }
+
+console.log(solution(123));
