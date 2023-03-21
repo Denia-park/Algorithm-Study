5e638270e22023f58230511712151036f422b624
HyunggiPark <phg9587@naver.com>
2023-03-21 14:30:55 +0900

    크래프톤 코딩테스트 프로그래머스 JS 연습 - Lv1 - 문자열을 정수로 바꾸기

--- JavaScript/Programmers/test.js
+++ JavaScript/Programmers/test.js
@@ -1,8 +1,3 @@
-function solution(n) {
-    let arr = Array.from(String(n));
-    arr = arr.sort().reverse();
-
-    let str = arr.reduce((a, b) => a + b, "");
-
-    return parseInt(str);
+function solution(s) {
+    return parseInt(s);
 }
