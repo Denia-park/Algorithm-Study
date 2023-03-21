03c6ce3a3befca7213c3433cc2d69abf48d110bd
HyunggiPark <phg9587@naver.com>
2023-03-21 14:35:18 +0900

    크래프톤 코딩테스트 프로그래머스 JS 연습 - Lv1 - 하샤드 수

--- JavaScript/Programmers/test.js
+++ JavaScript/Programmers/test.js
@@ -1,3 +1,17 @@
-function solution(s) {
-    return parseInt(s);
+function solution(x) {
+    let sum = getDigitSum(x);
+
+    return Math.floor(x / sum) === x / sum;
+}
+
+function getDigitSum(number) {
+    let sum = 0;
+
+    let arr = number.toString().split('');
+
+    for (let i = 0; i < arr.length; i++) {
+        sum += Number(arr[i]);
+    }
+
+    return sum;
 }
