e226ca5192a0a76f0c90a52a92b7f086b1ed18a9
HyunggiPark <phg9587@naver.com>
2023-03-26 12:15:23 +0900

    해커랭크 연습하기 - 기본 문제

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,6 +1,31 @@
 package CodingTest.Programmers;
 
+import java.util.List;
+
 class Solution {
+    public static void plusMinus(List<Integer> arr) {
+        // Write your code here
+        double size = arr.size();
+
+        double plus = 0;
+        double minus = 0;
+        double zero = 0;
+
+        for (Integer i : arr) {
+            if (i == 0) {
+                zero++;
+            } else if (i > 0) {
+                plus++;
+            } else {
+                minus++;
+            }
+        }
+
+        System.out.printf("%.6f%n", plus / size);
+        System.out.printf("%.6f%n", minus / size);
+        System.out.printf("%.6f%n", zero / size);
+    }
+
     public int solution(int totalSectionNum, int rollerLength, int[] sectionsToPaint) {
         int answer = 0;
 
