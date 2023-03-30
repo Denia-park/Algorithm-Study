4a1eab1f2fa416ac5169490f7936dcae4fa2efa4
Hyunggi <phg9587@naver.com>
2023-03-31 03:36:58 +0900

    크래프톤 알고리즘 주차 내용 복습

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -3,28 +3,36 @@ package CodingTest.Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BjSolution sol = new BjSolution();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sol.solution(br.readLine());
+        String firstLine = br.readLine();
+        String secondLine = br.readLine();
+
+        sol.solution(firstLine, secondLine);
 
     }
 }
 
 
 class BjSolution {
-    public void solution(String input) {
-        int val = Integer.parseInt(input);
-        for (int i = 1; i <= val; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < i; j++) {
-                sb.append("*");
-            }
-            System.out.println(sb);
+    public void solution(String firstLine, String secondLine) {
+        int standard = Integer.parseInt(firstLine.split(" ")[1]);
+        int[] arr = Arrays.stream(secondLine.split(" "))
+                .mapToInt(Integer::parseInt)
+                .filter(i -> i < standard)
+                .toArray();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : arr) {
+            sb.append(i).append(" ");
         }
+
+        System.out.println(sb);
     }
 }
 
