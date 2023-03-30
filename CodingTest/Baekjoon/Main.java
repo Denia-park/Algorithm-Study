b86d087525bb41e08f5ddbb58af824f3d55c3cb7
Hyunggi <phg9587@naver.com>
2023-03-31 03:48:26 +0900

    크래프톤 알고리즘 주차 내용 복습

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -3,48 +3,38 @@ package CodingTest.Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        BjSolution sol = new BjSolution();
+        BjSolution sol = new BjSolution();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] arr = new int[9];
-        int maxVal = Integer.MIN_VALUE;
-        int idx = 0;
-
-        for (int i = 0; i < 9; i++) {
-            int val = Integer.parseInt(br.readLine());
-            arr[i] = val;
-            if (maxVal < val) {
-                maxVal = val;
-                idx = i;
-            }
+        int testNum = Integer.parseInt(br.readLine());
+        for (int i = 0; i < testNum; i++) {
+            sol.solution(br.readLine());
         }
-        System.out.println(maxVal);
-        System.out.println(idx + 1);
-
-//        sol.solution(firstLine, secondLine);
     }
 }
 
 
 class BjSolution {
-    public void solution(String firstLine, String secondLine) {
-        int standard = Integer.parseInt(firstLine.split(" ")[1]);
-        int[] arr = Arrays.stream(secondLine.split(" "))
-                .mapToInt(Integer::parseInt)
-                .filter(i -> i < standard)
-                .toArray();
+    public void solution(String input) {
+        char[] quizResults = input.toCharArray();
 
-        StringBuilder sb = new StringBuilder();
-        for (int i : arr) {
-            sb.append(i).append(" ");
+        int answer = 0;
+
+        int score = 1;
+        for (char quizResult : quizResults) {
+            if (quizResult == 'O') {
+                answer += score;
+                score++;
+            } else {
+                score = 1;
+            }
         }
 
-        System.out.println(sb);
+        System.out.println(answer);
     }
 }
 
