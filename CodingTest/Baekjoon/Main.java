ee04ae46a439cb6b5a51423f763b988bfe54057a
Hyunggi <phg9587@naver.com>
2023-04-01 01:51:32 +0900

    크래프톤 알고리즘 주차 내용 복습

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -3,6 +3,9 @@ package CodingTest.Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,24 +13,37 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        sol.solution(br.readLine());
+        int[] arr = new int[9];
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            int val = Integer.parseInt(br.readLine());
+            arr[i] = val;
+            sum += val;
+        }
+        sol.solution(arr, sum);
     }
 }
 
 
 class BjSolution {
-    public void solution(String input) {
-        String[] inputs = input.split(" ");
-        StringBuilder sb = new StringBuilder();
-        int[] answers = new int[2];
-        int idx = 0;
-        for (String str : inputs) {
-            answers[idx] = Integer.parseInt(sb.append(str).reverse().toString());
-            idx++;
-            sb.setLength(0);
+    public void solution(int[] input, int sum) {
+        List<int[]> combis = new ArrayList<>();
+        for (int i = 0; i < input.length - 1; i++) {
+            for (int j = i + 1; j < input.length; j++) {
+                combis.add(new int[]{i, j});
+            }
         }
 
-        System.out.println(Math.max(answers[0], answers[1]));
+        for (int[] combi : combis) {
+            int tempVal1 = input[combi[0]];
+            int tempVal2 = input[combi[1]];
+
+            int restVal = sum - tempVal1 - tempVal2;
+            if (restVal == 100) {
+                Arrays.stream(input).filter(val -> val != tempVal1 && val != tempVal2).sorted().forEach(System.out::println);
+                return;
+            }
+        }
     }
 }
 
