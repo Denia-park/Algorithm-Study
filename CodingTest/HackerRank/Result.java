001b2ffbac73e315dcd2cc9e5666f5ed9305cf95
Hyunggi <phg9587@naver.com>
2023-04-03 16:10:48 +0900

    해커랭크 - 연습하기

--- CodingTest/HackerRank/Result.java
+++ CodingTest/HackerRank/Result.java
@@ -1,30 +1,31 @@
 package CodingTest.HackerRank;
 
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 public class Result {
     /*
-     * Complete the 'lonelyinteger' function below.
+     * Complete the 'diagonalDifference' function below.
      *
      * The function is expected to return an INTEGER.
-     * The function accepts INTEGER_ARRAY a as parameter.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
      */
 
-    public static int lonelyinteger(List<Integer> a) {
+    public static int diagonalDifference(List<List<Integer>> arr) {
         // Write your code here
-        Set<Integer> set = new HashSet<>();
+        int ans;
+        int size = arr.get(0).size();
 
-        for (Integer i : a) {
-            if (!set.add(i)) {
-                set.remove(i);
-            }
+        //11 -> 5  대각선
+        int sum1 = 0;
+        for (int i = 0; i < size; i++) {
+            sum1 += arr.get(i).get(i);
+        }
+        //1 -> 7  대각선
+        int sum2 = 0;
+        for (int i = 0; i < size; i++) {
+            sum2 += arr.get(i).get(size - 1 - i);
         }
 
-        Iterator<Integer> iterator = set.iterator();
-
-        return iterator.hasNext() ? iterator.next() : 0;
+        return Math.abs(sum1 - sum2);
     }
 }
--- CodingTest/HackerRank/Solution.java
+++ CodingTest/HackerRank/Solution.java
@@ -1,9 +1,16 @@
 package CodingTest.HackerRank;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
-        Result.lonelyinteger(Arrays.asList(1, 2, 3, 4, 3, 2, 1));
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        list.add(Arrays.asList(3));
+        list.add(Arrays.asList(11, 2, 4));
+        list.add(Arrays.asList(4, 5, 6));
+        list.add(Arrays.asList(10, 8, -12));
+        Result.diagonalDifference(list);
     }
 }
