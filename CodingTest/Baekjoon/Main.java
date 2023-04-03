adc7f2e89b94852704e8cb6297c8a323ab284034
Hyunggi <phg9587@naver.com>
2023-04-03 14:04:12 +0900

    크래프톤 알고리즘 주차 내용 복습

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -3,10 +3,10 @@ package CodingTest.Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
-//23년 4월 3일 오후 1시 51분
+//23년 4월 3일 오후 1시 58분
 //5분 걸림
 
 public class Main {
@@ -14,26 +14,33 @@ public class Main {
         BjSolution sol = new BjSolution();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        int[] inputs = new int[tc];
+        for (int i = 0; i < tc; i++) {
+            inputs[i] = Integer.parseInt(br.readLine());
+        }
 
-        sol.solution(br.readLine());
+        sol.solution(inputs);
     }
 }
 
 class BjSolution {
-    public void solution(String input) {
-        int count = Integer.parseInt(input);
-        Deque<Integer> deque = new ArrayDeque<Integer>();
+    public void solution(int[] inputs) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
 
-        for (int i = 1; i < count + 1; i++) {
-            deque.addLast(i);
+        for (int input : inputs) {
+            if (input == 0) {
+                int val = 0;
+
+                if (!pq.isEmpty()) {
+                    val = pq.poll();
+                }
+
+                System.out.println(val);
+            } else {
+                pq.offer(input);
+            }
         }
-
-        while (deque.size() != 1) {
-            deque.pollFirst();
-            deque.addLast(deque.pollFirst());
-        }
-
-        System.out.println(deque.pollFirst());
     }
 }
 
