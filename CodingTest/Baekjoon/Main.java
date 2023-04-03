43de5b7d87aa27cd63a4c25908be15eaeaae6bcf
Hyunggi <phg9587@naver.com>
2023-04-03 13:57:31 +0900

    크래프톤 알고리즘 주차 내용 복습

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -3,13 +3,11 @@ package CodingTest.Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
-import java.util.stream.Collectors;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-//23년 4월 3일 오후 1시 37분
-//6분 걸림
+//23년 4월 3일 오후 1시 51분
+//5분 걸림
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -17,36 +15,25 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int tcn = Integer.parseInt(br.readLine());
-        String[] strArr = new String[tcn];
-        for (int i = 0; i < tcn; i++) {
-            strArr[i] = br.readLine();
-        }
-
-        sol.solution(strArr);
+        sol.solution(br.readLine());
     }
 }
 
 class BjSolution {
-    public void solution(String[] strArr) {
-        List<Integer> arr = Arrays.stream(strArr).map(Integer::parseInt).collect(Collectors.toList());
+    public void solution(String input) {
+        int count = Integer.parseInt(input);
+        Deque<Integer> deque = new ArrayDeque<Integer>();
 
-        Stack<Integer> stack = new Stack<>();
-        stack.addAll(arr);
-
-        int ans = 1;
-        int top = stack.pop();
-        while (!stack.isEmpty()) {
-            int curTop = stack.pop();
-            if (curTop <= top) {
-                continue;
-            }
-
-            top = curTop;
-            ans++;
+        for (int i = 1; i < count + 1; i++) {
+            deque.addLast(i);
         }
 
-        System.out.println(ans);
+        while (deque.size() != 1) {
+            deque.pollFirst();
+            deque.addLast(deque.pollFirst());
+        }
+
+        System.out.println(deque.pollFirst());
     }
 }
 
