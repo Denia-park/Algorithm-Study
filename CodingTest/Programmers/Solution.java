d5bf82710daa984b02a184624c766605399f0e94
Hyunggi <phg9587@naver.com>
2023-04-11 19:07:14 +0900

    프로그래머스 코딩테스트 연습문제 - 연속 부분 수열 합의 개수 - 2차 풀이 완료 - 나머지 연산자 사용

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -5,20 +5,13 @@ import java.util.Set;
 
 class Solution {
     public int solution(int[] elements) {
-        int[] newElements = new int[elements.length * 2];
-
-        for (int i = 0; i < elements.length; i++) {
-            newElements[i] = elements[i];
-            newElements[i + elements.length] = elements[i];
-        }
-
         Set<Integer> set = new HashSet<>();
 
         for (int length = 1; length <= elements.length; length++) {
             for (int idx = 0; idx < elements.length; idx++) {
                 int tempVal = 0;
                 for (int count = 0; count < length; count++) {
-                    tempVal += newElements[idx + count];
+                    tempVal += elements[(idx + count) % elements.length];
                 }
                 set.add(tempVal);
             }
