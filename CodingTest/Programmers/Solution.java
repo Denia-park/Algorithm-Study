20f8fa36305599349bc6f2ee9989d5bc2fbcec41
Hyunggi <phg9587@naver.com>
2023-04-15 21:35:35 +0900

    프로그래머스 코딩테스트 연습문제 - 가장 긴 팰린드롬 - 정답 참고

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,40 +1,35 @@
 package CodingTest.Programmers;
 
+//정답 참고
+//https://velog.io/@sa833591/%EA%B0%80%EC%9E%A5-%EA%B8%B4-%ED%8C%B0%EB%A6%B0%EB%93%9C%EB%A1%AC-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-Lv.3
+
+//완전탐색 2중 for문으로는 풀었지만 도저히 시간을 줄이지 못해서 정답을 참고함
+//정답을 보니 좀 신기하면서 대단하다.
+
 class Solution {
     public int solution(String s) {
         int answer = 1;
 
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 1; j < s.length(); j++) {
-                if (answer > (j - i + 1)) continue;
+        int len = s.length();
 
-                String subString = s.substring(i, j + 1);
-                int subLen = subString.length();
-
-                if (isPalindrome(subString)) {
-                    answer = subLen;
-                }
-            }
+        for (int i = 0; i < len; i++) {
+            //odd
+            answer = Math.max(answer, isPalindrome(s, i, i));
+            //even
+            answer = Math.max(answer, isPalindrome(s, i, i + 1));
         }
 
         return answer;
     }
 
-    private boolean isPalindrome(String subString) {
-        int len = subString.length();
-        int limit;
-        if (len % 2 == 0) {
-            limit = len / 2 + 1;
-        } else {
-            limit = len / 2;
+    private int isPalindrome(String str, int left, int right) {
+        int len = str.length();
+
+        while (0 <= left && right < len && str.charAt(left) == str.charAt(right)) {
+            left--;
+            right++;
         }
 
-        for (int i = 0; i < limit; i++) {
-            if (subString.charAt(i) != subString.charAt(len - 1 - i)) {
-                return false;
-            }
-        }
-
-        return true;
+        return right - left - 1;
     }
 }
