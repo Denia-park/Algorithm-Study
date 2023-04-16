706aee8586fe617c32d51869bf635a02b28b11e8
Hyunggi <phg9587@naver.com>
2023-04-16 16:18:40 +0900

    프로그래머스 코딩테스트 연습문제 - 요격 시스템 - 2차 풀이 완료 - 문제풀 때 잘못 생각해서 시작좌표도 고려를 했더니 문제를 틀림 -> 끝좌표만 보면 된다. 테케가 운 좋게 시작 좌표를 봐도 풀리게끔 되어있었음

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -5,11 +5,11 @@ package CodingTest.Programmers;
 //최소한의 요격 미사일로 A나라의 미사일을 다 막야함
 //시작 좌표와 끝 좌표에서 쏘는 미사일로는 막을수가 없음
 
-//targets를 정렬 (시작 좌표가 짧은 것 + 끝 좌표가 짧은 순으로)
+//targets를 정렬 (끝 좌표가 짧은 순으로) -> 시작 좌표는 무조건 끝 좌표보다 이전이므로 볼 필요가 없다.
 
 import java.util.Arrays;
 
-//시작좌표 짧은 애를 기준으로 끝 좌표를 보면서 한번에 요격되는 애들은 하나의 미사일로 처리
+//끝 좌표를 기준으로 한번에 요격되는 애들은 하나의 미사일로 처리
 //도저히 처리할 수 없으면 다음 미사일로 처리
 class Solution {
     final int START = 0;
@@ -17,17 +17,10 @@ class Solution {
 
 
     public int solution(int[][] targets) {
-        Arrays.sort(targets, (t1, t2) -> {
-            if (t1[START] == t2[START]) {
-                return t1[END] - t2[END];
-            } else {
-                return t1[START] - t2[START];
-            }
-        });
+        Arrays.sort(targets, (t1, t2) -> t1[END] - t2[END]);
 
         //맨 처음에는 기준이 없으므로
         //첫번째 미사일을 기준으로 삼는다.
-        int missleStart = targets[0][START];
         int missleEnd = targets[0][END];
         int answer = 1;
 
