767b2b68b57a36f4de2200ae7956f4bdd32c719f
Hyunggi <phg9587@naver.com>
2023-04-20 01:27:38 +0900

    해커랭크 라이브 코테 준비

--- CodingTest/HackerRank/Result.java
+++ CodingTest/HackerRank/Result.java
@@ -1,41 +1,41 @@
 package CodingTest.HackerRank;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Result {
     /*
-     * Complete the 'caesarCipher' function below.
+     * Complete the 'gridChallenge' function below.
      *
      * The function is expected to return a STRING.
-     * The function accepts following parameters:
-     *  1. STRING s
-     *  2. INTEGER k
+     * The function accepts STRING_ARRAY grid as parameter.
      */
 
-    public static String caesarCipher(String s, int k) {
+    public static String gridChallenge(List<String> grid) {
         // Write your code here
-        int rotateCount = k % 26;
-        StringBuilder alphabet = new StringBuilder();
-        for (int i = 'a'; i < 'z' + 1; i++) {
-            alphabet.append((char) (i));
+        List<String> newGrid = new ArrayList<>();
+
+        //row - rearrange
+        for (String str : grid) {
+            char[] tempArr = str.toCharArray();
+            Arrays.sort(tempArr);
+            newGrid.add(String.valueOf(tempArr));
         }
-        alphabet.append(alphabet);
 
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            boolean upperFlag = Character.isUpperCase(c);
-
-            char c2 = Character.toLowerCase(c);
-
-            int index = alphabet.indexOf(String.valueOf(c2));
-
-            if (index == -1) {
-                result.append(s.charAt(i));
-            } else {
-                char val = alphabet.charAt(index + rotateCount);
-                result.append(upperFlag ? Character.toUpperCase(val) : val);
+        //check each colum while checking each row
+        int colNum = newGrid.get(0).length();
+        for (int col = 0; col < colNum; col++) {
+            int rowNum = newGrid.size();
+            char firstChar = newGrid.get(0).charAt(col);
+            for (int row = 1; row < rowNum; row++) {
+                char secondChar = newGrid.get(row).charAt(col);
+                if (firstChar > secondChar) {
+                    return "NO";
+                }
             }
         }
 
-        return result.toString();
+        return "YES";
     }
 }
--- CodingTest/HackerRank/Solution.java
+++ CodingTest/HackerRank/Solution.java
@@ -1,9 +1,12 @@
 package CodingTest.HackerRank;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) {
-        String val = Result.caesarCipher("middle-Outz", 2);
+        List<String> inputs = Arrays.asList("abc", "ade", "efg");
+        String val = Result.gridChallenge(inputs);
         System.out.println(val);
-        System.out.println(val.equals("okffng-Qwvb"));
     }
 }
