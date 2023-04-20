cc4b7e8b38583cc1e5aef4a46da7c9e76b0a5dff
Hyunggi <phg9587@naver.com>
2023-04-21 02:07:02 +0900

    프로그래머스 연습문제 과제 진행하기 - 2차 풀이 - 몇가지 테케만 실패 - 예외를 못찾은듯

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -10,65 +10,62 @@ class Solution {
     public String[] solution(String[][] plans) {
         List<Plan> answers = new ArrayList<>();
 
-        List<Plan> planList = new ArrayList<>();
+        PriorityQueue<Plan> planList = new PriorityQueue<>(Comparator.comparingInt(p -> p.startMinute));
         for (String[] plan : plans) {
             planList.add(new Plan(plan[0], plan[1], plan[2]));
         }
 
-        planList.sort(Comparator.comparingInt(p -> p.startMinute));
-
         Deque<Plan> waitQueue = new ArrayDeque<>();
 
-        Plan curPlan = planList.get(0);
-        int curTime = curPlan.startMinute;
+        while (!planList.isEmpty()) {
+            Plan curPlan = planList.poll();
 
-        int listIdx = 1;
-        while (true) {
-            if (listIdx == planList.size()) {
+            //curPlan 이 마지막 계획
+            if (planList.isEmpty()) {
                 answers.add(curPlan);
                 break;
             }
 
-            Plan nextPlan = planList.get(listIdx);
+            Plan nextPlan = planList.peek();
 
+            //다음 계획이 먼저 실행 -> 현재 계획은 wait으로
             if (curPlan.endMinute > nextPlan.startMinute) {
-                curPlan.remainMinute -= nextPlan.startMinute - curTime;
-                curTime = nextPlan.startMinute;
+                curPlan.startMinute = nextPlan.startMinute;
                 waitQueue.addLast(curPlan);
-                curPlan = nextPlan;
-                listIdx++;
-            } else if (curPlan.endMinute == nextPlan.startMinute) {
-                curPlan.remainMinute = 0;
-                curTime = nextPlan.startMinute;
+            }
+            //다음 계획이 딱 맞게 실행
+            else if (curPlan.endMinute == nextPlan.startMinute) {
                 answers.add(curPlan);
-                curPlan = nextPlan;
-                listIdx++;
-            } else {
-                curPlan.remainMinute = 0;
-                curTime = curPlan.endMinute;
+            }
+            //현재 계획이 먼저 끝남
+            else {
                 answers.add(curPlan);
+                //현재 시간
+                int curTime = curPlan.endMinute;
 
+                //대기 Queue를 돌면서 대기 Plan을 확인
                 while (!waitQueue.isEmpty()) {
+                    //대기 Plan
                     Plan waitPlan = waitQueue.pollLast();
 
-                    if (curTime + waitPlan.remainMinute > nextPlan.startMinute) {
-                        waitPlan.remainMinute -= (nextPlan.startMinute - (curTime + waitPlan.startMinute));
-                        curTime = waitPlan.startMinute;
+                    //대기 Plan의 남아있는 시간 확인
+                    int remainMinute = waitPlan.endMinute - waitPlan.startMinute;
+
+                    //다음 계획이 대기 Plan 보다 먼저 실행
+                    if (curTime + remainMinute > nextPlan.startMinute) {
+                        waitPlan.startMinute += nextPlan.startMinute - curTime;
                         waitQueue.addLast(waitPlan);
-                        curPlan = nextPlan;
-                        listIdx++;
                         break;
-                    } else if (curTime + waitPlan.remainMinute == nextPlan.startMinute) {
-                        waitPlan.remainMinute = 0;
-                        curTime = nextPlan.startMinute;
+                    }
+                    //다음 계획이 대기 Plan 과 딱 맞게 실행
+                    else if (curTime + remainMinute == nextPlan.startMinute) {
                         answers.add(waitPlan);
-                        curPlan = nextPlan;
-                        listIdx++;
                         break;
-                    } else {
-                        waitPlan.remainMinute = 0;
+                    }
+                    //대기 Plan이 먼저 끝남
+                    else {
+                        answers.add(waitPlan);
                         curTime = waitPlan.endMinute;
-                        answers.add(waitPlan);
                     }
                 }
             }
@@ -78,21 +75,19 @@ class Solution {
             Plan waitPlan = waitQueue.pollLast();
             answers.add(waitPlan);
         }
-
+        
         return answers.stream().map(p -> p.subject).toArray(String[]::new);
     }
 
     static class Plan {
         String subject;
         int startMinute;
-        int remainMinute;
         int endMinute;
 
         public Plan(String subject, String startTime, String remainMinute) {
             this.subject = subject;
             this.startMinute = convertTime(startTime);
-            this.remainMinute = Integer.parseInt(remainMinute);
-            this.endMinute = this.startMinute + this.remainMinute;
+            this.endMinute = this.startMinute + Integer.parseInt(remainMinute);
         }
 
         private int convertTime(String startTime) {
