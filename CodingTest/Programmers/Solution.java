5c69d1bb98946433850eb6befbcd7ed67676064a
Hyunggi <phg9587@naver.com>
2023-04-22 01:38:09 +0900

    프로그래머스 입문 합성수 찾기 - 1차 풀이 완료 - 에라토스테네스의 체를 써서 풀이

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,100 +1,27 @@
 package CodingTest.Programmers;
 
-//과제 진행하기
-
-//23년 4월 21일 오전 12시 30분 시작 -> 오전 2시 13분 끝
-
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
-    public String[] solution(String[][] plans) {
-        List<Plan> answers = new ArrayList<>();
+    public int solution(int n) {
+        boolean[] primeNumCheckArray = new boolean[101];
+        Arrays.fill(primeNumCheckArray, true);
 
-        PriorityQueue<Plan> planList = new PriorityQueue<>(Comparator.comparingInt(p -> p.startMinute));
-        for (String[] plan : plans) {
-            planList.add(new Plan(plan[0], plan[1], plan[2]));
-        }
-
-        Deque<Plan> waitQueue = new ArrayDeque<>();
-
-        while (!planList.isEmpty()) {
-            Plan curPlan = planList.poll();
-
-            //curPlan 이 마지막 계획
-            if (planList.isEmpty()) {
-                answers.add(curPlan);
-                break;
-            }
-
-            Plan nextPlan = planList.peek();
-
-            //다음 계획이 먼저 실행 -> 현재 계획은 wait으로
-            if (curPlan.endMinute > nextPlan.startMinute) {
-                curPlan.startMinute = nextPlan.startMinute;
-                waitQueue.addLast(curPlan);
-            }
-            //다음 계획이 딱 맞게 실행
-            else if (curPlan.endMinute == nextPlan.startMinute) {
-                answers.add(curPlan);
-            }
-            //현재 계획이 먼저 끝남
-            else {
-                answers.add(curPlan);
-                //현재 시간
-                int curTime = curPlan.endMinute;
-
-                //대기 Queue를 돌면서 대기 Plan을 확인
-                while (!waitQueue.isEmpty()) {
-                    //대기 Plan
-                    Plan waitPlan = waitQueue.pollLast();
-
-                    //대기 Plan의 남아있는 시간 확인
-                    int remainMinute = waitPlan.endMinute - waitPlan.startMinute;
-
-                    //다음 계획이 대기 Plan 보다 먼저 실행
-                    if (curTime + remainMinute > nextPlan.startMinute) {
-                        waitPlan.startMinute += nextPlan.startMinute - curTime;
-                        waitQueue.addLast(waitPlan);
-                        break;
-                    }
-                    //다음 계획이 대기 Plan 과 딱 맞게 실행
-                    else if (curTime + remainMinute == nextPlan.startMinute) {
-                        answers.add(waitPlan);
-                        break;
-                    }
-                    //대기 Plan이 먼저 끝남
-                    else {
-                        answers.add(waitPlan);
-                        curTime = curTime + remainMinute;
-                    }
-                }
+        //약수의 개수가 3개 이상인 수 -> 소수가 아닌 수
+        //100까지 이므로 10도 에라토스테네스의 체로 구해야함
+        for (int idx = 2; idx < 11; idx++) {
+            for (int multi = idx * 2; multi < primeNumCheckArray.length; multi += idx) {
+                primeNumCheckArray[multi] = false;
             }
         }
 
-        while (!waitQueue.isEmpty()) {
-            Plan waitPlan = waitQueue.pollLast();
-            answers.add(waitPlan);
+        int primeNumCount = 0;
+        for (int idx = 1; idx < n + 1; idx++) {
+            if (primeNumCheckArray[idx]) {
+                primeNumCount++;
+            }
         }
 
-        return answers.stream().map(p -> p.subject).toArray(String[]::new);
-    }
-
-    static class Plan {
-        String subject;
-        int startMinute;
-        int endMinute;
-
-        public Plan(String subject, String startTime, String remainMinute) {
-            this.subject = subject;
-            this.startMinute = convertTime(startTime);
-            this.endMinute = this.startMinute + Integer.parseInt(remainMinute);
-        }
-
-        private int convertTime(String startTime) {
-            String[] time = startTime.split(":");
-            int hour = Integer.parseInt(time[0]);
-            int minute = Integer.parseInt(time[1]);
-            return hour * 60 + minute;
-        }
+        return n - primeNumCount;
     }
 }
