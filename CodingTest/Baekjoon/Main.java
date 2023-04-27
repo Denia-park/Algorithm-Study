e681d8e385847fc5e11f3fc1692dd365313a36b4
HyunggiPark <phg9587@naver.com>
2023-04-27 14:51:21 +0900

    백준 단지번호붙이기 [2667] - 1차 풀이 완료 - 기본 문제

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -1,10 +1,21 @@
 package CodingTest.Baekjoon;
 
+//단지 번호 붙이기 - 2667
+
+//계획
+//2중 for문으로 "1인 곳, 아직 방문하지 않은" 집만 확인 후 DFS를 돈다.
+//      - DFS를 돌면서 단지의 사이즈를 확인하고 max값을 출력한다.
+//      - max 단지 값을 List에 저장 후 sort하고 출력한다.
+
+//출력
+//단지 출력
+//단지에 속하는 집을 오름차순으로 "정렬" 후 출력
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,16 +23,14 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] inputs = br.readLine().split(" ");
-        int row = Integer.parseInt(inputs[0]);
-        int col = Integer.parseInt(inputs[1]);
-        int[][] map = new int[row][col];
+        int mapSize = Integer.parseInt(br.readLine());
+        int[][] map = new int[mapSize][mapSize];
 
-        for (int i = 0; i < row; i++) {
-            inputs = br.readLine().split(" ");
+        for (int i = 0; i < mapSize; i++) {
+            String input = br.readLine();
             int tempIdx = 0;
-            for (String input : inputs) {
-                map[i][tempIdx++] = Integer.parseInt(input);
+            for (char ch : input.toCharArray()) {
+                map[i][tempIdx++] = ch - '0';
             }
         }
 
@@ -30,63 +39,51 @@ public class Main {
 }
 
 class BjSolution {
-    int gRow;
-    int gCol;
-    int drawCount;
-    int maxDrawWidth;
+    int gMapSize;
     int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     boolean[][] visited;
+    List<Integer> answers;
+    int visitCount;
 
     public void solution(int[][] map) {
-        gRow = map.length;
-        gCol = map[0].length;
+        gMapSize = map.length;
+        visited = new boolean[gMapSize][gMapSize];
+        answers = new ArrayList<>();
 
-        drawCount = 0;
-        maxDrawWidth = 0;
 
-        visited = new boolean[gRow][gCol];
-
-        for (int row = 0; row < gRow; row++) {
-            for (int col = 0; col < gCol; col++) {
+        for (int row = 0; row < gMapSize; row++) {
+            for (int col = 0; col < gMapSize; col++) {
                 if (map[row][col] == 0 || visited[row][col]) continue;
 
-                bfs(map, row, col);
-                drawCount++;
+                visitCount = 1;
+                visited[row][col] = true;
+                dfs(map, row, col);
+                answers.add(visitCount);
             }
         }
 
-        System.out.println(drawCount);
-        System.out.println(maxDrawWidth);
+        System.out.println(answers.size());
+        answers.sort(null);
+        for (Integer val : answers) {
+            System.out.println(val);
+        }
     }
 
-    private void bfs(int[][] map, int row, int col) {
-        Deque<int[]> queue = new ArrayDeque<>();
-        visited[row][col] = true;
-        queue.offer(new int[]{row, col});
+    private void dfs(int[][] map, int row, int col) {
+        for (int[] direction : directions) {
+            int nextRow = row + direction[0];
+            int nextCol = col + direction[1];
 
-        int tempWidth = 0;
+            if (isOutOfMap(nextRow, nextCol) || map[nextRow][nextCol] == 0 || visited[nextRow][nextCol]) continue;
 
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int curRow = cur[0];
-            int curCol = cur[1];
-            tempWidth++;
-
-            for (int[] direction : directions) {
-                int nextRow = curRow + direction[0];
-                int nextCol = curCol + direction[1];
-                if (isOutOfMap(nextRow, nextCol) || map[nextRow][nextCol] == 0 || visited[nextRow][nextCol]) continue;
-
-                visited[nextRow][nextCol] = true;
-                queue.offer(new int[]{nextRow, nextCol});
-            }
+            visited[nextRow][nextCol] = true;
+            visitCount++;
+            dfs(map, nextRow, nextCol);
         }
-
-        maxDrawWidth = Math.max(maxDrawWidth, tempWidth);
     }
 
     private boolean isOutOfMap(int row, int col) {
-        return row < 0 || row >= gRow || col < 0 || col >= gCol;
+        return row < 0 || row >= gMapSize || col < 0 || col >= gMapSize;
     }
 }
 
