a6936830f79cd65d9c961d1f6b53784fc38fdebe
HyunggiPark <phg9587@naver.com>
2023-05-02 18:23:58 +0900

    프로그래머스 2023 KAKAO BLIND RECRUITMENT - 양궁대회 - 1차 풀이 완료 - 전에 한번 정답 참고했었음 - 백트래킹이란걸 알고 보니까 문제가 그렇게 어렵지 않다.

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -6,9 +6,10 @@ public class ProgrammersMain {
     public static void main(String[] args) {
         Solution ts = new Solution();
 
-        System.out.println(Arrays.toString(ts.solution(new int[]{1, 2, 3, 4, 5}, 7)));
-        System.out.println(Arrays.toString(ts.solution(new int[]{1, 1, 1, 2, 3, 4, 5}, 5)));
-        System.out.println(Arrays.toString(ts.solution(new int[]{2, 2, 2, 2, 2}, 6)));
+//        System.out.println(Arrays.toString(ts.solution(5, new int[]{2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0})));
+//        System.out.println(Arrays.toString(ts.solution(1, new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})));
+        System.out.println(Arrays.toString(ts.solution(9, new int[]{0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1})));
+//        System.out.println(Arrays.toString(ts.solution(10, new int[]{0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3})));
     }
 }
 
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,61 +1,130 @@
 package CodingTest.Programmers;
 
-//연속된 부분 수열의 합 -> 연속된 ? 투포인터 써도 되나.
+//2022 KAKAO BLIND RECRUITMENT
+//양궁대회
 
-//input은 오름차순
+//라이언 우승자 , 어피치 도전자
 
-import java.util.PriorityQueue;
+//어피치에게 유리한 룰
+//      1.어피치가 먼저 쏘고 라이언이 쏜다.
+//      2.k점을 더 많은 화살을 k점에 맞힌 선수가 k점을 가져간다. (동일하게 맞춘 경우, 어피치가 가져간다.)
+//      3.k점을 여러발 맞춰도 k점만 가져간다. 그리고 모두 0발이면 아무도 점수 획득 못 함
+//      4.최종 점수가 높은 사람이 우승자 => 점수가 같아도 어피치가 우승
+
+//현재 어피치가 다 쏘고 나서 라이언이 쏠 차례
+//가장 큰 점수차로 이기기 위해 n발의 화살을 어떻게 맞춰야 하는지 계산
+//n발의 화살을 어떻게 맞춰야 하는지 정수 배열에 담아 return, 라이언이 우승할 수 없는 경우는 -1을 리턴 (무조건 지거나, 비기는 경우)
+
+//가장 큰 점수 차이가 여러개이면, 가장 낮은 점수를 더 많이 경우를 return
+
+//N의 수가 작으니까, 백트래킹을 적용하바.
+
+import java.util.Arrays;
 
 class Solution {
-    public int[] solution(int[] sequence, int k) {
-        PriorityQueue<SubArray> pq = new PriorityQueue<>();
-        int start = 0;
-        int end = 0;
-        int sum = 0;
-        sum = sequence[0];
+    int maxScoreDiff;
+    int[] answer;
+    int[] apeachScores;
+    int[] rionScores;
 
-        while (start <= end) {
-            if (sum < k) {
-                if (end >= (sequence.length - 1)) {
-                    break;
-                }
-                end++;
-                sum += sequence[end];
-            } else {
-                if (sum == k) {
-                    pq.add(new SubArray(start, end));
-                }
-                start++;
-                sum -= sequence[start - 1];
-            }
-        }
+    public int[] solution(int arrowNum, int[] info) {
+        maxScoreDiff = 0;
+        answer = new int[11];
+        apeachScores = info;
+        rionScores = new int[11];
 
-        return pq.peek().toArray();
+        //가장 큰 점수차 -> 큰 점수부터 먹는다. => 백트래킹 실행
+        fullScan(0, arrowNum);
+
+        //도저히 못 이기면 -1을 리턴한다.
+        return maxScoreDiff == 0 ? new int[]{-1} : answer;
     }
 
-    class SubArray implements Comparable<SubArray> {
-        int startIdx;
-        int endIdx;
+    private void fullScan(int curDepth, int arrowNum) {
+        if (curDepth == apeachScores.length) {
+            //어피치랑 라이언 점수 비교 함수
+            int tempScoreDiff = calculateScoreDiff();
 
-        public SubArray(int startIdx, int endIdx) {
-            this.startIdx = startIdx;
-            this.endIdx = endIdx;
-        }
-
-        @Override
-        public int compareTo(SubArray o) {
-            //짧은 수열 우선
-            int originDiff = (this.endIdx - this.startIdx);
-            int otherDiff = (o.endIdx - o.startIdx);
-            if (originDiff == otherDiff) {
-                //앞쪽 인덱스가 작은 수열 우선
-                return this.startIdx - o.startIdx;
+            if (tempScoreDiff == 0 || tempScoreDiff < maxScoreDiff) {
+                return;
             }
-            return originDiff - otherDiff;
+
+            if (tempScoreDiff > maxScoreDiff) {
+                maxScoreDiff = tempScoreDiff;
+                answer = Arrays.copyOf(rionScores, rionScores.length);
+                return;
+            }
+
+            //기존 점수랑 똑같을 경우 뭐가 더 우선인지 비교할 함수
+            calculatePriority();
+
+//            System.out.println("--------------------------------------");
+//            System.out.println(maxScoreDiff);
+//            System.out.println(Arrays.toString(answer));
+//            System.out.println(Arrays.toString(rionScores));
+            return;
         }
 
-        public int[] toArray() {
-            return new int[]{this.startIdx, this.endIdx};
+        if (curDepth == apeachScores.length - 1) {
+            rionScores[curDepth] = arrowNum;
+            fullScan(curDepth + 1, 0);
+            rionScores[curDepth] = 0;
+            return;
+        }
+
+        for (int count = 0; count < apeachScores[curDepth] + 2; count++) {
+            if (count > arrowNum) {
+                continue;
+            }
+
+            rionScores[curDepth] = count;
+            fullScan(curDepth + 1, arrowNum - count);
+            rionScores[curDepth] = 0;
+        }
+    }
+
+    //어피치랑 라이언 점수 비교
+    private int calculateScoreDiff() {
+        int apeachSum = 0;
+        int rionSum = 0;
+
+        for (int scoreIdx = 0; scoreIdx < rionScores.length; scoreIdx++) {
+            int rionNum = rionScores[scoreIdx];
+            int apeachNum = apeachScores[scoreIdx];
+
+            if (rionNum == 0 && apeachNum == 0) {
+                continue;
+            }
+
+            int score = 10 - scoreIdx;
+
+            if (rionNum > apeachNum) {
+                rionSum += score;
+            } else {
+                apeachSum += score;
+            }
+        }
+
+        return Math.max(rionSum - apeachSum, 0);
+    }
+
+    private void calculatePriority() {
+        boolean flag = true;
+
+        for (int idx = rionScores.length - 1; idx >= 0; idx--) {
+            int answerNum = answer[idx];
+            int rionNum = rionScores[idx];
+
+            if (answerNum > rionNum) {
+                flag = false;
+                break;
+            } else if (answerNum < rionNum) {
+                break;
+            }
+        }
+
+        if (flag) {
+            answer = Arrays.copyOf(rionScores, rionScores.length);
         }
     }
 }
