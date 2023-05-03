bf91be3adb7f98ad3bda972358db0c64aa442054
HyunggiPark <phg9587@naver.com>
2023-05-04 02:06:19 +0900

    프로그래머스 기초 공백으로 구분하기 - 1차 풀이 완료

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -6,10 +6,7 @@ public class ProgrammersMain {
     public static void main(String[] args) {
         Solution ts = new Solution();
 
-//        System.out.println(Arrays.toString(ts.solution(5, new int[]{2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0})));
-//        System.out.println(Arrays.toString(ts.solution(1, new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})));
-        System.out.println(Arrays.toString(ts.solution(9, new int[]{0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1})));
-//        System.out.println(Arrays.toString(ts.solution(10, new int[]{0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3})));
+        System.out.println(Arrays.toString(ts.solution(" i    love  you")));
     }
 }
 
