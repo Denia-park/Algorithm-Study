d12649d387bcb89741953871b7a302f2fbb47606
HyunggiPark <phg9587@naver.com>
2023-05-09 14:33:41 +0900

    백준 텀 프로젝트 [9466] - 복습 - 1차 풀이 - 시간 초과

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -23,69 +23,78 @@ public class Main {
 
 /*
 1. 아이디어
-싸이클을 이루는지 확인 - 계속 돌면서 자기 자신한테로 돌아오는지 확인한다.
+- 연결을 따라가다 보면 무조건 싸이클을 만나게 되어있다. (싸이클을 만나야 끝이 난다)
+- 싸이클을 확인해야 함
+- 싸이클을 만나면 해당 싸이클만큼 계산을 해야함
+- 참여하지 못한 학생들의 수를 표시해야 하므로 전체 인원수에서 사이클 인원수를 빼야 한다.
 
 2. 시간 복잡도
-O(N^2) 10^10 10억
-dp를 써서 중간에 캐싱을 해야 함
+- 그냥 완탐 돌리면 O(N^2)으로 시간 초과가 난다. 10억 나옴
+- O(N)으로 탐색을 해야함
 
 3. 자료 구조
-값을 저장할 dp int[]
+- 계속해서 들어가면서 값을 확인해야 하므로, 재귀 혹은 while을 쓴다. (dfs)
+- 방문했는지 확인할 boolean[] visited
+- 해당 싸이클이 이미 내가 검증한 싸이클인지 확인할 boolean[] done
 */
 
 class BjSolution {
-    //방문 확인
+    //싸이클 판별을 위해 방문을 했는지 체크
     boolean[] visited;
-    //사이클 계산을 한적이 있는지 확인
+    //이미 계산이 끝난 사람인지 확인을 위해 체크
     boolean[] done;
-    //전역변수
-    int[] gPeople;
-    private int answer;
+    int answer;
 
     public void solution(int peopleNum, int[] people) {
         answer = 0;
-
-        gPeople = people;
+        //계산을 편리하게 하려고 1 큰 값을 사용
         visited = new boolean[peopleNum + 1];
         done = new boolean[peopleNum + 1];
 
-        //people 만큼 돌면서 확인
         for (int i = 0; i < people.length; i++) {
-            int idx = i + 1;
+            int curIdx = i + 1;
 
-            //이미 방문한적이 있으면 어떻게든 계산이 되었으므로 확인 X
-            if (visited[idx]) continue;
+            //이미 계산이 끝난 사람이면 더 이상 확인할 필요가 없다.
+            if (done[curIdx]) continue;
 
-            //방문한적이 없는 경우에만 dfs로 돈다.
-            dfs(people[idx - 1]);
+            dfs(people, curIdx);
         }
 
-        //조를 이루지 못한 사람만 출력
         System.out.println(peopleNum - answer);
     }
 
-    private void dfs(int curIdx) {
-        //방문 처리
-        visited[curIdx] = true;
-        //다음 방문할 idx 확인
-        int nextIdx = gPeople[curIdx - 1];
+    //1 -> 3 -> 3 -> ...
+    //1번에서 시작했지만 끝은 싸이클에서 끝나는 것을 알 수 있다.
 
-        //방문한적이 없으면 계속해서 dfs
+    //4 -> 7 -> 6 -> 4 -> ...
+    //현재 6번 상황이면 방문한 곳을 또 방문했는데, 아직 done이 안되어있으므로 싸이클을 계산한다.
+    private void dfs(int[] people, int curIdx) {
+        visited[curIdx] = true;
+
+        int nextIdx = people[curIdx - 1];
+
         if (!visited[nextIdx]) {
-            dfs(nextIdx);
+            dfs(people, nextIdx);
+            done[curIdx] = true;
+            return;
         }
-        //방문한적이 있는데 계산한적이 없으면 사이클만큼 answer를 올린다.
-        else if (!done[nextIdx]) {
-            int cycleIdx = nextIdx;
-            while (cycleIdx != curIdx) {
+
+        //방문을 한번 한적이 있는데, 아직 계산을 안했으면 지금 딱 싸이클에 도달했다는 의미.
+        if (visited[nextIdx] && !done[nextIdx]) {
+            //계산 완료
+            done[curIdx] = true;
+
+            //싸이클 인원 수 만큼 계산을 해야함.
+            int tempNextIdx = nextIdx;
+
+            while (tempNextIdx != curIdx) {
                 answer++;
-                cycleIdx = gPeople[cycleIdx - 1];
+                tempNextIdx = people[tempNextIdx - 1];
             }
-            //curIdx를 count
+
+            //나도 카운팅 해야함
             answer++;
         }
-
-        done[curIdx] = true;
     }
 }
 
