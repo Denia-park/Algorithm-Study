9eae20ec1ab8ee49d9db40ef09b23827222124c2
HyunggiPark <phg9587@naver.com>
2023-05-12 22:26:16 +0900

    문자열 split 기준 변경

--- CodingTest/Programmers/Util.java
+++ CodingTest/Programmers/Util.java
@@ -7,13 +7,13 @@ public class Util {
     public static int[][] convertStringToJavaIntTwoDimensionalArray(String source) {
         String newString = source.substring(1, source.length() - 1);
 
-        String[] intArrays = newString.replace("], [", "]-[").split("-");
+        String[] intArrays = newString.replace("],[", "]&[").split("&");
 
         List<int[]> list = new ArrayList<>();
 
         for (String intArray : intArrays) {
             String tempString = intArray.substring(1, intArray.length() - 1);
-            String[] tempStringArray = tempString.split(", ");
+            String[] tempStringArray = tempString.split(",");
 
             int[] tempIntArray = new int[tempStringArray.length];
 
