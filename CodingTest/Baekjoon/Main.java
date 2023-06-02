314253148f6ed8f6b2ba4fcc17821f5e874bc75e
HyunggiPark <phg9587@naver.com>
2023-06-02 17:07:19 +0900

    코테 복습 - 로봇 청소기 14503 - 간만에 다시 풀어봤는데 객체지향적 풀이 성공

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -1,80 +1,175 @@
 package CodingTest.Baekjoon;
 
-//N과 M (1) 15649
+//로봇 청소기 14503
+//오후 4시 21분
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BjSolution sol = new BjSolution();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] splits = br.readLine().split(" ");
-        int maxNum = Integer.parseInt(splits[0]);
-        int numMaxSize = Integer.parseInt(splits[1]);
+        String[] splits1 = br.readLine().split(" ");
+        int mapRow = Integer.parseInt(splits1[0]);
+        int mapCol = Integer.parseInt(splits1[1]);
 
-        sol.solution(maxNum, numMaxSize);
+        String[] splits2 = br.readLine().split(" ");
+        int[][] map = new int[mapRow][mapCol];
+        for (int i = 0; i < mapRow; i++) {
+            String[] tempSplits = br.readLine().split(" ");
+            for (int j = 0; j < mapCol; j++) {
+                map[i][j] = Integer.parseInt(tempSplits[j]);
+            }
+        }
+
+        sol.solution(map, mapRow, mapCol, splits2);
     }
 }
 
 /*
 아이디어
-- 모두 돌아야함 -> 완탐 -> 백트래킹 (N이 작다)
-- 방문 체크 & 숫자 넣기
-- 백트래킹이니까 벗어나면 방문 체크 풀기 & 숫자 빼기
+시뮬레이션
 
 - 출력
-순서에 맞게 쭉 출력하기.
+총 청소한 칸
 
 시간복잡도
-- O(N^사이즈)
+제대로 구현만 하면 문제 없음
 
 자료구조
-- 완탐
+객체를 사용
  */
 
 class BjSolution {
-    StringBuilder sb;
-    Deque<Integer> dq;
-    boolean[] visited;
+    public void solution(int[][] map, int mapRow, int mapCol, String[] splits2) {
+        int machinRow = Integer.parseInt(splits2[0]);
+        int machinCol = Integer.parseInt(splits2[1]);
+        int direction = Integer.parseInt(splits2[2]);
 
-    public void solution(int maxNum, int numMaxSize) {
-        visited = new boolean[maxNum + 1];
-        dq = new ArrayDeque<>();
-        sb = new StringBuilder();
+        System.out.println(new Machin(machinRow, machinCol, direction, map).run());
+    }
+}
 
-        backTracking(maxNum, numMaxSize);
+class Machin {
+    final int PLACE = 0;
+    final int WALL = 1;
+    final int CLEAN = 2;
+    int r, c, d, count;
+    int[][] map;
+    int mapRow, mapCol;
+    //전진 : 현재 값 그대로 사용
+    //후진 : 0->2, 2->0, 1->3, 3->1 (후진은 현재 direction에 2 더하고 % 4 )
+    //반시계 방향 회전 : 0->3 , 1->0, 2->1,  3->2 (3 더하고 % 4)
+    int[][] directions = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}; //북,동,남,서
+    int[][] goBackDirection = new int[][]{{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
 
-        System.out.println(sb);
+    public Machin(int r, int c, int d, int[][] map) {
+        this.r = r;
+        this.c = c;
+        this.d = d;
+        this.map = map;
+        this.mapRow = map.length;
+        this.mapCol = map[0].length;
+        this.count = 0;
     }
 
-    private void backTracking(int maxNum, int numMaxSize) {
-        if (dq.size() == numMaxSize) {
-            print(dq);
-            return;
-        }
+    public int run() {
+        while (true) {
+            clean();
 
-        for (int i = 1; i <= maxNum; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                dq.addLast(i);
-                backTracking(maxNum, numMaxSize);
-                dq.removeLast();
-                visited[i] = false;
+            if (isAllCleanedAroundMachin()) {
+                //후진할수없으면 작동을 멈춘다.
+                if (!isAvailableGoBack()) {
+                    break;
+                }
+
+                //방향 유지 후 한칸 후진
+                goBack();
+                //1번으로 돌아간다.
+                continue;
+            } else {
+                while (true) {
+                    turnLeft();
+                    if (isDirtyPlaceFrontMachin()) {
+                        go();
+                        break;
+                    }
+                }
             }
         }
+
+        return this.count;
     }
 
-    private void print(Deque<Integer> dq) {
-        for (Integer val : dq) {
-            sb.append(val);
-            sb.append(" ");
+    private void go() {
+        int nR = this.r + directions[d][0];
+        int nC = this.c + directions[d][1];
+
+        this.r = nR;
+        this.c = nC;
+    }
+
+    private boolean isDirtyPlaceFrontMachin() {
+        int nR = this.r + directions[d][0];
+        int nC = this.c + directions[d][1];
+
+        return !isOutOfMap(nR, nC) && !isWall(nR, nC) && !isCleaned(nR, nC);
+    }
+
+    private void turnLeft() {
+        this.d = (this.d + 3) % 4;
+    }
+
+    private void clean() {
+        if (isCleaned(this.r, this.c)) return;
+        map[r][c] = CLEAN;
+        this.count++;
+    }
+
+    private boolean isCleaned(int r, int c) {
+        return map[r][c] == CLEAN;
+    }
+
+    private boolean isAllCleanedAroundMachin() {
+        //북동남서 확인
+        //청소되지 않은 빈칸인지만 확인
+        for (int[] direction : directions) {
+            int nR = r + direction[0];
+            int nC = c + direction[1];
+
+            if (isOutOfMap(nR, nC) || isWall(nR, nC) || isCleaned(nR, nC)) continue;
+
+            //더러우면
+            return false;
         }
-        sb.append("\n");
+
+        return true;
+    }
+
+    private boolean isWall(int r, int c) {
+        return map[r][c] == WALL;
+    }
+
+    private boolean isOutOfMap(int r, int c) {
+        return r < 0 || r >= mapRow || c < 0 || c >= mapCol;
+    }
+
+    private boolean isAvailableGoBack() {
+        int nR = this.r + goBackDirection[d][0];
+        int nC = this.c + goBackDirection[d][1];
+
+        return !isOutOfMap(nR, nC) && !isWall(nR, nC);
+    }
+
+    private void goBack() {
+        int nR = this.r + goBackDirection[d][0];
+        int nC = this.c + goBackDirection[d][1];
+
+        this.r = nR;
+        this.c = nC;
     }
 }
 
