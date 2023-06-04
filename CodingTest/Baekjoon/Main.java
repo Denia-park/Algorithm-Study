22adc7eef49d0582b3cfa90d6dd9cfae68bc207c
HyunggiPark <phg9587@naver.com>
2023-06-04 13:55:42 +0900

    코테 복습 - 플로이드 11404

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -1,56 +1,59 @@
 package CodingTest.Baekjoon;
 
-//최단 경로 1753
+//플로이드 11404
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BjSolution sol = new BjSolution();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] split = br.readLine().split(" ");
-        int vNum = Integer.parseInt(split[0]);
-        int eNum = Integer.parseInt(split[1]);
-        int sNum = Integer.parseInt(br.readLine());
+        int cityNum = Integer.parseInt(br.readLine());
+        int busNum = Integer.parseInt(br.readLine());
 
-        String[] edges = new String[eNum];
+        String[] edges = new String[busNum];
 
-        for (int i = 0; i < eNum; i++) {
+        for (int i = 0; i < busNum; i++) {
             edges[i] = br.readLine();
         }
 
-        sol.solution(vNum, sNum, edges);
+        sol.solution(cityNum, busNum, edges);
     }
 }
 
 /*
 아이디어
-다익스트라
+플로이드
+- 방향 리스트 정의
+- 가중치 값 2차원 배열 초기화
+    - 자기 자신은 0, 나머지는 INF, INF는 오버플로우 조심
+    - 3차 for문 실행 -> 중간 지점부터 시작할 것 !! 주의
 
 - 출력
 배열을 모두 출력
+- 자기자신 0 , 못 가면 0
 
 시간복잡도
-O(ELogV)
+O(V^3)
 
 자료구조
-가중치가 10이하의 자연수 이므로 가중치는 int 사용
-다익스트라 이므로 PQ 사용
+비용이 10만이하 이므로 모든 도시를 돌아서 int보다 작다.
+노선은 하나가 아닐수도 있다. -> 주의
  */
 
 class BjSolution {
     final int INF = (int) Math.pow(10, 9);
 
-    public void solution(int vNum, int sNum, String[] edges) {
-        //인접 리스트 사용
-        List<List<Edge>> graph = new ArrayList<>();
-        //0번째 포함
-        for (int i = 0; i < vNum + 1; i++) {
-            graph.add(new ArrayList<>());
+    public void solution(int cityNum, int busNum, String[] edges) {
+        //방향 리스트 정의
+        List<List<Edge>> map = new ArrayList<>();
+        for (int i = 0; i < cityNum + 1; i++) {
+            map.add(new ArrayList<>());
         }
 
         for (String edge : edges) {
@@ -59,47 +62,55 @@ class BjSolution {
             int to = Integer.parseInt(split[1]);
             int weight = Integer.parseInt(split[2]);
 
-            //방향 그래프
-            graph.get(from).add(new Edge(to, weight));
+            map.get(from).add(new Edge(to, weight));
         }
 
-        //다익스트라 배열 정리
-        int[] weights = new int[vNum + 1];
-        Arrays.fill(weights, INF);
+        int[][] arr = new int[cityNum + 1][cityNum + 1];
 
-        weights[sNum] = 0;
+        //2차원 배열 초기화
+        for (int from = 1; from < cityNum + 1; from++) {
+            for (int to = 1; to < cityNum + 1; to++) {
+                if (from == to) {
+                    arr[from][to] = 0;
+                    continue;
+                }
+                arr[from][to] = INF;
+            }
+        }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
-
-        pq.add(new int[]{sNum, 0});
-
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int curNum = cur[0];
-            int curWeight = cur[1];
-
-            if (weights[curNum] < curWeight) continue;
-
-            for (Edge edge : graph.get(curNum)) {
+        //List 통해서 추가적으로 초기화
+        for (int from = 1; from < cityNum + 1; from++) {
+            List<Edge> list = map.get(from);
+            for (Edge edge : list) {
                 int to = edge.to;
-                int weight = edge.weight;
+                int cost = edge.cost;
 
-                if (weights[to] > curWeight + weight) {
-                    weights[to] = curWeight + weight;
-                    pq.add(new int[]{to, weights[to]});
+                arr[from][to] = Math.min(arr[from][to], cost);
+            }
+        }
+
+        //floydWarshall
+        for (int mid = 1; mid < cityNum + 1; mid++) {
+            for (int from = 1; from < cityNum + 1; from++) {
+                for (int to = 1; to < cityNum + 1; to++) {
+                    if (arr[from][to] > arr[from][mid] + arr[mid][to]) {
+                        arr[from][to] = arr[from][mid] + arr[mid][to];
+                    }
                 }
             }
         }
 
+        //1부터 찍는다
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < weights.length; i++) {
-            int val = weights[i];
-            if (val == INF) {
-                sb.append("INF").append("\n");
-                continue;
-            }
 
-            sb.append(val).append("\n");
+        for (int i = 1; i < cityNum + 1; i++) {
+            for (int j = 1; j < cityNum + 1; j++) {
+                int val = arr[i][j];
+                if (val == INF) val = 0;
+
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
         }
 
         System.out.println(sb);
@@ -108,11 +119,11 @@ class BjSolution {
 
 class Edge {
     int to;
-    int weight;
+    int cost;
 
-    public Edge(int to, int weight) {
+    public Edge(int to, int cost) {
         this.to = to;
-        this.weight = weight;
+        this.cost = cost;
     }
 }
 
