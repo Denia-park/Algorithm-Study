a1f388768c1be35737fda367d05c2c7af4371e0f
HyunggiPark <phg9587@naver.com>
2023-06-04 13:19:01 +0900

    코테 복습 - 최단 경로 1753

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -1,14 +1,11 @@
 package CodingTest.Baekjoon;
 
-//최소 스패닝 트리 1197
+//최단 경로 1753
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -18,6 +15,7 @@ public class Main {
         String[] split = br.readLine().split(" ");
         int vNum = Integer.parseInt(split[0]);
         int eNum = Integer.parseInt(split[1]);
+        int sNum = Integer.parseInt(br.readLine());
 
         String[] edges = new String[eNum];
 
@@ -25,79 +23,96 @@ public class Main {
             edges[i] = br.readLine();
         }
 
-        sol.solution(vNum, edges);
+        sol.solution(vNum, sNum, edges);
     }
 }
 
 /*
 아이디어
-최소 스패닝 트리
-- 크루스칼
-- 프림
-    - 한 노드 선택
-    - 노드의 모든 Edge Heap 넣기
-    - 연결된 노드가 방문한적 있는지 체크
-    - 없으면 가중치 추가, 방문 체크
-    - 해당 노드에서 다시 1번부터 수행 -> 방문한 노드는 제외하자.
+다익스트라
 
 - 출력
-최소 가중치
+배열을 모두 출력
 
 시간복잡도
-O(ELogE)
+O(ELogV)
 
 자료구조
-출력값은 제한조건에 의해 int
-방문 처리 필요 int[]
+가중치가 10이하의 자연수 이므로 가중치는 int 사용
+다익스트라 이므로 PQ 사용
  */
 
 class BjSolution {
-    public void solution(int vNum, String[] edges) {
-        int ans = 0;
-        boolean[] visited = new boolean[vNum + 1];
+    final int INF = (int) Math.pow(10, 9);
 
-        //인접 리스트 만들기
-        List<List<int[]>> map = new ArrayList<>();
-
-        //계산을 편리하기 위해 0번째도 같이 넣기
+    public void solution(int vNum, int sNum, String[] edges) {
+        //인접 리스트 사용
+        List<List<Edge>> graph = new ArrayList<>();
+        //0번째 포함
         for (int i = 0; i < vNum + 1; i++) {
-            map.add(new ArrayList<>());
+            graph.add(new ArrayList<>());
         }
 
         for (String edge : edges) {
-            String[] tempSplit = edge.split(" ");
-            int v1 = Integer.parseInt(tempSplit[0]);
-            int v2 = Integer.parseInt(tempSplit[1]);
-            int weight = Integer.parseInt(tempSplit[2]);
+            String[] split = edge.split(" ");
+            int from = Integer.parseInt(split[0]);
+            int to = Integer.parseInt(split[1]);
+            int weight = Integer.parseInt(split[2]);
 
-            map.get(v1).add(new int[]{v2, weight});
-            map.get(v2).add(new int[]{v1, weight});
+            //방향 그래프
+            graph.get(from).add(new Edge(to, weight));
         }
 
+        //다익스트라 배열 정리
+        int[] weights = new int[vNum + 1];
+        Arrays.fill(weights, INF);
+
+        weights[sNum] = 0;
+
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
-        //시작 노드, 노드 Num, 노드 Weight
-        pq.add(new int[]{1, 0});
+
+        pq.add(new int[]{sNum, 0});
 
         while (!pq.isEmpty()) {
-            int[] curNode = pq.poll();
+            int[] cur = pq.poll();
+            int curNum = cur[0];
+            int curWeight = cur[1];
 
-            int curNodeNum = curNode[0];
-            int curWeight = curNode[1];
+            if (weights[curNum] < curWeight) continue;
 
-            if (visited[curNodeNum]) continue;
-            visited[curNodeNum] = true;
-            ans += curWeight;
+            for (Edge edge : graph.get(curNum)) {
+                int to = edge.to;
+                int weight = edge.weight;
 
-            for (int[] node : map.get(curNodeNum)) {
-                int nextNodeNum = node[0];
-                int nextNodeWeight = node[1];
-                if (visited[nextNodeNum]) continue;
-
-                pq.add(new int[]{nextNodeNum, nextNodeWeight});
+                if (weights[to] > curWeight + weight) {
+                    weights[to] = curWeight + weight;
+                    pq.add(new int[]{to, weights[to]});
+                }
             }
         }
 
-        System.out.println(ans);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < weights.length; i++) {
+            int val = weights[i];
+            if (val == INF) {
+                sb.append("INF").append("\n");
+                continue;
+            }
+
+            sb.append(val).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
+
+class Edge {
+    int to;
+    int weight;
+
+    public Edge(int to, int weight) {
+        this.to = to;
+        this.weight = weight;
     }
 }
 
