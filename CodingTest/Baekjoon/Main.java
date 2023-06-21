e4e8ba833b81983926f49109a03a7819f2f9e326
HyunggiPark <phg9587@naver.com>
2023-06-21 19:59:38 +0900

    코테 - 백준 빗물 14719

--- CodingTest/Baekjoon/Main.java
+++ CodingTest/Baekjoon/Main.java
@@ -1,13 +1,11 @@
 package CodingTest.Baekjoon;
 
-//플로이드 11404
+//빗물 14719
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Deque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -19,84 +17,59 @@ public class Main {
         int row = inputs[0];
         int col = inputs[1];
 
-        int[][] matrix = new int[row][col];
+        int[] heightArr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        for (int r = 0; r < row; r++) {
-            int[] rows = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            System.arraycopy(rows, 0, matrix[r], 0, col);
-        }
-
-        sol.solve(row, col, matrix);
+        sol.solve(row, col, heightArr);
     }
 }
 
-//아이디어
-
-//시간복잡도
-
-//자료구조
-
 class BjSolution {
-    int[] dR = {0, 1, 0, -1};
-    int[] dC = {1, 0, -1, 0};
-    boolean[][] visited;
+    public void solve(int row, int col, int[] heightArr) {
+        int answer = 0;
 
-    public void solve(int row, int col, int[][] matrix) {
-        int count = 0;
-        int maxSize = 0;
-        visited = new boolean[row][col];
+        int[][] map = new int[row][col];
+        int cIdx = 0;
+
+        for (int height : heightArr) {
+            int rIdx = row - 1;
+            for (int count = 0; count < height; count++) {
+                map[rIdx][cIdx] = 1;
+                rIdx--;
+            }
+
+            cIdx++;
+        }
 
         for (int r = 0; r < row; r++) {
-            for (int c = 0; c < col; c++) {
-                //1이 아니면 탐색X
-                if (matrix[r][c] == 0) continue;
-                //이미 방문햇으면 탐색X
-                if (visited[r][c]) continue;
+            //시작 값 초기화, 벽이 없는 상태
+            boolean isLeftWallClosed = false;
+            //빗물 저정할 값
+            int tempSaveWater = 0;
 
-                count++;
-                maxSize = Math.max(bfs(r, c, matrix), maxSize);
+            for (int c = 0; c < col; c++) {
+                //벽이 있으면
+                //벽 상태 초기화 + 이전에 고인 빗물이 있으면 answer에 추가
+                if (isWall(map, r, c)) {
+                    if (isLeftWallClosed) {
+                        answer += tempSaveWater;
+                        tempSaveWater = 0;
+                    }
+
+                    isLeftWallClosed = true;
+                } else {
+                    //지금이 비었고, 이전에 벽이 있었으면 비가 고임
+                    if (isLeftWallClosed) {
+                        tempSaveWater++;
+                    }
+                }
             }
         }
 
-        System.out.println(count);
-        System.out.println(maxSize);
+        System.out.println(answer);
     }
 
-    private int bfs(int r, int c, int[][] matrix) {
-        Deque<int[]> dq = new ArrayDeque<>();
-        dq.addLast(new int[]{r, c});
-        visited[r][c] = true;
-        int maxSize = 1;
-
-        while (!dq.isEmpty()) {
-            int[] cur = dq.pollFirst();
-            int cR = cur[0];
-            int cC = cur[1];
-
-            for (int i = 0; i < 4; i++) {
-                int newR = cR + dR[i];
-                int newC = cC + dC[i];
-
-                //map 벗어나면 탐색 X
-                if (newR < 0 || newR >= matrix.length || newC < 0 || newC >= matrix[0].length)
-                    continue;
-
-                //0이면 탐색 X
-                if (matrix[newR][newC] == 0)
-                    continue;
-
-                //이미 방문했으면 탐색 X
-                if (visited[newR][newC])
-                    continue;
-
-                //방문처리 + 사이즈 1 추가
-                dq.addLast(new int[]{newR, newC});
-                maxSize++;
-                visited[newR][newC] = true;
-            }
-        }
-
-        return maxSize;
+    private boolean isWall(final int[][] map, final int r, final int c) {
+        return map[r][c] == 1;
     }
 }
 
