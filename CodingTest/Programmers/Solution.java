c3020fe7b04e1d7409a522fd489b4aa7ca70a802
HyunggiPark <phg9587@naver.com>
2023-08-13 11:34:10 +0900

    프로그래머스 깊이/너비 우선 탐색(DFS/BFS) 여행경로 - 1차 풀이 완료 - 1번 항목 틀림

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -2,71 +2,92 @@ package CodingTest.Programmers;
 
 /*
 아이디어
-1. Map에 List를 넣어서 해당 경로에서 도착할 수 있는 모든 경로 정리
-2. ICN 부터 시작해서 모든 경로를 탐색
-3. 도착할때마다 List에 저장 후 마지막에 변환
+2. ICN 부터 시작
+1. 모든 경우의 수를 돌면서 경로 탐색
 
 시간복잡도
-N번 돌면 끝나지 않나 ?
 
 자료구조
 
  */
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
+    Deque<String> ticketDeque;
     private String START_DEPARTURE = "ICN";
-    private List<String> answer = new ArrayList<>();
-    private Map<String, List<String>> map = new HashMap<>();
-    private Map<String, Integer> haveToVisitMap = new HashMap<>();
+    private List<List<String>> answer = new ArrayList<>();
+    private Map<String, List<Ticket>> map = new HashMap<>();
+    private int ticketCount = 0;
 
     public String[] solution(String[][] tickets) {
         answer = new ArrayList<>();
         map = new HashMap<>();
-        haveToVisitMap = new HashMap<>();
+        ticketCount = tickets.length;
+        ticketDeque = new ArrayDeque<>();
+        ticketDeque.add(START_DEPARTURE);
 
         for (String[] ticket : tickets) {
             final String departure = ticket[0];
             final String arrival = ticket[1];
 
             //검색해서 List가 존재하면 거기에 추가, 없으면 새로 생성
-            final List<String> arrivalList = map.getOrDefault(departure, new ArrayList<>());
-            arrivalList.add(arrival);
+            final List<Ticket> arrivalList = map.getOrDefault(departure, new ArrayList<>());
+            arrivalList.add(new Ticket(arrival));
             map.put(departure, arrivalList);
         }
 
-        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
-            haveToVisitMap.put(entry.getKey(), 0);
-            entry.getValue().sort(null);
-        }
+        dfs(START_DEPARTURE, ticketDeque);
 
-        dfs(START_DEPARTURE);
+        answer.sort((o1, o2) -> {
+            for (int idx = 0; idx < o1.size(); idx++) {
+                final String s1 = o1.get(idx);
+                final String s2 = o2.get(idx);
 
-        final String[] array = answer.toArray(new String[answer.size()]);
-        return array;
+                if (s1.compareTo(s2) != 0) {
+                    return s1.compareTo(s2);
+                }
+            }
+            return 0;
+        });
+
+        return answer.get(0).toArray(new String[]{});
     }
 
-    private void dfs(String departure) {
-        answer.add(departure);
-        final List<String> arrivals = map.get(departure);
+    private void dfs(String departure, Deque<String> ticketDeque) {
+        final List<Ticket> arrivals = map.get(departure);
+
+        if (ticketDeque.size() == ticketCount + 1) {
+            answer.add(new ArrayList<>(ticketDeque));
+            return;
+        }
 
         if (arrivals == null) {
             return;
         }
 
-        for (int idx = 0; idx < arrivals.size(); idx++) {
-            final int checkIdx = haveToVisitMap.get(departure);
-            if (idx < checkIdx) {
+        for (final Ticket ticket : arrivals) {
+            final String arrival = ticket.arrival;
+            final boolean visited = ticket.visited;
+
+            if (visited) {
                 continue;
             }
 
-            haveToVisitMap.put(departure, checkIdx + 1);
+            ticket.visited = true;
+            ticketDeque.addLast(arrival);
+            dfs(arrival, ticketDeque);
+            ticket.visited = false;
+            ticketDeque.removeLast();
+        }
+    }
 
-            dfs(arrivals.get(idx));
+    class Ticket {
+        String arrival;
+        boolean visited;
+
+        public Ticket(final String arrival) {
+            this.arrival = arrival;
         }
     }
 }
