1900671b1a34e7bd98a6be0f7edb40f0f8c818e0
HyunggiPark <phg9587@naver.com>
2023-10-01 15:58:53 +0900

    leetcode Find the Index of the First Occurrence in a String - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -4,27 +4,13 @@ public class Quiz {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.removeDuplicates(new int[]{1}));
-        System.out.println(solution.removeDuplicates(new int[]{1, 1, 2}));
-        System.out.println(solution.removeDuplicates(new int[]{1, 2, 3}));
-        System.out.println(solution.removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}));
+        System.out.println(solution.strStr("sadbutsad", "sad"));
+        System.out.println(solution.strStr("leetcode", "leeto"));
     }
 }
 
 class Solution {
-    //0번째 1, 1번째 1 이면 -> insertIndex 유지, diffValue 변경
-    //0번째 1, 1번째 2 이면 -> insertIndex ++, diffValue 변경
-    public int removeDuplicates(int[] nums) {
-        int insertIndex = 1; //처음에 0번째는 고정이므로, 첫번째부터 시작
-
-        for (int i = 1; i < nums.length; i++) {
-
-            if (nums[i] != nums[i - 1]) {
-                nums[insertIndex] = nums[i];
-                insertIndex++;
-            }
-        }
-
-        return insertIndex;
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
     }
 }
