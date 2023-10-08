e2183eb5fd24cfbe8af48a09151561a363fd13ec
HyunggiPark <phg9587@naver.com>
2023-10-08 13:12:14 +0900

    leetcode Rearrange Characters to Make Target String - 1차 풀이 완료 - 시간과 메모리 사용이 많다.

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,16 +1,52 @@
 package CodingTest.LeetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Quiz {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.strStr("sadbutsad", "sad"));
-        System.out.println(solution.strStr("leetcode", "leeto"));
+        System.out.println(solution.rearrangeCharacters("ilovecodingonleetcode", "code") == 2);
+        System.out.println(solution.rearrangeCharacters("abcba", "abc") == 1);
+        System.out.println(solution.rearrangeCharacters("abbaccaddaeea", "aaaaa") == 1);
     }
 }
 
 class Solution {
-    public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+    public int rearrangeCharacters(String s, String target) {
+        int answer = 0;
+
+        Map<Character, Integer> sMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        Map<Character, Integer> tMap = new HashMap<>();
+        for (int i = 0; i < target.length(); i++) {
+            tMap.put(target.charAt(i), tMap.getOrDefault(target.charAt(i), 0) + 1);
+        }
+
+        while (true) {
+            for (Map.Entry<Character, Integer> entry : tMap.entrySet()) {
+                //tMap의 값이 다 있어야 개수를 셀수있다.
+                final Character key = entry.getKey();
+                final Integer tValue = entry.getValue();
+
+                if (sMap.containsKey(key)) {
+                    final Integer sValue = sMap.get(key);
+
+                    if (sValue >= tValue) {
+                        sMap.put(key, sValue - tValue);
+                    } else {
+                        return answer;
+                    }
+                } else {
+                    return answer;
+                }
+            }
+
+            answer++;
+        }
     }
 }
