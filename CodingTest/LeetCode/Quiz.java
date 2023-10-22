abffabff1806374c8e89a829e6dc73dc8ed69363
HyunggiPark <phg9587@naver.com>
2023-10-22 12:34:26 +0900

    leetcode Length of Last Word - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -4,32 +4,20 @@ public class Quiz {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.searchInsert(new int[]{1, 3, 5, 6}, 5) == 2);
-        System.out.println(solution.searchInsert(new int[]{1, 3, 5, 6}, 2) == 1);
-        System.out.println(solution.searchInsert(new int[]{1, 3, 5, 6}, 7) == 4);
-        System.out.println(solution.searchInsert(new int[]{1}, 0) == 0);
-        System.out.println(solution.searchInsert(new int[]{1}, 2) == 1);
-        System.out.println(solution.searchInsert(new int[]{1, 3}, 2) == 1);
+        System.out.println(solution.lengthOfLastWord("Hello World") == 5);
+        System.out.println(solution.lengthOfLastWord("   fly me   to   the moon  ") == 4);
+        System.out.println(solution.lengthOfLastWord("luffy is still joyboy") == 6);
     }
 }
 
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-        //이분탐색
-        int st = 0;
-        int ed = nums.length - 1;
+    public int lengthOfLastWord(String s) {
+        final String trimmed = s.trim();
 
-        while (st <= ed) {
-            int mid = st + (ed - st) / 2; // 이렇게 하면 int overflow를 피할 수 있다. 그러므로 이게 더 버그 없는 코드이다.
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                st = mid + 1;
-            } else {
-                ed = mid - 1;
-            }
-        }
+        final int firstCharIndex = trimmed.lastIndexOf(" ") + 1;
 
-        return st;
+        final int trimLength = trimmed.length();
+
+        return trimLength - firstCharIndex;
     }
 }
