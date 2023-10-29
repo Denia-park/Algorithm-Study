24344db46ff154ed7632ef40f85eb3181505f6fc
HyunggiPark <phg9587@naver.com>
2023-10-29 15:04:42 +0900

    leetcode Longest Substring Without Repeating Characters - 3차 풀이 완료 - 정답 보고 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,7 +1,7 @@
 package CodingTest.LeetCode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Quiz {
     public static void main(String[] args) {
@@ -16,28 +16,29 @@ public class Quiz {
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
         final char[] charArray = s.toCharArray();
-
+        int left = 0;
         int maxSize = 0;
-        int curSize = 0;
-        Map<Character, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < charArray.length; i++) {
-            char ch = charArray[i];
+        for (int right = 0; right < charArray.length; right++) {
+            char ch = charArray[right];
 
-            //map에 있는지 확인
-            if (map.containsKey(ch)) {
-                map.clear();
-                curSize = 1;
-            } else {
-                curSize++;
+            //set에 있는지 확인
+            if (set.contains(ch)) {
+                //set에 존재하면, 없어질때까지 left를 땡긴다.
+                while (set.contains(ch)) {
+                    set.remove(charArray[left]);
+                    left++;
+                }
             }
 
-            //없으면 길이 추가
-            map.put(ch, 1);
-            maxSize = Math.max(maxSize, curSize);
-        }
+            //  set에 추가한다.
+            set.add(ch);
 
+            //  Max값을 갱신한다.
+            maxSize = Math.max(maxSize, right - left + 1);
+        }
 
         return maxSize;
     }
