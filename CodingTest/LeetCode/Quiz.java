0892bf00785a6b868b5a058269d503714442167f
HyunggiPark <phg9587@naver.com>
2023-10-30 13:17:44 +0900

    leetcode Longest Repeating Character Replacement - 정답 보고 풀이

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,34 +1,47 @@
 package CodingTest.LeetCode;
 
-import java.util.Arrays;
-
 public class Quiz {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.lengthOfLongestSubstring("abcabcbb") == 3);
-        System.out.println(solution.lengthOfLongestSubstring("bbbbb") == 1);
-        System.out.println(solution.lengthOfLongestSubstring("pwwkew") == 3);
-        System.out.println(solution.lengthOfLongestSubstring("aab") == 2);
+        System.out.println(solution.characterReplacement("ABAB", 2) == 4);
+        System.out.println(solution.characterReplacement("AABABBA", 1) == 4);
     }
 }
 
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        int maxLength = 0;
-        int[] charIndex = new int[128];
-        Arrays.fill(charIndex, -1);
-        int left = 0;
-
-        for (int right = 0; right < n; right++) {
-            if (charIndex[s.charAt(right)] >= left) {
-                left = charIndex[s.charAt(right)] + 1;
-            }
-            charIndex[s.charAt(right)] = right;
-            maxLength = Math.max(maxLength, right - left + 1);
+    public int characterReplacement(String s, int k) {
+        if (s.length() <= k || s.length() == k - 1) {
+            return s.length();
         }
 
-        return maxLength;
+        char[] chars = s.toCharArray();
+
+        char[] charArray = new char[26];
+
+        int left = 0;
+        int maxCharCount = 0;
+        for (int right = 0; right < chars.length; right++) {
+            final int ch = chars[right] - 'A';
+
+            charArray[ch]++;
+
+            int curCharCount = charArray[ch];
+
+            maxCharCount = Math.max(maxCharCount, curCharCount);
+
+            int curStrLen = right - left + 1;
+
+            //현재 길이에서 가장 많은 ch의 수를 빼면, 교체해야 할 나머지 ch의 수
+            //이게 k보다 크면 안되므로, left를 옮긴다.
+            if (curStrLen - maxCharCount > k) {
+                charArray[chars[left] - 'A']--;
+                left++;
+            }
+        }
+
+        //right == chars.length
+        //sliding window의 너비를 구한다. => chars.length - left;
+        return chars.length - left;
     }
 }
