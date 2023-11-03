0740887940c6f94ca5f995d4273eb4e180e96932
HyunggiPark <phg9587@naver.com>
2023-11-03 12:55:38 +0900

    leetcode  Integer to Roman - 1차 풀이 완료 - 속도 및 메모리 사용량이 많다

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,36 +1,78 @@
 package CodingTest.LeetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Quiz {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+        System.out.println(solution.intToRoman(3).equals("III"));
+//        System.out.println(solution.intToRoman(58).equals("LVIII"));
+        System.out.println(solution.intToRoman(58));
+//        System.out.println(solution.intToRoman(1994).equals("MCMXCIV"));
+        System.out.println(solution.intToRoman(1994));
     }
 }
 
 class Solution {
-    public int maxArea(int[] height) {
-        int max = 0;
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
 
-        //좌우에 포인터를 두고 줄여가면서 값을 계산한다.
-        int start = 0;
-        int end = height.length - 1;
+        Map<Integer, String> map = new HashMap<>();
+        addRomanData(map);
 
-        while (start < end) {
-            //물을 담을 수 있는 최대 높이 계산 (두개의 벽중에 높이가 낮은 벽 선택)
-            int curHeight = height[start] < height[end] ? height[start] : height[end];
-            //두개의 인덱스 차이가 너비
-            int curWidth = end - start;
+        //숫자 하나씩 가져오고, num의 자리수를 파악
+        String numStr = String.valueOf(num);
+        int digit = 1;
+        for (int idx = numStr.length() - 1; idx >= 0; idx--) {
+            char ch = numStr.charAt(idx);
 
-            max = max > curHeight * curWidth ? max : curHeight * curWidth;
+            int numValue = ch - '0';
 
-            if (height[start] < height[end]) {
-                start++;
-            } else {
-                end--;
+            StringBuilder tempBuilder = new StringBuilder();
+
+            if (numValue != 0) {
+                if (1 < numValue && numValue < 4) { //2, 3
+                    for (int i = 0; i < numValue; i++) {
+                        tempBuilder.insert(0, map.get(1 * digit));
+                    }
+                } else if (5 < numValue && numValue < 9) { //6, 7, 8
+                    int numMinus5 = numValue - 5;
+                    for (int i = 0; i < numMinus5; i++) {
+                        tempBuilder.insert(0, map.get(1 * digit));
+                    }
+                    tempBuilder.insert(0, map.get(5 * digit));
+                } else {
+                    tempBuilder.insert(0, map.get(numValue * digit));
+                }
+
+                sb.insert(0, tempBuilder);
             }
+
+            digit *= 10;
         }
 
-        return max;
+        //해당 하는 값을 붙인다
+        return sb.toString();
+    }
+
+    private void addRomanData(final Map<Integer, String> map) {
+        map.put(1, "I");
+        map.put(4, "IV");
+        map.put(5, "V");
+        map.put(9, "IX");
+
+        map.put(10, "X");
+        map.put(40, "XL");
+        map.put(50, "L");
+        map.put(90, "XC");
+
+        map.put(100, "C");
+        map.put(400, "CD");
+        map.put(500, "D");
+        map.put(900, "CM");
+
+        map.put(1000, "M");
     }
 }
