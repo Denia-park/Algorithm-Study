ff07cde20dd45570097eec13b22d110de5009981
HyunggiPark <phg9587@naver.com>
2023-11-05 13:25:34 +0900

    leetcode Generate Parentheses - 정답 보고 풀이 완료 - 진짜 엄청난 풀이다.. 상상도 못함

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,9 +1,7 @@
 package CodingTest.LeetCode;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Quiz {
     public static void main(String[] args) {
@@ -16,38 +14,30 @@ public class Quiz {
 }
 
 class Solution {
+    private List<String> result;
+    private int maxBracketsNum;
+
     public List<String> generateParenthesis(int n) {
-        if (n == 1) {
-            return List.of("()");
+        result = new ArrayList<>();
+        this.maxBracketsNum = n;
+
+        addParenthesis("", 0, 0);
+
+        return result;
+    }
+
+    private void addParenthesis(final String curStr, final int openBrackets, final int closeBrackets) {
+        //괄호는 () 이렇게 생김 -> 문자열이 2배. 즉 maxBracketsNum 2개면 괄호 쌍이 2개 이므로 문자열 길이가 2*2 = 4 이다.
+        if (curStr.length() == maxBracketsNum * 2) {
+            result.add(curStr);
+            return;
         }
 
-        Set<String> set = new HashSet<>();
-
-        List<String> list = List.of("()");
-
-        for (int i = 2; i < n + 1; i++) {
-            for (String str : list) {
-                //str에서 ()를 찾고 교환하자.
-                int index = str.indexOf("()");
-
-                while (index != -1) {
-                    StringBuilder sb = new StringBuilder(str);
-                    sb.replace(index, index + 2, "(())");
-                    set.add(sb.toString());
-
-                    StringBuilder sb2 = new StringBuilder(str);
-                    sb2.replace(index, index + 2, "()()");
-                    set.add(sb2.toString());
-
-                    index = str.indexOf("()", index + 1);
-                }
-            }
-
-            list = new ArrayList<>(set);
-            set.clear();
+        if (openBrackets < maxBracketsNum) {
+            addParenthesis(curStr + "(", openBrackets + 1, closeBrackets);
         }
-
-        //set to list
-        return list;
+        if (closeBrackets < openBrackets) {
+            addParenthesis(curStr + ")", openBrackets, closeBrackets + 1);
+        }
     }
 }
