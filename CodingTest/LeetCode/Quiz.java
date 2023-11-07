d7659fe5c1a47ae12d603540919cefcff43e4c79
HyunggiPark <phg9587@naver.com>
2023-11-07 10:59:11 +0900

    leetcode Permutation in String - 정답 보고 풀이 - 슬라이딩 윈도우가 이렇게나 쓸모가 많은지 몰랐다.

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -15,60 +15,36 @@ public class Quiz {
 
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) {
+        int s1Len = s1.length();
+        int s2Len = s2.length();
+
+        if (s1Len > s2Len) {
             return false;
         }
 
-        int s1Len = s1.length();
         int[] s1Arr = new int[26];
+        int[] s2Arr = new int[26];
 
-        //기존 단어를 세어둔다.
-        countChar(s1, s1Arr);
+        for (int i = 0; i < s1Len; i++) {
+            s1Arr[s1.charAt(i) - 'a']++;
+        }
+        for (int start = 0; start < s1Len; start++) {
+            s2Arr[s2.charAt(start) - 'a']++;
+        }
 
-        //s2에서 s1의 첫글자를 찾고, s1에 해당하는 길이만큼 떼어내서 비교한다.
-        int searchIdx = -1;
-        String firstChar = s1.charAt(0) + "";
-        while (true) {
-            searchIdx = s2.indexOf(firstChar, searchIdx + 1);
-            if (searchIdx == -1) {
-                break;
+        if (Arrays.equals(s1Arr, s2Arr)) {
+            return true;
+        }
+
+        for (int end = s1Len, start = 0; end < s2Len; end++, start++) {
+            s2Arr[s2.charAt(end) - 'a']++;
+            s2Arr[s2.charAt(start) - 'a']--;
+
+            if (Arrays.equals(s1Arr, s2Arr)) {
+                return true;
             }
-
-            //위쪽으로 찾기
-            final int upStartIdx = searchIdx - (s1Len - 1);
-            if (-1 < upStartIdx) {
-                String tempStr = s2.substring(upStartIdx, searchIdx + 1);
-
-                int[] s2Arr = new int[26];
-                countChar(tempStr, s2Arr);
-
-                if (Arrays.equals(s1Arr, s2Arr)) {
-                    return true;
-                }
-            }
-
-            //아래쪽으로 찾기
-            final int downEndIdx = searchIdx + (s1Len - 1);
-            if (downEndIdx < s1Len) {
-                String tempStr = s2.substring(searchIdx, downEndIdx + 1);
-
-                int[] s2Arr = new int[26];
-                countChar(tempStr, s2Arr);
-
-                if (Arrays.equals(s1Arr, s2Arr)) {
-                    return true;
-                }
-            }
-
-            searchIdx++;
         }
 
         return false;
-    }
-
-    private void countChar(final String str, final int[] arr) {
-        for (int i = 0; i < str.length(); i++) {
-            arr[str.charAt(i) - 'a']++;
-        }
     }
 }
