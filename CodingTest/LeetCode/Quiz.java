49b68d748488d5af481f37afffc078958e73d6b2
HyunggiPark <phg9587@naver.com>
2023-11-08 18:25:19 +0900

    leetcode Permutations - 1차 풀이 완료 - Runtime이 느리다.

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,50 +1,68 @@
 package CodingTest.LeetCode;
 
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
 public class Quiz {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.checkInclusion("ab", "eidbaooo"));
-        System.out.println(solution.checkInclusion("ab", "eidboaoo"));
-        System.out.println(solution.checkInclusion("ab", "a"));
-        System.out.println(solution.checkInclusion("ky", "ainwkckifykxlribaypk"));
+        System.out.println(solution.permute(new int[]{1, 2, 3}));
+        System.out.println(solution.permute(new int[]{0, 1}));
+        System.out.println(solution.permute(new int[]{1}));
     }
 }
 
 class Solution {
-    public boolean checkInclusion(String s1, String s2) {
-        int s1Len = s1.length();
-        int s2Len = s2.length();
+    private List<List<Integer>> answer;
+    private List<Wrapper> nums;
 
-        if (s1Len > s2Len) {
-            return false;
+    public List<List<Integer>> permute(int[] params) {
+        answer = new ArrayList<>();
+        nums = new ArrayList<>();
+
+        for (int param : params) {
+            nums.add(new Wrapper(param, false));
         }
 
-        int[] s1Arr = new int[26];
-        int[] s2Arr = new int[26];
+        final Deque<Wrapper> dq = new ArrayDeque<>();
+        dfs(dq);
 
-        for (int i = 0; i < s1Len; i++) {
-            s1Arr[s1.charAt(i) - 'a']++;
-        }
-        for (int start = 0; start < s1Len; start++) {
-            s2Arr[s2.charAt(start) - 'a']++;
-        }
+        return answer;
+    }
 
-        if (Arrays.equals(s1Arr, s2Arr)) {
-            return true;
-        }
-
-        for (int end = s1Len, start = 0; end < s2Len; end++, start++) {
-            s2Arr[s2.charAt(end) - 'a']++;
-            s2Arr[s2.charAt(start) - 'a']--;
-
-            if (Arrays.equals(s1Arr, s2Arr)) {
-                return true;
+    private void dfs(final Deque<Wrapper> dq) {
+        if (dq.size() == nums.size()) {
+            List<Integer> list = new ArrayList<>();
+            for (Wrapper wrapper : dq) {
+                list.add(wrapper.value);
             }
+            answer.add(list);
+            return;
         }
 
-        return false;
+        for (Wrapper wrapper : nums) {
+            if (wrapper.visited) {
+                continue;
+            }
+
+            wrapper.visited = true;
+            dq.offer(wrapper);
+            dfs(dq);
+            dq.pollLast();
+            wrapper.visited = false;
+        }
+    }
+}
+
+class Wrapper {
+    int value;
+    boolean visited;
+
+    public Wrapper(final int value, final boolean visited) {
+        this.value = value;
+        this.visited = visited;
     }
 }
