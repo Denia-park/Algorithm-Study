1f0e7d09d7752147e822171ef4a08562da59b49c
HyunggiPark <phg9587@naver.com>
2023-11-11 10:56:11 +0900

    leetcode Count and Say - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -4,31 +4,46 @@ public class Quiz {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.countHomogenous("abbcccaa") == 13);
-        System.out.println(solution.countHomogenous("xy") == 2);
-        System.out.println(solution.countHomogenous("zzzzz") == 15);
-        System.out.println(solution.countHomogenous("oooorppppppppooooobbbjjjjcccccccccccceeeee") == 171);
+//        System.out.println(solution.countAndSay(1)); //1
+        System.out.println(solution.countAndSay(2)); //11
+        System.out.println(solution.countAndSay(3)); //21
+        System.out.println(solution.countAndSay(4)); //1211
     }
 }
 
 class Solution {
-    int powVal = (int) (Math.pow(10, 9) + 7);
-
-    public int countHomogenous(String quizStr) {
-        int sum = 0;
-
-        char[] array = quizStr.toCharArray();
-
-        int start = 0;
-        for (int end = 0; end < array.length; end++) {
-            if (array[start] == array[end]) {
-                sum = ((sum % powVal) + ((end - start + 1) % powVal)) % powVal;
-            } else {
-                start = end;
-                sum++;
-            }
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
         }
 
-        return sum;
+        final String sayString = countAndSay(n - 1);
+
+        StringBuilder result = new StringBuilder();
+        //sayString을 조작한다.
+        //  -subString으로 숫자를 확인하고, 개수를 센다.
+        int start = 0;
+        int count = 0;
+        for (int end = 0; end < sayString.length(); end++) {
+            if (sayString.charAt(start) == sayString.charAt(end)) {
+                //같은 문자이므로 기존 문자에 개수를 더한다.
+                count++;
+            } else {
+                //다른 문자로 바꾸기 전에 기존 문자를 활용하여 계산
+                result.append(makeString(sayString, start, count));
+
+                //새로운 문자이므로 초기화
+                start = end;
+                count = 1;
+            }
+        }
+        //마지막에 계산이 안된 문자열을 고려하여 추가한다.
+        result.append(makeString(sayString, start, count));
+
+        return result.toString();
+    }
+
+    private String makeString(final String sayString, final int start, final int count) {
+        return "" + count + sayString.charAt(start);
     }
 }
