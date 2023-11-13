de4ca9a6782f2d5d107406fb44ec72186ecfe120
HyunggiPark <phg9587@naver.com>
2023-11-13 11:30:40 +0900

    leetcode Sort Vowels in a String - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,64 +1,63 @@
 package CodingTest.LeetCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Quiz {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-//        final int[][] matrix = {{1, 2}, {3, 4}};
-//        final int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-//        solution.rotate(matrix);
-
-        final int[][] matrix2 = {
-                {5, 1, 9, 11},
-                {2, 4, 8, 10},
-                {13, 3, 6, 7},
-                {15, 14, 12, 16}
-        };
-
-        solution.rotate(matrix2);
+        System.out.println(solution.sortVowels("lEetcOde").equals("lEOtcede"));
+        System.out.println(solution.sortVowels("lYmpH").equals("lYmpH"));
     }
 }
 
+//permuation
+//자음 자리 유지
+//모음은 오름차순으로 정렬
+
 class Solution {
-    private int maxSize;
+    public String sortVowels(String s) {
+        List<Vowel> vowels = new ArrayList<>();
+        List<Integer> indexs = new ArrayList<>();
+        StringBuilder sb = new StringBuilder(s);
 
-    public void rotate(int[][] matrix) {
-        maxSize = matrix.length;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
 
-        int[][] matrix2 = new int[maxSize][maxSize];
-
-        for (int row = 0; row < matrix2.length; row++) {
-            System.arraycopy(matrix[row], 0, matrix2[row], 0, matrix[row].length);
-        }
-
-        for (int count = 0; count < maxSize / 2; count++) {
-            //바깥에서부터 시작
-            final int startIdx = count;
-            final int endIdx = maxSize - 1 - count;
-
-            //row 시작 -> col 끝 | 시작에서부터
-            for (int col = startIdx; col <= endIdx; col++) {
-                matrix2[col][endIdx] = matrix[startIdx][col];
-            }
-
-            //col 끝 -> row 끝 | 시작에서부터
-            for (int row = startIdx; row <= endIdx; row++) {
-                matrix2[endIdx][endIdx - row + count] = matrix[row][endIdx];
-            }
-
-            //row 끝 -> col 시작 | 끝에서부터
-            for (int col = endIdx; col >= startIdx; col--) {
-                matrix2[col][startIdx] = matrix[endIdx][col];
-            }
-
-            //col 시작 -> row 시작 | 끝에서부터
-            for (int row = endIdx; row >= startIdx; row--) {
-                matrix2[startIdx][endIdx - row + count] = matrix[row][startIdx];
+            if (isVowel(ch)) {
+                indexs.add(i);
+                vowels.add(new Vowel(ch));
             }
         }
 
-        for (int row = 0; row < matrix2.length; row++) {
-            System.arraycopy(matrix2[row], 0, matrix[row], 0, matrix2[row].length);
+        vowels.sort(null);
+
+        for (int i = 0; i < indexs.size(); i++) {
+            sb.setCharAt(indexs.get(i), vowels.get(i).ch);
+        }
+
+        return sb.toString();
+    }
+
+    private boolean isVowel(final char ch) {
+        return Character.toLowerCase(ch) == 'a'
+                || Character.toLowerCase(ch) == 'e'
+                || Character.toLowerCase(ch) == 'i'
+                || Character.toLowerCase(ch) == 'o'
+                || Character.toLowerCase(ch) == 'u';
+    }
+
+    private class Vowel implements Comparable<Vowel> {
+        char ch;
+
+        public Vowel(final char ch) {
+            this.ch = ch;
+        }
+
+        @Override
+        public int compareTo(final Vowel o) {
+            return Character.compare(this.ch, o.ch);
         }
     }
 }
