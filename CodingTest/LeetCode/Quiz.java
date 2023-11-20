11c95a3c465e99a970d173e4418662eafa37bbef
HyunggiPark <phg9587@naver.com>
2023-11-20 12:04:18 +0900

    leetcode Minimum Amount of Time to Collect Garbage - 1차 풀이 완료 - 구현 문제

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,30 +1,63 @@
 package CodingTest.LeetCode;
 
-import java.util.Arrays;
-
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.reductionOperations(new int[]{5, 1, 3}));
-        System.out.println(solution.reductionOperations(new int[]{1, 1, 1}));
-        System.out.println(solution.reductionOperations(new int[]{1, 1, 2, 2, 3}));
+        System.out.println(solution.garbageCollection(new String[]{"G", "P", "GP", "GG"}, new int[]{2, 4, 3}));
     }
 }
 
+//순서대로
+//모든 집을 들를 필요는 없다.
+//한번에 하나의 트럭만 이동이 가능하다.
 class Solution {
-    public int reductionOperations(final int[] nums) {
-        int count = 0;
+    private final String[] garbageType = new String[]{"M", "P", "G"};
+    private int[] gTravel;
+    private String[] gGarbage;
 
-        Arrays.sort(nums);
+    public int garbageCollection(final String[] garbage, final int[] travel) {
+        gTravel = travel;
+        gGarbage = garbage;
+        int result = 0;
 
-        final int numsLength = nums.length;
-        for (int i = numsLength - 1; i > 0; i--) {
-            if (nums[i] != nums[i - 1]) {
-                count += numsLength - i;
+        //쓰레기 종류 별로 차가 순회한다.
+        for (final String curType : garbageType) {
+            result += collectGarbage(curType);
+        }
+
+        return result;
+    }
+
+    private int collectGarbage(final String curType) {
+        int result = 0;
+
+        int tempDistance = 0;
+
+        for (int homeIdx = 0; homeIdx < gGarbage.length; homeIdx++) {
+            final String curHome = gGarbage[homeIdx];
+            int garbageCount = 0;
+
+            int index = curHome.indexOf(curType);
+            while (index != -1) {
+                garbageCount++;
+                index = curHome.indexOf(curType, index + 1);
+            }
+
+            if (garbageCount > 0) {
+                //거리 더하기.
+                result += tempDistance;
+                tempDistance = 0;
+            }
+
+            result += garbageCount;
+
+            if (homeIdx < gGarbage.length - 1) {
+                //거리 더해주기 - 쓰레기가 존재하는지 안하는지 모르므로 일단 저장만 해둔다.
+                tempDistance += gTravel[homeIdx];
             }
         }
 
-        return count;
+        return result;
     }
 }
