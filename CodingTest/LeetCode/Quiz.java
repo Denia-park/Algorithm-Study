61e222b7d9d4c064bee2d47ac67e6da296c4cb5c
HyunggiPark <phg9587@naver.com>
2023-11-21 20:04:38 +0900

    leetcode Count Nice Pairs in an Array - 1차 풀이 완료 - 시간 초과로 실패

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -4,54 +4,31 @@ public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.garbageCollection(new String[]{"G", "P", "GP", "GG"}, new int[]{2, 4, 3}));
+        System.out.println(solution.countNicePairs(new int[]{42, 11, 1, 97}));
     }
 }
 
-//1. 모든 쓰레기들을 줍는데 걸리는 시간은 garbage 문자열 배열의 모든 문자열 길이의 합과 같다.
-//2. 각 트럭은 본인 종류의 쓰레기가 위치하는 마지막 장소만 알면 트럭의 이동시간을 계산할 수 있다.
 class Solution {
-    private static final int METAL = 0, PAPER = 1, GARBAGE = 2;
-    private final int[] lastIndex = new int[3];
+    public int countNicePairs(final int[] nums) {
+        long answer = 0;
 
-    public int garbageCollection(final String[] garbage, final int[] travel) {
-        int result = 0;
-
-        //모든 쓰레기를 줍는데 걸리는 시간을 구한다.
-        int homeIdx = 0;
-        for (final String str : garbage) {
-            result += str.length();
-
-            //해당 하는 쓰레기를 처리하러 트럭이 어느집 까지 가야하는지 파악한다.
-            for (int charIdx = 0; charIdx < str.length(); charIdx++) {
-                final char curChar = str.charAt(charIdx);
-
-                if (curChar == 'M') {
-                    lastIndex[METAL] = homeIdx;
-                } else if (curChar == 'P') {
-                    lastIndex[PAPER] = homeIdx;
-                } else if (curChar == 'G') {
-                    lastIndex[GARBAGE] = homeIdx;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (isNicePair(nums, i, j)) {
+                    answer++;
                 }
             }
-
-            homeIdx++;
         }
 
-        //거리 값을 계산하기 쉽게 변경한다.
-        //거리를 집끼리의 간견이 아니라 해당 하는 집까지 가는데 걸리는 총 시간으로 바꾼다.
-        for (int i = 1; i < travel.length; i++) {
-            travel[i] = travel[i] + travel[i - 1];
-        }
+        return (int) (answer % (int) (Math.pow(10, 9) + 7));
+    }
 
-        for (final int index : lastIndex) {
-            if (index == 0) {
-                continue;
-            }
+    private boolean isNicePair(final int[] nums, final int i, final int j) {
+        final int num1 = nums[i];
+        final int revNum1 = Integer.valueOf(new StringBuilder(String.valueOf(num1)).reverse().toString());
+        final int num2 = nums[j];
+        final int revNum2 = Integer.valueOf(new StringBuilder(String.valueOf(num2)).reverse().toString());
 
-            result += travel[index - 1];
-        }
-
-        return result;
+        return num1 + revNum2 == revNum1 + num2;
     }
 }
