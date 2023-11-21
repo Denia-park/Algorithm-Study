6802672bf6d930f4d2b63e5cdf0bffcf6a811f60
HyunggiPark <phg9587@naver.com>
2023-11-21 21:06:18 +0900

    leetcode Count Nice Pairs in an Array - 정답 보고 풀이 - 조합으로 구하기 - 곱셈 버전

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,34 +1,54 @@
 package CodingTest.LeetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
         System.out.println(solution.countNicePairs(new int[]{42, 11, 1, 97}));
+        System.out.println(solution.countNicePairs(new int[]{432835222, 112141211, 5408045, 456281503, 283322436, 414281561, 37773, 286505682}));
     }
 }
 
 class Solution {
+    private final double MOD = (Math.pow(10, 9) + 7);
+
     public int countNicePairs(final int[] nums) {
         long answer = 0;
 
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (isNicePair(nums, i, j)) {
-                    answer++;
-                }
-            }
+        //x + rev(y) == y + rev(x) -> x - rev(x) == y - rev(y) -> x - rev(x)가 같은 값 끼리 연산을 해야함
+        final int[] newArr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            newArr[i] = nums[i] - rev(nums[i]);
         }
 
-        return (int) (answer % (int) (Math.pow(10, 9) + 7));
+        final Map<Integer, Integer> map = new HashMap<>();
+        for (final int value : newArr) {
+            map.put(value, map.getOrDefault(value, 0) + 1);
+        }
+
+        for (final Integer value : map.values()) {
+            if (value < 2) {
+                continue;
+            }
+
+            answer = (long) ((answer % MOD + getCombination(value) % MOD) % MOD);
+        }
+
+        return (int) (answer % (int) MOD);
     }
 
-    private boolean isNicePair(final int[] nums, final int i, final int j) {
-        final int num1 = nums[i];
-        final int revNum1 = Integer.valueOf(new StringBuilder(String.valueOf(num1)).reverse().toString());
-        final int num2 = nums[j];
-        final int revNum2 = Integer.valueOf(new StringBuilder(String.valueOf(num2)).reverse().toString());
+    private long getCombination(final Integer value) {
+        if (value % 2 == 0) {
+            return (long) (((value / 2) % MOD * (value - 1) % MOD) % MOD);
+        } else {
+            return (long) (((value) % MOD * ((value - 1) / 2) % MOD) % MOD);
+        }
+    }
 
-        return num1 + revNum2 == revNum1 + num2;
+    private int rev(final int num) {
+        return Integer.parseInt(new StringBuilder(String.valueOf(num)).reverse().toString());
     }
 }
