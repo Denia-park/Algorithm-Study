784bd3af3a73513558cd602f5d9254886e73d883
HyunggiPark <phg9587@naver.com>
2023-11-22 14:04:57 +0900

    leetcode Diagonal Traverse II - 1차 풀이 완료 - 예외 케이스 존재

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,42 +1,72 @@
 package CodingTest.LeetCode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.countNicePairs(new int[]{42, 11, 1, 97}));
-        System.out.println(solution.countNicePairs(new int[]{432835222, 112141211, 5408045, 456281503, 283322436, 414281561, 37773, 286505682}));
+        final int[] diagonalOrder = solution.findDiagonalOrder(List.of(
+                List.of(1, 2, 3),
+                List.of(4, 5, 6),
+                List.of(7, 8, 9))
+        );
+
+        System.out.println("diagonalOrder = " + Arrays.toString(diagonalOrder));
+
+        final int[] diagonalOrder1 = solution.findDiagonalOrder(List.of(
+                List.of(1, 2, 3, 4, 5),
+                List.of(6, 7),
+                List.of(8),
+                List.of(9, 10, 11),
+                List.of(12, 13, 14, 15, 16))
+        );
+
+        System.out.println("diagonalOrder1 = " + Arrays.toString(diagonalOrder1));
     }
 }
 
 class Solution {
-    private final int MOD = (int) (Math.pow(10, 9) + 7);
+    public int[] findDiagonalOrder(final List<List<Integer>> nums) {
+        final List<Integer> answer = new ArrayList<>();
 
-    public int countNicePairs(final int[] nums) {
-        int answer = 0;
+        final int totalCount = nums.stream().mapToInt(List::size).sum();
+        final int rowMax = nums.size() - 1;
+        int addRow = 0;
 
-        //x + rev(y) == y + rev(x) -> x - rev(x) == y - rev(y) -> x - rev(x)가 같은 값 끼리 연산을 해야함
-        final int[] newArr = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            newArr[i] = nums[i] - rev(nums[i]);
+        int row = 0;
+        int col = 0;
+
+        int rowCount = 0;
+
+        while (answer.size() < totalCount) {
+            try {
+                final int value = nums.get(row).get(col);
+
+                answer.add(value);
+            } catch (final Exception ignored) {
+            }
+
+            if (row == 0) {
+                if (rowCount < rowMax) {
+                    rowCount++;
+                }
+
+                row = rowCount;
+                col = addRow;
+                continue;
+            }
+
+            if (row == rowMax) {
+                addRow++;
+            }
+
+            row--;
+            col++;
         }
 
-        final Map<Integer, Integer> map = new HashMap<>();
-        for (final int value : newArr) {
-            final Integer preValue = map.getOrDefault(value, 0);
-
-            answer = (answer % MOD + preValue % MOD) % MOD;
-
-            map.put(value, preValue + 1);
-        }
-
-        return answer;
-    }
-
-    private int rev(final int num) {
-        return Integer.parseInt(new StringBuilder(String.valueOf(num)).reverse().toString());
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
