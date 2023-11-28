3579f94a25fb3e66fb2fa884335b97155fdbd67b
HyunggiPark <phg9587@naver.com>
2023-11-28 21:06:31 +0900

    leetcode Number of Ways to Divide a Long Corridor - 3차 풀이 완료 - 코드를 잘못 넣은 부분이 있어서 시간 초과 났음
    - string.toCharArray().length 를 계속 구하는 코드가 있어서 시간 초과

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -18,19 +18,19 @@ class Solution {
     private final int MOD = 1_000_000_000 + 7;
 
     public int numberOfWays(final String corridor) {
-        long divideCount = 0;
-
         final List<Integer> seatIndexes = new ArrayList<>();
 
-        for (int index = 0; index < corridor.toCharArray().length; index++) {
-            final char ch = corridor.charAt(index);
+        final char[] charArray = corridor.toCharArray();
+        final int corridorLength = charArray.length;
+        for (int index = 0; index < corridorLength; index++) {
+            final char ch = charArray[index];
             if (ch == 'S') {
                 seatIndexes.add(index);
             }
         }
 
         final int seatTotalCount = seatIndexes.size();
-        //홀수면 못 나누므로, 0
+        //0 혹은 홀수면 못 나누므로, 0
         if (seatTotalCount == 0 || seatTotalCount % 2 == 1) {
             return 0;
         }
@@ -40,18 +40,19 @@ class Solution {
             return 1;
         }
 
+
+        long divideCount = 1;
         int multiplesOf3Index = 2;
 
-        divideCount = 1;
         while (multiplesOf3Index < seatTotalCount) {
             final int seatIndex = seatIndexes.get(multiplesOf3Index);
             final int preSeatIndex = seatIndexes.get(multiplesOf3Index - 1);
 
-            divideCount = ((divideCount % MOD) * ((seatIndex - preSeatIndex) % MOD)) % MOD;
+            divideCount = (divideCount * (seatIndex - preSeatIndex)) % MOD;
 
             multiplesOf3Index += 2;
         }
 
-        return (int) (divideCount % MOD);
+        return (int) divideCount;
     }
 }
