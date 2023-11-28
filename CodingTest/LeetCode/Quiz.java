c54670f7a7083ad5060080787c589bfeedb26a08
HyunggiPark <phg9587@naver.com>
2023-11-28 20:45:29 +0900

    leetcode Number of Ways to Divide a Long Corridor - 1차 풀이 완료 - 틀린 경우 존재

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,74 +1,55 @@
 package CodingTest.LeetCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
-        System.out.println(solution.knightDialer(1));
-        System.out.println(solution.knightDialer(2));
-        System.out.println(solution.knightDialer(3));
-        System.out.println(solution.knightDialer(10));
-        System.out.println(solution.knightDialer(3131));
+        System.out.println(solution.numberOfWays("SSPPSPS"));
+        System.out.println(solution.numberOfWays("PPSPSP"));
+        System.out.println(solution.numberOfWays("S"));
     }
 }
 
 class Solution {
     private final int MOD = 1_000_000_000 + 7;
 
-    public int knightDialer(final int n) {
-        if (n == 1) {
-            return 10;
-        }
+    public int numberOfWays(final String corridor) {
+        long divideCount = 0;
 
-        long result = 0;
+        final List<Integer> seatIndexes = new ArrayList<>();
 
-        long[] dialerCount = new long[]{1, 1, 1, 1, 1, 0, 1, 1, 1, 1};
-
-        for (int i = 1; i < n - 1; i++) {
-            final long[] temp = new long[10];
-            for (int j = 0; j < 10; j++) {
-                final long count = dialerCount[j];
-                if (count == 0) {
-                    continue;
-                }
-
-                if (j == 0) {
-                    calculate(temp, count, 4, 6);
-                } else if (j == 1) {
-                    calculate(temp, count, 6, 8);
-                } else if (j == 2) {
-                    calculate(temp, count, 7, 9);
-                } else if (j == 3) {
-                    calculate(temp, count, 4, 8);
-                } else if (j == 4) {
-                    calculate(temp, count, 0, 3, 9);
-                } else if (j == 6) {
-                    calculate(temp, count, 0, 1, 7);
-                } else if (j == 7) {
-                    calculate(temp, count, 2, 6);
-                } else if (j == 8) {
-                    calculate(temp, count, 1, 3);
-                } else if (j == 9) {
-                    calculate(temp, count, 2, 4);
-                }
-            }
-
-            dialerCount = temp;
-        }
-
-        for (int i = 0; i < 10; i++) {
-            if (i == 4 || i == 6) {
-                result = (result + ((dialerCount[i] * 3) % MOD)) % MOD;
-            } else {
-                result = (result + ((dialerCount[i] * 2) % MOD)) % MOD;
+        for (int index = 0; index < corridor.toCharArray().length; index++) {
+            final char ch = corridor.charAt(index);
+            if (ch == 'S') {
+                seatIndexes.add(index);
             }
         }
 
-        return (int) result;
-    }
-
-    private void calculate(final long[] temp, final long count, final int... indexs) {
-        for (final int index : indexs) {
-            temp[index] = (temp[index] + (count % MOD)) % MOD;
+        final int seatTotalCount = seatIndexes.size();
+        //홀수면 못 나누므로, 0
+        if (seatTotalCount == 0 || seatTotalCount % 2 == 1) {
+            return 0;
         }
+
+        //앉는 자리가 2개 이하면, 1
+        if (seatTotalCount == 2) {
+            return 1;
+        }
+
+        int multiplesOf3Index = 2;
+
+        divideCount = 1;
+        while (multiplesOf3Index < seatTotalCount) {
+            final int seatIndex = seatIndexes.get(multiplesOf3Index);
+            final int preSeatIndex = seatIndexes.get(multiplesOf3Index - 1);
+
+            divideCount = ((divideCount % MOD) * ((seatIndex - preSeatIndex) % MOD)) % MOD;
+
+            multiplesOf3Index += 3;
+        }
+
+        return (int) (divideCount % MOD);
     }
 }
