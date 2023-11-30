f8f2cdcdc2798ef58916fd8ed6dcc05373a9e2bf
HyunggiPark <phg9587@naver.com>
2023-11-30 23:06:46 +0900

    leetcode Minimum One Bit Operations to Make Integers Zero - 정답 보고 풀이 - 아직 이해 못함

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -3,20 +3,20 @@ package CodingTest.LeetCode;
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
-        System.out.println(solution.hammingWeight(11) == 3);
+        System.out.println(solution.minimumOneBitOperations(4) == 7);
     }
 }
 
 class Solution {
-    // you need to treat n as an unsigned value
-    public int hammingWeight(int n) {
-        int ones = 0;
+    public int minimumOneBitOperations(final int n) {
+        return minimumOneBitOperations(n, 0);
+    }
 
-        while (n != 0) {
-            ones = ones + (n & 1);
-            n = n >>> 1;
-        }
-
-        return ones;
+    public int minimumOneBitOperations(final int n, final int res) {
+        if (n == 0) return res;
+        int b = 1;
+        while ((b << 1) <= n)
+            b = b << 1;
+        return minimumOneBitOperations((b >> 1) ^ b ^ n, res + b);
     }
 }
