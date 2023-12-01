eed611edf77b1d648136d0e99c5635c4650beae4
HyunggiPark <phg9587@naver.com>
2023-12-01 20:27:10 +0900

    leetcode Check If Two String Arrays are Equivalent - 1차 풀이

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -3,20 +3,23 @@ package CodingTest.LeetCode;
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
-        System.out.println(solution.minimumOneBitOperations(4) == 7);
+        System.out.println(solution.arrayStringsAreEqual(new String[]{"ab", "c"}, new String[]{"a", "bc"}));
     }
 }
 
 class Solution {
-    public int minimumOneBitOperations(final int n) {
-        return minimumOneBitOperations(n, 0);
-    }
+    public boolean arrayStringsAreEqual(final String[] word1, final String[] word2) {
+        String answer1 = "";
+        String answer2 = "";
 
-    public int minimumOneBitOperations(final int n, final int res) {
-        if (n == 0) return res;
-        int b = 1;
-        while ((b << 1) <= n)
-            b = b << 1;
-        return minimumOneBitOperations((b >> 1) ^ b ^ n, res + b);
+        for (int i = 0; i < word1.length; i++) {
+            answer1 += word1[i];
+        }
+
+        for (int i = 0; i < word2.length; i++) {
+            answer2 += word2[i];
+        }
+
+        return answer1.equals(answer2);
     }
 }
