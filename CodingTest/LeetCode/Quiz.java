f54211831c1c1c9820e344a5eea1078216f68c68
HyunggiPark <phg9587@naver.com>
2023-12-02 13:52:36 +0900

    leetcode Find Words That Can Be Formed by Characters - 1차 풀이

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -3,23 +3,40 @@ package CodingTest.LeetCode;
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
-        System.out.println(solution.arrayStringsAreEqual(new String[]{"ab", "c"}, new String[]{"a", "bc"}));
+        System.out.println(solution.countCharacters(new String[]{"cat", "bt", "hat", "tree"}, "atach"));
     }
 }
 
 class Solution {
-    public boolean arrayStringsAreEqual(final String[] word1, final String[] word2) {
-        String answer1 = "";
-        String answer2 = "";
+    public int countCharacters(final String[] words, final String chars) {
+        final int[] charsCount = new int[26];
+        extractCharArray(charsCount, chars);
 
-        for (int i = 0; i < word1.length; i++) {
-            answer1 += word1[i];
+        int answer = 0;
+
+        for (final String tempWord : words) {
+            boolean isEnable = true;
+
+            final int[] tempWordCount = new int[26];
+            extractCharArray(tempWordCount, tempWord);
+            for (int j = 0; j < charsCount.length; j++) {
+                if (charsCount[j] < tempWordCount[j]) {
+                    isEnable = false;
+                    break;
+                }
+            }
+
+            if (isEnable) {
+                answer += tempWord.length();
+            }
         }
 
-        for (int i = 0; i < word2.length; i++) {
-            answer2 += word2[i];
-        }
+        return answer;
+    }
 
-        return answer1.equals(answer2);
+    private void extractCharArray(final int[] charsArray, final String string) {
+        for (int i = 0; i < string.length(); i++) {
+            charsArray[string.charAt(i) - 'a']++;
+        }
     }
 }
