17f549b2f00aaf3e1e8f6b624fbc7ae4b0dce845
HyunggiPark <phg9587@naver.com>
2023-12-08 18:19:48 +0900

    leetcode  Construct String from Binary Tree - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -3,29 +3,66 @@ package CodingTest.LeetCode;
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
-        System.out.println(solution.largestOddNumber("52"));
-        System.out.println(solution.largestOddNumber("4206"));
-        System.out.println(solution.largestOddNumber("35427"));
-        System.out.println(solution.largestOddNumber("7542351161"));
+//        System.out.println(solution.tree2str("52"));
+//        System.out.println(solution.tree2str("4206"));
+//        System.out.println(solution.tree2str("35427"));
+//        System.out.println(solution.tree2str("7542351161"));
     }
 }
 
 class Solution {
-    public String largestOddNumber(final String num) {
-        String answer = "";
-        for (int right = num.length() - 1; right >= 0; right--) {
-            if (isOdd(num.charAt(right))) {
-                answer = num.substring(0, right + 1);
-                break;
-            }
+    public String tree2str(final TreeNode root) {
+        if (root == null) {
+            return "";
         }
 
-        return answer;
+        //재귀를 써야할 것 같다.
+        final StringBuilder sb = new StringBuilder();
+
+        //val을 표시한다.
+        sb.append(root.val);
+
+        if (root.left == null && root.right == null) {
+            return sb.toString();
+        }
+
+        //왼쪽부터 탐색한다
+        if (root.left != null) {
+            sb.append("(");
+            sb.append(tree2str(root.left));
+            sb.append(")");
+        }
+
+        //오른쪽을 탐색한다.
+        if (root.right != null) {
+            if (root.left == null) {
+                sb.append("()");
+            }
+
+            sb.append("(");
+            sb.append(tree2str(root.right));
+            sb.append(")");
+        }
+
+        return sb.toString();
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
     }
 
-    private boolean isOdd(final char ch) {
-        final int value = ch - '0';
+    TreeNode(final int val) {
+        this.val = val;
+    }
 
-        return value % 2 != 0;
+    TreeNode(final int val, final TreeNode left, final TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
