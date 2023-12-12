6572108e35283fe8f86a6350b9ee88151016d049
HyunggiPark <phg9587@naver.com>
2023-12-12 10:40:43 +0900

    leetcode  Maximum Product of Two Elements in an Array - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,36 +1,25 @@
 package CodingTest.LeetCode;
 
+import java.util.Arrays;
+
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.findSpecialInteger(new int[]{1, 2, 2, 6, 6, 6, 6, 7, 10}));
+        System.out.println(solution.maxProduct(new int[]{3, 4, 5, 2}));
     }
 }
 
 class Solution {
-    public int findSpecialInteger(final int[] arr) {
-        final int length = arr.length;
+    public int maxProduct(final int[] nums) {
+        Arrays.sort(nums);
 
-        final double ratio = (double) length / 4;
+        int answer = 1;
 
-        int saveValue = arr[0];
-        int count = 0;
-        for (int i = 0; i < length; i++) {
-            final int val = arr[i];
-
-            if (val == saveValue) {
-                count++;
-
-                if (count > ratio) {
-                    return val;
-                }
-            } else {
-                saveValue = val;
-                count = 1;
-            }
+        for (int i = nums.length - 1; i >= nums.length - 2; i--) {
+            answer *= (nums[i] - 1);
         }
 
-        return arr[length - 1];
+        return answer;
     }
 }
