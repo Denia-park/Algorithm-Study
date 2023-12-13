45d680e30362105ab9a2076f8482a3021e17083c
HyunggiPark <phg9587@naver.com>
2023-12-13 13:26:46 +0900

    leetcode Special Positions in a Binary Matrix - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,23 +1,56 @@
 package CodingTest.LeetCode;
 
-import java.util.Arrays;
+import CodingTest.Programmers.BracketUtil;
 
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.maxProduct(new int[]{3, 4, 5, 2}));
+        System.out.println(solution.numSpecial(BracketUtil.convertStringToJavaIntTwoDimensionalArray("[[1,0,0],[0,0,1],[1,0,0]]")));
+        System.out.println(solution.numSpecial(BracketUtil.convertStringToJavaIntTwoDimensionalArray("[[1,0,0],[0,1,0],[0,0,1]]")));
     }
 }
 
 class Solution {
-    public int maxProduct(final int[] nums) {
-        Arrays.sort(nums);
+    public int numSpecial(final int[][] mat) {
+        int answer = 0;
 
-        int answer = 1;
+        for (int row = 0; row < mat.length; row++) {
+            for (int col = 0; col < mat[0].length; col++) {
+                if (mat[row][col] == 0) {
+                    continue;
+                }
 
-        for (int i = nums.length - 1; i >= nums.length - 2; i--) {
-            answer *= (nums[i] - 1);
+                boolean check = true;
+                //행 체크
+                for (int i = 0; i < mat[0].length; i++) {
+                    final int val = mat[row][i];
+
+                    if (val == 1 && i != col) {
+                        check = false;
+                        break;
+                    }
+                }
+
+                if (!check) {
+                    continue;
+                }
+
+                //열 체크
+                for (int i = 0; i < mat.length; i++) {
+                    final int val = mat[i][col];
+
+                    if (val == 1 && i != row) {
+                        check = false;
+                        break;
+                    }
+                }
+                if (!check) {
+                    continue;
+                }
+
+                answer++;
+            }
         }
 
         return answer;
