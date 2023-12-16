81df100f3f89dcfac24f2511b9ededa7dcffbe14
HyunggiPark <phg9587@naver.com>
2023-12-16 17:18:22 +0900

    leetcode Valid Anagram - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,35 +1,31 @@
 package CodingTest.LeetCode;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        final List<List<String>> lists = List.of(List.of("London", "New York"),
-                List.of("New York", "Lima"),
-                List.of("Lima", "Sao Paulo"));
-
-        System.out.println(solution.destCity(lists));
+        System.out.println(solution.isAnagram("anagram", "nagaram"));
     }
 }
 
 class Solution {
-    public String destCity(final List<List<String>> paths) {
-        final Map<String, String> map = new HashMap<>();
+    public boolean isAnagram(final String s, final String t) {
 
-        for (final List<String> path : paths) {
-            map.put(path.get(0), path.get(1));
+        final Map<Character, Integer> sMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            final char c = s.charAt(i);
+            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
         }
 
-        for (final String value : map.values()) {
-            if (!map.containsKey(value)) {
-                return value;
-            }
+        final Map<Character, Integer> tMap = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            final char c = t.charAt(i);
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
         }
 
-        return "";
+        return sMap.equals(tMap);
     }
 }
