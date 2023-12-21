707f1ea06603f1e187c2e4918938a57490054f7d
HyunggiPark <phg9587@naver.com>
2023-12-21 15:06:21 +0900

    프로그래머스 자연수 뒤집어 배열로 만들기 - 1차 풀이

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,92 +1,23 @@
 package CodingTest.Programmers;
 
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 class Solution {
-    public int[] solution(final int n) {
-        //빈 배열 만들기
-        final int[][] arr = new int[n][n];
+    public int[] solution(final long n) {
+        final String str = String.valueOf(n);
+        final Deque<Character> stack = new ArrayDeque<>();
 
-        //시작 숫자 및 최대 값 지정하기
-        int count = 1;
-        final long maxCount = ((long) n * (n + 1)) / 2;
-
-        //방향 지정하기
-        Direction direction = Direction.DOWN;
-
-        //시작 인덱스 정하기
-        int rowIdx = 0;
-        int colIdx = 0;
-
-        //삼각형 사이즈를 조절할 변수 지정
-        int height = n;
-        int width = 1;
-
-        //값 채우기
-        while (count <= maxCount) {
-            arr[rowIdx][colIdx] = count;
-
-            switch (direction) {
-                case DOWN:
-                    if ((rowIdx + 1) == height) {
-                        colIdx++;
-
-                        //끝에 도달했으므로 방향을 바꿔야 한다.
-                        direction = Direction.RIGHT;
-                        break;
-                    }
-
-                    rowIdx++;
-
-                    break;
-                case UP:
-                    //이미 채워진 값을 만났다 == 꼭대기에 도달했다.
-                    if (arr[rowIdx - 1][colIdx - 1] != 0) {
-                        rowIdx++;
-
-                        //한번 삼각형을 완성했으므로, 삼각형의 사이즈를 줄여야 한다.
-                        height--;
-                        width--;
-
-                        //끝에 도달했으므로 방향을 바꿔야 한다.
-                        direction = Direction.DOWN;
-                        break;
-                    }
-
-                    rowIdx--;
-                    colIdx--;
-
-                    break;
-                case RIGHT:
-                    if ((colIdx + 1) == rowIdx + width) {
-                        colIdx--;
-                        rowIdx--;
-
-                        //끝에 도달했으므로 방향을 바꿔야 한다.
-                        direction = Direction.UP;
-                        break;
-                    }
-
-                    colIdx++;
-
-                    break;
-                default:
-                    break;
-            }
-
-            count++;
+        for (final char ch : str.toCharArray()) {
+            stack.push(ch);
         }
 
-        //배열을 값으로 옮기기
-        return Arrays.stream(arr)
-                .flatMapToInt(Arrays::stream)
-                .filter(value -> value != 0)
-                .toArray();
-    }
+        final int[] answer = new int[stack.size()];
 
-    enum Direction {
-        UP,
-        DOWN,
-        RIGHT
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = Character.getNumericValue(stack.pop());
+        }
+
+        return answer;
     }
 }
