376e91663fea3109187d5b21e5017dc5a06a6277
HyunggiPark <phg9587@naver.com>
2023-12-21 16:02:51 +0900

    프로그래머스 2021 카카오 채용연계형 인턴십 거리두기 확인하기 - 1차 풀이 - 실패

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -6,7 +6,18 @@ public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(Arrays.toString(ts.solution(4)));
+        System.out.println(
+                Arrays.toString(
+                        ts.solution(
+                                new String[][]{
+                                        {"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"},
+                                        {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"},
+                                        {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"},
+                                        {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"},
+                                        {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}}
+                        )
+                )
+        );
 //        System.out.println(Arrays.toString(ts.solution(5)));
 //        System.out.println(Arrays.toString(ts.solution(6)));
 //        System.out.println(Arrays.equals(ts.solution(6), new int[]{1, 2, 15, 3, 16, 14, 4, 17, 21, 13, 5, 18, 19, 20, 12, 6, 7, 8, 9, 10, 11}));
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,26 +1,102 @@
 package CodingTest.Programmers;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
-    public int[][] solution(final int[][] arr1, final int[][] arr2) {
-        final int newRowNum = arr1.length;
-        final int newColNum = arr2[0].length;
+    private static final char PLAYER = 'P';
+    private static final char EMPTY = 'O';
+    private static final char WALL = 'X';
+    private static final int[][] DIRECTIONS = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        final int[][] answer = new int[newRowNum][newColNum];
 
-        //행렬의 곱셈
-        for (int row = 0; row < newRowNum; row++) {
-            for (int col = 0; col < newColNum; col++) {
-                int tempValue = 0;
+    public int[] solution(final String[][] places) {
+        final int[] answer = new int[places.length];
 
-                //arr1 col만큼, arr2 row만큼
-                for (int idx = 0; idx < arr1[0].length; idx++) {
-                    tempValue += arr1[row][idx] * arr2[idx][col];
+        int idx = 0;
+        for (final String[] map : places) {
+            boolean isPass = true;
+
+            //P인 모든 자리를 탐색
+            for (int row = 0; row < map.length; row++) {
+                for (int col = 0; col < map[row].length(); col++) {
+                    if (map[row].charAt(col) != PLAYER) {
+                        continue;
+                    }
+
+                    isPass = bfs(map, row, col);
+
+                    if (!isPass) {
+                        break;
+                    }
                 }
 
-                answer[row][col] = tempValue;
+                if (!isPass) {
+                    break;
+                }
             }
+
+            answer[idx++] = isPass ? 1 : 0;
         }
 
         return answer;
+    }
+
+    private boolean bfs(final String[] map, final int row, final int col) {
+        final Set<String> isVisited = new HashSet<>();
+        final Deque<int[]> deque = new ArrayDeque<>();
+        deque.push(new int[]{row, col, 0});
+        isVisited.add(row + "," + col);
+
+        while (!deque.isEmpty()) {
+            final int[] cur = deque.poll();
+
+            final int curRow = cur[0];
+            final int curCol = cur[1];
+            final int curDist = cur[2];
+
+            if ((curRow != row && curCol != col)
+                    && (map[curRow].charAt(curCol) == PLAYER)
+                    && (curDist <= 2)) {
+                return false;
+            }
+
+            for (final int[] direction : DIRECTIONS) {
+                final int nextRow = curRow + direction[0];
+                final int nextCol = curCol + direction[1];
+
+                //밖으로 나가면 이동 못함
+                if (isOutOfMap(map, nextRow, nextCol)) {
+                    continue;
+                }
+
+                //벽이면 이동 못함
+                if (map[nextRow].charAt(nextCol) == WALL) {
+                    continue;
+                }
+
+                //방문 했으면 이동 못함
+                if (isVisited.contains(nextRow + "," + nextCol)) {
+                    continue;
+                }
+
+                //거리를 2까지만 확인한다.
+                if (curDist >= 2) {
+                    continue;
+                }
+
+                deque.push(new int[]{nextRow, nextCol, curDist + 1});
+                //이동 하면 isVisted에 추가해주기
+                isVisited.add(nextRow + "," + nextCol);
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isOutOfMap(final String[] map, final int nextRow, final int nextCol) {
+        return nextRow < 0 || nextRow >= map.length || nextCol < 0 || nextCol >= map[nextRow].length();
     }
 }
