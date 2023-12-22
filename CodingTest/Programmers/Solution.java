c754556f618581a3e2bf278bf349908efd0c6aef
HyunggiPark <phg9587@naver.com>
2023-12-22 13:57:00 +0900

    프로그래머스 2020 KAKAO BLIND RECRUITMENT 문자열 압축 - 2차 풀이 - 리팩터링

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -7,42 +7,45 @@ class Solution {
     public int solution(final String original) {
         int answer = Integer.MAX_VALUE;
 
+        //길이가 1이면 바로 1을 리턴하자.
         if (original.length() == 1) {
             return 1;
         }
 
-        //stack을 사용해서 같은 단어면 Count 하도록 하면 어떨까 ?
+        //Deque를 사용해서 방금 추가한 단어가 같은 단어면 Count 하자.
         for (int count = 1; count < original.length(); count++) {
-            final Deque<Word> stack = new ArrayDeque<>();
+            final Deque<Word> deque = new ArrayDeque<>();
+
             //처음부터 탐색
             int searchIdx = 0;
             for (; searchIdx <= original.length() - count; searchIdx += count) {
+                //단어로 자르기
                 final String str = original.substring(searchIdx, searchIdx + count);
 
-//                System.out.println("count = " + count + ", searchIdx = " + searchIdx + ", str = " + str);
-
-                if (stack.isEmpty()) {
-                    stack.addLast(new Word(str, 1));
+                //deque가 비어있으면 추가
+                if (deque.isEmpty()) {
+                    deque.addLast(new Word(str, 1));
                     continue;
                 }
 
-                final Word peek = stack.peekLast();
+                final Word peek = deque.peekLast();
 
+                //마지막에 추가한 단어랑 같으면 count를 증가시키자.
+                //다르면 새로운 단어로 추가하자.
                 if (peek.getStr().equals(str)) {
                     peek.increaseCount();
                 } else {
-                    stack.addLast(new Word(str, 1));
+                    deque.addLast(new Word(str, 1));
                 }
             }
 
             //마지막 단어 처리
             final String str = original.substring(searchIdx);
-            stack.addLast(new Word(str, 1));
+            deque.addLast(new Word(str, 1));
 
+            //문자열 길이를 구해준다.
             final StringBuilder stringBuilder = new StringBuilder();
-            while (!stack.isEmpty()) {
-                final Word word = stack.pollFirst();
-
+            for (final Word word : deque) {
                 if (word.getCount() == 1) {
                     stringBuilder.append(word.getStr());
                 } else {
@@ -50,10 +53,8 @@ class Solution {
                 }
             }
 
-
-//            System.out.println(stringBuilder);
-
-            answer = Math.min(answer, stringBuilder.toString().length());
+            //문자열 길이를 기존의 값과 비교한다.
+            answer = Math.min(answer, stringBuilder.length());
         }
 
         return answer;
