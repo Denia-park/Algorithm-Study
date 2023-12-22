5ca12a528a43b70993bcca374e8ab23322a480b1
HyunggiPark <phg9587@naver.com>
2023-12-22 13:37:41 +0900

    프로그래머스 2020 KAKAO BLIND RECRUITMENT 문자열 압축 - 1차 풀이 - 5번 틀림

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -4,33 +4,10 @@ public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(
-                ts.solution(
-                                "try hello world"
-                        )
-                        .equals("TrY HeLlO WoRlD")
-        );
-//        System.out.println(Arrays.toString(ts.solution(5)));
-//        System.out.println(Arrays.toString(ts.solution(6)));
-//        System.out.println(Arrays.equals(ts.solution(6), new int[]{1, 2, 15, 3, 16, 14, 4, 17, 21, 13, 5, 18, 19, 20, 12, 6, 7, 8, 9, 10, 11}));
+        System.out.println(ts.solution("aabbaccc") == 7);
+        System.out.println(ts.solution("ababcdcdababcdcd") == 9);
+        System.out.println(ts.solution("abcabcdede") == 8);
+        System.out.println(ts.solution("abcabcabcabcdededededede") == 14);
+        System.out.println(ts.solution("xababcdcdababcdcd") == 17);
     }
 }
-
-
-// 12345
-
-//        *StringTokenizer*
-//        StringTokenizer st;
-//        st = new StringTokenizer(br.readLine());
-//        int stNum = Integer.parseInt(st.nextToken());
-//        int testValue = Integer.parseInt(br.readLine());
-
-//        *BufferedReader*
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//
-//        int testCaseNum = Integer.parseInt(br.readLine());
-//        String[] testCase = new String[testCaseNum];
-//        for (int i = 0; i < testCaseNum; i++) {
-//            testCase[i] = br.readLine();
-//        }
-
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,28 +1,78 @@
 package CodingTest.Programmers;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 class Solution {
-    public String solution(final String s) {
-        String answer = "";
+    public int solution(final String original) {
+        int answer = Integer.MAX_VALUE;
 
-        int numCount = 1;
+        //stack을 사용해서 같은 단어면 Count 하도록 하면 어떨까 ?
+        for (int count = 1; count < original.length(); count++) {
+            final Deque<Word> stack = new ArrayDeque<>();
+            //처음부터 탐색
+            int searchIdx = 0;
+            for (; searchIdx <= original.length() - count; searchIdx += count) {
+                final String str = original.substring(searchIdx, searchIdx + count);
 
-        for (int idx = 0; idx < s.length(); idx++) {
-            final char ch = s.charAt(idx);
+//                System.out.println("count = " + count + ", searchIdx = " + searchIdx + ", str = " + str);
 
-            if (ch == ' ') {
-                answer += " ";
-                numCount = 1;
-                continue;
+                if (stack.isEmpty()) {
+                    stack.addLast(new Word(str, 1));
+                    continue;
+                }
+
+                final Word peek = stack.peekLast();
+
+                if (peek.getStr().equals(str)) {
+                    peek.increaseCount();
+                } else {
+                    stack.addLast(new Word(str, 1));
+                }
             }
 
-            if (numCount % 2 == 1) {
-                answer += Character.toUpperCase(ch);
-            } else {
-                answer += Character.toLowerCase(ch);
+            //마지막 단어 처리
+            final String str = original.substring(searchIdx);
+            stack.addLast(new Word(str, 1));
+
+            final StringBuilder stringBuilder = new StringBuilder();
+            while (!stack.isEmpty()) {
+                final Word word = stack.pollFirst();
+
+                if (word.getCount() == 1) {
+                    stringBuilder.append(word.getStr());
+                } else {
+                    stringBuilder.append(word.getCount()).append(word.getStr());
+                }
             }
-            numCount++;
+
+//            System.out.println(stringBuilder);
+
+            answer = Math.min(answer, stringBuilder.toString().length());
         }
 
         return answer;
+    }
+
+    class Word {
+        private final String str;
+        private int count;
+
+        public Word(final String str, final int count) {
+            this.str = str;
+            this.count = count;
+        }
+
+        public void increaseCount() {
+            this.count++;
+        }
+
+        public String getStr() {
+            return str;
+        }
+
+        public int getCount() {
+            return count;
+        }
     }
 }
