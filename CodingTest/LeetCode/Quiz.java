ee624669cf160cb13de18398ff989642e2400fe1
HyunggiPark <phg9587@naver.com>
2023-12-24 13:36:08 +0900

    leetcode Minimum Changes To Make Alternating Binary String - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -6,39 +6,45 @@ public class Quiz {
 
 //        System.out.println(new Food("abc", "B", 1).equals(new Food("abc", "B", 2)));
 
-        System.out.println(solution.maxScore("011101"));
-        System.out.println(solution.maxScore("00111"));
-        System.out.println(solution.maxScore("1111"));
+        System.out.println(solution.minOperations("0100") == 1);
+        System.out.println(solution.minOperations("10") == 0);
+        System.out.println(solution.minOperations("1111") == 2);
+        System.out.println(solution.minOperations("110") == 1);
+        System.out.println(solution.minOperations("0011100") == 3);
     }
 }
 
 class Solution {
-    public int maxScore(final String s) {
-        int oneCount = 0;
+    public int minOperations(final String s) {
+        int zeroOneCount = 0;
+        int oneZeroCount = 0;
 
-        for (int i = 1; i < s.length(); i++) {
-            final char ch = s.charAt(i);
-
-            if (ch == '1') {
-                oneCount++;
-            }
-        }
-
-        int zeroCount = s.charAt(0) == '0' ? 1 : 0;
-        int maxScore = Math.max(0, zeroCount + oneCount);
-
-        for (int i = 1; i < s.length() - 1; i++) {
-            final char ch = s.charAt(i);
-
-            if (ch == '0') {
-                zeroCount++;
+        //2개씩 잘라서 10 만들어서 값 가지기
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0) {
+                if (s.charAt(i) == '0') {
+                    zeroOneCount++;
+                }
             } else {
-                oneCount--;
+                if (s.charAt(i) == '1') {
+                    zeroOneCount++;
+                }
             }
-
-            maxScore = Math.max(maxScore, zeroCount + oneCount);
         }
 
-        return maxScore;
+        //2개씩 잘라서 01 만들어서 값 가지기
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0) {
+                if (s.charAt(i) == '1') {
+                    oneZeroCount++;
+                }
+            } else {
+                if (s.charAt(i) == '0') {
+                    oneZeroCount++;
+                }
+            }
+        }
+
+        return Math.min(zeroOneCount, oneZeroCount);
     }
 }
