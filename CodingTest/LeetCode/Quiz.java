86775ea27567d86e41e591d06f7c550198d23227
HyunggiPark <phg9587@naver.com>
2023-12-25 14:27:41 +0900

    leetcode Decode Ways - 1차 풀이 완료 - 타임 오버

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,41 +1,67 @@
 package CodingTest.LeetCode;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-//        System.out.println(new Food("abc", "B", 1).equals(new Food("abc", "B", 2)));
-
-        System.out.println(solution.minOperations("0100") == 1);
-        System.out.println(solution.minOperations("10") == 0);
-        System.out.println(solution.minOperations("1111") == 2);
-        System.out.println(solution.minOperations("110") == 1);
-        System.out.println(solution.minOperations("0011100") == 3);
+        System.out.println(solution.numDecodings("12") == 2);
+        System.out.println(solution.numDecodings("226") == 3);
+        System.out.println(solution.numDecodings("06") == 0);
+        System.out.println(solution.numDecodings("111111111111111111111111111111111111111111111"));
     }
 }
 
 class Solution {
-    public int minOperations(final String s) {
-        int zeroOneCount = 0;
-        int oneZeroCount = 0;
+    private Map<String, Character> mappingCode;
+    private Set<String> answerSet;
 
-        //10 순서 혹은 01 순서로 비교해서 값 비교하기
-        for (int i = 0; i < s.length(); i++) {
-            if (i % 2 == 0) {
-                if (s.charAt(i) == '0') {
-                    zeroOneCount++;
-                } else {
-                    oneZeroCount++;
-                }
-            } else {
-                if (s.charAt(i) == '1') {
-                    zeroOneCount++;
-                } else {
-                    oneZeroCount++;
-                }
-            }
+    public int numDecodings(final String s) {
+        mappingCode = new HashMap<>();
+        answerSet = new HashSet<>();
+        mappingCode();
+
+        // dfs를 사용하자.
+        dfs(s, "");
+
+        for (final String answer : answerSet) {
+            System.out.println("answer = " + answer);
+        }
+        System.out.println("answerSet Size() = " + answerSet.size());
+        return answerSet.size();
+    }
+
+    private void dfs(final String restString, final String answerString) {
+        if (restString.isEmpty()) {
+            answerSet.add(answerString);
+            return;
         }
 
-        return Math.min(zeroOneCount, oneZeroCount);
+        // 1. 1개의 문자를 사용하는 경우
+        // 2. 2개의 문자를 사용하는 경우
+        for (int idx = 1; idx <= 2; idx++) {
+            if (restString.length() < idx) {
+                continue;
+            }
+
+            final String code = restString.substring(0, idx);
+            final String rest = restString.substring(idx);
+
+            if (!mappingCode.containsKey(code)) {
+                continue;
+            }
+
+            dfs(rest, answerString + mappingCode.get(code));
+        }
+    }
+
+    private void mappingCode() {
+        for (int i = 0; i < 26; i++) {
+            mappingCode.put(String.valueOf(i + 1), (char) ('A' + i));
+        }
     }
 }
