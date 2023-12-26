5d04dec9c55cfb4ef14ab94e1551c3a608906962
HyunggiPark <phg9587@naver.com>
2023-12-26 13:39:51 +0900

    leetcode Number of Dice Rolls With Target Sum - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -4,44 +4,36 @@ public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.numDecodings("12") == 2);
-        System.out.println(solution.numDecodings("226") == 3);
-        System.out.println(solution.numDecodings("06") == 0);
-        System.out.println(solution.numDecodings("111111111111111111111111111111111111111111111"));
+        System.out.println(solution.numRollsToTarget(1, 6, 3) == 1);
+        System.out.println(solution.numRollsToTarget(2, 6, 7) == 6);
+        System.out.println(solution.numRollsToTarget(30, 30, 500) == 222616187);
     }
 }
 
 class Solution {
-    public int numDecodings(final String s) {
-        //s가 null 이거나, 길이가 0이거나, 0으로 시작하면 0을 리턴한다.
-        if (s == null || s.isEmpty() || s.charAt(0) == '0') {
-            return 0;
-        }
+    public static final int MOD = 1_000_000_007;
 
-        //길이에 대해서 정의
-        final int len = s.length();
+    public int numRollsToTarget(final int n, final int k, final int target) {
+        //주사위 개수를 늘려가면서 dp를 만들자
+        final int[][] dp = new int[n + 1][target + 1];
 
-        //dp 배열 초기화
-        final int[] dp = new int[len + 1];
-        dp[0] = 1; //dp[0]은 1로 초기화
-        dp[1] = 1; //dp[1]은 1로 초기화
+        dp[0][0] = 1;
 
-        for (int count = 2; count <= len; count++) {
-            //한자리 숫자 추가하는 경우
-            final int oneDigit = s.charAt(count - 1) - '0';
+        //1번째 주사위부터 계산을 하자.
+        for (int diceIdx = 1; diceIdx < n + 1; diceIdx++) {
+            for (int targetIdx = 1; targetIdx < target + 1; targetIdx++) {
+                for (int numIdx = 1; numIdx < k + 1; numIdx++) {
+                    final int restValue = targetIdx - numIdx;
 
-            //  0으로 시작하면, decode 방법이 없으므로 제외
-            if (oneDigit != 0) {
-                dp[count] += dp[count - 1];
-            }
+                    if (restValue < 0) {
+                        continue;
+                    }
 
-            //두자리 숫자 추가하는 경우
-            final int twoDigit = Integer.parseInt(s.substring(count - 2, count));
-            if (10 <= twoDigit && twoDigit <= 26) {
-                dp[count] += dp[count - 2];
+                    dp[diceIdx][targetIdx] = (dp[diceIdx][targetIdx] + (dp[diceIdx - 1][restValue] % MOD)) % MOD;
+                }
             }
         }
 
-        return dp[len];
+        return dp[n][target];
     }
 }
