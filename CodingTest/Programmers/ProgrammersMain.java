71805e3ab197a4136ef769d20bdb2798742015fc
HyunggiPark <phg9587@naver.com>
2023-12-26 14:54:13 +0900

    프로그래머스 연습문제 하노이의 탑 - 1차 풀이

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -6,11 +6,6 @@ public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(Arrays.toString(ts.solution(BracketUtil.convertStringToJavaIntTwoDimensionalArray(
-                "[[1,1,0,0],[1,0,0,0],[1,0,0,1],[1,1,1,1]]"
-        ))));
-        System.out.println(Arrays.toString(ts.solution(BracketUtil.convertStringToJavaIntTwoDimensionalArray(
-                "[[1,1,1,1,1,1,1,1],[0,1,1,1,1,1,1,1],[0,0,0,0,1,1,1,1],[0,1,0,0,1,1,1,1],[0,0,0,0,0,0,1,1],[0,0,0,0,0,0,0,1],[0,0,0,0,1,0,0,1],[0,0,0,0,1,1,1,1]]"
-        ))));
+        System.out.println(Arrays.deepToString(ts.solution(2)));
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,46 +1,33 @@
 package CodingTest.Programmers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    int[][] globalArr;
 
-    public int[] solution(final int[][] arr) {
-        globalArr = arr;
-        return calculate(0, 0, arr.length);
+    private List<int[]> answer;
+
+    public int[][] solution(final int totalNum) {
+        answer = new ArrayList<>();
+
+        hanoi(totalNum, 1, 2, 3);
+
+        return answer.toArray(int[][]::new);
     }
 
-    private int[] calculate(final int startRow, final int startCol, final int length) {
-        //모든 값이 같은지 판단
-        if (isAllSameValue(startRow, startCol, length)) {
-            return globalArr[startRow][startCol] == 0 ? new int[]{1, 0} : new int[]{0, 1};
+    private void hanoi(final int totalNum, final int from, final int by, final int to) {
+        if (totalNum == 1) {
+            answer.add(new int[]{from, to});
+            return;
         }
 
-        //다르면 4등분
-        final int[] sumArr = {0, 0};
-        final int halfLength = length / 2;
-        sum(sumArr, calculate(startRow, startCol, halfLength)); //11시
-        sum(sumArr, calculate(startRow, startCol + halfLength, halfLength)); //1시
-        sum(sumArr, calculate(startRow + halfLength, startCol, halfLength)); //7시
-        sum(sumArr, calculate(startRow + halfLength, startCol + halfLength, halfLength)); //5시
+        //totalNum - 1개를 from에서 by로 옮긴다.
+        hanoi(totalNum - 1, from, to, by);
 
-        return sumArr;
-    }
+        //1개를 from에서 to로 옮긴다.
+        hanoi(1, from, by, to);
 
-    private boolean isAllSameValue(final int startRow, final int startCol, final int length) {
-        final int saveValue = globalArr[startRow][startCol];
-
-        for (int row = startRow; row < startRow + length; row++) {
-            for (int col = startCol; col < startCol + length; col++) {
-                if (saveValue != globalArr[row][col]) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    private void sum(final int[] sumArr, final int[] calculate) {
-        sumArr[0] += calculate[0];
-        sumArr[1] += calculate[1];
+        //totalNum - 1개를 by에서 to로 옮긴다.
+        hanoi(totalNum - 1, by, from, to);
     }
 }
