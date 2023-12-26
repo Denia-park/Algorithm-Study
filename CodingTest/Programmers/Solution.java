1a9d2b2fbf550329e91870ec4988141e14c4defc
HyunggiPark <phg9587@naver.com>
2023-12-26 14:42:40 +0900

    프로그래머스 월간 코드 챌린지 시즌1 쿼드압축 후 개수 세기 - 리팩터링

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -5,46 +5,38 @@ class Solution {
 
     public int[] solution(final int[][] arr) {
         globalArr = arr;
-
         return calculate(0, 0, arr.length);
     }
 
     private int[] calculate(final int startRow, final int startCol, final int length) {
         //모든 값이 같은지 판단
-        final int saveValue = globalArr[startRow][startCol];
-        boolean sameFlag = true;
-        for (int row = startRow; row < startRow + length; row++) {
-            for (int col = startCol; col < startCol + length; col++) {
-                final int curValue = globalArr[row][col];
-
-                if (saveValue != curValue) {
-                    sameFlag = false;
-                    break;
-                }
-            }
-
-            if (!sameFlag) {
-                break;
-            }
-        }
-
-        if (sameFlag) {
+        if (isAllSameValue(startRow, startCol, length)) {
             return globalArr[startRow][startCol] == 0 ? new int[]{1, 0} : new int[]{0, 1};
         }
 
         //다르면 4등분
         final int[] sumArr = {0, 0};
         final int halfLength = length / 2;
-        //11시
-        sum(sumArr, calculate(startRow, startCol, halfLength));
-        //1시
-        sum(sumArr, calculate(startRow, startCol + halfLength, halfLength));
-        //7시
-        sum(sumArr, calculate(startRow + halfLength, startCol, halfLength));
-        //5시
-        sum(sumArr, calculate(startRow + halfLength, startCol + halfLength, halfLength));
+        sum(sumArr, calculate(startRow, startCol, halfLength)); //11시
+        sum(sumArr, calculate(startRow, startCol + halfLength, halfLength)); //1시
+        sum(sumArr, calculate(startRow + halfLength, startCol, halfLength)); //7시
+        sum(sumArr, calculate(startRow + halfLength, startCol + halfLength, halfLength)); //5시
 
         return sumArr;
+    }
+
+    private boolean isAllSameValue(final int startRow, final int startCol, final int length) {
+        final int saveValue = globalArr[startRow][startCol];
+
+        for (int row = startRow; row < startRow + length; row++) {
+            for (int col = startCol; col < startCol + length; col++) {
+                if (saveValue != globalArr[row][col]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     private void sum(final int[] sumArr, final int[] calculate) {
