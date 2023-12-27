5f9628db3bebae88c7385dc4e8c8e7a0cc2ad923
HyunggiPark <phg9587@naver.com>
2023-12-27 15:06:49 +0900

    프로그래머스 완전탐색 모의고사 - 1차 풀이 - 틀림

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,12 +1,12 @@
 package CodingTest.Programmers;
 
+import java.util.Arrays;
+
 public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(ts.solution("AAAAE") == 6);
-        System.out.println(ts.solution("AAAE") == 10);
-        System.out.println(ts.solution("I") == 1563);
-        System.out.println(ts.solution("EIO") == 1189);
+        System.out.println(Arrays.toString(ts.solution(new int[]{1, 2, 3, 4, 5})));
+        System.out.println(Arrays.toString(ts.solution(new int[]{1, 3, 2, 4, 2})));
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,40 +1,75 @@
 package CodingTest.Programmers;
 
+//1번 : 1 2 3 4 5
+//2번 : 2 1 2 3 2 4 2 5
+//3번 : 3 3 1 1 2 2 4 4 5 5
+
 import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    private final String[] group = new String[]{"A", "E", "I", "O", "U"};
-    List<String> answerList;
-    private int answer;
-    private String target;
+    public int[] solution(final int[] answers) {
+        final int[] answer = {};
 
-    public int solution(final String word) {
-        answerList = new ArrayList<>();
-        answer = -1;
-        target = word;
+        final List<Student> students = new ArrayList<>(
+                List.of(
+                        new Student(1, new int[]{1, 2, 3, 4, 5}),
+                        new Student(2, new int[]{2, 1, 2, 3, 2, 4, 2, 5}),
+                        new Student(3, new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5})
+                )
+        );
 
-        search("");
+        for (final int val : answers) {
+            for (final Student student : students) {
+                student.checkAnswer(val);
+            }
+        }
 
-        return answer;
+        students.sort(null);
+
+        if (students.get(0).score == students.get(1).score
+                && students.get(1).score == students.get(2).score) {
+
+            return students.stream()
+                    .mapToInt(Student::getNumber)
+                    .toArray();
+        }
+
+        return new int[]{students.get(0).number};
     }
 
-    private void search(final String curString) {
-        if (curString.length() > 5 || answer != -1) {
-            return;
+    static class Student implements Comparable<Student> {
+        int number;
+        int score;
+        int[] pattern;
+        int patternIdx;
+
+        public Student(final int number, final int[] pattern) {
+            this.number = number;
+            this.score = 0;
+            this.pattern = pattern;
+            this.patternIdx = 0;
         }
 
-        if (!curString.isEmpty()) {
-            answerList.add(curString);
+        public int getNumber() {
+            return number;
         }
 
-        if (curString.equals(target)) {
-            answer = answerList.size();
-            return;
+        public void checkAnswer(final int answer) {
+            if (this.pattern[this.patternIdx] == answer) {
+                this.score++;
+            }
+
+            this.patternIdx = (this.patternIdx + 1) % this.pattern.length;
         }
 
-        for (final String str : group) {
-            search(curString + str);
+        @Override
+        public int compareTo(final Student o) {
+            if (o.score == this.score) {
+                return Integer.compare(this.number, o.number);
+            }
+
+            return Integer.compare(o.score, this.score);
         }
     }
 }
