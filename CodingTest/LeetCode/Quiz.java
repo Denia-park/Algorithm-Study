3d49681bf4dd9e5e6c21a30cbafce6a7380a3b2e
HyunggiPark <phg9587@naver.com>
2023-12-27 15:49:14 +0900

    leetcode Minimum Time to Make Rope Colorful - 1차 풀이 - 실패

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -4,32 +4,59 @@ public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.numRollsToTarget(1, 6, 3) == 1);
-        System.out.println(solution.numRollsToTarget(2, 6, 7) == 6);
-        System.out.println(solution.numRollsToTarget(30, 30, 500) == 222616187);
+        System.out.println(solution.minCost("abaac", new int[]{1, 2, 3, 4, 5}));
+        System.out.println(solution.minCost("abc", new int[]{1, 2, 3}));
+        System.out.println(solution.minCost("aabaa", new int[]{1, 2, 3, 4, 1}));
+        System.out.println(solution.minCost("cddcdcae", new int[]{4, 8, 8, 4, 4, 5, 4, 2}));
     }
 }
 
 class Solution {
-    public static final int MOD = 1_000_000_007;
+    private final char EMPTY = 'E';
 
-    public int numRollsToTarget(final int n, final int k, final int target) {
-        //주사위 개수를 늘려가면서 dp를 만들자
-        final int[][] dp = new int[n + 1][target + 1];
+    public int minCost(final String colors, final int[] neededTime) {
+        //연속적인 풍선을 쳐내야 하므로 -> 투포인터로 풀면 될 듯
+        //투 포인터 중에서 시간이 더 짧은 쪽을 쳐내자.
+        int answer = 0;
 
-        dp[0][0] = 1;
+        final char[] chars = colors.toCharArray();
+        final int length = chars.length;
 
-        //1번째 주사위부터 계산을 하자.
-        for (int diceIdx = 1; diceIdx <= n; diceIdx++) {
-            for (int targetIdx = diceIdx; targetIdx <= Math.min(diceIdx * k, target); targetIdx++) {
-                for (int numIdx = 1; numIdx <= Math.min(k, targetIdx); numIdx++) {
-                    final int restValue = targetIdx - numIdx;
+        int left = 0;
+        int right = 1;
+        while (right < length && left < right) {
+            //풍선 색이 다르면 넘어간다
+            if (chars[left] != chars[right]) {
+                right++;
+                left++;
+                continue;
+            }
 
-                    dp[diceIdx][targetIdx] = (dp[diceIdx][targetIdx] + (dp[diceIdx - 1][restValue] % MOD)) % MOD;
+            //풍선 색이 같으면 자르는 시간을 비교한다.
+            if (neededTime[left] < neededTime[right]) {
+                //더 짧은 시간을 answer에 더한다.
+                answer += neededTime[left];
+                //자르고 나서는 해당 char에 'E'을 넣어준다.
+                chars[left] = EMPTY;
+
+                //left가 터졌으니까 left를 옮겨준다.
+                while (chars[left] == EMPTY) {
+                    left++;
                 }
+
+                //연속된 풍선을 비교해야 하니까 right도 같이 옮긴다.
+                right++;
+            } else {
+                //더 짧은 시간을 answer에 더한다.
+                answer += neededTime[right];
+                //자르고 나서는 해당 char에 'E'을 넣어준다.
+                chars[right] = EMPTY;
+
+                //right가 터졌으니까 right를 옮겨준다.
+                right++;
             }
         }
 
-        return dp[n][target];
+        return answer;
     }
 }
