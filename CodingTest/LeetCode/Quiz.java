f69770caf4722f6d510e15bfe36fc0d30c8001b2
HyunggiPark <phg9587@naver.com>
2023-12-29 00:08:20 +0900

    leetcode String Compression II - 정답 보고 풀이

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -4,66 +4,51 @@ public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.minCost("abaac", new int[]{1, 2, 3, 4, 5}));
-        System.out.println(solution.minCost("abc", new int[]{1, 2, 3}));
-        System.out.println(solution.minCost("aabaa", new int[]{1, 2, 3, 4, 1}));
-        System.out.println(solution.minCost("cddcdcae", new int[]{4, 8, 8, 4, 4, 5, 4, 2}));
+        System.out.println(solution.getLengthOfOptimalCompression("aaabcccd", 2));
     }
 }
 
 class Solution {
-    private final char EMPTY = 'E';
+    public int getLengthOfOptimalCompression(final String s, final int k) {
+        final int n = s.length();
+        final int[][] dp = new int[n + 1][k + 1];
 
-    public int minCost(final String colors, final int[] neededTime) {
-        //연속적인 풍선을 쳐내야 하므로 -> 투포인터로 풀면 될 듯
-        //투 포인터 중에서 시간이 더 짧은 쪽을 쳐내자.
-        int answer = 0;
-
-        final char[] chars = colors.toCharArray();
-        final int length = chars.length;
-
-        int left = 0;
-        int right = 1;
-        while (right < length && left < right) {
-            //풍선 색이 다르면 넘어간다
-            if (chars[left] != chars[right]) {
-                left++;
-
-                while (chars[left] == EMPTY) {
-                    left++;
+        for (int i = n; i >= 0; i--) {
+            for (int j = 0; j <= k; j++) {
+                // Base case - initial length declaration
+                if (i == n) {
+                    dp[n][j] = 0;
+                    continue;
                 }
 
-                right = left + 1;
-                continue;
-            }
+                // Case 1 - delete the ith character if possible (j > 0)
+                dp[i][j] = (j > 0) ? dp[i + 1][j - 1] : Integer.MAX_VALUE;
 
-            //풍선 색이 같으면 자르는 시간을 비교한다.
-            if (neededTime[left] < neededTime[right]) {
-                //더 짧은 시간을 answer에 더한다.
-                answer += neededTime[left];
-                //자르고 나서는 해당 char에 'E'을 넣어준다.
-                chars[left] = EMPTY;
+                // Case 2 - we keep the ith index.
+                // We have to find the current char's index endpoint
+                int possible_del = j, count = 0;
+                for (int end = i; end < n && possible_del >= 0; end++) {
+                    // Check about the same char "b b b .." for encoding
+                    if (s.charAt(end) == s.charAt(i)) {
+                        count++;
 
-                //left가 터졌으니까 left를 옮겨준다.
-                left++;
-
-                while (chars[left] == EMPTY) {
-                    left++;
+                        // Assuming the block ends here "aaaa"
+                        dp[i][j] = Math.min(dp[i][j], getLength(count) + dp[end + 1][possible_del]);
+                    } else {
+                        // This character should be deleted to make the length min.
+                        possible_del--;
+                    }
                 }
-
-                //연속된 풍선을 비교해야 하니까 right도 같이 옮긴다.
-                right = left + 1;
-            } else {
-                //더 짧은 시간을 answer에 더한다.
-                answer += neededTime[right];
-                //자르고 나서는 해당 char에 'E'을 넣어준다.
-                chars[right] = EMPTY;
-
-                //right가 터졌으니까 right를 옮겨준다.
-                right++;
             }
         }
 
-        return answer;
+        return dp[0][k];
+    }
+
+    public int getLength(final int count) {
+        if (count == 1) return 1;
+        else if (count < 10) return 2;
+        else if (count < 100) return 3;
+        else return 4;
     }
 }
