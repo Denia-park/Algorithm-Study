1a7c4df1f467acf8ce40d2e2665176c6f0aa2121
HyunggiPark <phg9587@naver.com>
2023-12-28 21:27:09 +0900

    프로그래머스 2020 카카오 인턴십 수식 최대화 - 리팩터링

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,116 +1,122 @@
 package CodingTest.Programmers;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
 class Solution {
-    List<String> operators = List.of(
-            "+-*",
-            "+*-",
-            "-+*",
-            "-*+",
-            "*+-",
-            "*-+"
-    );
-
-    List<String> operatorString = List.of(
-            "+",
-            "-",
-            "*"
+    List<List<String>> operatorPriorities = List.of(
+            List.of("+", "-", "*"),
+            List.of("+", "*", "-"),
+            List.of("-", "+", "*"),
+            List.of("-", "*", "+"),
+            List.of("*", "+", "-"),
+            List.of("*", "-", "+")
     );
 
     public long solution(final String expression) {
         long answer = 0;
 
-        final String newExpression = expression.replace("-", ",").replace("*", ",").replace("+", ",");
-        final String[] originNumbers = newExpression.split(",");
+        final String[] originNumbers = getOriginNumbers(expression);
         final List<String> originOperator = getOriginOperator(expression);
 
-        for (final String operatorPriority : operators) {
-            final Deque<String> postfixQueue = new ArrayDeque<>();
-            final Stack<String> operatorStack = new Stack<>();
+        for (final List<String> operatorPriority : operatorPriorities) {
+            final Deque<String> postfixQueue = calculatePostfixQueue(operatorPriority, originNumbers, originOperator);
 
-            //postfix로 만들기
-            for (int idx = 0; idx < originNumbers.length; idx++) {
-                //숫자 넣기
-                postfixQueue.addLast(originNumbers[idx]);
-
-                //연산자 넣기
-                if (idx < originOperator.size()) {
-                    final String curOperator = originOperator.get(idx);
-
-                    if (operatorStack.isEmpty()) {
-                        operatorStack.push(curOperator);
-                        continue;
-                    }
-
-                    while (!operatorStack.isEmpty()) {
-                        final String peekOperator = operatorStack.peek();
-                        final int peekOperatorIndex = operatorPriority.indexOf(peekOperator);
-                        final int curOperatorIndex = operatorPriority.indexOf(curOperator);
-
-                        if (peekOperatorIndex < curOperatorIndex) {
-                            break;
-                        }
-
-                        final String popOperator = operatorStack.pop();
-                        postfixQueue.addLast(popOperator);
-                    }
-
-                    operatorStack.push(curOperator);
-                }
-            }
-
-            while (!operatorStack.isEmpty()) {
-                postfixQueue.addLast(operatorStack.pop());
-            }
-
-            final Stack<Long> calculateStack = new Stack<>();
-            while (!postfixQueue.isEmpty()) {
-                final String popped = postfixQueue.pop();
-
-                //연산자 아니면 Stack에 넣기
-                if (!operatorString.contains(popped)) {
-                    calculateStack.push(Long.valueOf(popped));
-                    continue;
-                }
-
-                //연산자이면 계산
-                final long value2 = calculateStack.pop();
-                final long value1 = calculateStack.pop();
-                long answerValue = 0;
-
-                switch (popped) {
-                    case "+":
-                        answerValue = value1 + value2;
-                        break;
-                    case "-":
-                        answerValue = value1 - value2;
-                        break;
-                    case "*":
-                        answerValue = value1 * value2;
-                        break;
-                }
-
-                calculateStack.push(answerValue);
-            }
-
-            answer = Math.max(answer, Math.abs(calculateStack.pop()));
+            answer = Math.max(answer, Math.abs(calculateAnswer(postfixQueue)));
         }
 
         return answer;
+    }
+
+    private String[] getOriginNumbers(final String expression) {
+        return expression.replace("-", ",")
+                .replace("*", ",")
+                .replace("+", ",")
+                .split(",");
     }
 
     private List<String> getOriginOperator(final String expression) {
         final List<String> originOperators = new ArrayList<>();
 
         for (int i = 0; i < expression.length(); i++) {
-            final char c = expression.charAt(i);
+            final char ch = expression.charAt(i);
 
-            if (c == '-' || c == '*' || c == '+') {
-                originOperators.add(String.valueOf(c));
+            if (ch == '-' || ch == '*' || ch == '+') {
+                originOperators.add(String.valueOf(ch));
             }
         }
 
         return originOperators;
+    }
+
+    private Deque<String> calculatePostfixQueue(final List<String> operatorPriority, final String[] originNumbers, final List<String> originOperator) {
+        final Deque<String> postfixQueue = new ArrayDeque<>();
+        final Deque<String> operatorStack = new ArrayDeque<>();
+
+        //postfix로 만들기
+        for (int idx = 0; idx < originNumbers.length; idx++) {
+            //숫자 넣기
+            postfixQueue.addLast(originNumbers[idx]);
+
+            //연산자 넣기
+            if (idx < originOperator.size()) {
+                final String curOperator = originOperator.get(idx);
+
+                while (!operatorStack.isEmpty()) {
+                    final String peekOperator = operatorStack.peek();
+
+                    //현재 연산자가 우선순위가 더 높으면 그냥 push, 아니면 다 꺼내서 Queue에 넣기
+                    if (operatorPriority.indexOf(peekOperator) < operatorPriority.indexOf(curOperator)) {
+                        break;
+                    }
+
+                    postfixQueue.addLast(operatorStack.pop());
+                }
+
+                operatorStack.push(curOperator);
+            }
+        }
+
+        //남은 연산자 다 꺼내서 Queue에 넣기
+        while (!operatorStack.isEmpty()) {
+            postfixQueue.addLast(operatorStack.pop());
+        }
+
+        return postfixQueue;
+    }
+
+    private long calculateAnswer(final Deque<String> postfixQueue) {
+        final Deque<Long> calculateStack = new ArrayDeque<>();
+
+        while (!postfixQueue.isEmpty()) {
+            final String popped = postfixQueue.pop();
+
+            //연산자 아니면 Stack에 넣기
+            final List<String> operators = List.of("+", "-", "*");
+            if (!operators.contains(popped)) {
+                calculateStack.push(Long.valueOf(popped));
+                continue;
+            }
+
+            //연산자 맞으면 계산
+            final long value2 = calculateStack.pop();
+            final long value1 = calculateStack.pop();
+
+            switch (popped) {
+                case "+":
+                    calculateStack.push(value1 + value2);
+                    break;
+                case "-":
+                    calculateStack.push(value1 - value2);
+                    break;
+                case "*":
+                    calculateStack.push(value1 * value2);
+                    break;
+            }
+        }
+
+        return calculateStack.pop();
     }
 }
