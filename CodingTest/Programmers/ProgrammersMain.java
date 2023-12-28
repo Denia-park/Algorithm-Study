1bdfe754f4ba863664ba0678032d5556f93cd7b2
HyunggiPark <phg9587@naver.com>
2023-12-28 20:55:10 +0900

    프로그래머스 2020 카카오 인턴십 수식 최대화 - 1차 풀이 - 실패

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,13 +1,10 @@
 package CodingTest.Programmers;
 
-import java.util.Arrays;
-
 public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(Arrays.toString(ts.solution(10, 2)));
-        System.out.println(Arrays.toString(ts.solution(8, 1)));
-        System.out.println(Arrays.toString(ts.solution(24, 24)));
+        System.out.println(ts.solution("100-200*300-500+20") == 60420);
+        System.out.println(ts.solution("50*6-3*2") == 300);
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,30 +1,116 @@
 package CodingTest.Programmers;
 
+import java.util.*;
+
 class Solution {
-    public int[] solution(final int brown, final int yellow) {
-        int[] answer = {};
+    List<String> operators = List.of(
+            "+-*",
+            "+*-",
+            "-+*",
+            "-*+",
+            "*+-",
+            "*-+"
+    );
 
-        //y 높이로 가능한 숫자들을 돌리면서, 총 개수를 구해야 한다.
-        //brown 개수 => 4 + 높이 * 2 + 너비 * 2
+    List<String> operatorString = List.of(
+            "+",
+            "-",
+            "*"
+    );
 
-        final double minWidth = Math.sqrt(yellow);
-        for (int width = yellow; width >= minWidth; width--) {
-            final double height = (double) yellow / width;
+    public long solution(final String expression) {
+        long answer = 0;
 
-            //정수가 아니면 넘어간다.
-            if (height % 1 != 0) {
-                continue;
+        final String newExpression = expression.replace("-", ",").replace("*", ",").replace("+", ",");
+        final String[] originNumbers = newExpression.split(",");
+        final List<String> originOperator = getOriginOperator(expression);
+
+        for (final String operatorPriority : operators) {
+            final Deque<String> postfixQueue = new ArrayDeque<>();
+            final Stack<String> operatorStack = new Stack<>();
+
+            //postfix로 만들기
+            for (int idx = 0; idx < originNumbers.length; idx++) {
+                //숫자 넣기
+                postfixQueue.addLast(originNumbers[idx]);
+
+                //연산자 넣기
+                if (idx < originOperator.size()) {
+                    final String curOperator = originOperator.get(idx);
+
+                    if (operatorStack.isEmpty()) {
+                        operatorStack.push(curOperator);
+                        continue;
+                    }
+
+                    while (!operatorStack.isEmpty()) {
+                        final String peekOperator = operatorStack.peek();
+                        final int peekOperatorIndex = operatorPriority.indexOf(peekOperator);
+                        final int curOperatorIndex = operatorPriority.indexOf(curOperator);
+
+                        if (peekOperatorIndex < curOperatorIndex) {
+                            break;
+                        }
+
+                        final String popOperator = operatorStack.pop();
+                        postfixQueue.addLast(popOperator);
+                    }
+
+                    operatorStack.push(curOperator);
+                }
             }
 
-            final int intHeight = (int) height;
-            final int tempBrown = 4 + intHeight * 2 + width * 2;
-
-            if (tempBrown == brown) {
-                answer = new int[]{width + 2, intHeight + 2};
-                break;
+            while (!operatorStack.isEmpty()) {
+                postfixQueue.addLast(operatorStack.pop());
             }
+
+            final Stack<Integer> calculateStack = new Stack<>();
+            while (!postfixQueue.isEmpty()) {
+                final String popped = postfixQueue.pop();
+
+                //연산자 아니면 Stack에 넣기
+                if (!operatorString.contains(popped)) {
+                    calculateStack.push(Integer.valueOf(popped));
+                    continue;
+                }
+
+                //연산자이면 계산
+                final int value2 = calculateStack.pop();
+                final int value1 = calculateStack.pop();
+                int answerValue = 0;
+
+                switch (popped) {
+                    case "+":
+                        answerValue = value1 + value2;
+                        break;
+                    case "-":
+                        answerValue = value1 - value2;
+                        break;
+                    case "*":
+                        answerValue = value1 * value2;
+                        break;
+                }
+
+                calculateStack.push(answerValue);
+            }
+
+            answer = Math.max(answer, Math.abs(calculateStack.pop()));
         }
 
         return answer;
+    }
+
+    private List<String> getOriginOperator(final String expression) {
+        final List<String> originOperators = new ArrayList<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            final char c = expression.charAt(i);
+
+            if (c == '-' || c == '*' || c == '+') {
+                originOperators.add(String.valueOf(c));
+            }
+        }
+
+        return originOperators;
     }
 }
