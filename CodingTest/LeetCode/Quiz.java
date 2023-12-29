f9c330a844d272dad567ea0d88bd057b8e8aae93
HyunggiPark <phg9587@naver.com>
2023-12-29 16:14:26 +0900

    leetcode Minimum Difficulty of a Job Schedule - 정답 보고 풀이

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,9 +1,6 @@
 package CodingTest.LeetCode;
 
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Quiz {
     public static void main(final String[] args) {
@@ -12,28 +9,41 @@ public class Quiz {
         System.out.println(solution.minDifficulty(new int[]{6, 5, 4, 3, 2, 1}, 2));
         System.out.println(solution.minDifficulty(new int[]{9, 9, 9}, 4));
         System.out.println(solution.minDifficulty(new int[]{1, 1, 1}, 3));
+        System.out.println(solution.minDifficulty(new int[]{11, 111, 22, 222, 33, 333, 44, 444}, 6));
     }
 }
 
 class Solution {
-    public int minDifficulty(final int[] jobDifficulty, final int d) {
-        final int len = jobDifficulty.length;
+    public int minDifficulty(final int[] jobDifficulty, final int dayLimit) {
+        final int jobsLength = jobDifficulty.length;
 
-        if (len < d) return -1;
+        if (jobsLength < dayLimit) return -1;
+        if (jobsLength == dayLimit) return Arrays.stream(jobDifficulty).sum();
 
-        final List<Integer> jobDiffList = Arrays.stream(jobDifficulty)
-                .boxed()
-                .sorted(Comparator.reverseOrder())
-                .collect(Collectors.toList());
+        // Initialize a 2D array to store difficulty for each day and job
+        final int[][] dp = new int[dayLimit + 1][jobsLength + 1];
 
-        final int maxDiff = jobDiffList.get(0);
-        int restDiff = 0;
-        for (int i = jobDifficulty.length - 1; i >= len - (d - 1); i--) {
-            final int diff = jobDifficulty[i];
-
-            restDiff += diff;
+        // Fill the first day's difficulties
+        dp[1][1] = jobDifficulty[0];
+        for (int jobCountIdx = 2; jobCountIdx <= jobsLength; jobCountIdx++) {
+            dp[1][jobCountIdx] = Math.max(dp[1][jobCountIdx - 1], jobDifficulty[jobCountIdx - 1]);
         }
 
-        return maxDiff + restDiff;
+        // Iterate over each day (start from 2nd day)
+        for (int dayCount = 2; dayCount <= dayLimit; dayCount++) {
+            //Iterate over each job (start from (day * 1) because we need at least 1 job per day)
+            for (int jobDoneCount = dayCount; jobDoneCount <= jobsLength; jobDoneCount++) {
+                int localMax = jobDifficulty[jobDoneCount - 1];
+                dp[dayCount][jobDoneCount] = Integer.MAX_VALUE; // Set an initial high value
+
+                // Iterate over previous jobs to find optimal difficulty
+                for (int prevJobDoneCount = jobDoneCount; prevJobDoneCount >= dayCount; prevJobDoneCount--) {
+                    localMax = Math.max(localMax, jobDifficulty[prevJobDoneCount - 1]);
+                    dp[dayCount][jobDoneCount] = Math.min(dp[dayCount][jobDoneCount], dp[dayCount - 1][prevJobDoneCount - 1] + localMax);
+                }
+            }
+        }
+
+        return dp[dayLimit][jobsLength];
     }
 }
