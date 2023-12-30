b444d0f9fbcb209c2fcaf657c93375c7050edfe1
HyunggiPark <phg9587@naver.com>
2023-12-30 16:23:48 +0900

    프로그래머스 월간 코드 챌린지 시즌1 두 개 뽑아서 더하기 - 1차 풀이- 정렬 부분 수정

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,48 +1,48 @@
 package CodingTest.Programmers;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
-    public int[] solution(final int[] array, final int[][] commands) {
-        final int[] answer = new int[commands.length];
+    public int[] solution(final int[] numbers) {
+        final Set<Integer> set = new HashSet<>();
 
-        for (int commandIdx = 0; commandIdx < commands.length; commandIdx++) {
-            final int[] command = commands[commandIdx];
-
-            final int startIdx = command[0] - 1;
-            final int endIdx = command[1] - 1;
-            final int targetIdx = command[2] - 1;
-
-            answer[commandIdx] = getValue(array, startIdx, endIdx, targetIdx);
-        }
-
-        return answer;
-    }
-
-    private int getValue(final int[] array, final int startIdx, final int endIdx, final int targetIdx) {
-        final int[] tempArr = new int[endIdx - startIdx + 1];
-        System.arraycopy(array, startIdx, tempArr, 0, tempArr.length);
-
-        //버블 정렬
-        bubbleSort(tempArr);
-
-        return tempArr[targetIdx];
-    }
-
-    private void bubbleSort(final int[] tempArr) {
-        for (int count = 0; count < tempArr.length - 1; count++) {
-            for (int diffIdx = 0; diffIdx < tempArr.length - 1 - count; diffIdx++) {
-                final int first = tempArr[diffIdx];
-                final int second = tempArr[diffIdx + 1];
-
-                if (first > second) {
-                    swap(tempArr, diffIdx, diffIdx + 1);
-                }
+        for (int firstIdx = 0; firstIdx < numbers.length; firstIdx++) {
+            for (int secondIdx = firstIdx + 1; secondIdx < numbers.length; secondIdx++) {
+                set.add(numbers[firstIdx] + numbers[secondIdx]);
             }
         }
+
+        final int[] array = set.stream().mapToInt(i -> i).toArray();
+
+        //정렬
+        selectionSort(array);
+
+        return array;
     }
 
-    private void swap(final int[] tempArr, final int idxA, final int idxB) {
-        final int temp = tempArr[idxA];
-        tempArr[idxA] = tempArr[idxB];
-        tempArr[idxB] = temp;
+    private void selectionSort(final int[] numbers) {
+        //맨 앞부터 채워나간다.
+        for (int positionIdx = 0; positionIdx < numbers.length; positionIdx++) {
+            //제일 작은 수를 하나 고른다
+            int minIdx = positionIdx;
+
+            for (int idx = positionIdx; idx < numbers.length; idx++) {
+                final int minValue = numbers[minIdx];
+                final int selectValue = numbers[idx];
+
+                if (minValue > selectValue) {
+                    minIdx = idx;
+                }
+            }
+
+            swap(numbers, positionIdx, minIdx);
+        }
+    }
+
+    private void swap(final int[] numbers, final int positionIdx, final int minIdx) {
+        final int temp = numbers[positionIdx];
+        numbers[positionIdx] = numbers[minIdx];
+        numbers[minIdx] = temp;
     }
 }
