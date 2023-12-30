58399f7ed4580e36bea45c3fa827ac65db5f7b41
HyunggiPark <phg9587@naver.com>
2023-12-30 13:05:55 +0900

    프로그래머스 정렬 k번째 수 - 1차 풀이 실패

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,14 +1,12 @@
 package CodingTest.Programmers;
 
+import java.util.Arrays;
+
 public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(ts.solution(new String[]{"frodo", "fradi", "crodo", "abc123", "frodoc"}, new String[]{"fr*d*", "abc1**"}));
-        System.out.println(ts.solution(new String[]{"frodo", "fradi", "crodo", "abc123", "frodoc"}, new String[]{"*rodo", "*rodo", "******"}));
-        System.out.println(ts.solution(new String[]{"frodo", "fradi", "crodo", "abc123", "frodoc"}, new String[]{"fr*d*", "*rodo", "******", "******"}));
-
-        System.out.println(ts.solution(new String[]{"12345", "12453", "aaaaa"}, new String[]{"*****", "*****"}));
+        System.out.println(Arrays.toString(ts.solution(new int[]{1, 5, 2, 6, 3, 7, 4}, new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}})));
 
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,90 +1,48 @@
 package CodingTest.Programmers;
 
-import java.util.*;
-
 class Solution {
-    public int solution(final String[] user_id, final String[] banned_id) {
-        final Set<String> answerSet = new HashSet<>();
+    public int[] solution(final int[] array, final int[][] commands) {
+        final int[] answer = new int[commands.length];
 
-        //banId에 맞는 후보(Set<String>)를 구하기 위해서 Map 초기화
-        final Map<Integer, Set<String>> digitCandidateMap = initMap(banned_id);
+        for (int commandIdx = 0; commandIdx < commands.length; commandIdx++) {
+            final int[] command = commands[commandIdx];
 
-        //banId에 해당할 수 있는 모든 userId 목록 구하기
-        findAllCandidateForBanId(user_id, banned_id, digitCandidateMap);
+            final int startIdx = command[0] - 1;
+            final int endIdx = command[1] - 1;
+            final int targetIdx = command[2] - 1;
 
-
-        //가능한 BanId 조합 구하기
-        final ArrayDeque<String> permutationSaveDeque = new ArrayDeque<>();
-        getIdPermutation(answerSet, digitCandidateMap, 0, permutationSaveDeque);
-
-        return answerSet.size();
-    }
-
-    private Map<Integer, Set<String>> initMap(final String[] banned_id) {
-        final Map<Integer, Set<String>> digitCandidateMap = new HashMap<>();
-        for (int i = 0; i < banned_id.length; i++) {
-            digitCandidateMap.put(i, new HashSet<>());
+            answer[commandIdx] = getValue(array, startIdx, endIdx, targetIdx);
         }
-        return digitCandidateMap;
+
+        return answer;
     }
 
-    private void findAllCandidateForBanId(final String[] user_id, final String[] banned_id, final Map<Integer, Set<String>> digitCandidateMap) {
-        for (int banIdx = 0; banIdx < banned_id.length; banIdx++) {
-            final String banId = banned_id[banIdx];
+    private int getValue(final int[] array, final int startIdx, final int endIdx, final int targetIdx) {
+        final int[] tempArr = new int[endIdx - startIdx + 1];
+        System.arraycopy(array, startIdx, tempArr, 0, tempArr.length);
 
-            final Set<String> digitCandidate = digitCandidateMap.get(banIdx);
+        //버블 정렬
+        bubbleSort(tempArr);
 
-            //모든 userId랑 비교하기
-            for (final String userId : user_id) {
-                //userId가 후보가 되는지 확인하기
+        return tempArr[targetIdx];
+    }
 
-                //길이가 다르면 후보가 될 수 없다.
-                if (userId.length() != banId.length()) continue;
+    private void bubbleSort(final int[] tempArr) {
+        for (int startIdx = 0; startIdx < tempArr.length - 1; startIdx++) {
+            for (int diffIdx = startIdx; diffIdx < tempArr.length - 1; diffIdx++) {
+                final int first = tempArr[diffIdx];
+                final int second = tempArr[diffIdx + 1];
 
-                //각 자리수를 비교해서 *이 아닌 경우에는 같아야 한다.
-                boolean isCandidate = true;
-
-                for (int digitIdx = 0; digitIdx < userId.length(); digitIdx++) {
-                    final char banIdChar = banId.charAt(digitIdx);
-                    final char userIdChar = userId.charAt(digitIdx);
-
-                    // *인 경우는 무시
-                    if (banIdChar == '*') continue;
-
-                    // *이 아닌 경우에는 같아야 한다.
-                    if (userIdChar != banIdChar) {
-                        isCandidate = false;
-                        break;
-                    }
-                }
-
-                if (isCandidate) {
-                    digitCandidate.add(userId);
+                if (first > second) {
+                    swap(tempArr, diffIdx, diffIdx + 1);
                 }
             }
         }
     }
 
-    private void getIdPermutation(final Set<String> answerSet, final Map<Integer, Set<String>> banTargetIdMap, final int curIdx, final ArrayDeque<String> permutationSaveDeque) {
-        //가능한 조합을 끝까지 찾은 경우
-        if (curIdx == banTargetIdMap.size()) {
-            final String combinationString = permutationSaveDeque.stream()
-                    .sorted()
-                    .reduce((a, b) -> a + b).get();
-
-            answerSet.add(combinationString);
-
-            return;
-        }
-
-        final Set<String> candidates = banTargetIdMap.get(curIdx);
-
-        for (final String candidate : candidates) {
-            if (permutationSaveDeque.contains(candidate)) continue;
-
-            permutationSaveDeque.addLast(candidate);
-            getIdPermutation(answerSet, banTargetIdMap, curIdx + 1, permutationSaveDeque);
-            permutationSaveDeque.pollLast();
-        }
+    private void swap(final int[] tempArr, final int idxA, final int idxB) {
+        final int temp = tempArr[idxA];
+        tempArr[idxA] = tempArr[idxB];
+        tempArr[idxB] = temp;
     }
 }
