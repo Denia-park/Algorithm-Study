e9102fcc2ba2bea19bb37aaad1f37f7183221734
HyunggiPark <phg9587@naver.com>
2023-12-31 15:15:16 +0900

    프로그래머스 정렬 H-Index - 2차 풀이

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -5,5 +5,9 @@ public class ProgrammersMain {
         final Solution ts = new Solution();
 
         System.out.println(ts.solution(new int[]{3, 0, 6, 1, 5}));
+        System.out.println(ts.solution(new int[]{20, 21, 22, 23}));
+        System.out.println(ts.solution(new int[]{4, 4, 4}));
+        System.out.println(ts.solution(new int[]{12, 11, 10, 9, 8, 1}));
+        System.out.println(ts.solution(new int[]{6, 6, 6, 6, 6, 1}));
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -4,21 +4,28 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(final int[] citations) {
-        int answer = 0;
+        final int length = citations.length;
+        int answer = length;
 
         Arrays.sort(citations);
 
-        final int length = citations.length;
-
         for (int idx = 0; idx < citations.length; idx++) {
-            final int citation = citations[idx];
+            //현재 인용 개수
+            final int curCitation = citations[idx];
 
+            //H-index 이상 인용 개수
             final int upperCount = length - idx;
+            //H-index 이하 인용 개수
             final int lowerCount = idx;
 
-            if (lowerCount <= citation && citation <= upperCount) {
-                answer = citation;
+            //H-index 조건을 만족하는지 확인
+            if (answer <= curCitation
+                    && (lowerCount <= answer && answer <= upperCount)) {
+                break;
             }
+
+            //H-index 조건을 만족하지 않으면 H-index 감소
+            answer--;
         }
 
         return answer;
