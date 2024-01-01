26786198ff73f68f43ea237638927aae9ec360bc
HyunggiPark <phg9587@naver.com>
2024-01-01 15:06:00 +0900

    프로그래머스 연습문제 가장 큰 수 - 2차 풀이 - 실패

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -13,20 +13,34 @@ class Solution {
                 return o2.compareTo(o1);
             }
 
-            //길이가 다르면, 짧은 놈의 길이를 기준으로 모든 자리수 비교
-            final int minLength = Math.min(o1.length(), o2.length());
+            //길이가 다르면, 모든 자리 수를 비교해야 함
+            //예를 들어, (3, 34) 비교 하면 34가 앞으로 가야함
+            final int bigLength = Math.max(o1.length(), o2.length());
 
-            for (int i = 0; i < minLength; i++) {
-                final int v1 = o1.charAt(i) - '0';
-                final int v2 = o2.charAt(i) - '0';
+            for (int i = 0; i < bigLength; i++) {
+                int o1Idx = i;
+                int o2Idx = i;
 
-                if (v1 == v2) continue;
+                if (o1Idx >= o1.length()) {
+                    o1Idx = o1.length() - 1;
+                }
+
+                if (o2Idx >= o2.length()) {
+                    o2Idx = o2.length() - 1;
+                }
+
+                final int v1 = o1.charAt(o1Idx) - '0';
+                final int v2 = o2.charAt(o2Idx) - '0';
+
+                if (v1 == v2) {
+                    continue;
+                }
 
                 //내림차순 비교
                 return Integer.compare(v2, v1);
             }
 
-            return Integer.compare(o1.length(), o2.length());
+            return 0;
         };
 
         return Arrays.stream(numbers)
