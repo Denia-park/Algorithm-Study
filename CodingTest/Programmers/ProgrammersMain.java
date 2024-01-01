6f381307f407c23a210d4fbca3619191ccb342ba
HyunggiPark <phg9587@naver.com>
2024-01-01 14:31:40 +0900

    프로그래머스 연습문제 문자열 내 마음대로 정렬하기 - 1차 풀이

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -4,10 +4,7 @@ public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(ts.solution(new int[]{3, 0, 6, 1, 5}));
-        System.out.println(ts.solution(new int[]{20, 21, 22, 23}));
-        System.out.println(ts.solution(new int[]{4, 4, 4}));
-        System.out.println(ts.solution(new int[]{12, 11, 10, 9, 8, 1}));
-        System.out.println(ts.solution(new int[]{6, 6, 6, 6, 6, 1}));
+        System.out.println(ts.solution(new String[]{"sun", "bed", "car"}, 1));
+        System.out.println(ts.solution(new String[]{"abce", "abcd", "cdx"}, 2));
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,33 +1,39 @@
 package CodingTest.Programmers;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 class Solution {
-    public int solution(final int[] citations) {
-        final int length = citations.length;
-        int answer = length;
+    public String[] solution(final String[] strings, final int n) {
+        final List<Word> list = new ArrayList<>();
 
-        Arrays.sort(citations);
-
-        for (int idx = 0; idx < citations.length; idx++) {
-            //현재 인용 개수
-            final int curCitation = citations[idx];
-
-            //H-index 이상 인용 개수
-            final int upperCount = length - idx;
-            //H-index 이하 인용 개수
-            final int lowerCount = idx;
-
-            //H-index 조건을 만족하는지 확인
-            if (answer <= curCitation
-                    && (lowerCount <= answer && answer <= upperCount)) {
-                break;
-            }
-
-            //H-index 조건을 만족하지 않으면 H-index 감소
-            answer--;
+        for (final String string : strings) {
+            list.add(new Word(string, string.charAt(n)));
         }
 
-        return answer;
+        list.sort(Comparator.comparing(Word::getN).thenComparing(Word::getWord));
+
+        return list.stream()
+                .map(Word::getWord)
+                .toArray(String[]::new);
+    }
+
+    static class Word {
+        private final String word;
+        private final char n;
+
+        public Word(final String word, final char n) {
+            this.word = word;
+            this.n = n;
+        }
+
+        public String getWord() {
+            return this.word;
+        }
+
+        public char getN() {
+            return this.n;
+        }
     }
 }
