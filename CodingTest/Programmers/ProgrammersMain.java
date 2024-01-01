69a56a20e741290d0d7d5825423e833f5c109c52
HyunggiPark <phg9587@naver.com>
2024-01-01 14:59:06 +0900

    프로그래머스 연습문제 가장 큰 수 - 1차 풀이 - 실패

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -4,7 +4,7 @@ public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(ts.solution(new String[]{"sun", "bed", "car"}, 1));
-        System.out.println(ts.solution(new String[]{"abce", "abcd", "cdx"}, 2));
+        System.out.println(ts.solution(new int[]{6, 10, 2}));
+        System.out.println(ts.solution(new int[]{3, 30, 34, 5, 9}));
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,39 +1,37 @@
 package CodingTest.Programmers;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
+//자리수가 다르면, 짧은 놈의 길이를 기준으로 모든 자리수 비교
+//자리수가 같으면, 그냥 값 비교
 class Solution {
-    public String[] solution(final String[] strings, final int n) {
-        final List<Word> list = new ArrayList<>();
+    public String solution(final int[] numbers) {
+        final Comparator<String> comp = (o1, o2) -> {
+            //길이가 같으면, 내림차순
+            if (o1.length() == o2.length()) {
+                return o2.compareTo(o1);
+            }
 
-        for (final String string : strings) {
-            list.add(new Word(string, string.charAt(n)));
-        }
+            //길이가 다르면, 짧은 놈의 길이를 기준으로 모든 자리수 비교
+            final int minLength = Math.min(o1.length(), o2.length());
 
-        list.sort(Comparator.comparing(Word::getN).thenComparing(Word::getWord));
+            for (int i = 0; i < minLength; i++) {
+                final int v1 = o1.charAt(i) - '0';
+                final int v2 = o2.charAt(i) - '0';
 
-        return list.stream()
-                .map(Word::getWord)
-                .toArray(String[]::new);
-    }
+                if (v1 == v2) continue;
 
-    static class Word {
-        private final String word;
-        private final char n;
+                //내림차순 비교
+                return Integer.compare(v2, v1);
+            }
 
-        public Word(final String word, final char n) {
-            this.word = word;
-            this.n = n;
-        }
+            return Integer.compare(o1.length(), o2.length());
+        };
 
-        public String getWord() {
-            return this.word;
-        }
-
-        public char getN() {
-            return this.n;
-        }
+        return Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .sorted(comp)
+                .reduce((s1, s2) -> s1 + s2).get();
     }
 }
