c8a8ac30db616e5a3e11e6fee0d554b835c56aad
HyunggiPark <phg9587@naver.com>
2024-01-01 17:13:25 +0900

    프로그래머스 연습문제 가장 큰 수 - 정답 확인

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -6,5 +6,12 @@ public class ProgrammersMain {
 
         System.out.println(ts.solution(new int[]{6, 10, 2}));
         System.out.println(ts.solution(new int[]{3, 30, 34, 5, 9}));
+        System.out.println(ts.solution(new int[]{3, 32, 34}));
+        System.out.println(ts.solution(new int[]{0, 0, 1000, 1}));
+        System.out.println(ts.solution(new int[]{87, 878}));
+        System.out.println(ts.solution(new int[]{121, 1211, 12}));
+        System.out.println(ts.solution(new int[]{3, 30}));
+        System.out.println(ts.solution(new int[]{48, 484, 533}));
+        System.out.println(ts.solution(new int[]{34, 3444, 32, 3222}));
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -3,49 +3,15 @@ package CodingTest.Programmers;
 import java.util.Arrays;
 import java.util.Comparator;
 
-//자리수가 다르면, 짧은 놈의 길이를 기준으로 모든 자리수 비교
-//자리수가 같으면, 그냥 값 비교
 class Solution {
     public String solution(final int[] numbers) {
-        final Comparator<String> comp = (o1, o2) -> {
-            //길이가 같으면, 내림차순
-            if (o1.length() == o2.length()) {
-                return o2.compareTo(o1);
-            }
-
-            //길이가 다르면, 모든 자리 수를 비교해야 함
-            //예를 들어, (3, 34) 비교 하면 34가 앞으로 가야함
-            final int bigLength = Math.max(o1.length(), o2.length());
-
-            for (int i = 0; i < bigLength; i++) {
-                int o1Idx = i;
-                int o2Idx = i;
-
-                if (o1Idx >= o1.length()) {
-                    o1Idx = o1.length() - 1;
-                }
-
-                if (o2Idx >= o2.length()) {
-                    o2Idx = o2.length() - 1;
-                }
-
-                final int v1 = o1.charAt(o1Idx) - '0';
-                final int v2 = o2.charAt(o2Idx) - '0';
-
-                if (v1 == v2) {
-                    continue;
-                }
-
-                //내림차순 비교
-                return Integer.compare(v2, v1);
-            }
-
-            return 0;
-        };
+        final Comparator<String> comp = (o1, o2) -> (o2 + o1).compareTo(o1 + o2);
 
         return Arrays.stream(numbers)
                 .mapToObj(String::valueOf)
                 .sorted(comp)
-                .reduce((s1, s2) -> s1 + s2).get();
+                .reduce((s1, s2) -> s1 + s2)
+                .filter(s -> !s.startsWith("0"))
+                .orElse("0");
     }
 }
