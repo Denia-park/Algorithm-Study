7b049b933947094f434f9eca731eae218d707129
HyunggiPark <phg9587@naver.com>
2024-01-02 16:16:26 +0900

    프로그래머스 2021 KAKAO BLIND RECRUITMENT 메뉴 리뉴얼 - 1차 풀이 완료 - 시간은 50분 걸림

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -4,14 +4,8 @@ public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(ts.solution(new int[]{6, 10, 2}));
-        System.out.println(ts.solution(new int[]{3, 30, 34, 5, 9}));
-        System.out.println(ts.solution(new int[]{3, 32, 34}));
-        System.out.println(ts.solution(new int[]{0, 0, 1000, 1}));
-        System.out.println(ts.solution(new int[]{87, 878}));
-        System.out.println(ts.solution(new int[]{121, 1211, 12}));
-        System.out.println(ts.solution(new int[]{3, 30}));
-        System.out.println(ts.solution(new int[]{48, 484, 533}));
-        System.out.println(ts.solution(new int[]{34, 3444, 32, 3222}));
+        System.out.println(ts.solution(new String[]{"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"}, new int[]{2, 3, 4}));
+//        System.out.println(ts.solution(new String[]{"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"}, new int[]{2, 3, 5}));
+//        System.out.println(ts.solution(new String[]{"XYZ", "XWY", "WXA"}, new int[]{2, 3, 4}));
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,17 +1,77 @@
 package CodingTest.Programmers;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
-    public String solution(final int[] numbers) {
-        final Comparator<String> comp = (o1, o2) -> (o2 + o1).compareTo(o1 + o2);
+    Map<String, Integer> foodCountMap;
 
-        return Arrays.stream(numbers)
-                .mapToObj(String::valueOf)
-                .sorted(comp)
-                .reduce((s1, s2) -> s1 + s2)
-                .filter(s -> !s.startsWith("0"))
-                .orElse("0");
+    public String[] solution(final String[] orders, final int[] course) {
+        foodCountMap = new HashMap<>();
+        final List<Integer> courseList = Arrays.stream(course).boxed().collect(Collectors.toList());
+
+        //orders를 기반으로 모든 조합들을 구한다. -> Map에 메뉴 이름을 기준으로 개수를 count한다.
+        for (final String order : orders) {
+            //split 한다.
+            final String[] foods = order.split("");
+
+            //정렬 한다.
+            Arrays.sort(foods);
+
+            //모든 조합들을 map에 저장한다. (최소 조합이 2개)
+            for (int combiLimit = 2; combiLimit <= foods.length; combiLimit++) {
+                combination(foods, combiLimit, 0, "");
+            }
+        }
+
+        //글자 수에 따라 분배를 하고, 정렬을 해서 가장 많이 주문한 조합을 구한다.
+        final Map<Integer, List<String>> collect = foodCountMap.entrySet().stream()
+                .filter(entry -> entry.getValue() >= 2)
+                .collect(Collectors.groupingBy(entry -> entry.getKey().length(), Collectors.mapping(Map.Entry::getKey, Collectors.toList())));
+
+        for (final Map.Entry<Integer, List<String>> entry : collect.entrySet()) {
+            final List<String> strings = entry.getValue();
+            strings.sort((o1, o2) -> foodCountMap.get(o2) - foodCountMap.get(o1));
+        }
+
+        final List<String> answer = new ArrayList<>();
+
+        //answer를 정렬하고, return한다.
+        for (final Map.Entry<Integer, List<String>> entry : collect.entrySet()) {
+            final int combiCount = entry.getKey();
+            final List<String> strings = entry.getValue();
+            final int maxValue = foodCountMap.get(strings.get(0));
+
+            if (!courseList.contains(combiCount)) {
+                continue;
+            }
+
+            for (final String string : strings) {
+                if (foodCountMap.get(string) == maxValue) {
+                    answer.add(string);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        answer.sort(null);
+
+        return answer.toArray(new String[0]);
+    }
+
+    private void combination(final String[] foods, final int limit, final int curIdx, final String curFoodCombination) {
+        if (curFoodCombination.length() == limit) {
+            //map에 저장한다.
+            foodCountMap.put(curFoodCombination, foodCountMap.getOrDefault(curFoodCombination, 0) + 1);
+            return;
+        }
+
+        //2개부터 foods.length개까지 조합을 구한다.
+        for (int addIdx = curIdx; addIdx < foods.length; addIdx++) {
+            final String curFood = foods[addIdx];
+
+            combination(foods, limit, addIdx + 1, curFoodCombination + curFood);
+        }
     }
 }
