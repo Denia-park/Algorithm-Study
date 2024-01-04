7bcd006e5835c9edcb684c0f35c0f2622cc07632
HyunggiPark <phg9587@naver.com>
2024-01-04 13:34:37 +0900

    프로그래머스 이분탐색 입국심사 - 1차 풀이 완료

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,29 +1,9 @@
 package CodingTest.Programmers;
 
-import java.util.Arrays;
-
 public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(Arrays.toString(ts.solution(
-                        new String[]{
-                                "java backend junior pizza 150",
-                                "python frontend senior chicken 210",
-                                "python frontend senior chicken 150",
-                                "cpp backend senior pizza 260",
-                                "java backend junior chicken 80",
-                                "python backend senior chicken 50"
-                        },
-                        new String[]{
-                                "java and backend and junior and pizza 100",
-                                "python and frontend and senior and chicken 200",
-                                "cpp and - and senior and pizza 250",
-                                "- and backend and senior and - 150",
-                                "- and - and - and chicken 100",
-                                "- and - and - and - 150"
-                        }
-                ))
-        );
+        System.out.println(ts.solution(6, new int[]{7, 10}));
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,77 +1,44 @@
 package CodingTest.Programmers;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+/*
+아이디어
+- 이분탐색을 사용한다.
+
+시간복잡도
+- O(N * lnN)
+
+자료형
+- long
+ */
+
+import java.util.Arrays;
 
 class Solution {
-    Map<String, List<Integer>> conditionScoreMap;
+    public long solution(final int n, final int[] times) {
+        long answer = 0;
 
-    public int[] solution(final String[] infos, final String[] query) {
-        conditionScoreMap = new HashMap<>();
+        Arrays.sort(times);
 
-        makeConditions(infos);
+        long left = 0;
+        long right = (long) times[0] * n;
 
-        for (final List<Integer> list : conditionScoreMap.values()) {
-            list.sort(null);
-        }
+        while (left <= right) {
+            final long curTime = left + (right - left) / 2;
 
-        final List<Integer> answer = new ArrayList<>();
+            long completeCheckNum = 0;
 
-        for (final String str : query) {
-            final String editStr = str.replace(" and ", "");
-            final int splitIndex = editStr.lastIndexOf(" ");
-            final int targetScore = Integer.parseInt(editStr.substring(splitIndex + 1));
-            final String targetStr = editStr.substring(0, splitIndex);
+            for (final int eachTime : times) {
+                completeCheckNum += curTime / eachTime;
+            }
 
-            final List<Integer> valueList = conditionScoreMap.getOrDefault(targetStr, new ArrayList<>());
-
-            answer.add(valueList.size() - bisectLeft(valueList, targetScore));
-        }
-
-        return answer.stream().mapToInt(Integer::intValue).toArray();
-    }
-
-    private void makeConditions(final String[] infos) {
-        for (final String info : infos) {
-            final String[] splitInfo = info.split(" ");
-
-            dfs(splitInfo, "", 0);
-        }
-    }
-
-    private void dfs(final String[] splitInfo, final String str, final int curIndex) {
-        //마지막 인덱스
-        if (curIndex == splitInfo.length - 1) {
-            final List<Integer> scoreList = conditionScoreMap.getOrDefault(str, new ArrayList<>());
-
-            final String score = splitInfo[curIndex];
-            scoreList.add(Integer.valueOf(score));
-
-            conditionScoreMap.put(str, scoreList);
-            return;
-        }
-
-        //일반 조합
-        dfs(splitInfo, str + splitInfo[curIndex], curIndex + 1);
-        // - 조합
-        dfs(splitInfo, str + "-", curIndex + 1);
-    }
-
-    private int bisectLeft(final List<Integer> scoreList, final int targetScore) {
-        int left = 0;
-        int right = scoreList.size();
-
-        while (left < right) {
-            final int mid = (left + right) / 2;
-            if (scoreList.get(mid) >= targetScore) {
-                right = mid;
+            if (completeCheckNum >= n) {
+                answer = curTime;
+                right = curTime - 1;
             } else {
-                left = mid + 1;
+                left = curTime + 1;
             }
         }
 
-        return right;
+        return answer;
     }
 }
