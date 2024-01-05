e191b404ce86fed81b2209ff2156fec6e222a841
HyunggiPark <phg9587@naver.com>
2024-01-05 15:27:34 +0900

    프로그래머스 이분탐색 징검다리 - 정답 확인

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -4,6 +4,6 @@ public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(ts.solution(6, new int[]{7, 10}));
+        System.out.println(ts.solution(25, new int[]{2, 14, 11, 21, 17}, 2));
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,5 +1,7 @@
 package CodingTest.Programmers;
 
+import java.util.Arrays;
+
 /*
 아이디어
 - 이분탐색을 사용한다.
@@ -8,37 +10,72 @@ package CodingTest.Programmers;
 - O(N * lnN)
 
 자료형
-- long
+- int
  */
-
-import java.util.Arrays;
-
 class Solution {
-    public long solution(final int n, final int[] times) {
-        long answer = 0;
+    public int solution(final int destinationDistance, final int[] rocksPosition, final int maxDeleteCount) {
+        int answer = 0;
 
-        Arrays.sort(times);
+        //계산을 편리하게 하기 위해서 정렬을 한다.
+        Arrays.sort(rocksPosition);
 
-        long left = 0;
-        long right = (long) times[0] * n;
+        // 모든 바위 사이의 거리를 저장할 배열
+        final int[] distance = new int[rocksPosition.length + 1];
+        distance[0] = rocksPosition[0]; // 0에서 첫번째 바위와의 거리
+        distance[rocksPosition.length] = destinationDistance - rocksPosition[rocksPosition.length - 1]; // 마지막 바위에서 도착지점과의 거리
 
+        // 모든 바위 사이의 거리를 구함
+        for (int i = 1; i < rocksPosition.length; i++) {
+            distance[i] = rocksPosition[i] - rocksPosition[i - 1];
+        }
+
+        //바위들 사이의 거리를 이분탐색으로 조절을 해가면서,
+        //바위들 사이의 거리가 최소 값 중에서 최대가 되도록 한다.
+        int left = 0;
+        int right = destinationDistance;
         while (left <= right) {
-            final long curTime = left + (right - left) / 2;
+            //바위들 사이의 거리의 최소값
+            final int minDistance = left + (right - left) / 2;
 
-            long completeCheckNum = 0;
+            //제거한 바위의 수
+            final int removeCount = getRemoveCount(distance, minDistance);
 
-            for (final int eachTime : times) {
-                completeCheckNum += curTime / eachTime;
+            //제거한 바위의 수가 제거할 수 있는 바위의 수보다 많다면,
+            //거리를 너무 좁게한 것 -> 최소 거리를 늘린다.
+            if (removeCount > maxDeleteCount) {
+                right = minDistance - 1;
             }
-
-            if (completeCheckNum >= n) {
-                answer = curTime;
-                right = curTime - 1;
-            } else {
-                left = curTime + 1;
+            //최소한의 거리 중에 최대값을 찾아야 하므로, 바위를 제대로 지웠어도
+            //최소 거리를 늘려서 다시 시도해본다.
+            else {
+                left = minDistance + 1;
+                answer = Math.max(answer, minDistance);
             }
         }
 
         return answer;
+    }
+
+    private int getRemoveCount(final int[] distance, final int minDistance) {
+        int removeCount = 0; //제거한 바위의 개수
+        int removedDistance = 0; //제거한 바위들 사이의 거리
+
+        //내가 정한 바위 사이의 최소 거리를 기준으로
+        //없애야 하는 바위의 수를 카운트한다.
+        for (final int betweenDistance : distance) {
+            //바위들 사이의 거리가 최소값보다 작다면, 바위를 제거한다.
+            //내가 정한 최소 값의 거리만큼 바위들이 떨어져있을 수 있도록 한다.
+            removedDistance += betweenDistance;
+
+            if (removedDistance < minDistance) {
+                removeCount++;
+            }
+            //바위를 제거한 후, 바위들 사이의 거리를 다시 계산한다.
+            else {
+                removedDistance = 0;
+            }
+        }
+
+        return removeCount;
     }
 }
