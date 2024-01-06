b07808dfa3a92775b86af3749b4811ec082ed458
HyunggiPark <phg9587@naver.com>
2024-01-06 16:39:51 +0900

    leetcode Maximum Profit in Job Scheduling - 정답 확인

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,42 +1,75 @@
 package CodingTest.LeetCode;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println("1 : " + solution.lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
-        System.out.println("2 : " + solution.lengthOfLIS(new int[]{0, 1, 0, 3, 2, 3}));
-        System.out.println("3 : " + solution.lengthOfLIS(new int[]{7, 7, 7, 7, 7, 7, 7}));
+        System.out.println("1 : " + solution.jobScheduling(new int[]{1, 2, 3, 3}, new int[]{3, 4, 5, 6}, new int[]{50, 10, 40, 70}));
     }
 }
 
 class Solution {
-    public int lengthOfLIS(final int[] nums) {
-        final int legnth = nums.length;
-        //dp를 사용한다.
-        final int[] dp = new int[legnth];
-        //모든 부분 수열의 최대 길이는 1이다.
-        Arrays.fill(dp, 1);
+    public int jobScheduling(final int[] startTime, final int[] endTime, final int[] profit) {
+        final int numJobs = profit.length; // Number of jobs
+        final Job[] jobs = initJobs(startTime, endTime, profit);
 
-        //현재 인덱스를 기준으로 이전 인덱스들을 탐색한다.
-        for (int curIdx = 1; curIdx < legnth; curIdx++) {
-            for (int prevIdx = 0; prevIdx < curIdx; prevIdx++) {
-                final int prevValue = nums[prevIdx];
-                final int curValue = nums[curIdx];
+        Arrays.sort(jobs, Comparator.comparingInt(job -> job.endTime));
 
-                //현재 값이 이전 값보다 크다면,
-                //이전 값의 dp에 1을 더한 값과 현재 dp를 비교하여 더 큰 값을 현재 dp에 넣는다.
-                if (prevValue < curValue) {
-                    dp[curIdx] = Math.max(dp[curIdx], dp[prevIdx] + 1);
-                }
+        final int[] dp = new int[numJobs + 1];
+
+        for (int curCount = 1; curCount <= numJobs; curCount++) {
+            final int jobIdx = curCount - 1;
+
+            final int curStartTime = jobs[jobIdx].startTime;
+            final int curProfit = jobs[jobIdx].profit;
+
+            final int latestNonConflictJobIndex = upperBound(jobs, jobIdx, curStartTime);
+            
+            dp[curCount] = Math.max(dp[curCount - 1], dp[latestNonConflictJobIndex] + curProfit);
+        }
+
+        return dp[numJobs];
+    }
+
+    private Job[] initJobs(final int[] startTime, final int[] endTime, final int[] profit) {
+        final int numJobs = profit.length; // Number of jobs
+        final Job[] jobs = new Job[numJobs];
+
+        for (int i = 0; i < numJobs; ++i) {
+            jobs[i] = new Job(endTime[i], startTime[i], profit[i]);
+        }
+
+        return jobs;
+    }
+
+    private int upperBound(final Job[] jobs, final int endIndex, final int targetTime) {
+        int low = 0;
+        int high = endIndex;
+
+        while (low < high) {
+            final int mid = (low + high) / 2;
+            if (jobs[mid].endTime <= targetTime) {
+                low = mid + 1;
+            } else {
+                high = mid;
             }
         }
 
-        //dp의 최대값을 반환한다.
-        return Arrays.stream(dp)
-                .max()
-                .orElse(0);
+        return high;
+    }
+
+    private static class Job {
+        int endTime;
+        int startTime;
+        int profit;
+
+        public Job(final int endTime, final int startTime, final int profit) {
+            this.endTime = endTime;
+            this.startTime = startTime;
+            this.profit = profit;
+        }
     }
 }
