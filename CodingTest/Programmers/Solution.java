6dbb0449dff1ce8e10bca41e9a1ddecebfbd082b
HyunggiPark <phg9587@naver.com>
2024-01-06 12:35:52 +0900

    프로그래머스 코딩테스트 입문 중복된 문자 제거 - 1차 풀이

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,37 +1,24 @@
 package CodingTest.Programmers;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
-    public int solution(final int[][] dots) {
-        int answer = 0;
+    public String solution(final String my_string) {
+        final StringBuilder sb = new StringBuilder();
+        final Set<Character> set = new HashSet<>();
 
-        //0,1 / 2,3
-        double a = getLeanDegree(dots[0], dots[1]);
-        double b = getLeanDegree(dots[2], dots[3]);
+        for (int i = 0; i < my_string.length(); i++) {
+            final char ch = my_string.charAt(i);
 
-        if (a == b) {
-            answer = 1;
+            if (set.contains(ch)) {
+                continue;
+            }
+
+            set.add(ch);
+            sb.append(ch);
         }
 
-        //0,2 / 1,3
-        a = getLeanDegree(dots[0], dots[2]);
-        b = getLeanDegree(dots[1], dots[3]);
-
-        if (a == b) {
-            answer = 1;
-        }
-
-        //0,3 / 1,2
-        a = getLeanDegree(dots[0], dots[3]);
-        b = getLeanDegree(dots[1], dots[2]);
-
-        if (a == b) {
-            answer = 1;
-        }
-
-        return answer;
-    }
-
-    private double getLeanDegree(final int[] point1, final int[] point2) {
-        return (double) (point1[1] - point2[1]) / (point1[0] - point2[0]);
+        return sb.toString();
     }
 }
