4c3fe42f175a424b90b10e62783f8d9ac6ea2f1c
HyunggiPark <phg9587@naver.com>
2024-01-09 14:07:17 +0900

    프로그래머스 동적계획법(Dynamic Programming) 정수 삼각형 - 리팩터링

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,30 +1,27 @@
 package CodingTest.Programmers;
 
-import java.util.Arrays;
-
 class Solution {
     public int solution(final int[][] triangle) {
         final int length = triangle.length;
+        int maxValue = Integer.MIN_VALUE;
 
         final int[][] dp = new int[length][length];
         dp[0][0] = triangle[0][0];
 
         for (int row = 1; row < dp.length; row++) {
-            for (int col = 0; col < row + 1; col++) {
+            dp[row][0] = dp[row - 1][0] + triangle[row][0];
+
+            for (int col = 1; col < row + 1; col++) {
                 final int curValue = triangle[row][col];
+
                 final int prevRowValue = dp[row - 1][col];
-
-                if (col == 0) {
-                    dp[row][col] = prevRowValue + curValue;
-                    continue;
-                }
-
                 final int diagonalPrevRowValue = dp[row - 1][col - 1];
 
                 dp[row][col] = Math.max(prevRowValue, diagonalPrevRowValue) + curValue;
+                maxValue = Math.max(maxValue, dp[row][col]);
             }
         }
 
-        return Arrays.stream(dp[length - 1]).max().getAsInt();
+        return maxValue;
     }
 }
