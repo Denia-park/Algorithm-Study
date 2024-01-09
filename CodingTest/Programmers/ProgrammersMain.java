22e3405c6abdd48622b2cbd3e3c0844f0b1a25b6
HyunggiPark <phg9587@naver.com>
2024-01-09 14:04:18 +0900

    프로그래머스 동적계획법(Dynamic Programming) 정수 삼각형 - 1차 풀이 완료

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -4,7 +4,7 @@ public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(ts.solution(3));
-        System.out.println(ts.solution(5));
+        System.out.println(ts.solution(BracketUtil.convertStringToJavaIntTwoDimensionalArray("[[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]")));
+
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,18 +1,30 @@
 package CodingTest.Programmers;
 
+import java.util.Arrays;
+
 class Solution {
-    static final int DIV_NUM = 1234567;
+    public int solution(final int[][] triangle) {
+        final int length = triangle.length;
 
-    public int solution(final int n) {
-        final int[] dp = new int[n + 1];
+        final int[][] dp = new int[length][length];
+        dp[0][0] = triangle[0][0];
 
-        dp[0] = 0;
-        dp[1] = 1;
+        for (int row = 1; row < dp.length; row++) {
+            for (int col = 0; col < row + 1; col++) {
+                final int curValue = triangle[row][col];
+                final int prevRowValue = dp[row - 1][col];
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] += (dp[i - 1] + dp[i - 2]) % DIV_NUM;
+                if (col == 0) {
+                    dp[row][col] = prevRowValue + curValue;
+                    continue;
+                }
+
+                final int diagonalPrevRowValue = dp[row - 1][col - 1];
+
+                dp[row][col] = Math.max(prevRowValue, diagonalPrevRowValue) + curValue;
+            }
         }
 
-        return dp[n];
+        return Arrays.stream(dp[length - 1]).max().getAsInt();
     }
 }
