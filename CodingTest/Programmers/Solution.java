798f91e6eb418e2a541fa22d66c5654e1e00ea33
HyunggiPark <phg9587@naver.com>
2024-01-10 17:16:08 +0900

    프로그래머스 동적계획법(Dynamic Programming) 등굣길 - 2차 풀이 - 효율성 부분 때문에 정답 확인

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,34 +1,29 @@
 package CodingTest.Programmers;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
+    private static final int MOD = 1000000007;
+
     public int solution(final int col, final int row, final int[][] puddles) {
         final int[][] dp = new int[row + 1][col + 1];
-        dp[1][1] = 1;
+        dp[0][1] = 1;
 
-        final Set<String> checkSet = new HashSet<>();
-        checkSet.add("1,1");
         for (final int[] puddle : puddles) {
             final int puddleCol = puddle[0];
             final int puddleRow = puddle[1];
 
-            checkSet.add(puddleCol + "," + puddleRow);
+            dp[puddleRow][puddleCol] = -1;
         }
 
         for (int rowIdx = 1; rowIdx < row + 1; rowIdx++) {
             for (int colIdx = 1; colIdx < col + 1; colIdx++) {
-                if (checkSet.contains(colIdx + "," + rowIdx)) {
+                if (dp[rowIdx][colIdx] == -1) {
+                    dp[rowIdx][colIdx] = 0;
                     continue;
                 }
 
-                dp[rowIdx][colIdx] = dp[rowIdx - 1][colIdx] + dp[rowIdx][colIdx - 1];
+                dp[rowIdx][colIdx] = (dp[rowIdx - 1][colIdx] + dp[rowIdx][colIdx - 1]) % MOD;
             }
         }
-
-        System.out.println(Arrays.deepToString(dp));
 
         return dp[row][col];
     }
