069ce8700fecee846a553c080407eecb9f6ace6d
HyunggiPark <phg9587@naver.com>
2024-01-10 15:59:19 +0900

    leetcode Amount of Time for Binary Tree to Be Infected - 정답 참고 [Convert to Graph and Breadth-First Search]

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,42 +1,106 @@
 package CodingTest.LeetCode;
 
+import java.util.*;
+
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println("1 : " + solution.amountOfTime(null, 3));
+        final TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.left.right.left = new TreeNode(9);
+        root.left.right.right = new TreeNode(2);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(6);
+
+        System.out.println("1 : " + solution.amountOfTime(root, 3));
+
+        final TreeNode root2 = new TreeNode(1);
+        System.out.println("2 : " + solution.amountOfTime(root2, 1));
     }
 }
 
 class Solution {
-    private int maxDistance = 0;
+    Map<Integer, Set<Integer>> graph;
 
     public int amountOfTime(final TreeNode root, final int start) {
-        traverse(root, start);
+        graph = new HashMap<>();
+        binaryTreeToGraph(root);
+
+        System.out.println(graph);
+
+        return bfs(start);
+    }
+
+    private void binaryTreeToGraph(final TreeNode root) {
+        //자식 연결하기
+        final int rootVal = root.val;
+
+        //왼쪽 자식 연결하기
+        final TreeNode leftChild = root.left;
+        if (leftChild != null) {
+            connectChild(leftChild, rootVal);
+            binaryTreeToGraph(leftChild);
+        }
+
+        //오른쪽 자식 연결하기
+        final TreeNode rightChild = root.right;
+        if (rightChild != null) {
+            connectChild(rightChild, rootVal);
+            binaryTreeToGraph(rightChild);
+        }
+    }
+
+    private void connectChild(final TreeNode child, final int rootVal) {
+        final Set<Integer> rootSet = graph.getOrDefault(rootVal, new HashSet<>());
+
+        final int childVal = child.val;
+        rootSet.add(childVal);
+        graph.put(rootVal, rootSet);
+
+        final Set<Integer> leftSet = graph.getOrDefault(childVal, new HashSet<>());
+        leftSet.add(rootVal);
+        graph.put(childVal, leftSet);
+    }
+
+    private int bfs(final int start) {
+        int maxDistance = 0;
+
+        final Set<Integer> isVisited = new HashSet<>();
+
+        final Deque<MyNode> deque = new ArrayDeque<>();
+        deque.add(new MyNode(start, 0));
+        isVisited.add(start);
+
+        while (!deque.isEmpty()) {
+            final MyNode current = deque.pollFirst();
+            final Set<Integer> currentSet = graph.get(current.val);
+            final int currentDistance = current.distance;
+            maxDistance = Math.max(maxDistance, currentDistance);
+
+            if (currentSet == null) continue;
+
+            for (final int next : currentSet) {
+                if (!isVisited.contains(next)) {
+                    deque.add(new MyNode(next, currentDistance + 1));
+                    isVisited.add(next);
+                }
+            }
+        }
+
         return maxDistance;
     }
 
-    public int traverse(final TreeNode root, final int start) {
-        int depth = 0;
-        if (root == null) {
-            return depth;
+    class MyNode {
+        int val;
+        int distance;
+
+        public MyNode(final int val, final int distance) {
+            this.val = val;
+            this.distance = distance;
         }
-
-        final int leftDepth = traverse(root.left, start);
-        final int rightDepth = traverse(root.right, start);
-
-        if (root.val == start) {
-            maxDistance = Math.max(leftDepth, rightDepth);
-            depth = -1;
-        } else if (leftDepth >= 0 && rightDepth >= 0) {
-            depth = Math.max(leftDepth, rightDepth) + 1;
-        } else {
-            final int distance = Math.abs(leftDepth) + Math.abs(rightDepth);
-            maxDistance = Math.max(maxDistance, distance);
-            depth = Math.min(leftDepth, rightDepth) - 1;
-        }
-
-        return depth;
     }
 }
 
