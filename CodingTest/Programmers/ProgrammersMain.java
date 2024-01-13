34e5e0a995f86c1104f59ff03b6a1b27facdafd5
HyunggiPark <phg9587@naver.com>
2024-01-13 22:33:05 +0900

    프로그래머스 스택/큐 올바른 괄호 - 1차 풀이 완료

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -4,7 +4,9 @@ public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(ts.solution(new String[]{"1", "-", "3", "+", "5", "-", "8"}));
-        System.out.println(ts.solution(new String[]{"5", "-", "3", "+", "1", "+", "2", "-", "4"}));
+        System.out.println(ts.solution("()()"));
+        System.out.println(ts.solution("(())()"));
+        System.out.println(ts.solution(")()("));
+        System.out.println(ts.solution("(()("));
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,73 +1,32 @@
 package CodingTest.Programmers;
 
-//정답 해설
-//https://school.programmers.co.kr/questions/35224
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 class Solution {
-    public int solution(final String[] arr) {
-        // 숫자와 연산자를 분리해서 저장
-        // numbers : 숫자를 저장하는 리스트
-        // operators : 연산자를 저장하는 리스트
-        final List<Integer> numbers = new ArrayList<>();
-        final List<String> operators = new ArrayList<>();
+    boolean solution(final String s) {
+        final Deque<Character> deque = new ArrayDeque<>();
 
-        // arr의 원소를 순회하면서, 숫자와 연산자를 분리해서 numbers와 operators에 저장
-        for (final String str : arr) {
-            if (str.equals("+") || str.equals("-")) {
-                operators.add(str);
-            } else {
-                numbers.add(Integer.parseInt(str));
+        final char[] charArr = s.toCharArray();
+
+        for (final char ch : charArr) {
+            if (ch == '(') {
+                deque.push(ch);
+                continue;
             }
+
+            if (deque.isEmpty()) {
+                return false;
+            }
+
+            final char top = deque.peek();
+            if (top == ')') {
+                return false;
+            }
+
+            deque.pop();
         }
 
-        final int numsLength = numbers.size();
-
-        // maxDp[i][j] : i번째 숫자부터 j번째 숫자까지의 연산 결과의 최대값
-        final int[][] maxDp = new int[numsLength][numsLength];
-        // minDp[i][j] : i번째 숫자부터 j번째 숫자까지의 연산 결과의 최소값
-        final int[][] minDp = new int[numsLength][numsLength];
-
-        // step을 조금씩 늘려가면서 계산 (bottom-up 방식)
-        for (int step = 0; step < numsLength; step++) {
-            // startIdx는 0부터 numsLength-step까지 이동 가능
-            for (int startIdx = 0; startIdx < numsLength - step; startIdx++) {
-                // endIdx는 startIdx에서 step만큼 떨어진 위치
-                final int endIdx = startIdx + step;
-
-                if (startIdx == endIdx) { // startIdx == endIdx인 경우, 해당 숫자를 그대로 사용
-                    maxDp[startIdx][endIdx] = numbers.get(startIdx);
-                    minDp[startIdx][endIdx] = numbers.get(startIdx);
-                } else { // startIdx != endIdx인 경우, startIdx ~ endIdx까지의 연산 결과를 구해야 함
-                    //초기값 설정
-                    int max = Integer.MIN_VALUE;
-                    int min = Integer.MAX_VALUE;
-
-                    // 괄호를 한칸씩 이동하면서, 최대값과 최소값을 구함
-                    // bracketIdx는 괄호의 위치를 의미, 괄호의 위치를 기준으로 왼쪽과 오른쪽으로 나누어서 계산
-                    // 괄호는 startIdx ~ endIdx-1까지 이동 가능 (숫자 및 연산자의 위치는 고정이, 연산자는 숫자보다 개수가 1개 적음)
-                    for (int bracketIdx = startIdx; bracketIdx < endIdx; bracketIdx++) {
-                        final String op = operators.get(bracketIdx);
-
-                        if (op.equals("+")) { // 덧셈인 경우
-                            max = Math.max(max, maxDp[startIdx][bracketIdx] + maxDp[bracketIdx + 1][endIdx]);
-                            min = Math.min(min, minDp[startIdx][bracketIdx] + minDp[bracketIdx + 1][endIdx]);
-                        } else { // 뺄셈인 경우
-                            max = Math.max(max, maxDp[startIdx][bracketIdx] - minDp[bracketIdx + 1][endIdx]);
-                            min = Math.min(min, minDp[startIdx][bracketIdx] - maxDp[bracketIdx + 1][endIdx]);
-                        }
-                    }
-
-                    // 최대값과 최소값을 저장
-                    maxDp[startIdx][endIdx] = max;
-                    minDp[startIdx][endIdx] = min;
-                }
-            }
-        }
-
-        // 최종 결과는 0번째 숫자부터 마지막 숫자까지의 연산 결과의 최대값
-        return maxDp[0][numsLength - 1];
+        return deque.isEmpty();
     }
 }
