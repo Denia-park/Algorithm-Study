928293e3ea402acdd4665d6c881f5007e349dfe8
HyunggiPark <phg9587@naver.com>
2024-01-14 10:28:26 +0900

    프로그래머스 월간 코드 챌린지 시즌2 괄호 회전하기 - 1차 풀이 완료

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -4,9 +4,9 @@ public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(ts.solution("()()"));
-        System.out.println(ts.solution("(())()"));
-        System.out.println(ts.solution(")()("));
-        System.out.println(ts.solution("(()("));
+        System.out.println(ts.solution("[](){}"));
+        System.out.println(ts.solution("}]()[{"));
+        System.out.println(ts.solution("[)(]"));
+        System.out.println(ts.solution("}}}"));
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -4,29 +4,48 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 class Solution {
-    boolean solution(final String s) {
-        final Deque<Character> deque = new ArrayDeque<>();
+    public int solution(final String s) {
+        int answer = 0;
 
-        final char[] charArr = s.toCharArray();
+        final int length = s.length();
+        final String newStr = s + s;
 
-        for (final char ch : charArr) {
-            if (ch == '(') {
-                deque.push(ch);
+        //length만큼 반복 -> 왼쪽으로 이동
+        //매번 스택이 되는지 확인
+        for (int startIdx = 0; startIdx < s.length(); startIdx++) {
+            final String checkStr = newStr.substring(startIdx, startIdx + length);
+
+            if (isRightBracket(checkStr)) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean isRightBracket(final String checkStr) {
+        final Deque<Character> stack = new ArrayDeque<>();
+        final char[] chars = checkStr.toCharArray();
+
+        for (final char ch : chars) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
                 continue;
             }
 
-            if (deque.isEmpty()) {
+            if (stack.isEmpty()) {
                 return false;
             }
 
-            final char top = deque.peek();
-            if (top == ')') {
+            final char pop = stack.pop();
+
+            if (ch == ')' && pop != '('
+                    || ch == '}' && pop != '{'
+                    || ch == ']' && pop != '[') {
                 return false;
             }
-
-            deque.pop();
         }
 
-        return deque.isEmpty();
+        return stack.isEmpty();
     }
 }
