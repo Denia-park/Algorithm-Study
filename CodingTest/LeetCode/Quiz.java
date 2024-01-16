d591926dfacbf759d96b30b3297743f133eb9049
HyunggiPark <phg9587@naver.com>
2024-01-16 12:55:49 +0900

    leetcode Insert Delete GetRandom O(1) - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,41 +1,46 @@
 package CodingTest.LeetCode;
 
-import CodingTest.Programmers.BracketUtil;
+import CodingTest.HackerRank.Solution;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println("1 : " + solution.findWinners(BracketUtil.convertStringToJavaIntTwoDimensionalArray("[[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]")));
-        System.out.println("2 : " + solution.findWinners(BracketUtil.convertStringToJavaIntTwoDimensionalArray("[[2,3],[1,3],[5,4],[6,4]]")));
+//        System.out.println("1 : " + solution.findWinners(BracketUtil.convertStringToJavaIntTwoDimensionalArray("[[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]")));
+//        System.out.println("2 : " + solution.findWinners(BracketUtil.convertStringToJavaIntTwoDimensionalArray("[[2,3],[1,3],[5,4],[6,4]]")));
     }
 }
 
-class Solution {
-    public List<List<Integer>> findWinners(final int[][] matches) {
-        final Set<Integer> winner = new HashSet<>();
-        final Map<Integer, Integer> loser = new HashMap<>();
+class RandomizedSet {
+    Set<Integer> set;
 
-        for (final int[] match : matches) {
-            final int winnerNum = match[0];
-            final int loserNum = match[1];
+    public RandomizedSet() {
+        set = new HashSet<>();
+    }
 
-            winner.add(winnerNum);
-            loser.put(loserNum, loser.getOrDefault(loserNum, 0) + 1);
-        }
+    public boolean insert(final int val) {
+        return set.add(val);
+    }
 
-        return List.of(
-                winner.stream()
-                        .filter(winnerNum -> !loser.containsKey(winnerNum))
-                        .sorted()
-                        .collect(Collectors.toList()),
-                loser.keySet().stream()
-                        .filter(loserNum -> loser.get(loserNum) < 2)
-                        .sorted()
-                        .collect(Collectors.toList())
-        );
+    public boolean remove(final int val) {
+        return set.remove(val);
+    }
+
+    public int getRandom() {
+        final List<Integer> list = List.copyOf(set);
+
+        return list.get((int) (Math.random() * list.size()));
     }
 }
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
