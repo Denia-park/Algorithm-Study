8267c2080c2c048269b65f4f4bb4dbf5e608fd0c
HyunggiPark <phg9587@naver.com>
2024-01-18 11:17:50 +0900

    leetcode Climbing Stairs - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,38 +1,24 @@
 package CodingTest.LeetCode;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println("1 : " + solution.uniqueOccurrences(new int[]{1, 2, 2, 1, 1, 3}));
-        System.out.println("2 : " + solution.uniqueOccurrences(new int[]{1, 2}));
-        System.out.println("3 : " + solution.uniqueOccurrences(new int[]{-3, 0, 1, -3, 1, 1, 1, -3, 10, 0}));
+        System.out.println("1 : " + solution.climbStairs(2));
+        System.out.println("2 : " + solution.climbStairs(3));
     }
 }
 
 class Solution {
-    public boolean uniqueOccurrences(final int[] arr) {
-        final Map<Integer, Integer> map = new HashMap<>();
+    public int climbStairs(final int n) {
+        final int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
 
-        for (final int val : arr) {
-            map.put(val, map.getOrDefault(val, 0) + 1);
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
         }
 
-        final Set<Integer> occurrences = new HashSet<>();
-
-        for (final Integer occur : map.values()) {
-            if (occurrences.contains(occur)) {
-                return false;
-            }
-
-            occurrences.add(occur);
-        }
-
-        return true;
+        return dp[n];
     }
 }
