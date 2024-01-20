b91e44348178e9b2c6253813c15d408c7a067e8f
HyunggiPark <phg9587@naver.com>
2024-01-20 12:38:28 +0900

    leetcode Sum of Subarray Minimums - 4차 풀이 완료 - 시간 초과

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -20,41 +20,23 @@ class Solution {
         final int len = arr.length;
 
         //start ~ end 까지의 최소 값
-        final int[][] dp = new int[len][len];
+        final int[] dp = new int[len + 1];
 
-        //최소 값을 구해야 하므로
-        //자기 자신의 subarray 제외하고는, 처음엔 다 최대 값 넣기
-        for (int start = 0; start < len; start++) {
-            for (int end = 0; end < len; end++) {
-                if (start != end) {
-                    dp[start][end] = Integer.MAX_VALUE;
-                } else {
-                    dp[start][end] = arr[start];
-                }
-            }
-        }
+        int answer = 0;
 
         //부분 수열의 개수를 늘리면서, 최소 값을 구하기
-        for (int count = 1; count < len; count++) {
-            for (int start = 0; start < len; start++) {
-                for (int end = start + count; end < len; end++) {
-                    dp[start][end] = Math.min(dp[start][end - 1], dp[end][end]);
+        for (int startIdx = 1; startIdx <= len; startIdx++) {
+            for (int endIdx = startIdx; endIdx <= len; endIdx++) {
+                if (startIdx == endIdx) {
+                    dp[endIdx] = arr[startIdx - 1];
+                } else {
+                    dp[endIdx] = Math.min(dp[endIdx - 1], arr[endIdx - 1]);
                 }
+
+                answer = (answer + dp[endIdx]) % MOD;
             }
         }
 
-        //Integer.MaxValue가 아닌 값들만 구해서 더하면 최소 값이 된다.
-        long sum = 0;
-        for (final int[] ints : dp) {
-            for (final int val : ints) {
-                if (val == Integer.MAX_VALUE) {
-                    continue;
-                }
-
-                sum = (sum + val) % MOD;
-            }
-        }
-
-        return (int) sum;
+        return answer;
     }
 }
