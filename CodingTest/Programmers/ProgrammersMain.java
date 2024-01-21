c2ca0fe88d9f9226a98ae01976abcd54e00fcfa0
HyunggiPark <phg9587@naver.com>
2024-01-21 16:39:18 +0900

    프로그래머스 2019 KAKAO BLIND RECRUITMENT 길 찾기 게임 - 1차 풀이 완료 - 2시간 걸림 ..
    - 문제 조건을 읽고 시간복잡도 계산 후에 그냥 이진탐색트리 따라서 계산했으면 바로 풀었는데, 혼자서 이상한 생각 가지고 풀이법 만들어 내다가 풀이도 꼬이고 시간도 엄청 썼다 .ㅠㅠ

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,9 +1,16 @@
 package CodingTest.Programmers;
 
+import java.util.Arrays;
+
 public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(ts.solution(new int[]{6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0}));
+        System.out.println(Arrays.deepToString(ts.solution(
+                        BracketUtil.convertStringToJavaIntTwoDimensionalArray(
+                                "[[5,3],[11,5],[13,3],[3,5],[6,1],[1,3],[8,6],[7,2],[2,2]]"
+                        )
+                ))
+        );
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,96 +1,149 @@
 package CodingTest.Programmers;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 class Solution {
+    public int[][] solution(final int[][] nodeinfo) {
+        //이진 트리 만들기 전 정렬할 List
+        final List<Point> preTreeList = new ArrayList<>();
 
-    //방향 배열 설정
-    final int[][] directions = new int[][]{
-            {-1, 0},
-            {-1, 1},
-            {0, 1},
-            {1, 1},
-            {1, 0},
-            {1, -1},
-            {0, -1},
-            {-1, -1}
-    };
+        //Point 삽입
+        final int length = nodeinfo.length;
+        for (int i = 0; i < length; i++) {
+            final int[] ints = nodeinfo[i];
+            final int x = ints[0];
+            final int y = ints[1];
 
-    public int solution(final int[] arrows) {
-        int answer = 0;
+            preTreeList.add(new Point(i + 1, x, y));
+        }
 
-        //시작 노드
-        Node curNode = new Node(0, 0);
+        //preTreeList 정렬 -> Y가 높은 순, x가 작은 순
+        preTreeList.sort(Comparator.comparingInt(Point::getY).reversed().thenComparingInt(Point::getX));
 
-        //방문 여부 관련 선언
-        //key = 시작 노드, value = 연결 노드
-        final Map<Node, Set<Node>> visited = new HashMap<>();
+        //높이 따라 List로 Point 정리하기
+        final List<List<Point>> heightSortedPoints = new ArrayList<>();
 
-        for (final int arrow : arrows) {
-            //교차점 처리를 위한 스케일 업
-            for (int count = 0; count < 2; count++) {
-                final Node nextNode = new Node(
-                        curNode.row + directions[arrow][0],
-                        curNode.col + directions[arrow][1]
-                );
+        int curHeight = preTreeList.get(0).y;
+        List<Point> tempList = new ArrayList<>();
+        for (final Point point : preTreeList) {
+            if (curHeight == point.getY()) {
+                tempList.add(point);
+            } else {
+                heightSortedPoints.add(tempList);
+                tempList = new ArrayList<>();
 
-                //처음 방문하는 경우 => map에 키 값이 없는 경우
-                if (!visited.containsKey(nextNode)) {
-                    //리스트에 연결되었다고 표시 (리스트에 값 추가)
-                    visited.put(nextNode, makeNewSet(curNode));
-
-                    if (visited.containsKey(curNode)) {
-                        visited.get(curNode).add(nextNode);
-                    } else {
-                        visited.put(curNode, makeNewSet(nextNode));
-                    }
-                }
-
-                //해당 노드에 이미 방문했고, 간선을 처음 통과하는 경우
-                else if (!visited.get(nextNode).contains(curNode)) {
-                    visited.get(nextNode).add(curNode);
-                    visited.get(curNode).add(nextNode);
-
-                    answer++;
-                }
-
-                //방에 대한 처리를 했으므로
-                //점을 이동시켜야 함
-                curNode = nextNode;
+                tempList.add(point);
+                curHeight = point.getY();
             }
         }
 
-        return answer;
+        //남은 List 추가하기
+        heightSortedPoints.add(tempList);
+
+        final List<Point> points = heightSortedPoints.get(0);
+        final Point root = points.get(0);
+
+        for (int curChildIdx = 1; curChildIdx < heightSortedPoints.size(); curChildIdx++) {
+            //추가해야 하는 자식 List
+            final List<Point> childList = heightSortedPoints.get(curChildIdx);
+
+            for (final Point child : childList) {
+                final int x = child.x;
+                Point curPoint = root;
+                while (true) {
+                    if (curPoint.left != null && x < curPoint.x) {
+                        curPoint = curPoint.left;
+                    } else if (x < curPoint.x) {
+                        curPoint.left = child;
+                        break;
+                    } else if (curPoint.right != null && x > curPoint.x) {
+                        curPoint = curPoint.right;
+                    } else {
+                        curPoint.right = child;
+                        break;
+                    }
+                }
+            }
+        }
+
+        //전위 순회
+        final List<Integer> preOrders = new ArrayList<>();
+        calculatePreOrderList(root, preOrders);
+
+        final int[] preOrderInts = new int[length];
+        for (int i = 0; i < length; i++) {
+            preOrderInts[i] = preOrders.get(i);
+        }
+
+        //후위 순회
+        final List<Integer> postOrders = new ArrayList<>();
+        calculateetPostOrderList(root, postOrders);
+
+        final int[] postOrderInts = new int[length];
+        for (int i = 0; i < length; i++) {
+            postOrderInts[i] = postOrders.get(i);
+        }
+
+        return new int[][]{
+                preOrderInts,
+                postOrderInts
+        };
     }
 
-    private Set<Node> makeNewSet(final Node nextNode) {
-        final Set<Node> returnList = new HashSet<>();
+    private void calculatePreOrderList(final Point root, final List<Integer> preOrders) {
+        if (root == null) {
+            return;
+        }
 
-        returnList.add(nextNode);
+        preOrders.add(root.number);
 
-        return returnList;
+        calculatePreOrderList(root.left, preOrders);
+
+        calculatePreOrderList(root.right, preOrders);
     }
 
-    static class Node {
-        int row;
-        int col;
+    private void calculateetPostOrderList(final Point root, final List<Integer> postOrders) {
+        if (root == null) {
+            return;
+        }
 
-        Node(final int row, final int col) {
-            this.row = row;
-            this.col = col;
+        calculateetPostOrderList(root.left, postOrders);
+
+        calculateetPostOrderList(root.right, postOrders);
+
+        postOrders.add(root.number);
+    }
+
+    static class Point {
+        int number;
+        int x;
+        int y;
+        Point left;
+        Point right;
+
+        public Point(final int number, final int x, final int y) {
+            this.number = number;
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
         }
 
         @Override
-        public boolean equals(final Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            final Node node = (Node) o;
-            return row == node.row && col == node.col;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(row, col);
+        public String toString() {
+            return "Point{" +
+                    "number=" + number +
+                    ", x=" + x +
+                    ", y=" + y +
+                    '}';
         }
     }
 }
