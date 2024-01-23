cd2e1a626bdcf87bd2aecaea9fd55fc75ef0ca21
HyunggiPark <phg9587@naver.com>
2024-01-23 12:21:58 +0900

    leetcode  Maximum Length of a Concatenated String with Unique Characters - 정답 참고하여 dfs 수정 버전

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,8 +1,5 @@
 package CodingTest.LeetCode;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
 import java.util.List;
 
 public class Quiz {
@@ -32,56 +29,38 @@ public class Quiz {
  */
 
 class Solution {
-    List<String> combi;
+
+    private int answer;
 
     public int maxLength(final List<String> arr) {
-        combi = new ArrayList<>();
+        answer = 0;
 
-        //백 트래킹으로 구하자. -> 모든 조합 구하기
-        for (int combiCount = 1; combiCount <= arr.size(); combiCount++) {
-            getCombi(-1, arr, combiCount, new ArrayDeque<>());
-        }
-
-        int answer = 0;
-        //각 조합에 대해서 unique 한지 확인하기, 가능 하면 max 값 업데이트
-        for (final String target : combi) {
-            if (isUnique(target)) {
-                answer = Math.max(answer, target.length());
-            }
-        }
+        getCombi(arr, 0, "");
 
         return answer;
     }
 
+    private void getCombi(final List<String> arr, final int startIdx, final String path) {
+        if (!isUnique(path)) return;
+
+        answer = Math.max(answer, path.length());
+
+        for (int idx = startIdx; idx < arr.size(); idx++) {
+            getCombi(arr, idx + 1, path + arr.get(idx));
+        }
+    }
+
     private boolean isUnique(final String target) {
         final int[] arr = new int[26];
-        final char[] chars = target.toCharArray();
 
-        for (final char ch : chars) {
+        for (final char ch : target.toCharArray()) {
             final int curIdx = ch - 'a';
 
-            if (arr[curIdx] > 0) {
-                return false;
-            }
+            if (arr[curIdx] > 0) return false;
 
             arr[curIdx]++;
         }
 
         return true;
-    }
-
-    private void getCombi(final int startIdx, final List<String> arr, final int combiCount, final Deque<String> saveQueue) {
-        if (saveQueue.size() == combiCount) {
-            combi.add(String.join("", saveQueue));
-            return;
-        }
-
-        for (int idx = startIdx + 1; idx < arr.size(); idx++) {
-            final String curString = arr.get(idx);
-
-            saveQueue.addLast(curString);
-            getCombi(idx, arr, combiCount, saveQueue);
-            saveQueue.pollLast();
-        }
     }
 }
