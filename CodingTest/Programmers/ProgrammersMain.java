24a5bb8c9538fc108e52874a83b3271aee505c11
HyunggiPark <phg9587@naver.com>
2024-01-23 13:40:45 +0900

    프로그래머스 힙(Heap) 이중우선순위큐 - 디스크 컨트롤러 - 1차 풀이 실패

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,20 +1,14 @@
 package CodingTest.Programmers;
 
-import java.util.Arrays;
-
 public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(Arrays.toString(ts.solution(
-                        new String[]{"I 16", "I -5643", "D - 1", "D 1", "D 1", "I 123", "D - 1"}
+        System.out.println((ts.solution(
+                        BracketUtil.convertStringToJavaIntTwoDimensionalArray(
+                                "[[0, 3], [1, 9], [2, 6]]"
+                        )
                 ))
         );
-        System.out.println(Arrays.toString(ts.solution(
-                        new String[]{"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"}
-                ))
-        );
-
-
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,100 +1,83 @@
 package CodingTest.Programmers;
 
-import java.util.*;
+/*
+아이디어
+- 그리디 (정렬해서 처리하기)
+
+시간 복잡도
+- n * log n
+
+자료 구조
+- List 정렬을 사용 (Comparator)
+ */
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.OptionalDouble;
 
 class Solution {
-    public int[] solution(final String[] operations) {
-        final DoublePriQue dq = new DoublePriQue();
-
-        for (final String oper : operations) {
-            final String[] split = oper.split(" " );
-            final String command = split[0];
-            final String number = split[1];
-
-            if (command.equals("I" )) {
-                dq.add(Integer.valueOf(number)); // 삽입
-            } else {
-                if (number.equals("1" )) {
-                    dq.delMax(); //최대 값 삭제
-                } else {
-                    dq.delMin(); //최소 값 삭제
-                }
-            }
+    public int solution(final int[][] jobs) {
+        final List<Disk> disks = new ArrayList<>();
+        //jobs를 Disk List로 변경
+        for (final int[] job : jobs) {
+            disks.add(new Disk(job[0], job[0] + job[1]));
         }
 
-        if (dq.isEmpty()) {
-            return new int[]{0, 0};
+        disks.sort(Comparator.comparingInt(Disk::getEnd));
+
+        int curTime = 0;
+        for (final Disk disk : disks) {
+            curTime += disk.getProcessTime();
+
+            disk.setComplete(curTime);
         }
 
-        return new int[]{
-                dq.delMax(),
-                dq.delMin(),
-        };
-    }
-}
+        final OptionalDouble average = disks.stream()
+                .mapToInt(disk -> disk.getComplete() - disk.getStart())
+                .average();
 
-class DoublePriQue {
-    Queue<Integer> maxQueue = new PriorityQueue<>(Comparator.reverseOrder());
-    Queue<Integer> minQueue = new PriorityQueue<>();
-    Map<Integer, Integer> map = new HashMap<>();
-
-    public void add(final Integer number) {
-        maxQueue.add(number);
-        minQueue.add(number);
-
-        map.put(number, map.getOrDefault(number, 0) + 1);
+        return (int) average.getAsDouble();
     }
 
-    public int delMax() {
-        //maxQueue의 최대 값을 가져오고, minQueue를 refresh
-        final int rtVal = getTopValueFromQueue(maxQueue);
-        refreshQueue(minQueue);
+    static class Disk {
+        int start;
+        int end;
+        int complete;
 
-        return rtVal;
-    }
-
-    public int delMin() {
-        //minQueue의 최소 값을 가져오고, maxQueue를 refresh
-        final int rtVal = getTopValueFromQueue(minQueue);
-        refreshQueue(maxQueue);
-
-        return rtVal;
-    }
-
-    private void refreshQueue(final Queue<Integer> queue) {
-        //맨위에 존재하는 숫자가 존재하는 숫자 인지 확인
-        //존재 하지 않는 숫자면 빼주고, 존재하는 숫자면 refresh 종료
-        while (!queue.isEmpty()) {
-            final int numCount = map.get(queue.peek());
-
-            if (numCount > 0) {
-                break;
-            }
-
-            queue.poll();
-        }
-    }
-
-    private int getTopValueFromQueue(final Queue<Integer> queue) {
-        int rtVal = 0;
-
-        //맨위의 값이 존재하는 숫자면, 카운트 1개 빼고 해당 값을 반환
-        //맨위의 값이 존재하지 않는 값이면, 존재하는 값이 나올때까지 순환
-        while (!queue.isEmpty()) {
-            rtVal = queue.poll();
-            final int numCount = map.get(rtVal);
-
-            if (numCount > 0) {
-                map.put(rtVal, numCount - 1);
-                break;
-            }
+        Disk(final int start, final int end) {
+            this.start = start;
+            this.end = end;
+            this.complete = 0;
         }
 
-        return rtVal;
-    }
+        public int getStart() {
+            return start;
+        }
 
-    public boolean isEmpty() {
-        //모든 값이 존재하지 않으면 empty
-        return maxQueue.isEmpty() && minQueue.isEmpty();
+        public int getEnd() {
+            return end;
+        }
+
+        public int getProcessTime() {
+            return end - start;
+        }
+
+        public int getComplete() {
+            return complete;
+        }
+
+        public void setComplete(final int complete) {
+            this.complete = complete;
+        }
+
+        @Override
+        public String toString() {
+            return "Disk{" +
+                    "start=" + start +
+                    ", end=" + end +
+                    ", complete=" + complete +
+                    '}';
+        }
     }
 }
