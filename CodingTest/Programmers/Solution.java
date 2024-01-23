6ff7965b2125f383434b3ebc375ce58619322847
HyunggiPark <phg9587@naver.com>
2024-01-23 14:15:37 +0900

    프로그래머스 힙(Heap) 이중우선순위큐 - 디스크 컨트롤러 - 3차 풀이 실패

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -11,31 +11,61 @@ package CodingTest.Programmers;
 - List 정렬을 사용 (Comparator)
  */
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.OptionalDouble;
+import java.util.*;
 
 class Solution {
     public int solution(final int[][] jobs) {
         final List<Disk> disks = new ArrayList<>();
+
         //jobs를 Disk List로 변경
         for (final int[] job : jobs) {
-            disks.add(new Disk(job[0], job[0] + job[1]));
+            final int start = job[0];
+            final int process = job[1];
+
+            disks.add(new Disk(start, start + process));
         }
 
-        disks.sort(Comparator.comparingInt(Disk::getEnd));
+        disks.sort(Comparator.comparingInt(Disk::getStart));
+
+        final PriorityQueue<Disk> pq = new PriorityQueue<>(
+                Comparator.comparingInt(Disk::getEnd)
+        );
 
         int curTime = 0;
+
         for (final Disk disk : disks) {
-            final int start = disk.getStart();
-            if (curTime < start) {
-                curTime = start;
+            if (pq.isEmpty()) {
+                curTime = disk.getStart();
+                pq.add(disk);
+                continue;
             }
 
-            curTime += disk.getProcessTime();
+            final Disk peek = pq.peek();
+            final int end = peek.getEnd();
+            final int start = disk.getStart();
 
-            disk.setComplete(curTime);
+            if (start <= (curTime + end)) {
+                pq.add(disk);
+                continue;
+            }
+
+            while (!pq.isEmpty()) {
+                final Disk top = pq.poll();
+
+                curTime += top.getProcessTime();
+                top.setComplete(curTime);
+            }
+
+            //시작시간이 현재 시간보다 이후 인 경우, 시작 시간 변경
+            curTime = start;
+            pq.add(disk);
+        }
+
+        while (!pq.isEmpty()) {
+            final Disk top = pq.poll();
+
+            curTime += top.getProcessTime();
+            top.setComplete(curTime);
         }
 
         for (final Disk disk : disks) {
