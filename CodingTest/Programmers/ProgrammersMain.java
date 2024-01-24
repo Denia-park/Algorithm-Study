111be1aaedd40976e3e03cca5e99774b17554cb2
HyunggiPark <phg9587@naver.com>
2024-01-24 16:40:47 +0900

    프로그래머스 2020 카카오 인턴십 보석 쇼핑 - 1차 풀이 완료 - 40분 걸림

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,26 +1,30 @@
 package CodingTest.Programmers;
 
+import java.util.Arrays;
+
 public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println((ts.solution(
-                        BracketUtil.convertStringToJavaIntTwoDimensionalArray(
-                                "[[0, 3], [1, 9], [2, 6]]"
-                        )
-                ))
+        System.out.println((Arrays.toString(ts.solution(
+                        new String[]{"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"}
+                )))
         );
-        System.out.println((ts.solution(
-                        BracketUtil.convertStringToJavaIntTwoDimensionalArray(
-                                "[[24, 10], [28, 39], [43, 20], [37, 5], [47, 22], [20, 47], [15, 34], [15, 2], [35, 43], [26, 1]]"
-                        )
-                ))
+        System.out.println((Arrays.toString(ts.solution(
+                        new String[]{"AA", "AB", "AC", "AA", "AC"}
+                )))
         );
-        System.out.println((ts.solution(
-                        BracketUtil.convertStringToJavaIntTwoDimensionalArray(
-                                "[[0, 1], [1, 1], [50, 7]]"
-                        )
-                ))
+        System.out.println((Arrays.toString(ts.solution(
+                        new String[]{"XYZ", "XYZ", "XYZ"}
+                )))
         );
+        System.out.println((Arrays.toString(ts.solution(
+                        new String[]{"ZZZ", "YYY", "NNNN", "YYY", "BBB"}
+                )))
+        );
+
     }
 }
+
+
+
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,142 +1,89 @@
 package CodingTest.Programmers;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
 /*
 아이디어
-- 그리디 (정렬해서 처리하기)
+- 투포인터
+- 동일한 보석이 포함되지 않은 길이가 가장 긴 포인터를 반환
 
 시간 복잡도
-- n * log n
+- N
 
-자료 구조
-- List 정렬을 사용 (Comparator)
+자료구조
+- Set 사용
  */
-
-import java.util.*;
-
 class Solution {
-    public int solution(final int[][] jobs) {
-        final List<Disk> disks = new ArrayList<>();
+    public int[] solution(final String[] gems) {
+        final int[] answer = new int[2];
+        int answerLength = Integer.MAX_VALUE;
 
-        //jobs를 Disk List로 변경
-        for (final int[] job : jobs) {
-            disks.add(new Disk(job[0], job[1]));
-        }
+        final int gemTypeCount = new HashSet<>(List.of(gems)).size();
 
-        disks.sort(Comparator.comparingInt(Disk::getStart).thenComparingInt(Disk::getProcess));
+        final int gemLength = gems.length;
 
-        final PriorityQueue<Disk> pq = new PriorityQueue<>(Comparator.comparingInt(Disk::getProcess));
+        final Map<String, Integer> gemMap = new HashMap<>();
 
-        int curTime = 0;
+        int startIdx = 0;
+        int endIdx = -1;
 
-        for (final Disk curDisk : disks) {
-            final int curStart = curDisk.getStart();
+        while (startIdx < gemLength) {
+            if (gemMap.size() == gemTypeCount) {
+                //모든 보석을 다 구했으니까 answer 계산
+                final int tempGemCount = endIdx - startIdx + 1;
 
-            //현재 시간 보다 요청시간이 빠르면 다 추가
-            if (curStart <= curTime) {
-                pq.add(curDisk);
-                continue;
-            }
+                if (tempGemCount < answerLength) {
+                    answerLength = tempGemCount;
 
-            if (pq.isEmpty()) {
-                curTime = curStart;
-                pq.add(curDisk);
-                continue;
-            }
+                    answer[0] = startIdx + 1;
+                    answer[1] = endIdx + 1;
+                }
 
-            //현재 시간 보다 요청 시간이 느리면 우선 현재 큐에 있는 작업들을 처리
-            //queue에 있는 작업을 처리
-            while (!pq.isEmpty() && curTime < curStart) {
-                final Disk topDisk = pq.poll();
-                curTime += topDisk.getProcess();
-                topDisk.setComplete(curTime);
-            }
+                if (startIdx == endIdx && endIdx < gemLength - 1) {
+                    endIdx++;
 
-            if (curTime < curStart) {
-                curTime = curStart;
-            }
+                    final String endGemName = gems[endIdx];
 
-            pq.add(curDisk);
-        }
+                    gemMap.put(endGemName, gemMap.getOrDefault(endGemName, 0) + 1);
+                } else {
+                    final String startGemName = gems[startIdx];
 
-        //남은 작업 처리
-        while (!pq.isEmpty()) {
-            final Disk topDisk = pq.poll();
-            curTime += topDisk.getProcess();
-            topDisk.setComplete(curTime);
-        }
+                    final int startGemCount = gemMap.getOrDefault(startGemName, 0) - 1;
 
-        final OptionalDouble average = disks.stream()
-                .mapToInt(disk -> disk.getComplete() - disk.getStart())
-                .average();
+                    if (startGemCount == 0) {
+                        gemMap.remove(startGemName);
+                    } else {
+                        gemMap.put(startGemName, startGemCount);
+                    }
 
-        return (int) average.getAsDouble();
-    }
-
-    static class Disk {
-        int start;
-        int process;
-        int complete;
-
-        Disk(final int start, final int process) {
-            this.start = start;
-            this.process = process;
-            this.complete = 0;
-        }
-
-        public int getStart() {
-            return start;
-        }
-
-        public int getProcess() {
-            return process;
-        }
-
-        public int getComplete() {
-            return complete;
-        }
-
-        public void setComplete(final int complete) {
-            this.complete = complete;
-        }
-    }
-}
-
-/*
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
-//정답 참고
-//https://codevang.tistory.com/316
-
-class Solution {
-    public int solution(int[][] jobs) {
-        int answer = 0;
-        int curTime = 0;
-        int jobsIdx = 0;
-        int completeCount = 0;
-
-        Arrays.sort(jobs, Comparator.comparingInt(a -> a[0]));
-
-        PriorityQueue<int[]> waitingQueue = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-
-        while (completeCount < jobs.length) {
-            while (jobsIdx < jobs.length && jobs[jobsIdx][0] <= curTime) {
-                waitingQueue.add(jobs[jobsIdx]);
-                jobsIdx++;
-            }
-
-            if (waitingQueue.isEmpty()) {
-                curTime = jobs[jobsIdx][0];
+                    startIdx++;
+                }
             } else {
-                int[] job = waitingQueue.poll();
-                answer += curTime + job[1] - job[0];
-                curTime += job[1];
-                completeCount++;
+                if (endIdx < gemLength - 1) {
+                    endIdx++;
+
+                    final String endGemName = gems[endIdx];
+
+                    gemMap.put(endGemName, gemMap.getOrDefault(endGemName, 0) + 1);
+                } else {
+                    final String startGemName = gems[startIdx];
+
+                    final int startGemCount = gemMap.getOrDefault(startGemName, 0) - 1;
+
+                    if (startGemCount == 0) {
+                        gemMap.remove(startGemName);
+                    } else {
+                        gemMap.put(startGemName, startGemCount);
+                    }
+
+                    startIdx++;
+                }
             }
         }
 
-        return answer / jobs.length;
+        return answer;
     }
 }
- */
