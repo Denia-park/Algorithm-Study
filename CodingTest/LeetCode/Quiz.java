cef62d4a4e38b53d16de94403a1db2843e91181d
HyunggiPark <phg9587@naver.com>
2024-01-24 12:19:05 +0900

    leetcode  Pseudo-Palindromic Paths in a Binary Tree - 1차 풀이 실패 - 시간 초과

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,65 +1,124 @@
 package CodingTest.LeetCode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println("1 : " + solution.maxLength(
-                List.of("un", "iq", "ue")
-        ));
-        System.out.println("2 : " + solution.maxLength(
-                List.of("cha", "r", "act", "ers")
+        final TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(1);
+
+        root.right = new TreeNode(1);
+        root.right.right = new TreeNode(1);
+
+        System.out.println("1 : " + solution.pseudoPalindromicPaths(
+                root
         ));
     }
 }
 
 /*
 아이디어
-- Bit Operation
+- DFS
 
 시간복잡도
-- O(N * M), where N is the number of strings in the array and M is the maximum length of a string in the array.
-
-자료구조
 -
 
+자료구조
+- 이진 트리
  */
 
 class Solution {
-    static final int EMPTY_STRING = 0;
+    int answer;
 
-    public int maxLength(final List<String> arr) {
-        final List<Integer> dp = new ArrayList<>();
-        dp.add(EMPTY_STRING);
+    public int pseudoPalindromicPaths(final TreeNode root) {
+        answer = 0;
+        final Deque<Integer> deque = new ArrayDeque<>();
 
-        int result = 0;
+        //leaf 노드까지 가야함 -> 양쪽 노드가 null인 노드
+        deque.addLast(root.val);
+        dfs(root, deque);
 
-        for (final String s : arr) {
-            int a = 0;
-            int dup = 0;
+        return answer;
+    }
 
-            for (final char c : s.toCharArray()) {
-                final int bitValue = 1 << (c - 'a');
-                dup |= a & bitValue;
-                a |= bitValue;
+    private void dfs(final TreeNode root, final Deque<Integer> deque) {
+        if (root.left == null && root.right == null) {
+            if (isPalindrome(deque)) {
+                answer++;
             }
 
-            if (dup > 0) continue;
-
-            for (int i = dp.size() - 1; i >= 0; i--) {
-                final int dpVal = dp.get(i);
-
-                if ((dpVal & a) > 0) continue;
-
-                dp.add(dpVal | a);
-
-                result = Math.max(result, Integer.bitCount(dpVal | a));
-            }
+            return;
         }
 
-        return result;
+        if (root.left != null) {
+            final int leftVal = root.left.val;
+            deque.addLast(leftVal);
+            dfs(root.left, deque);
+            deque.pollLast();
+        }
+
+        if (root.right != null) {
+            final int rightVal = root.right.val;
+            deque.addLast(rightVal);
+            dfs(root.right, deque);
+            deque.pollLast();
+        }
+    }
+
+    public boolean isPalindrome(final Deque<Integer> deque) {
+        final int[] arr = new int[10];
+
+        for (final int val : deque) {
+            arr[val]++;
+        }
+
+        final int size = deque.size();
+
+        //deque size가 짝수면, 모두 짝수
+        if (size % 2 == 0) {
+            for (final int count : arr) {
+                if (count % 2 == 1) {
+                    return false;
+                }
+            }
+        }
+        //deque size가 홀수면, 한개는 홀수, 나머지 짝수
+        else {
+            int odd = 0;
+
+            for (final int count : arr) {
+                if (count % 2 == 1) {
+                    odd++;
+                }
+            }
+
+            return odd <= 1;
+        }
+
+        return true;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(final int val) {
+        this.val = val;
+    }
+
+    TreeNode(final int val, final TreeNode left, final TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
