c43c9b344c5b2d220242b4205eb8318dd59cc78c
HyunggiPark <phg9587@naver.com>
2024-01-25 15:27:39 +0900

    프로그래머스 탐욕법(Greedy) 섬 연결하기 - 리팩터링

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -4,8 +4,14 @@ package CodingTest.Programmers;
 아이디어
 - MST
 
+- MST -> 크루스칼 및 프림 풀이법이 있다
+- 크루스칼 - Union + Find 써야함 (O(e * log e))
+- 프림 - 우선순위 큐를 이용함 (O(node * node))
+
+- 프림 알고리즘 사용함
+
 시간복잡도
-- n*logN
+- n*n
 
 자료 구조
 - Heao -> priorityHeap (거리가 짧은 순)
@@ -23,39 +29,32 @@ class Solution {
             graph.put(i, new ArrayList<>());
         }
 
-        for (final int[] cost : costs) {
-            final int start = cost[0];
-            final int end = cost[1];
-            final int costVal = cost[2];
+        for (final int[] costArr : costs) {
+            final int from = costArr[0];
+            final int to = costArr[1];
+            final int cost = costArr[2];
 
-            graph.get(start).add(new Island(end, costVal));
-            graph.get(end).add(new Island(start, costVal));
+            graph.get(from).add(new Island(to, cost));
+            graph.get(to).add(new Island(from, cost));
         }
 
         final boolean[] isVisited = new boolean[n];
 
         //우선순위 큐 만들기
-        final PriorityQueue<Island> pq = new PriorityQueue<>(Comparator.comparingInt(Island::getCost));
+        final PriorityQueue<Island> pq = new PriorityQueue<>(Comparator.comparingInt((Island island) -> island.cost));
         pq.add(new Island(0, 0));
 
         while (!pq.isEmpty()) {
             final Island current = pq.poll();
             final int curNum = current.num;
 
-            if (isVisited[curNum]) {
-                continue;
-            }
-
-            final int cost = current.cost;
-            answer += cost;
+            if (isVisited[curNum]) continue;
 
             isVisited[curNum] = true;
+            answer += current.cost;
 
-            final List<Island> nextList = graph.get(curNum);
-            for (final Island island : nextList) {
-                if (isVisited[island.num]) {
-                    continue;
-                }
+            for (final Island island : graph.get(curNum)) {
+                if (isVisited[island.num]) continue;
 
                 pq.add(new Island(island.num, island.cost));
             }
@@ -64,17 +63,12 @@ class Solution {
         return answer;
     }
 
-    class Island {
-        int num;
-        int cost;
+    static class Island {
+        int num, cost;
 
         Island(final int num, final int cost) {
             this.num = num;
             this.cost = cost;
-        }
-
-        int getCost() {
-            return this.cost;
         }
     }
 }
