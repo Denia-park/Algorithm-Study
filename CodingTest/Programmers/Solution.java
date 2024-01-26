46587000f0ea7dab97b9ca6c49692e7ddaed1e7d
HyunggiPark <phg9587@naver.com>
2024-01-26 14:05:24 +0900

    프로그래머스 2019 카카오 개발자 겨울 인턴십 호텔 방 배정 - 정답 참고
    - 리팩터링

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -28,20 +28,20 @@ class Solution {
         return answer;
     }
 
-    private long getRoom(final Map<Long, Long> occupiedRoom, final long room) {
+    private long getRoom(final Map<Long, Long> occupiedRoom, final long curRoomNumber) {
         //방이 비어있으면 해당 방을 배정하고, 해당 방을 원할 경우 줘야하는 방을 기입
-        if (!occupiedRoom.containsKey(room)) {
-            final long nextRoom = room + 1;
+        if (!occupiedRoom.containsKey(curRoomNumber)) {
+            final long nextRoomNumber = curRoomNumber + 1;
 
-            occupiedRoom.put(room, nextRoom);
-            return room;
+            occupiedRoom.put(curRoomNumber, nextRoomNumber);
+            return curRoomNumber;
         }
 
         //방이 이미 차있으면, 해당 방 번호를 통해서 줘야하는 방에 대해서 알아낸다.
-        final Long nextRoom = occupiedRoom.get(room);
+        final Long nextRoom = occupiedRoom.get(curRoomNumber);
         //줘야하는 방이 그새 뺏기지 않았는지 확인한다.
         final long returnRoom = getRoom(occupiedRoom, nextRoom);
-        occupiedRoom.put(room, returnRoom);
+        occupiedRoom.put(curRoomNumber, returnRoom);
 
         return returnRoom;
     }
