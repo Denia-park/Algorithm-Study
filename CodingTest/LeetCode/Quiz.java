a441d0ee820e3e82076090f3f7deb4740dfc10cf
HyunggiPark <phg9587@naver.com>
2024-01-27 19:45:32 +0900

    leetcode K Inverse Pairs Array - 정답 참고
    - 이해 하는데만 1시간 썼다 ㅎㅎ.. 문제 진짜
    어렵다.

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,92 +1,27 @@
 package CodingTest.LeetCode;
 
-import java.util.Arrays;
-
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println("1 : " + solution.findPaths(
-                2,
-                2,
-                2,
-                0,
-                0
-        ));
-        System.out.println("2 : " + solution.findPaths(
-                1,
-                3,
-                3,
-                0,
-                1
-        ));
+        System.out.println("1 : " + solution.kInversePairs(3, 0));
+        System.out.println("1 : " + solution.kInversePairs(3, 1));
     }
 }
 
-/*
-아이디어
-- DFS로 이동하면서 거리 더하기
-
-시간복잡도
-- O(mnN)
-
-자료구조
--
- */
-
 class Solution {
-    static final int MOD = (int) (Math.pow(10, 9) + 7);
-    int maxRow;
-    int maxCol;
+    public int kInversePairs(final int n, final int k) {
+        final int[][] dp = new int[n + 1][k + 1];
+        dp[0][0] = 1;
 
-    int[][] directions = new int[][]{
-            {-1, 0},
-            {0, 1},
-            {1, 0},
-            {0, -1}
-    };
-
-    public int findPaths(final int m, final int n, final int maxMove, final int startRow, final int startColumn) {
-        maxRow = m;
-        maxCol = n;
-        final int[][][] dp = new int[maxRow][maxCol][maxMove + 1];
-
-        initDpArray(dp);
-
-        return dfs(startRow, startColumn, maxMove, dp);
-    }
-
-    private void initDpArray(final int[][][] dp) {
-        for (final int[][] rows : dp) {
-            for (final int[] row : rows) {
-                Arrays.fill(row, -1);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                for (int x = 0; x <= Math.min(j, i - 1); x++) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - x]) % 1000000007;
+                }
             }
         }
-    }
 
-    private int dfs(final int curRow, final int curCol, final int curMoveCount, final int[][][] dp) {
-        if (isOutOfBoundary(curRow, curCol)) return 1;
-        if (curMoveCount == 0) return 0;
-
-        final int dpVal = dp[curRow][curCol][curMoveCount];
-        if (dpVal >= 0) return dpVal;
-
-        int sumVal = 0;
-
-        for (final int[] direction : directions) {
-            final int nextRow = curRow + direction[0];
-            final int nextCol = curCol + direction[1];
-            final int nextCount = curMoveCount - 1;
-
-            sumVal = (sumVal + (dfs(nextRow, nextCol, nextCount, dp) % MOD)) % MOD;
-        }
-
-        dp[curRow][curCol][curMoveCount] = sumVal;
-
-        return sumVal;
-    }
-
-    public boolean isOutOfBoundary(final int row, final int col) {
-        return row < 0 || row >= maxRow || col < 0 || col >= maxCol;
+        return dp[n][k];
     }
 }
