c7448f1ece574e7148e16f5b4b3b6ef7b447493e
HyunggiPark <phg9587@naver.com>
2024-01-28 14:22:26 +0900

    프로그래머스 Summer/Winter Coding(~2018) 스킬트리 - 1차 풀이 완료 - 실패

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,15 +1,11 @@
 package CodingTest.Programmers;
 
-import java.util.Arrays;
-
 public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println((Arrays.toString(ts.solution(
-                        new String[]{"frodo", "front", "frost", "frozen", "frame", "kakao"},
-                        new String[]{"fro??", "????o", "fr???", "fro???", "pro?"}
-                )))
+        System.out.println(
+                ts.solution("CBD", new String[]{"BACDE", "CBADF", "AECB", "BDA"})
         );
 
     }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,67 +1,30 @@
 package CodingTest.Programmers;
 
-/*
-아이디어
-- 트리 (Trie)
-
-정답 참고
-- https://girawhale.tistory.com/110
- */
-
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    public int[] solution(final String[] words, final String[] queries) {
-        final Trie front = new Trie();
-        final Trie back = new Trie();
+    public int solution(final String skill, final String[] skill_trees) {
+        int answer = 0;
 
-        for (final String word : words) {
-            front.insert(word);
-            back.insert(reverse(word));
-        }
+        for (final String skillTree : skill_trees) {
+            final int[] skilIdxArray = new int[skill.length()];
 
-        return Arrays.stream(queries)
-                .mapToInt(
-                        query -> query.charAt(0) == '?' ?
-                                back.find(reverse(query), 0) :
-                                front.find(query, 0))
-                .toArray();
-    }
+            for (int skillIdx = 0; skillIdx < skill.length(); skillIdx++) {
+                skilIdxArray[skillIdx] = skillTree.indexOf(skill.charAt(skillIdx));
+            }
 
-    private String reverse(final String word) {
-        return new StringBuilder(word).reverse().toString();
-    }
-
-    static class Trie {
-        Map<Integer, Integer> lenMap = new HashMap<>();
-        Trie[] child = new Trie[26];
-
-        void insert(final String str) {
-            Trie node = this;
-            final int len = str.length();
-            lenMap.put(len, lenMap.getOrDefault(len, 0) + 1);
-
-            for (final char ch : str.toCharArray()) {
-                final int idx = ch - 'a';
-                if (node.child[idx] == null) {
-                    node.child[idx] = new Trie();
+            boolean isOkay = true;
+            for (int i = 1; i < skill.length(); i++) {
+                if (skilIdxArray[i] < skilIdxArray[i - 1]) {
+                    isOkay = false;
+                    break;
                 }
+            }
 
-                node = node.child[idx];
-                node.lenMap.put(len, node.lenMap.getOrDefault(len, 0) + 1);
+            if (isOkay) {
+                answer++;
             }
         }
 
-        int find(final String str, final int curIdx) {
-            if (str.charAt(curIdx) == '?') {
-                return lenMap.getOrDefault(str.length(), 0);
-            }
 
-            final int charIdx = str.charAt(curIdx) - 'a';
-            return child[charIdx] == null ? 0 : child[charIdx].find(str, curIdx + 1);
-        }
+        return answer;
     }
 }
