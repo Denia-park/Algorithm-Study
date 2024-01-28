8b78b0cae1549e5bbffc06042c260feca573ccd0
HyunggiPark <phg9587@naver.com>
2024-01-28 14:29:17 +0900

    프로그래머스 Summer/Winter Coding(~2018) 스킬트리 - 2차 풀이 완료
    - 문제를 잘 못 읽어서 시간이 더 걸림

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,21 +1,30 @@
 package CodingTest.Programmers;
 
+import java.util.Arrays;
+
 class Solution {
     public int solution(final String skill, final String[] skill_trees) {
         int answer = 0;
 
         for (final String skillTree : skill_trees) {
             final int[] skilIdxArray = new int[skill.length()];
+            Arrays.fill(skilIdxArray, -1);
 
             for (int skillIdx = 0; skillIdx < skill.length(); skillIdx++) {
                 skilIdxArray[skillIdx] = skillTree.indexOf(skill.charAt(skillIdx));
             }
 
             boolean isOkay = true;
-            for (int i = 1; i < skill.length(); i++) {
-                if (skilIdxArray[i] < skilIdxArray[i - 1]) {
-                    isOkay = false;
-                    break;
+            for (int curIdx = 1; curIdx < skill.length(); curIdx++) {
+                if (skilIdxArray[curIdx] == -1) {
+                    continue;
+                }
+
+                for (int startIdx = 0; startIdx < curIdx; startIdx++) {
+                    if (skilIdxArray[startIdx] == -1 || skilIdxArray[curIdx] < skilIdxArray[startIdx]) {
+                        isOkay = false;
+                        break;
+                    }
                 }
             }
 
