88da4d05e5eb913e3e5c8acf054b726d59f25313
HyunggiPark <phg9587@naver.com>
2024-01-28 12:56:11 +0900

    leetcode Number of Submatrices That Sum to Target - 1차 풀이 - 시간 초과 - DP로 구현 해봄

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,27 +1,69 @@
 package CodingTest.LeetCode;
 
+import CodingTest.Programmers.BracketUtil;
+
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println("1 : " + solution.kInversePairs(3, 0));
-        System.out.println("1 : " + solution.kInversePairs(3, 1));
+        System.out.println("1 : " + solution.numSubmatrixSumTarget(
+                BracketUtil.convertStringToJavaIntTwoDimensionalArray("[[0,1,0],[1,1,1],[0,1,0]]"),
+                0));
+        System.out.println("2 : " + solution.numSubmatrixSumTarget(
+                BracketUtil.convertStringToJavaIntTwoDimensionalArray("[[1,-1],[-1,1]]"),
+                0));
+        System.out.println("3 : " + solution.numSubmatrixSumTarget(
+                BracketUtil.convertStringToJavaIntTwoDimensionalArray("[[904]]"),
+                0));
     }
 }
 
-class Solution {
-    public int kInversePairs(final int n, final int k) {
-        final int[][] dp = new int[n + 1][k + 1];
-        dp[0][0] = 1;
+/*
+아이디어 - DP
+ */
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= k; j++) {
-                for (int x = 0; x <= Math.min(j, i - 1); x++) {
-                    dp[i][j] = (dp[i][j] + dp[i - 1][j - x]) % 1000000007;
+class Solution {
+    public int numSubmatrixSumTarget(final int[][] matrix, final int target) {
+        int answer = 0;
+
+        final int totalRow = matrix.length;
+        final int totalCol = matrix[0].length;
+
+        final int[][][] dp = new int[totalRow][totalCol][totalCol];
+
+        for (int row = 0; row < totalRow; row++) {
+            for (int startCol = 0; startCol < totalCol; startCol++) {
+                for (int endCol = startCol; endCol < totalCol; endCol++) {
+                    if (startCol == endCol) {
+                        dp[row][startCol][endCol] = matrix[row][startCol];
+                        continue;
+                    }
+
+                    dp[row][startCol][endCol] = (dp[row][startCol][endCol - 1] + matrix[row][endCol]);
                 }
             }
         }
 
-        return dp[n][k];
+        //가로가 1, 세로가 1 인거부터 가로 1 세로 2 가로 1 세로 3, 등등 모든 조합을 구한다?
+        for (int rowNum = 1; rowNum <= totalRow; rowNum++) {
+            for (int colNum = 1; colNum <= totalCol; colNum++) {
+                for (int startRow = 0; startRow <= totalRow - rowNum; startRow++) {
+                    for (int startCol = 0; startCol <= totalCol - colNum; startCol++) {
+                        int sum = 0;
+
+                        for (int rowCount = 0; rowCount < rowNum; rowCount++) {
+                            sum += dp[startRow + rowCount][startCol][startCol + colNum - 1];
+                        }
+
+                        if (sum == target) {
+                            answer++;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        return answer;
     }
 }
