729c30aa11a056775e472b77eccb5d30ee1058f5
HyunggiPark <phg9587@naver.com>
2024-01-29 12:34:20 +0900

    프로그래머스 2020 카카오 인턴십 키패드 누르기 - 리팩터링

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,60 +1,50 @@
 package CodingTest.Programmers;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 class Solution {
-    Map<Integer, int[]> coordiMap;
+    int[][] coordiPos = {
+            {3, 1}, //0
+            {0, 0}, //1
+            {0, 1}, //2
+            {0, 2}, //3
+            {1, 0}, //4
+            {1, 1}, //5
+            {1, 2}, //6
+            {2, 0}, //7
+            {2, 1}, //8
+            {2, 2}  //9
+    };
     private String myHand;
 
     public String solution(final int[] numbers, final String hand) {
         final StringBuilder sb = new StringBuilder();
-        myHand = hand;
-        coordiMap = new HashMap<>();
-        init(coordiMap);
+        myHand = hand.equals("left") ? "L" : "R";
 
-        final List<Integer> leftNums = List.of(1, 4, 7);
-        final List<Integer> middleNums = List.of(2, 5, 8, 0);
         int[] left = new int[]{3, 0};
         int[] right = new int[]{3, 2};
 
         for (final int number : numbers) {
-            if (middleNums.contains(number)) {
-                final String value = check(number, left, right);
-                if (value.equals("L")) {
-                    left = coordiMap.get(number);
-                } else {
-                    right = coordiMap.get(number);
-                }
-                sb.append(value);
-            } else if (leftNums.contains(number)) {
-                sb.append("L");
-                left = coordiMap.get(number);
+            final String value = check(number, left, right);
+
+            if (value.equals("L")) {
+                left = coordiPos[number];
             } else {
-                sb.append("R");
-                right = coordiMap.get(number);
+                right = coordiPos[number];
             }
+
+            sb.append(value);
         }
 
         return sb.toString();
     }
 
-    private void init(final Map<Integer, int[]> coordiMap) {
-        coordiMap.put(1, new int[]{0, 0});
-        coordiMap.put(2, new int[]{0, 1});
-        coordiMap.put(3, new int[]{0, 2});
-        coordiMap.put(4, new int[]{1, 0});
-        coordiMap.put(5, new int[]{1, 1});
-        coordiMap.put(6, new int[]{1, 2});
-        coordiMap.put(7, new int[]{2, 0});
-        coordiMap.put(8, new int[]{2, 1});
-        coordiMap.put(9, new int[]{2, 2});
-        coordiMap.put(0, new int[]{3, 1});
-    }
-
     private String check(final int number, final int[] left, final int[] right) {
-        final int[] numberCoordi = coordiMap.get(number);
+        if (number == 1 || number == 4 || number == 7) {
+            return "L";
+        } else if (number == 3 || number == 6 || number == 9) {
+            return "R";
+        }
+
+        final int[] numberCoordi = coordiPos[number];
 
         final int leftDistance = Math.abs(numberCoordi[0] - left[0]) + Math.abs(numberCoordi[1] - left[1]);
         final int rightDistance = Math.abs(numberCoordi[0] - right[0]) + Math.abs(numberCoordi[1] - right[1]);
@@ -70,6 +60,6 @@ class Solution {
         }
 
         //같으면 myHand
-        return myHand.substring(0, 1).toUpperCase();
+        return myHand;
     }
 }
