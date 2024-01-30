87e1df49ba818fcbbcfdf0849670090e8a562112
HyunggiPark <phg9587@naver.com>
2024-01-30 12:03:08 +0900

    프로그래머스 깊이/너비 우선 탐색(DFS/BFS) 타겟 넘버 - 1차 풀이 완료

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -4,9 +4,8 @@ public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(ts.solution(new int[]{1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5}, "right").equals("LRLLLRLLRRL"));
-        System.out.println(ts.solution(new int[]{7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2}, "left").equals("LRLLRRLLLRR"));
-        System.out.println(ts.solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}, "right").equals("LLRLLRLLRL"));
+        System.out.println(ts.solution(new int[]{1, 1, 1, 1, 1}, 3));
+        System.out.println(ts.solution(new int[]{4, 1, 2, 1}, 4));
 
     }
 }
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,65 +1,31 @@
 package CodingTest.Programmers;
 
+/*
+아이디어 - DFS
+ */
 class Solution {
-    int[][] coordiPos = {
-            {3, 1}, //0
-            {0, 0}, //1
-            {0, 1}, //2
-            {0, 2}, //3
-            {1, 0}, //4
-            {1, 1}, //5
-            {1, 2}, //6
-            {2, 0}, //7
-            {2, 1}, //8
-            {2, 2}  //9
-    };
-    private String myHand;
+    int gTarget;
+    int answer;
 
-    public String solution(final int[] numbers, final String hand) {
-        final StringBuilder sb = new StringBuilder();
-        myHand = hand.equals("left") ? "L" : "R";
+    public int solution(final int[] numbers, final int target) {
+        gTarget = target;
+        answer = 0;
 
-        int[] left = new int[]{3, 0};
-        int[] right = new int[]{3, 2};
+        dfs(numbers, 0, 0);
 
-        for (final int number : numbers) {
-            final String value = check(number, left, right);
-
-            if (value.equals("L")) {
-                left = coordiPos[number];
-            } else {
-                right = coordiPos[number];
-            }
-
-            sb.append(value);
-        }
-
-        return sb.toString();
+        return answer;
     }
 
-    private String check(final int number, final int[] left, final int[] right) {
-        if (number == 1 || number == 4 || number == 7) {
-            return "L";
-        } else if (number == 3 || number == 6 || number == 9) {
-            return "R";
+    private void dfs(final int[] numbers, final int index, final int sum) {
+        if (index == numbers.length) {
+            if (sum == gTarget) {
+                answer++;
+            }
+
+            return;
         }
 
-        final int[] numberCoordi = coordiPos[number];
-
-        final int leftDistance = Math.abs(numberCoordi[0] - left[0]) + Math.abs(numberCoordi[1] - left[1]);
-        final int rightDistance = Math.abs(numberCoordi[0] - right[0]) + Math.abs(numberCoordi[1] - right[1]);
-
-        //왼쪽이 가까우면 왼손
-        if (leftDistance < rightDistance) {
-            return "L";
-        }
-
-        //오른손이 가까우면 오른손
-        if (leftDistance > rightDistance) {
-            return "R";
-        }
-
-        //같으면 myHand
-        return myHand;
+        dfs(numbers, index + 1, sum + numbers[index]);
+        dfs(numbers, index + 1, sum - numbers[index]);
     }
 }
