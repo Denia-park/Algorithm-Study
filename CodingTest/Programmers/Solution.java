7a4403bb567b8969d87a915a0a1370cf17fb6cfb
HyunggiPark <phg9587@naver.com>
2024-01-31 12:42:53 +0900

    프로그래머스 깊이/너비 우선 탐색(DFS/BFS) 네트워크 - graph 안 만들고 순회하기

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -6,45 +6,25 @@ package CodingTest.Programmers;
  */
 
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Deque;
-import java.util.List;
 
 class Solution {
     public int solution(final int n, final int[][] computers) {
         int answer = 0;
-        final List<List<Integer>> graph = new ArrayList<>();
-
-        //처음에 초기화
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int start = 0; start < computers.length; start++) {
-            final int[] computer = computers[start];
-
-            for (int end = start + 1; end < computer.length; end++) {
-                if (computer[end] == 1) {
-                    graph.get(start).add(end);
-                    graph.get(end).add(start);
-                }
-            }
-        }
-
         final boolean[] isVisited = new boolean[n];
 
         //네트워크 파악을 위해 0 ~ n-1 까지 순회
         for (int start = 0; start < n; start++) {
             if (isVisited[start]) continue;
 
-            bfs(graph, isVisited, start);
+            bfs(computers, isVisited, start);
             answer++;
         }
 
         return answer;
     }
 
-    private void bfs(final List<List<Integer>> graph, final boolean[] isVisited, final int start) {
+    private void bfs(final int[][] computers, final boolean[] isVisited, final int start) {
         final Deque<Integer> deque = new ArrayDeque<>();
         deque.addLast(start);
         isVisited[start] = true;
@@ -52,8 +32,11 @@ class Solution {
         while (!deque.isEmpty()) {
             final Integer poll = deque.pollLast();
 
-            for (final Integer next : graph.get(poll)) {
-                if (isVisited[next]) continue;
+            final int[] computer = computers[poll];
+
+            for (int next = 0; next < computer.length; next++) {
+                final Integer isAble = computer[next];
+                if (isVisited[next] || isAble == 0) continue;
 
                 deque.addLast(next);
                 isVisited[next] = true;
