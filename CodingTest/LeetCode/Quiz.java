ecdff5e53419f9daad715e674ac907f5ea4920d5
HyunggiPark <phg9587@naver.com>
2024-01-31 13:19:23 +0900

    leetcode Daily Temperatures - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,52 +1,58 @@
 package CodingTest.LeetCode;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
-import java.util.List;
 
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.evalRPN(new String[]{"2", "1", "+", "3", "*"}));
-        System.out.println(solution.evalRPN(new String[]{"4", "13", "5", "/", "+"}));
-        System.out.println(solution.evalRPN(new String[]{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}));
+        System.out.println(Arrays.toString(solution.dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73})));
+        System.out.println(Arrays.toString(solution.dailyTemperatures(new int[]{30, 40, 50, 60})));
+        System.out.println(Arrays.toString(solution.dailyTemperatures(new int[]{30, 60, 90})));
     }
 }
 
 class Solution {
-    List<String> operators = List.of("+", "-", "*", "/");
+    public int[] dailyTemperatures(final int[] temperatures) {
+        final int[] answer = new int[temperatures.length];
 
-    public int evalRPN(final String[] tokens) {
-        final Deque<Integer> stack = new ArrayDeque<>();
+        final Deque<Temp> dq = new ArrayDeque<>();
+        for (int idx = 0; idx < temperatures.length; idx++) {
+            final int temp = temperatures[idx];
 
-        for (final String token : tokens) {
-            if (!operators.contains(token)) {
-                stack.push(Integer.valueOf(token));
+            //비어있으면 추가한다.
+            if (dq.isEmpty()) {
+                dq.push(new Temp(idx, temp));
                 continue;
             }
 
-            final int num2 = stack.pop();
-            final int num1 = stack.pop();
+            //비어있지 않으면 현재 기온과 비교한다.
+            //Stack을 뒤지면서 다 처리해야 한다.
+            while (!dq.isEmpty()) {
+                //지금이 더 따뜻하면, 기존 값을 빼고 answer에 값을 추가한다.
+                final Temp curTemp = dq.peek();
 
-            switch (token) {
-                case "+":
-                    stack.push(num1 + num2);
-                    break;
-                case "-":
-                    stack.push(num1 - num2);
-                    break;
-                case "*":
-                    stack.push(num1 * num2);
-                    break;
-                case "/":
-                    stack.push(num1 / num2);
-                    break;
-                default:
-                    break;
+                if (curTemp.value >= temp) break;
+
+                dq.poll();
+                answer[curTemp.idx] = idx - curTemp.idx;
             }
+
+            dq.push(new Temp(idx, temp));
         }
 
-        return stack.pop();
+        return answer;
+    }
+
+    static class Temp {
+        int idx;
+        int value;
+
+        public Temp(final int idx, final int value) {
+            this.idx = idx;
+            this.value = value;
+        }
     }
 }
