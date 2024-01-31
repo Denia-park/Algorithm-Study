3091d41ce736b3d9663f779dc542396e31445394
HyunggiPark <phg9587@naver.com>
2024-01-31 13:50:36 +0900

    프로그래머스 코딩 기초 트레이닝 이어 붙인 수 - 1차 풀이 완료

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -4,15 +4,8 @@ public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(ts.solution(
-                3,
-                BracketUtil.convertStringToJavaIntTwoDimensionalArray("[[1, 1, 0], [1, 1, 0], [0, 0, 1]]")
-        ));
-        System.out.println(ts.solution(
-                3,
-                BracketUtil.convertStringToJavaIntTwoDimensionalArray("[[1, 1, 0], [1, 1, 1], [0, 1, 1]]")
-        ));
-
+        System.out.println(ts.solution(new int[]{3, 4, 5, 2, 1}));
+        System.out.println(ts.solution(new int[]{5, 7, 8, 3}));
     }
 }
 
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,36 +1,18 @@
 package CodingTest.Programmers;
 
-/*
-아이디어 - DFS
-네트워크 개수 구하기
- */
-
 class Solution {
-    public int solution(final int n, final int[][] computers) {
-        int answer = 0;
-        final boolean[] isVisited = new boolean[n];
+    public int solution(final int[] num_list) {
+        String oddNums = "";
+        String evenNums = "";
 
-        //네트워크 파악을 위해 0 ~ n-1 까지 순회
-        for (int start = 0; start < n; start++) {
-            if (isVisited[start]) continue;
-
-            dfs(computers, isVisited, start);
-            answer++;
+        for (final int i : num_list) {
+            if (i % 2 == 1) {
+                oddNums += i;
+            } else {
+                evenNums += i;
+            }
         }
 
-        return answer;
-    }
-
-    private void dfs(final int[][] computers, final boolean[] isVisited, final int start) {
-        isVisited[start] = true;
-        final int[] computer = computers[start];
-
-        for (int next = 0; next < computer.length; next++) {
-            final int isAble = computer[next];
-            if (isVisited[next] || isAble == 0) continue;
-
-            isVisited[next] = true;
-            dfs(computers, isVisited, next);
-        }
+        return Integer.parseInt(oddNums) + Integer.parseInt(evenNums);
     }
 }
