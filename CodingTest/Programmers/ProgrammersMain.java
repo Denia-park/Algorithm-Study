2150ae4efa16d65fb480c54e4a9788eaeff9127d
HyunggiPark <phg9587@naver.com>
2024-01-31 14:39:13 +0900

    프로그래머스 2024 KAKAO WINTER INTERNSHIP 가장 많이 받은 선물 - 1차 풀이 완료
    - 중간에 로직 실수해서 시간이 오래 걸림

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -4,8 +4,19 @@ public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(ts.solution(new int[]{3, 4, 5, 2, 1}));
-        System.out.println(ts.solution(new int[]{5, 7, 8, 3}));
+        System.out.println(ts.solution(
+                new String[]{"muzi", "ryan", "frodo", "neo"},
+                new String[]{"muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"}
+        ));
+        System.out.println(ts.solution(
+                new String[]{"joy", "brad", "alessandro", "conan", "david"},
+                new String[]{"alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"}
+        ));
+        System.out.println(ts.solution(
+                new String[]{"a", "b", "c"},
+                new String[]{"a b", "b a", "c a", "a c", "a c", "c a"}
+        ));
+
     }
 }
 
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,18 +1,99 @@
 package CodingTest.Programmers;
 
-class Solution {
-    public int solution(final int[] num_list) {
-        String oddNums = "";
-        String evenNums = "";
+import java.util.HashMap;
+import java.util.Map;
 
-        for (final int i : num_list) {
-            if (i % 2 == 1) {
-                oddNums += i;
-            } else {
-                evenNums += i;
+/*
+아이디어 - 구현
+ */
+
+class Solution {
+    public int solution(final String[] friends, final String[] gifts) {
+
+        //선물 준거, 받은거 기록 필요함
+        final Map<String, int[]> giftSendReceive = new HashMap<>();
+        //각 친구들에대해서 선물 준거 기록 필요함
+        final Map<String, Map<String, Integer>> giftFriend = new HashMap<>();
+        //다음 달에 받을 선물
+        final Map<String, Integer> nextMonthReceive = new HashMap<>();
+
+        //Map 초기화
+        for (final String curName : friends) {
+            giftSendReceive.put(curName, new int[2]);
+            nextMonthReceive.put(curName, 0);
+
+            final Map<String, Integer> tempMap = new HashMap<>();
+            for (final String toFriend : friends) {
+                if (curName.equals(toFriend)) continue;
+                tempMap.put(toFriend, 0);
+            }
+            giftFriend.put(curName, tempMap);
+        }
+
+        //선물을 주자
+        for (final String gift : gifts) {
+            final String[] split = gift.split(" ");
+            final String from = split[0];
+            final String to = split[1];
+
+            final int[] fromSendReceive = giftSendReceive.get(from);
+            final int[] toSendReceive = giftSendReceive.get(to);
+
+            fromSendReceive[0]++; //from이 준거 업데이트
+            toSendReceive[1]++; //to가 받은거 업데이트
+
+            final Integer giftedValue = giftFriend.get(from).get(to);
+            giftFriend.get(from).put(to, giftedValue + 1);
+        }
+
+        //조합으로 친구 모두 비교하면서 개수 세기
+        for (int aIdx = 0; aIdx < friends.length; aIdx++) {
+            for (int bIdx = aIdx + 1; bIdx < friends.length; bIdx++) {
+                final String aFriend = friends[aIdx];
+                final String bFriend = friends[bIdx];
+
+                //선물을 주고 받은 기록이 있으면, 더 많은 선물을 준 사람이 받는다
+                if (giftFriend.get(aFriend).get(bFriend) != 0 ||
+                        giftFriend.get(bFriend).get(aFriend) != 0) {
+                    final int faToFb = giftFriend.get(aFriend).get(bFriend);
+                    final int fbToFA = giftFriend.get(bFriend).get(aFriend);
+
+                    //둘이서 주고 받은 선물의 수가 다르면
+                    if (faToFb != fbToFA) {
+                        //fa가 준게 더 많으면
+                        if (faToFb > fbToFA) {
+                            nextMonthReceive.put(aFriend, nextMonthReceive.get(aFriend) + 1);
+                        }
+                        //fb가 준게 더 많으면
+                        else {
+                            nextMonthReceive.put(bFriend, nextMonthReceive.get(bFriend) + 1);
+                        }
+
+                        continue;
+                    }
+                }
+
+                //받은 기록이 없거나, 같으면 선물지수가 큰 사람이 받는다.
+                final int[] aSendReceive = giftSendReceive.get(aFriend);
+                final int aGiftValue = aSendReceive[0] - aSendReceive[1];
+                final int[] bSendReceive = giftSendReceive.get(bFriend);
+                final int bGiftValue = bSendReceive[0] - bSendReceive[1];
+
+
+                //선물지수 까지 같으면 주고 받지 않는다.
+                if (aGiftValue == bGiftValue) {
+                    continue;
+                }
+
+                if (aGiftValue > bGiftValue) {
+                    nextMonthReceive.put(aFriend, nextMonthReceive.get(aFriend) + 1);
+                } else {
+                    nextMonthReceive.put(bFriend, nextMonthReceive.get(bFriend) + 1);
+                }
             }
         }
 
-        return Integer.parseInt(oddNums) + Integer.parseInt(evenNums);
+        return nextMonthReceive.values().stream()
+                .mapToInt(i -> i).max().getAsInt();
     }
 }
