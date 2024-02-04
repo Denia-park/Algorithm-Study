a7b59cfc5f66ec96cb556c88b03819785b8336dd
HyunggiPark <phg9587@naver.com>
2024-02-04 19:02:40 +0900

    leetcode Minimum Window Substring - 1차 풀이 틀림 - 처음에 NPE 떠서 완전 잘못 풀었었음

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,49 +1,77 @@
 package CodingTest.LeetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println((solution.maxSumAfterPartitioning(new int[]{1, 15, 7, 9, 2, 5, 10}, 3)));
-        System.out.println((solution.maxSumAfterPartitioning(new int[]{1, 4, 1, 5, 7, 3, 6, 1, 9, 9, 3}, 4)));
-        System.out.println((solution.maxSumAfterPartitioning(new int[]{1}, 1)));
+        System.out.println((solution.minWindow("ADOBECODEBANC", "ABC")));
+        System.out.println((solution.minWindow("a", "a")));
+        System.out.println((solution.minWindow("a", "aa")));
+        System.out.println((solution.minWindow("ab", "a")));
     }
 }
 
 class Solution {
-    public int maxSumAfterPartitioning(final int[] arr, final int k) {
-        // 입력 배열의 길이
-        final int len = arr.length;
+    private static final int INF = (int) Math.pow(10, 8);
 
-        // dp 테이블 초기화: dp[i]는 배열의 첫 번째 요소부터 i번째 요소까지 나눌 수 있는 최대 합을 저장
-        final int[] dp = new int[len + 1];
+    public String minWindow(final String str, final String target) {
+        final int strLen = str.length();
+        final Map<Character, Integer> strMap = new HashMap<>();
+        final Map<Character, Integer> targetMap = new HashMap<>();
 
-        // 배열의 각 요소를 순회
-        for (int i = 0; i < len; i++) {
-            int curMax = 0; // 현재 파티션(부분 배열)의 최대값
-            int curSum = 0; // 현재까지 계산된 최대 합
-
-            // 마지막 k개의 요소를 고려하거나 배열의 시작까지 반복
-            // 이 반복문은 현재 위치에서 k 크기 내의 모든 가능한 부분 배열을 고려하여 최대 합을 찾음
-            for (int j = i; j >= Math.max(0, i - k + 1); j--) {
-                // 현재 파티션 내의 최대값을 업데이트
-                curMax = Math.max(curMax, arr[j]);
-
-                // 현재 파티션을 고려한 새로운 합을 계산
-                // curMax * (i - j + 1)은 현재 고려 중인 부분 배열의 최대값과 그 크기를 곱한 값
-                // dp[j]는 현재 부분 배열 이전까지의 최대 합
-                final int curCount = i - j + 1;
-                final int sum = (curMax * curCount) + dp[j];
-
-                // 현재까지의 최대 합을 업데이트
-                curSum = Math.max(curSum, sum);
-            }
-
-            // dp 테이블을 현재 위치에서의 최대 합으로 업데이트
-            dp[i + 1] = curSum;
+        for (final char ch : target.toCharArray()) {
+            targetMap.put(ch, targetMap.getOrDefault(ch, 0) + 1);
         }
 
-        // dp 테이블의 마지막 요소가 전체 배열을 나눌 수 있는 최대 합을 나타냄
-        return dp[len];
+        int st = 0;
+        int ed = 0;
+
+        int answerLen = INF;
+        String answerStr = "";
+
+        while (st < str.length()) {
+            final char edChar = str.charAt(ed);
+
+            strMap.put(edChar, strMap.getOrDefault(edChar, 0) + 1);
+
+            final int curLen = ed - st + 1;
+            if (st <= ed && isRight(strMap, targetMap) && curLen < answerLen) {
+                answerLen = curLen;
+                answerStr = str.substring(st, ed + 1);
+
+                //빼기
+                final char stChar = str.charAt(st);
+                strMap.put(stChar, strMap.get(stChar) - 1);
+                st++;
+                continue;
+            }
+
+            if (ed == (strLen - 1)) {
+                //빼기
+                final char stChar = str.charAt(st);
+                strMap.put(stChar, strMap.get(stChar) - 1);
+                st++;
+            } else {
+                ed++;
+            }
+        }
+
+        return answerStr;
+    }
+
+    boolean isRight(final Map<Character, Integer> origin, final Map<Character, Integer> target) {
+        for (final Character ch : target.keySet()) {
+            final Integer originVal = origin.getOrDefault(ch, 0);
+            final Integer targetVal = target.getOrDefault(ch, 0);
+
+            if (originVal < targetVal) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
