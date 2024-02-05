a6e9a031ee42283c75e092ec631d41dd3ae140e2
HyunggiPark <phg9587@naver.com>
2024-02-05 12:46:25 +0900

    프로그래머스 탐욕법(Greedy) 단속카메라 - 리팩터링

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -4,20 +4,27 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 class Solution {
-    static final int ST = 0;
-    static final int ED = 1;
 
     public int solution(final int[][] routes) {
         int answer = 0;
 
-        Arrays.sort(routes, Comparator.comparingInt((int[] value) -> value[ED]));
+        //끝나는 거리 순으로 정렬을 한다.
+        //시작하는 거리는 무조건 끝나는 거리 이전이다.
+        Arrays.sort(routes, Comparator.comparingInt((int[] value) -> value[1]));
 
-        int preCarStart = -30001;
+        //0번째 차를 비교하기 위해 시작하는 거리 -1로 값을 초기화한다.
+        int preCarEnd = -30001;
+
+        //모든 차를 돌면서 비교한다.
         for (final int[] route : routes) {
-            if (preCarStart < route[ST]) {
-                answer++;
-                preCarStart = route[ED];
-            }
+            //현재 차의 시작하는 거리가 이전 차의 끝나는 거리보다 안쪽이면 (작으면)
+            //현재 차가 이전 차가 끝나기 전에 들어왔으므로 카메라는 추가할 필요가 없다.
+            if (preCarEnd >= route[0]) continue;
+
+            //조건에 맞지 않으면, 이전 차랑 현재 차가 만나지 않으므로 카메라를 추가한다.
+            answer++;
+            //새로운 차에 대해서 끝나는 거리를 업데이트 한다.
+            preCarEnd = route[1];
         }
 
         return answer;
