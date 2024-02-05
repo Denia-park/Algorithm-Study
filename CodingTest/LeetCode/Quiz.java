f358eae869429b0fdd90e8700b5cd4ccb7b59a6f
HyunggiPark <phg9587@naver.com>
2024-02-05 12:05:01 +0900

    leetcode First Unique Character in a String - 정답 확인 후 리팩터링

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,11 +1,5 @@
 package CodingTest.LeetCode;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
@@ -19,36 +13,19 @@ public class Quiz {
 class Solution {
     public int firstUniqChar(final String s) {
         final char[] chars = s.toCharArray();
+        final int[] arr = new int[26];
 
-        final Map<Character, MyChar> map = new HashMap<>();
-
-        for (int i = 0; i < chars.length; i++) {
-            final char ch = chars[i];
-
-            final MyChar myChar = map.getOrDefault(ch, new MyChar(i));
-            myChar.count();
-            map.put(ch, myChar);
+        for (final char ch : chars) {
+            arr[ch - 'a']++;
         }
 
-        final List<Map.Entry<Character, MyChar>> list = map.entrySet().stream()
-                .filter(entry -> entry.getValue().count < 2)
-                .sorted(Comparator.comparingInt(value -> value.getValue().idx))
-                .collect(Collectors.toList());
-
-        return list.isEmpty() ? -1 : list.get(0).getValue().idx;
-    }
-
-    static class MyChar {
-        int idx;
-        int count;
-
-        public MyChar(final int idx) {
-            this.idx = idx;
-            this.count = 0;
+        final int charsLen = chars.length;
+        for (int i = 0; i < charsLen; i++) {
+            if (arr[chars[i] - 'a'] == 1) {
+                return i;
+            }
         }
 
-        void count() {
-            count++;
-        }
+        return -1;
     }
 }
