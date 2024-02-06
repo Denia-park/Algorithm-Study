9d18f009333e25ebd5e8cfc78e7d800d9d6578c3
HyunggiPark <phg9587@naver.com>
2024-02-06 10:47:31 +0900

    프로그래머스 2022 KAKAO BLIND RECRUITMENT 신고 결과 받기 - 1차 풀이 실패

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,32 +1,61 @@
 package CodingTest.Programmers;
 
-import java.util.Arrays;
-import java.util.Comparator;
+//다른 유저에 대해서는 계속 신고 가능
+//동일한 유저에 대해서는 1회만 처리
 
+//k번 이상 신고되면 메일을 발송
+
+import java.util.*;
+
+//신고당한 유저가 정지가 되면, 신고한 사람은 메일을 받는 구조
+//-> 신고할때 누가 신고했는지도 저장해야함
 class Solution {
+    public int[] solution(final String[] id_list, final String[] report, final int k) {
+        final Map<String, Set<String>> reportMap = new HashMap<>();
+        final Map<String, Integer> countMap = new HashMap<>();
+        final Map<String, List<String>> resultMap = new HashMap<>();
 
-    public int solution(final int[][] routes) {
-        int answer = 0;
+        for (final String str : report) {
+            final String[] split = str.split(" ");
+            final String from = split[0];
+            final String to = split[1];
 
-        //끝나는 거리 순으로 정렬을 한다.
-        //시작하는 거리는 무조건 끝나는 거리 이전이다.
-        Arrays.sort(routes, Comparator.comparingInt((int[] value) -> value[1]));
+            //신고한 사람이 동일한 사람을 신고하지 못하도록, Set에 넣어서 관리
+            final Set<String> set = reportMap.getOrDefault(from, new HashSet<>());
+            final boolean addResult = set.add(to);
+            reportMap.put(from, set);
 
-        //0번째 차를 비교하기 위해 시작하는 거리 -1로 값을 초기화한다.
-        int preCarEnd = -30001;
+            //addResult에 따라 신고가 들어갔는지 판단 가능
+            if (addResult) {
+                countMap.put(to, countMap.getOrDefault(to, 0) + 1);
 
-        //모든 차를 돌면서 비교한다.
-        for (final int[] route : routes) {
-            //현재 차의 시작하는 거리가 이전 차의 끝나는 거리보다 안쪽이면 (작으면)
-            //현재 차가 이전 차가 끝나기 전에 들어왔으므로 카메라는 추가할 필요가 없다.
-            if (preCarEnd >= route[0]) continue;
-
-            //조건에 맞지 않으면, 이전 차랑 현재 차가 만나지 않으므로 카메라를 추가한다.
-            answer++;
-            //새로운 차에 대해서 끝나는 거리를 업데이트 한다.
-            preCarEnd = route[1];
+                //신고 대상이 신고되면 메일을 받아야 하므로, 따로 Map에서 관리
+                final List<String> list = resultMap.getOrDefault(to, new ArrayList<>());
+                list.add(from);
+                resultMap.put(to, list);
+            }
         }
 
-        return answer;
+
+        final Map<String, Integer> mailMap = new HashMap<>();
+
+        //countMap을 돌면서, k번 이상이면 해당 유저들은 신고 처리됨
+        //신고 처리된 유저들을 신고한 사람들도 메일을 받는다.
+        countMap.entrySet().stream()
+                .filter(entry -> entry.getValue() >= k)
+                .map(Map.Entry::getKey)
+                .forEach(
+                        name -> {
+                            final List<String> list = resultMap.get(name);
+
+                            for (final String str : list) {
+                                mailMap.put(str, mailMap.getOrDefault(str, 0));
+                            }
+                        }
+                );
+
+        return Arrays.stream(id_list)
+                .mapToInt(id -> mailMap.getOrDefault(id_list, 0))
+                .toArray();
     }
 }
