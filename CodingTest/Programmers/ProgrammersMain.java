4c2ddb7f440917584c7dbdddf766ed6d298adea5
HyunggiPark <phg9587@naver.com>
2024-02-07 11:44:41 +0900

    프로그래머스 2022 KAKAO BLIND RECRUITMENT k진수에서 소수 - 1차 풀이 실패 - 런타임 에러

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,22 +1,17 @@
 package CodingTest.Programmers;
 
-import java.util.Arrays;
-
 public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(Arrays.toString(ts.solution(
-                new String[]{"muzi", "frodo", "apeach", "neo"},
-                new String[]{"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"},
-                2
-        )));
-
-        System.out.println(Arrays.toString(ts.solution(
-                new String[]{"con", "ryan"},
-                new String[]{"ryan con", "ryan con", "ryan con", "ryan con"},
+        System.out.println(ts.solution(
+                437674,
                 3
-        )));
+        ));
+        System.out.println(ts.solution(
+                110011,
+                10
+        ));
     }
 }
 
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,59 +1,39 @@
 package CodingTest.Programmers;
 
-//다른 유저에 대해서는 계속 신고 가능
-//동일한 유저에 대해서는 1회만 처리
-
-//k번 이상 신고되면 메일을 발송
-
-import java.util.*;
-
-//신고당한 유저가 정지가 되면, 신고한 사람은 메일을 받는 구조
-//-> 신고할때 누가 신고했는지도 저장해야함
 class Solution {
-    public int[] solution(final String[] id_list, final String[] report, final int k) {
-        final Map<String, Set<String>> reportMap = new HashMap<>();
-        final Map<String, Integer> countMap = new HashMap<>();
-        final Map<String, List<String>> resultMap = new HashMap<>();
+    public int solution(final int n, final int k) {
+        int answer = 0;
 
-        for (final String str : report) {
-            final String[] split = str.split(" ");
-            final String from = split[0];
-            final String to = split[1];
+        //k진수에 맞춰서 n을 변환
+        final String convert = Integer.toUnsignedString(n, k);
 
-            //신고한 사람이 동일한 사람을 신고하지 못하도록, Set에 넣어서 관리
-            final Set<String> set = reportMap.getOrDefault(from, new HashSet<>());
-            final boolean addResult = set.add(to);
-            reportMap.put(from, set);
+        //0으로 split
+        final String[] split = convert.split("0");
 
-            //addResult에 따라 신고가 들어갔는지 판단 가능
-            if (addResult) {
-                countMap.put(to, countMap.getOrDefault(to, 0) + 1);
-
-                //신고 대상이 신고되면 메일을 받아야 하므로, 따로 Map에서 관리
-                final List<String> list = resultMap.getOrDefault(to, new ArrayList<>());
-                list.add(from);
-                resultMap.put(to, list);
+        //해당 값이 소수인지 판단
+        for (final String s : split) {
+            if (isPrimeNum(s)) {
+                answer++;
             }
         }
 
+        return answer;
+    }
 
-        final Map<String, Integer> mailMap = new HashMap<>();
+    private boolean isPrimeNum(final String s) {
+        if (s.isBlank()) return false;
 
-        //countMap을 돌면서, k번 이상이면 해당 유저들은 신고 처리됨
-        //신고 처리된 유저들을 신고한 사람들도 메일을 받는다.
-        countMap.entrySet().stream()
-                .filter(entry -> entry.getValue() >= k)
-                .map(Map.Entry::getKey)
-                .forEach(
-                        name -> {
-                            for (final String str : resultMap.get(name)) {
-                                mailMap.put(str, mailMap.getOrDefault(str, 0) + 1);
-                            }
-                        }
-                );
+        final int val = Integer.parseInt(s);
 
-        return Arrays.stream(id_list)
-                .mapToInt(id -> mailMap.getOrDefault(id, 0))
-                .toArray();
+        if (val <= 1) return false;
+
+        final int root = (int) Math.sqrt(val);
+        for (int i = 2; i <= root; i++) {
+            if (val % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
