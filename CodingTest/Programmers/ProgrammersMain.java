337d97dc51feda7da3e9b8b1b26583d1117c592d
HyunggiPark <phg9587@naver.com>
2024-02-08 13:42:33 +0900

    프로그래머스 2022 KAKAO BLIND RECRUITMENT 주차 요금 계산 - 1차 풀이 실패

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,17 +1,23 @@
 package CodingTest.Programmers;
 
+import java.util.Arrays;
+
 public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(ts.solution(
-                437674,
-                3
-        ));
-        System.out.println(ts.solution(
-                110011,
-                10
-        ));
+        System.out.println(Arrays.toString(ts.solution(
+                new int[]{180, 5000, 10, 600},
+                new String[]{"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"}
+        )));
+        System.out.println(Arrays.toString(ts.solution(
+                new int[]{120, 0, 60, 591},
+                new String[]{"16:00 3961 IN", "16:00 0202 IN", "18:00 3961 OUT", "18:00 0202 OUT", "23:58 3961 IN"}
+        )));
+        System.out.println(Arrays.toString(ts.solution(
+                new int[]{1, 461, 1, 10},
+                new String[]{"00:00 1234 IN"}
+        )));
     }
 }
 
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,39 +1,93 @@
 package CodingTest.Programmers;
 
+import java.util.*;
+
 class Solution {
-    public int solution(final int n, final int k) {
-        int answer = 0;
+    public int[] solution(final int[] fees, final String[] records) {
+        //모든 출차 기록을 가지고 차들에 대한, 누적 주차 시간을 구한다.
+        //출입시 찍힘
+        final Map<String, String> parkInput = new HashMap<>();
 
-        //k진수에 맞춰서 n을 변환
-        final String convert = Integer.toUnsignedString(n, k);
+        //Set으로 주차장 관리
+        final Set<String> park = new HashSet<>();
 
-        //0으로 split
-        final String[] split = convert.split("0");
+        //주차장을 이용한 이용내역
+        final Map<String, Integer> cars = new TreeMap<>();
 
-        //해당 값이 소수인지 판단
-        for (final String s : split) {
-            if (isPrimeNum(s)) {
-                answer++;
+        for (final String record : records) {
+            //In, Out 판단
+            final String[] split = record.split(" ");
+            final String time = split[0];
+            final String carNumber = split[1];
+            final String ioStr = split[2];
+
+            //In이면 주차장에 넣고, 시간 넣기
+            if (ioStr.equals("IN")) {
+                park.add(carNumber);
+                parkInput.put(carNumber, time);
             }
+            //Out이면 주차장에서 빼기, 시간 빼기, 이용내역에 업데이트
+            else {
+                park.remove(carNumber);
+
+                final String inputTime = parkInput.get(carNumber);
+                final String outputTime = time;
+                final int originFee = cars.getOrDefault(carNumber, 0);
+                final int cost = calculateCost(fees, inputTime, outputTime, originFee);
+                cars.put(carNumber, cost);
+            }
+        }
+
+        //아직 주차장에 남아있는 차들을 시간 계산하기 -> 이용내역에 업데이트
+        for (final String carNumber : park) {
+            final String inputTime = parkInput.get(carNumber);
+            final String outputTime = "23:59";
+            final int originFee = cars.getOrDefault(carNumber, 0);
+            final int cost = calculateCost(fees, inputTime, outputTime, originFee);
+            cars.put(carNumber, cost);
+        }
+
+        //차량번호가 작은 자동차부터 차례대로 return (TreeMap 사용)
+        final int[] answer = new int[cars.size()];
+
+        int idx = 0;
+        for (final Integer val : cars.values()) {
+            answer[idx++] = val;
         }
 
         return answer;
     }
 
-    private boolean isPrimeNum(final String s) {
-        if (s.isBlank()) return false;
+    private int calculateCost(final int[] fees, final String inputTime, final String outTime, final int originFee) {
+        //들어온 시간, 나간 시간
+        final int totalTime = convertTimeToMin(inputTime, outTime);
 
-        final long val = Long.parseLong(s);
+        return calculateCostByTime(totalTime, fees) + originFee;
+    }
 
-        if (val <= 1) return false;
+    private int convertTimeToMin(final String inputTime, final String outTime) {
+        final int inMin = convertStringToInt(inputTime);
+        final int outMin = convertStringToInt(outTime);
 
-        final long root = (long) Math.sqrt(val);
-        for (int i = 2; i <= root; i++) {
-            if (val % i == 0) {
-                return false;
-            }
+        return outMin - inMin;
+    }
+
+    private int convertStringToInt(final String inputTime) {
+        final String[] split = inputTime.split(":");
+        return Integer.parseInt(split[0]) * 60 + Integer.parseInt(split[1]);
+    }
+
+    private int calculateCostByTime(final int totalTime, final int[] fees) {
+        final int defaultTime = fees[0];
+        final int defaultFee = fees[1];
+        final int perTime = fees[2];
+        final int perFee = fees[3];
+
+        if (totalTime <= defaultTime) {
+            return defaultFee;
         }
 
-        return true;
+        final int restTime = totalTime - defaultTime;
+        return defaultFee + ((int) Math.ceil((double) restTime / perTime) * perFee);
     }
 }
