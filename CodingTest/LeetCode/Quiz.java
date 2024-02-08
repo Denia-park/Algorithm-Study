11573dd8f44a61d00623ee0a600537087d97a736
HyunggiPark <phg9587@naver.com>
2024-02-08 13:03:11 +0900

    leetcode Perfect Squares - 정답 참고

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,50 +1,35 @@
 package CodingTest.LeetCode;
 
-import java.util.*;
+import java.util.Arrays;
 
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.frequencySort("tree"));
-        System.out.println(solution.frequencySort("cccaaa"));
-        System.out.println(solution.frequencySort("Aabb"));
+        System.out.println(solution.numSquares(12));
+        System.out.println(solution.numSquares(13));
     }
 }
 
 class Solution {
-    public String frequencySort(final String s) {
-        final Map<Character, MyCh> map = new HashMap<>();
-        for (final char ch : s.toCharArray()) {
-            map.computeIfAbsent(ch, MyCh::new).up();
+    public int numSquares(final int n) {
+        final int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        dp[0] = 0;
+
+        //Bottom-up Calculate
+        for (int i = 1; i <= n; i++) {
+            int minValue = Integer.MAX_VALUE;
+            final int sqrt = (int) Math.sqrt(i);
+
+            for (int j = 1; j <= sqrt; j++) {
+                minValue = Math.min(minValue, dp[i - (j * j)] + 1);
+            }
+
+            dp[i] = minValue;
         }
 
-        final List<MyCh> list = new ArrayList<>(map.values());
-        list.sort(Comparator.comparingInt(MyCh::getCount).reversed());
-
-        final StringBuilder sb = new StringBuilder();
-        for (final MyCh myCh : list) {
-            sb.append(String.valueOf(myCh.ch).repeat(myCh.count));
-        }
-
-        return sb.toString();
-    }
-
-    static class MyCh {
-        char ch;
-        int count;
-
-        public MyCh(final char ch) {
-            this.ch = ch;
-            this.count = 0;
-        }
-
-        public void up() {
-            count++;
-        }
-
-        public int getCount() {
-            return count;
-        }
+        return dp[n];
     }
 }
