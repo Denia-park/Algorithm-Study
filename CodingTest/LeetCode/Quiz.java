81000c536821864d8cb76d3ebef6816fd6480de6
HyunggiPark <phg9587@naver.com>
2024-02-11 19:38:36 +0900

    leetcode Cherry Pickup II - 문제 잘못된거 골라서 수정 했지만 - 시간 초과

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -15,7 +15,7 @@ public class Quiz {
 
         System.out.println(solution.cherryPickup(
                 new int[][]{
-                        {1, 0, 0, 0, 0, 0, 0},
+                        {1, 0, 0, 0, 0, 0, 1},
                         {2, 0, 0, 0, 0, 3, 0},
                         {2, 0, 9, 0, 0, 0, 0},
                         {0, 3, 0, 5, 4, 0, 0},
