69f80310f9b757b86952673196e25881645da58f
HyunggiPark <phg9587@naver.com>
2024-02-11 20:35:07 +0900

    leetcode Cherry Pickup II - 정답 확인 - 틀린 버전

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -26,55 +26,38 @@ public class Quiz {
 }
 
 class Solution {
-    int rowLen = 0;
-    int colLen = 0;
+    int rowLen;
+    int colLen;
+    Integer[][][] dp;
 
     public int cherryPickup(final int[][] grid) {
         rowLen = grid.length;
         colLen = grid[0].length;
+        dp = new Integer[rowLen + 1][colLen + 1][colLen + 1];
 
-        //소프티어처럼 1번 끝나면, 2번이 출발한다.
-        //모든 경우의 수를 고려해서 최대 값을 구한다.
-        return calculate(grid, 0, 1, 0, 0);
+        return calculate(grid, 0, 0, colLen - 1);
     }
 
-    private int calculate(final int[][] grid, final int collect, final int robotNum, final int r, final int c) {
-        //1번이 끝남 -> 2번 시작
-        if (robotNum == 1 && r == rowLen) {
-            return collect + calculate(grid, 0, 2, 0, colLen - 1);
-        } else if (robotNum == 2 && r == rowLen) {
-            return 0;
+    private int calculate(final int[][] grid, final int currRow, final int aCol, final int bCol) {
+        if (aCol < 0 || bCol < 0 ||
+                aCol >= colLen || bCol >= colLen) return 0;
+
+        if (currRow == rowLen) return 0; // last row
+
+        int result = 0;
+        result += grid[currRow][aCol];
+        result += grid[currRow][bCol];
+
+        int max = 0; //DFS for next row;
+        for (int aC = aCol - 1; aC <= aCol + 1; aC++) {
+            for (int bC = bCol - 1; bC <= bCol + 1; bC++) {
+                if (aC < bC) {
+                    max = Math.max(max, calculate(grid, currRow + 1, aC, bC));
+                }
+            }
         }
 
-        int max = 0;
-
-        //현재 자리에서 체리 수집
-        final int cherry = grid[r][c];
-        grid[r][c] = 0;
-
-        //왼쪽
-        final int leftC = c - 1;
-        if (0 <= leftC && leftC < colLen) {
-            final int temp = cherry + calculate(grid, collect, robotNum, r + 1, leftC);
-            max = Math.max(max, temp);
-        }
-
-        //아래
-        final int downC = c;
-        if (0 <= downC && downC < colLen) {
-            final int temp = cherry + calculate(grid, collect, robotNum, r + 1, downC);
-            max = Math.max(max, temp);
-        }
-
-        //오른쪽
-        final int rightC = c + 1;
-        if (0 <= rightC && rightC < colLen) {
-            final int temp = cherry + calculate(grid, collect, robotNum, r + 1, rightC);
-            max = Math.max(max, temp);
-        }
-
-        grid[r][c] = cherry;
-
-        return max;
+        result += max; //add maximum result
+        return result;
     }
 }
