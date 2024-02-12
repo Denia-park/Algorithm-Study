6a37f868feea4e7bf3bb6ee03d75a052660a4bb9
HyunggiPark <phg9587@naver.com>
2024-02-12 13:13:19 +0900

    leetcode Majority Element - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,70 +1,39 @@
 package CodingTest.LeetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.cherryPickup(
-                new int[][]{
-                        {3, 1, 1},
-                        {2, 5, 1},
-                        {1, 5, 5},
-                        {2, 1, 1}
-                }
+        System.out.println(solution.majorityElement(
+                new int[]{3, 2, 3}
         ));
-
-        System.out.println(solution.cherryPickup(
-                new int[][]{
-                        {1, 0, 0, 0, 0, 0, 1},
-                        {2, 0, 0, 0, 0, 3, 0},
-                        {2, 0, 9, 0, 0, 0, 0},
-                        {0, 3, 0, 5, 4, 0, 0},
-                        {1, 0, 2, 3, 0, 0, 6}
-                }
+        System.out.println(solution.majorityElement(
+                new int[]{2, 2, 1, 1, 1, 2, 2}
         ));
     }
 }
 
 class Solution {
-    int rowLen;
-    int colLen;
-    Integer[][][] dp;
+    public int majorityElement(final int[] nums) {
+        int max = 0;
+        int key = 0;
 
-    public int cherryPickup(final int[][] grid) {
-        rowLen = grid.length;
-        colLen = grid[0].length;
-        dp = new Integer[rowLen + 1][colLen + 1][colLen + 1];
+        final Map<Integer, Integer> map = new HashMap<>();
 
-        return calculate(grid, 0, 0, colLen - 1);
-    }
+        for (final int num : nums) {
+            final Integer i = map.getOrDefault(num, 0);
 
-    private int calculate(final int[][] grid, final int currRow, final int aCol, final int bCol) {
-        if (aCol < 0 || bCol < 0 ||
-                aCol >= colLen || bCol >= colLen) return 0;
-
-        if (currRow == rowLen) return 0; // last row
-
-        if (dp[currRow][aCol][bCol] != null) {
-            return dp[currRow][aCol][bCol];
-        }
-
-        int result = 0;
-        result += grid[currRow][aCol];
-        result += grid[currRow][bCol];
-
-        int max = 0; //DFS for next row;
-        for (int aC = aCol - 1; aC <= aCol + 1; aC++) {
-            for (int bC = bCol - 1; bC <= bCol + 1; bC++) {
-                if (aC < bC) {
-                    max = Math.max(max, calculate(grid, currRow + 1, aC, bC));
-                }
+            if (i + 1 > max) {
+                max = i + 1;
+                key = num;
             }
+
+            map.put(num, i + 1);
         }
 
-        result += max; //add maximum result
-
-        dp[currRow][aCol][bCol] = result;
-
-        return result;
+        return key;
     }
 }
