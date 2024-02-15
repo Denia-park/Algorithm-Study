6707afc71c1f43248633071be00e079fa1c3680f
HyunggiPark <phg9587@naver.com>
2024-02-15 11:31:27 +0900

    프로그래머스 2022 KAKAO BLIND RECRUITMENT 파괴되지 않은 건물 - 정답 확인

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,11 +1,24 @@
 package CodingTest.Programmers;
 
+//https://school.programmers.co.kr/questions/25471
+
 class Solution {
     static final int ATTACK = 1;
     static final int HEAL = 2;
 
     public int solution(final int[][] board, final int[][] skill) {
         int answer = 0;
+
+        final int[][] newBoard = new int[board.length + 1][board[0].length + 1];
+
+        //누적합 구하기
+        //(r1, c1) ~ (r2, c2) 좌표에 n의 값을 변화주고 싶다.
+        //r1, c1 = n
+        //r1, c2 + 1 = -n
+        //r2 + 1, c1 = -n
+        //r2 + 1 , c2 + 1 = n
+
+        //오른쪽으로 다 더하고 아래쪽으로 더하기
 
         //skill을 다 적용
         for (final int[] ints : skill) {
@@ -16,17 +29,33 @@ class Solution {
             final int c2 = ints[4];
             final int degree = ints[5];
 
-            for (int r = r1; r <= r2; r++) {
-                for (int c = c1; c <= c2; c++) {
-                    board[r][c] += type * degree;
-                }
+            final int add = type * degree;
+
+            newBoard[r1][c1] += add;
+            newBoard[r1][c2 + 1] -= add;
+            newBoard[r2 + 1][c1] -= add;
+            newBoard[r2 + 1][c2 + 1] += add;
+        }
+
+        //누적합 계산
+        for (int r = 0; r < newBoard.length; r++) {
+            for (int c = 1; c < newBoard[0].length; c++) {
+                newBoard[r][c] += newBoard[r][c - 1];
+            }
+        }
+
+        for (int c = 0; c < newBoard[0].length; c++) {
+            for (int r = 1; r < newBoard.length; r++) {
+                newBoard[r][c] += newBoard[r - 1][c];
             }
         }
 
         //멀쩡한 건물의 수를 센다.
-        for (final int[] ints : board) {
-            for (final int val : ints) {
-                if (val > 0) {
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                final int sum = newBoard[r][c] + board[r][c];
+
+                if (sum > 0) {
                     answer++;
                 }
             }
