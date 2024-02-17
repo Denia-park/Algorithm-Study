be39277c945c6f591f600d502615d2c375d698e6
HyunggiPark <phg9587@naver.com>
2024-02-17 17:08:21 +0900

    leetcode Furthest Building You Can Reach - 1차 풀이 실패 - 시간 초과

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,46 +1,68 @@
 package CodingTest.LeetCode;
 
-import java.util.*;
-
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
         System.out.println(
-                solution.findLeastNumOfUniqueInts(
-                        new int[]{5, 5, 4}, 1
+                solution.furthestBuilding(
+                        new int[]{4, 2, 7, 6, 9, 14, 12}, 5, 1
                 )
         );
         System.out.println(
-                solution.findLeastNumOfUniqueInts(
-                        new int[]{4, 3, 1, 1, 3, 3, 2}, 3
+                solution.furthestBuilding(
+                        new int[]{4, 12, 2, 7, 3, 18, 20, 3, 19}, 10, 2
+                )
+        );
+        System.out.println(
+                solution.furthestBuilding(
+                        new int[]{14, 3, 19, 3}, 17, 0
                 )
         );
     }
 }
 
+//벽돌 및 사다리 중에 어떤게 더 옳은 선택인지 모르니까, 둘 다 도전해봐야 한다.
 class Solution {
-    public int findLeastNumOfUniqueInts(final int[] arr, int k) {
-        final Map<Integer, Integer> countMap = new HashMap<>();
-        for (final int val : arr) {
-            final Integer num = countMap.getOrDefault(val, 0);
-            countMap.put(val, num + 1);
+    int[] gHeights;
+
+    public int furthestBuilding(final int[] heights, final int bricks, final int ladders) {
+        gHeights = heights;
+        final int curIdx = 0;
+        int answer = -1;
+
+        answer = Math.max(answer, go(curIdx, bricks, ladders));
+
+        return answer;
+    }
+
+    private int go(final int curIdx, final int bricks, final int ladders) {
+        int result = curIdx;
+        final int nextIdx = curIdx + 1;
+
+        if (nextIdx >= gHeights.length) {
+            return result;
         }
 
-        final List<Map.Entry<Integer, Integer>> entries = new ArrayList<>(countMap.entrySet());
-        entries.sort(Comparator.comparingInt(Map.Entry::getValue));
+        final int curHeight = gHeights[curIdx];
+        final int nextHeight = gHeights[nextIdx];
 
-        for (final Map.Entry<Integer, Integer> entry : entries) {
-            final Integer count = entry.getValue();
+        if (curHeight >= nextHeight) {
+            result = Math.max(result, go(nextIdx, bricks, ladders));
+        } else {
+            //벽돌 쓴 경우 (가능한 경우에만 사용 가능)
+            final int diffHeight = nextHeight - curHeight;
+            if (diffHeight <= bricks) {
+                result = Math.max(result, go(nextIdx, bricks - diffHeight, ladders));
+            }
 
-            if (count <= k) {
-                k -= count;
-                countMap.remove(entry.getKey());
-            } else {
-                break;
+            //사다리 쓴 경우 (가능한 경우에만 사용 가능)
+            if (ladders > 0) {
+                result = Math.max(result, go(nextIdx, bricks, ladders - 1));
             }
         }
 
-        return countMap.size();
+        //마지막 Idx를 반환한다.
+        return result;
     }
 }
