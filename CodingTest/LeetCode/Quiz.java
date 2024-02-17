64dbaedda8cf808e9edff442018b316cd705de3a
HyunggiPark <phg9587@naver.com>
2024-02-17 18:10:19 +0900

    leetcode Furthest Building You Can Reach - 다른 사람 풀이

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -7,56 +7,49 @@ public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-//        System.out.println(
-//                solution.furthestBuilding(
-//                        new int[]{4, 2, 7, 6, 9, 14, 12}, 5, 1
-//                )
-//        );
+        System.out.println(
+                solution.furthestBuilding(
+                        new int[]{4, 2, 7, 6, 9, 14, 12}, 5, 1
+                )
+        );
         System.out.println(
                 solution.furthestBuilding(
                         new int[]{4, 12, 2, 7, 3, 18, 20, 3, 19}, 10, 2
                 )
         );
-//        System.out.println(
-//                solution.furthestBuilding(
-//                        new int[]{14, 3, 19, 3}, 17, 0
-//                )
-//        );
+        System.out.println(
+                solution.furthestBuilding(
+                        new int[]{14, 3, 19, 3}, 17, 0
+                )
+        );
     }
 }
 
 class Solution {
-    public int furthestBuilding(final int[] heights, int bricks, int ladders) {
+    public int furthestBuilding(final int[] h, int b, int l) {
         final PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
 
         int idx;
-        for (idx = 0; idx < heights.length - 1; idx++) {
-            final int height = heights[idx];
-            final int nextHeight = heights[idx + 1];
+        for (idx = 0; idx < h.length - 1; idx++) {
+            final int diff = h[idx + 1] - h[idx];
 
-            if (height >= nextHeight) {
+            if (diff <= 0) {
                 continue;
             }
 
-            final int diff = nextHeight - height;
-
-            bricks -= diff;
+            b -= diff;
             //우선순위 큐에 사용한 벽돌 수를 저장
             pq.offer(diff);
 
-            if (bricks >= 0) {
-                continue;
+            if (b < 0) {
+                l--;
+                b += pq.poll();
             }
 
             //벽돌도 모자르고, 사다리로 모자르면 끝
-            if (ladders <= 0) {
+            if (l < 0) {
                 break;
             }
-
-            //벽돌이 모자를 때, 최고로 벽돌을 많이 쓴 타이밍에 대신 사다리를 사용
-            //사다리가 남아 있으면 -> 사다리를 하나 차감하고, 많이 쓴 벽돌을 원상복구 시키면서 현재 쓰는 벽돌 차감
-            ladders -= 1;
-            bricks += pq.poll();
         }
 
         return idx;
