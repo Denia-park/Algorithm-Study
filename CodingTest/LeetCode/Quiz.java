d90fb75b6c33e8475679adee501d19c93b61fb4a
HyunggiPark <phg9587@naver.com>
2024-02-18 14:45:28 +0900

    leetcode Meeting Rooms III - 내 코드의 문제점 파악 후 수정

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -2,8 +2,7 @@ package CodingTest.LeetCode;
 
 import CodingTest.Programmers.BracketUtil;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 public class Quiz {
     public static void main(final String[] args) {
@@ -29,55 +28,95 @@ public class Quiz {
 }
 
 class Solution {
+    PriorityQueue<Room> playRoom;
+    PriorityQueue<Room> waitRoom;
+
     public int mostBooked(final int n, final int[][] meetings) {
-        final int[] ans = new int[n];
-        final long[] times = new long[n];
-        Arrays.sort(meetings, Comparator.comparingInt(o -> o[0]));
+        //현재 사용중인 방을 우선순위 큐로 다룬다
+        //빨리 끝나는 순으로 정렬이 되도록 한다.
+        playRoom = new PriorityQueue<>(Comparator.comparingLong(Room::getEndTime));
+        waitRoom = new PriorityQueue<>(Comparator.comparingInt(Room::getIdx));
 
+        final List<Room> list = new ArrayList<>();
+
+        for (int idx = 0; idx < n; idx++) {
+            final Room room = new Room(idx, 0, new long[2]);
+            list.add(room);
+            waitRoom.add(room);
+        }
+
+        Arrays.sort(meetings, Comparator.comparingInt(arr -> arr[0]));
+
+        //모든 미팅이 끝날때까지 순환
         for (final int[] meeting : meetings) {
-            final int start = meeting[0];
-            final int end = meeting[1];
+            //미팅 시작 시간
+            long curTime = meeting[0];
+            final int duration = meeting[1] - meeting[0];
 
-            boolean flag = false;
-            int minIdx = -1;
-            long val = Long.MAX_VALUE;
-
-            //모든 방을 돌면서, 제일 빨리 끝나는 방을 찾는다.
-            for (int idx = 0; idx < n; idx++) {
-                //제일 빨리 끝나는 방 저장
-                if (times[idx] < val) {
-                    val = times[idx];
-                    minIdx = idx;
-                }
-
-                //시작할 수 있는 방을 찾았으면 멈춘다.
-                if (times[idx] <= start) {
-                    flag = true;
-                    ans[idx]++;
-                    times[idx] = end;
-                    break;
-                }
+            //현재 시작하는 미팅시간보다 빨리 끝나는 애들은 다 빼준다.
+            while (!playRoom.isEmpty() && playRoom.peek().getEndTime() <= curTime) {
+                waitRoom.add(playRoom.poll());
             }
 
-            //방을 못 찾았으면, 제일 빨리 끝나는 방에서 시작한다.
-            if (!flag) {
-                ans[minIdx]++;
-                times[minIdx] += (end - start);
+            //빈 방이 있으면 할당한다.
+            if (!waitRoom.isEmpty()) {
+                //빈 방을 하나 꺼낸다 (idx 정렬)
+                final Room room = waitRoom.poll();
+                //빈 방이 끝나는 시간이랑 현재 시작 시간이랑 비교해서 늦은 시간을 사용한다.
+                final long tempCurTime = Math.max(room.getEndTime(), curTime);
+
+                occupyRoom(room, new long[]{tempCurTime, tempCurTime + duration});
+                continue;
             }
+
+            //빈 방이 없으면, 기다렸다가 들어가야함
+            //제일 빨리 끝나는 방을 기다린다.
+            curTime = playRoom.peek().getEndTime();
+            //같이 끝나는 방들도 빈방으로 처리한다.
+            while (!playRoom.isEmpty() && playRoom.peek().getEndTime() <= curTime) {
+                waitRoom.add(playRoom.poll());
+            }
+
+            occupyRoom(waitRoom.poll(), new long[]{curTime, curTime + duration});
         }
 
-        //가장 많이 사용한 방을 찾는다.
-        int max = -1;
-        int resultIdx = -1;
-        for (int i = 0; i < ans.length; i++) {
-            final int val = ans[i];
+        //많이 쓴 방 순으로 정렬, 값이 작은 순으로 정렬
+        list.sort(
+                Comparator
+                        .comparingLong(Room::getCount).reversed()
+                        .thenComparingInt(Room::getIdx)
+        );
 
-            if (val > max) {
-                max = val;
-                resultIdx = i;
-            }
+        return list.get(0).idx;
+    }
+
+    private void occupyRoom(final Room room, final long[] newMeetingTime) {
+        room.count++;
+        room.time = newMeetingTime;
+        playRoom.add(room);
+    }
+
+    class Room {
+        int idx;
+        long count;
+        long[] time;
+
+        Room(final int idx, final int count, final long[] time) {
+            this.idx = idx;
+            this.count = count;
+            this.time = time;
         }
 
-        return resultIdx;
+        int getIdx() {
+            return idx;
+        }
+
+        long getCount() {
+            return count;
+        }
+
+        long getEndTime() {
+            return time[1];
+        }
     }
 }
