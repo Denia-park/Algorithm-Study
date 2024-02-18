81a1c4a64ea897ccc4fd7f7b4d7ff86792bdc86b
HyunggiPark <phg9587@naver.com>
2024-02-18 11:57:08 +0900

    leetcode Meeting Rooms III - 6차 풀이 실패

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
-
+//
 //        System.out.println(
 //                solution.mostBooked(
 //                        2,
@@ -32,22 +32,22 @@ public class Quiz {
 //                        )
 //                )
 //        );
-//        System.out.println(
-//                solution.mostBooked(
-//                        4,
-//                        BracketUtil.convertStrToIntArr(
-//                                "[[18,19],[3,12],[17,19],[2,13],[7,10]]"
-//                        )
-//                )
-//        );
-//        System.out.println(
-//                solution.mostBooked(
-//                        2,
-//                        BracketUtil.convertStrToIntArr(
-//                                "[[10,11],[2,10],[1,17],[9,13],[18,20]]"
-//                        )
-//                )
-//        );
+        System.out.println(
+                solution.mostBooked(
+                        4,
+                        BracketUtil.convertStrToIntArr(
+                                "[[18,19],[3,12],[17,19],[2,13],[7,10]]"
+                        )
+                )
+        );
+        System.out.println(
+                solution.mostBooked(
+                        2,
+                        BracketUtil.convertStrToIntArr(
+                                "[[10,11],[2,10],[1,17],[9,13],[18,20]]"
+                        )
+                )
+        );
         System.out.println(
                 solution.mostBooked(
                         4,
@@ -84,36 +84,51 @@ class Solution {
 
         //모든 미팅이 끝날때까지 순환
         for (final int[] meeting : meetings) {
-            if (playRoom.size() < n) { //방을 만든다.
-                final Room addRoom = waitRoom.poll();
-                addRoom.up();
-                addRoom.time = meeting;
+            //play 방이 없으면 바로 waiting 방에 진입
+            if (playRoom.isEmpty()) {
+                final Room room = waitRoom.poll();
+                room.up();
+                room.time = meeting;
 
-                playRoom.add(addRoom);
-            } else { //방이 끝날때까지 기다렸다가 들어간다.
-                //현재 시간을 구한다 (미팅이 끝난 시간이랑 현재 미팅 시작시간을 비교)
-                //애초에 현재 미팅이 늦게 시작하면 현재 미팅은 방이 다 비고 시작함
-                final int curTime = Math.max(playRoom.peek().time[1], meeting[0]);
-                //현재 시작하는 미팅시간보다 빨리 끝나는 애들은 다 빼준다.
-                while (!playRoom.isEmpty() && playRoom.peek().getEndTime() <= curTime) {
-                    waitRoom.add(playRoom.poll());
-                }
-
-                final int duration = meeting[1] - meeting[0];
-                final int newStartTime = curTime;
-                final int[] newMeeting = new int[]{newStartTime, newStartTime + duration};
-
-                final Room addRoom = waitRoom.poll();
-                addRoom.up();
-                addRoom.time = newMeeting;
-
-                playRoom.add(addRoom);
+                playRoom.add(room);
+                continue;
             }
+
+            final int firstEndTime = playRoom.peek().time[1];
+            int curTime = 0;
+            //빈 방이 남아있으면, 현재 시간은 현재 미팅 시작 시간
+            if (!waitRoom.isEmpty()) {
+                curTime = Math.min(firstEndTime, meeting[0]);
+            }
+            //빈 방이 없으면, 제일 빨리 미팅이 끝나는 시간이 현재 미팅 시작 시간
+            else {
+                curTime = Math.max(firstEndTime, meeting[0]);
+            }
+
+            //play 방이 있으면 현재 시간을 비교해서 현재 방이 끝났는지 확인한다.
+            //현재 시간을 구한다 (미팅이 끝난 시간이랑 현재 미팅 시작시간을 비교)
+            //애초에 현재 미팅이 늦게 시작하면 현재 미팅은 방이 다 비고 시작함
+
+            //현재 시작하는 미팅시간보다 빨리 끝나는 애들은 다 빼준다.
+            while (!playRoom.isEmpty() && playRoom.peek().getEndTime() <= curTime) {
+                waitRoom.add(playRoom.poll());
+            }
+
+            final int duration = meeting[1] - meeting[0];
+            final int newStartTime = curTime;
+            final int[] newMeeting = new int[]{newStartTime, newStartTime + duration};
+
+            final Room addRoom = waitRoom.poll();
+            addRoom.up();
+            addRoom.time = newMeeting;
+
+            playRoom.add(addRoom);
+
         }
 
-//        for (Room room : list) {
-//            System.out.println(room);
-//        }
+        for (final Room room : list) {
+            System.out.println(room);
+        }
 
         //많이 쓴 방 순으로 정렬, 값이 작은 순으로 정렬
         list.sort(
