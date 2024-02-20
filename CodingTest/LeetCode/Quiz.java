868611d5cef8b832f17c7d481f33fc28f1284736
HyunggiPark <phg9587@naver.com>
2024-02-20 13:25:25 +0900

    leetcode Missing Number - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,17 +1,29 @@
 package CodingTest.LeetCode;
 
+import java.util.Arrays;
+
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.isPowerOfTwo(1));
-        System.out.println(solution.isPowerOfTwo(16));
-        System.out.println(solution.isPowerOfTwo(3));
+        System.out.println(solution.missingNumber(new int[]{3, 0, 1}));
+        System.out.println(solution.missingNumber(new int[]{0, 1}));
+        System.out.println(solution.missingNumber(new int[]{9, 6, 4, 2, 3, 5, 7, 0, 1}));
     }
 }
 
 class Solution {
-    public boolean isPowerOfTwo(final int n) {
-        return n > 0 && (n & n - 1) == 0;
+    public int missingNumber(final int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            final int val = nums[i];
+
+            if (val != i) {
+                return i;
+            }
+        }
+
+        return nums.length;
     }
 }
