bb0066f971dd3b1e2a33a5c94d6a55842fa88c31
HyunggiPark <phg9587@naver.com>
2024-02-21 12:07:21 +0900

    leetcode Bitwise AND of Numbers Range - 정답 참고

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,22 +1,25 @@
 package CodingTest.LeetCode;
 
-import java.util.Arrays;
-
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.missingNumber(new int[]{3, 0, 1}));
-        System.out.println(solution.missingNumber(new int[]{0, 1}));
-        System.out.println(solution.missingNumber(new int[]{9, 6, 4, 2, 3, 5, 7, 0, 1}));
+        System.out.println(solution.rangeBitwiseAnd(5, 7));
+        System.out.println(solution.rangeBitwiseAnd(0, 0));
+        System.out.println(solution.rangeBitwiseAnd(1, 2147483647));
     }
 }
 
 class Solution {
-    public int missingNumber(final int[] nums) {
-        final int n = nums.length;
-        final int tsum = (n * (n + 1)) / 2;
-        final int actualSum = Arrays.stream(nums).sum();
-        return tsum - actualSum;
+    public int rangeBitwiseAnd(int left, int right) {
+        int cnt = 0;
+
+        while (left != right) {
+            left = left >> 1;
+            right = right >> 1;
+            cnt++;
+        }
+
+        return (left << cnt);
     }
 }
