988df6af68f603046b579fd20c07a748908a5ded
HyunggiPark <phg9587@naver.com>
2024-02-22 15:13:19 +0900

    leetcode Find the Town Judge - 1차 풀이 실패

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,25 +1,58 @@
 package CodingTest.LeetCode;
 
+import CodingTest.Programmers.BracketUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.rangeBitwiseAnd(5, 7));
-        System.out.println(solution.rangeBitwiseAnd(0, 0));
-        System.out.println(solution.rangeBitwiseAnd(1, 2147483647));
+        System.out.println(solution.findJudge(2,
+                BracketUtil.convertStrToIntArr(
+                        "[[1, 2]]"
+                )));
+        System.out.println(solution.findJudge(3,
+                BracketUtil.convertStrToIntArr(
+                        "[[1, 3], [2, 3]]"
+                )));
+        System.out.println(solution.findJudge(3,
+                BracketUtil.convertStrToIntArr(
+                        "[[1, 3], [2, 3], [3, 1]]"
+                )));
     }
 }
 
 class Solution {
-    public int rangeBitwiseAnd(int left, int right) {
-        int cnt = 0;
+    public int findJudge(final int n, final int[][] trust) {
+        //입력이 n -1개 이면서 출력이 0개
+        final Map<Integer, Man> map = new HashMap<>();
 
-        while (left != right) {
-            left = left >> 1;
-            right = right >> 1;
-            cnt++;
+        for (final int[] ints : trust) {
+            final int from = ints[0];
+            final int to = ints[1];
+
+            final Man fMan = map.computeIfAbsent(from, Man::new);
+            fMan.out++;
+
+            final Man tMan = map.computeIfAbsent(to, Man::new);
+            tMan.in++;
         }
 
-        return (left << cnt);
+        return map.values().stream()
+                .filter(man -> man.in == n - 1 && man.out == 0)
+                .mapToInt(man -> man.idx)
+                .findFirst().orElse(-1);
+    }
+
+    public class Man {
+        int idx;
+        int in;
+        int out;
+
+        Man(final int idx) {
+            this.idx = idx;
+        }
     }
 }
