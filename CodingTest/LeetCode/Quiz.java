035d8d36e87acce32a585ffc2fd3ac624c64416d
HyunggiPark <phg9587@naver.com>
2024-02-24 17:52:14 +0900

    leetcode Find All People With Secret - 정답 참고 했으나 틀림 - union-find 이용

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -3,68 +3,91 @@ package CodingTest.LeetCode;
 import CodingTest.Programmers.BracketUtil;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.findCheapestPrice(4,
+        System.out.println(solution.findAllPeople(6,
                 BracketUtil.convertStrToIntArr(
-                        "[[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]]"
-                ),
-                0, 3, 1));
-        System.out.println(solution.findCheapestPrice(3,
+                        "[[1,2,5],[2,3,8],[1,5,10]]"),
+                1));
+        System.out.println(solution.findAllPeople(4,
                 BracketUtil.convertStrToIntArr(
-                        "[[0,1,100],[1,2,100],[0,2,500]]"
-                ),
-                0, 2, 1));
-        System.out.println(
-                solution.findCheapestPrice(5,
-                        BracketUtil.convertStrToIntArr(
-                                "[[0,1,100],[1,2,100],[0,2,500]]"
-                        ),
-                        0, 2, 0)
-        );
+                        "[[3,1,3],[1,2,2],[0,3,3]]"),
+                3));
+        System.out.println(solution.findAllPeople(5,
+                BracketUtil.convertStrToIntArr(
+                        "[[3,4,2],[1,2,1],[2,3,1]]"),
+                1));
     }
 }
 
 class Solution {
-    public int findCheapestPrice(final int n, final int[][] flights, final int src, final int dst, final int k) {
-        final Map<Integer, List<int[]>> adj = new HashMap<>();
-        for (final int[] flight : flights) {
-            adj.computeIfAbsent(flight[0], key -> new ArrayList<>()).add(new int[]{flight[1], flight[2]});
+    public List<Integer> findAllPeople(final int n, final int[][] meetings, final int firstPerson) {
+        final List<int[]> list = Arrays.stream(meetings)
+                .sorted(Comparator.comparingInt(v -> v[2]))
+                .collect(Collectors.toList());
+
+        //parents 초기화
+        final int[] parents = new int[n];
+        for (int i = 0; i < parents.length; i++) {
+            parents[i] = i;
         }
+        //처음에 비밀을 들은 사람
+        parents[firstPerson] = 0;
 
-        final int[] dist = new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[src] = 0;
+        //유니온 - 파인드로 묶어줄 로직
+        int idx = 0;
+        final int size = list.size();
+        final Set<Integer> listeners = new HashSet<>();
+        while (idx < size) {
+            final int time = list.get(idx)[2];
+            listeners.clear();
 
-        final Deque<int[]> dq = new ArrayDeque<>();
-        dq.offerLast(new int[]{src, 0});
-        int stops = 0;
+            //같은 시간인 애들은 한번에 다 계산을 해야함
+            while (idx < size && time == list.get(idx)[2]) {
+                final int[] meeting = list.get(idx);
+                final int parent0 = find(parents, meeting[0]);
+                final int parent1 = find(parents, meeting[1]);
+                if (parent0 != parent1) {//부모가 다르면
+                    //유니온
+                    parents[Math.max(parent0, parent1)] = Math.min(parent0, parent1);
+                }
+                listeners.add(meeting[0]);
+                listeners.add(meeting[1]);
+                idx++;
+            }
 
-        while (!dq.isEmpty() && stops <= k) {
-            int size = dq.size();
-            while (size-- > 0) {
-                final int[] curr = dq.poll();
-                final int curNode = curr[0];
-                final int curPrice = curr[1];
 
-                if (!adj.containsKey(curNode)) continue;
-
-                for (final int[] next : adj.get(curNode)) {
-                    final int nextNode = next[0];
-                    final int nextPrice = next[1];
-
-                    if (curPrice + nextPrice >= dist[nextNode]) continue;
-                    dist[nextNode] = curPrice + nextPrice;
-
-                    dq.offerLast(new int[]{nextNode, dist[nextNode]});
+            for (final Integer man : listeners) {
+                final int parent = find(parents, man);
+                //비밀을 못 들은 애들 -> 유니온을 푼다. (못 들었으니까)
+                if (parent != 0) {
+                    parents[man] = man;
                 }
             }
-            stops++;
         }
 
-        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+        //제대로 비밀을 아는 사람을 구할 로직 -> 부모 같은 데, 그 부모가 비밀을 알고 있으면 모두 다 비밀을 안다.
+        final List<Integer> answer = new ArrayList<>();
+        for (int man = 0; man < n; man++) {
+            final int parent = find(parents, man);
+            if (parent == 0) {
+                answer.add(man);
+            }
+        }
+
+        return answer;
+    }
+
+    private int find(final int[] parents, final int child) {
+        if (parents[child] == child) {
+            return parents[child];
+        }
+
+        parents[child] = find(parents, parents[child]);
+        return parents[child];
     }
 }
