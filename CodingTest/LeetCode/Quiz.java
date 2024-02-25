33eab3c3e25e25e2551b9fcc521ef3cb54a8a83b
HyunggiPark <phg9587@naver.com>
2024-02-25 17:02:50 +0900

    leetcode Greatest Common Divisor Traversal - 정답 참조

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,5 +1,10 @@
 package CodingTest.LeetCode;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
@@ -16,54 +21,80 @@ public class Quiz {
     }
 }
 
+//https://leetcode.com/problems/greatest-common-divisor-traversal/solutions/3569177/java-prime-factors-and-disjoint-set-union-code-with-comments/?envType=daily-question&envId=2024-02-25
 class Solution {
     public boolean canTraverseAllPairs(final int[] nums) {
-        //서로 연결되어 있는가 ? -> 유니온-파인드
-        final int length = nums.length;
-        final int[] parents = new int[length];
-        for (int i = 0; i < length; i++) {
-            parents[i] = i;
-        }
+        if (nums.length == 1) return true;
 
-        //일단 가능한 조합에 대해서 묶기
-        for (int i = 0; i < length; i++) {
-            for (int j = i + 1; j < length; j++) {
-                if (gcd(nums[i], nums[j]) <= 1) {
-                    continue;
-                }
+        final Map<Integer, Integer> dsu = new HashMap<>();
 
-                final int parent1 = find(parents, i);
-                final int parent2 = find(parents, j);
+        for (final int num : nums) {
+            // if num is 1 GCD with any other number will be 1 so return false.
+            if (num == 1) return false;
 
-                parents[Math.max(parent1, parent2)] = Math.min(parent1, parent2);
+            // Get all unique [pri]
+            final Set<Integer> factors = findFactors(num);
+
+            // First time visiting this node, it will be a separate component itself.
+            if (!dsu.containsKey(num)) dsu.put(num, num);
+
+            // For all prime factors merge factor and num.
+            for (final int factor : factors) {
+                // If first time visiting this factor, it will be a separate component itself.
+                if (!dsu.containsKey(factor)) dsu.put(factor, factor);
+
+                // merge num and factor in one component.
+                union(dsu, num, factor);
             }
         }
 
-        //모든 조합에 대해서 부모가 같은지 확인
-        final int saveParent = find(parents, 0);
+        // collect parent of all the components.
+        final Set<Integer> components = new HashSet<>();
+        for (final int num : nums) {
+            components.add(find(dsu, num));
+        }
 
-        for (int i = 1; i < length; i++) {
-            if (saveParent != find(parents, i)) {
-                return false;
+        // if all nodes are part of same component components count will be 1.
+        return components.size() == 1;
+    }
+
+    // Union operation to merge two components.
+    private void union(final Map<Integer, Integer> dsu, final int num1, final int num2) {
+        final int p1 = find(dsu, num1);
+        final int p2 = find(dsu, num2);
+
+        dsu.put(p1, p2);
+    }
+
+    // Find operation to get the parent of DSU node.
+    private int find(final Map<Integer, Integer> dsu, final int num) {
+        if (dsu.getOrDefault(num, num) != num) {
+            dsu.put(num, find(dsu, dsu.get(num)));
+        }
+
+        return dsu.getOrDefault(num, num);
+    }
+
+    // Method to get all the unique factors of the number.
+    private HashSet<Integer> findFactors(int number) {
+        final HashSet<Integer> primeFactors = new HashSet<>();
+
+        while (number % 2 == 0) {
+            primeFactors.add(2);
+            number /= 2;
+        }
+
+        for (int i = 3; i <= Math.sqrt(number); i += 2) {
+            while (number % i == 0) {
+                primeFactors.add(i);
+                number /= i;
             }
         }
 
-        return true;
-    }
-
-    private int find(final int[] parents, int idx) {
-        while (parents[idx] != idx) {
-            idx = parents[idx];
+        if (number > 2) {
+            primeFactors.add(number);
         }
 
-        return parents[idx];
-    }
-
-    private int gcd(final int first, final int second) {
-        if (first % second == 0) {
-            return second;
-        } else {
-            return gcd(second % first, first);
-        }
+        return primeFactors;
     }
 }
