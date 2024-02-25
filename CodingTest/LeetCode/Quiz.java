b7df51f323624a1b547db3fd371bfa1e523a3017
HyunggiPark <phg9587@naver.com>
2024-02-25 16:42:44 +0900

    leetcode Greatest Common Divisor Traversal - 3차 풀이 시간 초과 - 원인 파악 못함

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,9 +1,5 @@
 package CodingTest.LeetCode;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
@@ -21,13 +17,7 @@ public class Quiz {
 }
 
 class Solution {
-    Map<String, Integer> dp;
-
     public boolean canTraverseAllPairs(final int[] nums) {
-        dp = new HashMap<>();
-
-        Arrays.sort(nums);
-
         //서로 연결되어 있는가 ? -> 유니온-파인드
         final int length = nums.length;
         final int[] parents = new int[length];
@@ -38,9 +28,7 @@ class Solution {
         //일단 가능한 조합에 대해서 묶기
         for (int i = 0; i < length; i++) {
             for (int j = i + 1; j < length; j++) {
-                final int gcd = gcd(nums[i], nums[j]);
-
-                if (gcd <= 1) {
+                if (gcd(nums[i], nums[j]) <= 1) {
                     continue;
                 }
 
@@ -52,14 +40,11 @@ class Solution {
         }
 
         //모든 조합에 대해서 부모가 같은지 확인
-        for (int i = 0; i < length; i++) {
-            for (int j = i + 1; j < length; j++) {
-                final int parent1 = find(parents, i);
-                final int parent2 = find(parents, j);
+        final int saveParent = find(parents, 0);
 
-                if (parent1 != parent2) {
-                    return false;
-                }
+        for (int i = 1; i < length; i++) {
+            if (saveParent != find(parents, i)) {
+                return false;
             }
         }
 
@@ -75,18 +60,10 @@ class Solution {
     }
 
     private int gcd(final int first, final int second) {
-        final String key = first + ":" + second;
-        if (dp.get(key) != null) {
-            return dp.get(key);
-        }
-
         if (first % second == 0) {
-            dp.put(key, second);
             return second;
         } else {
-            final int gcd = gcd(second % first, first);
-            dp.put(key, gcd);
-            return gcd;
+            return gcd(second % first, first);
         }
     }
 }
