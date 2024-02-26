6b221f26d693ad3ee61ebde90edc9d7535a59541
HyunggiPark <phg9587@naver.com>
2024-02-26 15:34:25 +0900

    leetcode Same Tree - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,100 +1,57 @@
 package CodingTest.LeetCode;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.canTraverseAllPairs(
-                new int[]{2, 3, 6}
-        ));
-        System.out.println(solution.canTraverseAllPairs(
-                new int[]{3, 9, 5}
-        ));
-        System.out.println(solution.canTraverseAllPairs(
-                new int[]{4, 3, 12, 8}
-        ));
+//        System.out.println(solution.isSameTree(
+//                new int[]{2, 3, 6}
+//        ));
+//        System.out.println(solution.isSameTree(
+//                new int[]{3, 9, 5}
+//        ));
+//        System.out.println(solution.isSameTree(
+//                new int[]{4, 3, 12, 8}
+//        ));
     }
 }
 
-//https://leetcode.com/problems/greatest-common-divisor-traversal/solutions/3569177/java-prime-factors-and-disjoint-set-union-code-with-comments/?envType=daily-question&envId=2024-02-25
 class Solution {
-    public boolean canTraverseAllPairs(final int[] nums) {
-        if (nums.length == 1) return true;
+    public boolean isSameTree(final TreeNode p, final TreeNode q) {
+        final String strP = inorder(p);
+        final String strQ = inorder(q);
 
-        final Map<Integer, Integer> dsu = new HashMap<>();
-
-        for (final int num : nums) {
-            // if num is 1 GCD with any other number will be 1 so return false.
-            if (num == 1) return false;
-
-            // Get all unique [pri]
-            final Set<Integer> factors = findFactors(num);
-
-            // First time visiting this node, it will be a separate component itself.
-            if (!dsu.containsKey(num)) dsu.put(num, num);
-
-            // For all prime factors merge factor and num.
-            for (final int factor : factors) {
-                // If first time visiting this factor, it will be a separate component itself.
-                if (!dsu.containsKey(factor)) dsu.put(factor, factor);
-
-                // merge num and factor in one component.
-                union(dsu, num, factor);
-            }
-        }
-
-        // collect parent of all the components.
-        final Set<Integer> components = new HashSet<>();
-        for (final int num : nums) {
-            components.add(find(dsu, num));
-        }
-
-        // if all nodes are part of same component components count will be 1.
-        return components.size() == 1;
+        return strP.equals(strQ);
     }
 
-    // Union operation to merge two components.
-    private void union(final Map<Integer, Integer> dsu, final int num1, final int num2) {
-        final int p1 = find(dsu, num1);
-        final int p2 = find(dsu, num2);
+    private String inorder(final TreeNode p) {
+        if (p == null) {
+            return "";
+        }
 
-        dsu.put(p1, p2);
+        final String sb = "l:" + inorder(p.left) +
+                "c:" + p.val + "," +
+                "r:" + inorder(p.right);
+
+        return sb;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
     }
 
-    // Find operation to get the parent of DSU node.
-    private int find(final Map<Integer, Integer> dsu, final int num) {
-        if (dsu.getOrDefault(num, num) != num) {
-            dsu.put(num, find(dsu, dsu.get(num)));
-        }
-
-        return dsu.getOrDefault(num, num);
+    TreeNode(final int val) {
+        this.val = val;
     }
 
-    // Method to get all the unique factors of the number.
-    private HashSet<Integer> findFactors(int number) {
-        final HashSet<Integer> primeFactors = new HashSet<>();
-
-        while (number % 2 == 0) {
-            primeFactors.add(2);
-            number /= 2;
-        }
-
-        for (int i = 3; i <= Math.sqrt(number); i += 2) {
-            while (number % i == 0) {
-                primeFactors.add(i);
-                number /= i;
-            }
-        }
-
-        if (number > 2) {
-            primeFactors.add(number);
-        }
-
-        return primeFactors;
+    TreeNode(final int val, final TreeNode left, final TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
