327acdef4aad32ddfbc92e98020a7a72be6f04fc
HyunggiPark <phg9587@naver.com>
2024-03-01 13:38:46 +0900

    프로그래머스 코딩테스트 연습 선입 선출 스케줄링 - 정답 참고
    - 런타임 에러 및 실패가 존재함

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,37 +1,52 @@
 package CodingTest.Programmers;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
 class Solution {
     public int solution(final int n, final int[] cores) {
-        final PriorityQueue<Core> pq = new PriorityQueue<>(
-                Comparator.comparingLong((Core c) -> c.endTime).thenComparingInt((Core c) -> c.idx)
-        );
-
-        for (int i = 0; i < cores.length; i++) {
-            pq.add(new Core(i + 1, cores[i]));
+        if (n <= cores.length) {
+            return n;
         }
 
-        for (int i = 0; i < n - 1; i++) {
-            final Core core = pq.poll();
+        final int restWork = n - cores.length;
 
-            core.endTime += core.spend;
+        long minTime = Long.MAX_VALUE;
 
-            pq.add(core);
+        //이분 탐색을 통해 모두 처리가 가능한 시간을 구하자
+        long l = 1;
+        long r = 10000L * (restWork / cores.length);
+        while (l <= r) {
+            final long mid = l + ((r - l) / 2);
+
+            final int work = calculate(mid, cores);
+
+            if (work >= restWork) {
+                minTime = Math.min(minTime, mid);
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
         }
 
-        return pq.poll().idx;
+        final int finishedWork = calculate(minTime - 1, cores);
+        int lastRestWork = restWork - finishedWork;
+
+        int idx = 0;
+        while (lastRestWork > 0) {
+            if (minTime % cores[idx] == 0) {
+                lastRestWork--;
+            }
+            idx++;
+        }
+
+        return idx;
     }
 
-    class Core {
-        int idx;
-        long endTime;
-        int spend;
+    private int calculate(final long mid, final int[] cores) {
+        int count = 0;
 
-        public Core(final int idx, final int spend) {
-            this.idx = idx;
-            this.spend = spend;
+        for (final int core : cores) {
+            count += (int) (mid / core);
         }
+
+        return count;
     }
 }
