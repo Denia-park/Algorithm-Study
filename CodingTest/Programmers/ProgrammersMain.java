4cd2c13b8df5bdacb846a71b8e77216286102cde
HyunggiPark <phg9587@naver.com>
2024-03-01 12:41:14 +0900

    프로그래머스 코딩테스트 연습 선입 선출 스케줄링 - 1차 풀이 실패

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,21 +1,12 @@
 package CodingTest.Programmers;
 
-import java.util.Arrays;
-
 public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(Arrays.toString(ts.solution(
-                BracketUtil.convertStrToIntArr(
-                        "[[2, 3], [4, 3], [1, 1], [2, 1]]"
-                )
-        )));
-        System.out.println(Arrays.toString(ts.solution(
-                BracketUtil.convertStrToIntArr(
-                        "[[4, 11], [1, 12], [8, 3], [12, 7], [4, 2], [7, 11], [4, 8], [9, 6], [10, 11], [6, 10], [3, 5], [11, 1], [5, 3], [11, 9], [3, 8]]"
-                )
-        )));
+        System.out.println(ts.solution(
+                6, new int[]{1, 2, 3}
+        ));
     }
 }
 
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,86 +1,41 @@
 package CodingTest.Programmers;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 class Solution {
+    public int solution(final int n, final int[] cores) {
+        final PriorityQueue<Core> pq = new PriorityQueue<>(
+                Comparator.comparingInt((Core c) -> c.endTime).thenComparingInt((Core c) -> c.idx)
+        );
 
-    private static final int DONUT = 0;
-    private static final int BLOCK = 1;
-    private static final int EIGHT = 2;
-    private Map<Integer, Node> map;
-
-    public int[] solution(final int[][] edges) {
-        final int[] answer = new int[4];
-
-        map = new HashMap<>();
-
-        //정점을 저장
-        for (final int[] edge : edges) {
-            final int from = edge[0];
-            final int to = edge[1];
-
-            //out 증가
-            final Node fromNode = map.computeIfAbsent(from, Node::new);
-            fromNode.outCnt++;
-            fromNode.add(to);
-
-
-            //in 증가
-            final Node toNode = map.computeIfAbsent(to, Node::new);
-            toNode.inCnt++;
+        for (int i = 0; i < cores.length; i++) {
+            pq.add(new Core(i + 1, cores[i]));
         }
 
-        //정점 찾기
-        final Node rootNode = map.values().stream()
-                .filter(node -> node.outCnt >= 2 && node.inCnt == 0)
-                .findFirst().get();
+        for (int i = 0; i < n; i++) {
+            final Core core = pq.poll();
 
-        answer[0] = rootNode.num;
+            core.endTime += core.spend;
 
-        for (final Integer startNum : rootNode.outNums) {
-            final int idx = dfs(startNum, true, startNum);
-            answer[idx + 1]++;
+            pq.add(core);
         }
 
-        return answer;
+        while (pq.size() != 1) {
+            pq.poll();
+        }
+
+        return pq.poll().idx;
     }
 
-    private int dfs(final Integer startNum, final boolean isFirstNode, final int curNum) {
-        //8자 인지 확인하기
-        final Node curNode = map.get(curNum);
-        if (curNode.inCnt >= 2 && curNode.outCnt >= 2) {
-            return EIGHT;
-        }
+    class Core {
+        int idx;
+        int endTime;
+        int spend;
 
-        //처음이 아닌데, 자기 자신을 다시 만나면 도넛
-        if (!isFirstNode && startNum == curNum) {
-            return DONUT;
-        }
-
-        //더 이상 못가면 막대
-        final List<Integer> outNums = map.get(curNum).outNums;
-        if (outNums.isEmpty()) {
-            return BLOCK;
-        }
-
-        return dfs(startNum, false, outNums.get(0));
-    }
-
-    class Node {
-        int num;
-        int inCnt;
-        int outCnt;
-        List<Integer> outNums = new ArrayList<>();
-
-        public Node(final int num) {
-            this.num = num;
-        }
-
-        void add(final int val) {
-            outNums.add(val);
+        public Core(final int idx, final int spend) {
+            this.idx = idx;
+            this.spend = spend;
         }
     }
 }
