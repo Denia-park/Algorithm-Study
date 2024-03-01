e52d2dcd52a67c26a7aab3ea0d741d1ae03fdedf
HyunggiPark <phg9587@naver.com>
2024-03-01 12:12:49 +0900

    leetcode Maximum Odd Binary Number - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,77 +1,35 @@
 package CodingTest.LeetCode;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-//        System.out.println(solution.isEvenOddTree(
-//        ));
-//        System.out.println(solution.isEvenOddTree(
-//        ));
+        System.out.println(solution.maximumOddBinaryNumber(
+                "010"
+        ));
+        System.out.println(solution.maximumOddBinaryNumber(
+                "0101"
+        ));
     }
 }
 
 class Solution {
-    public boolean isEvenOddTree(final TreeNode root) {
-        final Queue<TreeNode> queue = new LinkedList<>();
-        TreeNode current = root;
-        queue.add(current);
+    public String maximumOddBinaryNumber(final String s) {
+        int one = 0;
+        int zero = 0;
 
-        boolean even = true;
+        final char[] chars = s.toCharArray();
+        final int length = chars.length;
+        for (int i = 0; i < length; i++) {
+            final char c = chars[i];
 
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-
-            int prev = Integer.MAX_VALUE;
-            if (even) {
-                prev = Integer.MIN_VALUE;
+            if (c == '0') {
+                zero++;
+            } else {
+                one++;
             }
-
-            while (size > 0) {
-                current = queue.poll();
-
-                if ((even && (current.val % 2 == 0 || current.val <= prev))
-                        || !even && (current.val % 2 == 1 || current.val >= prev)) {
-                    return false;
-                }
-
-                prev = current.val;
-                if (current.left != null) {
-                    queue.add(current.left);
-                }
-
-                if (current.right != null) {
-                    queue.add(current.right);
-                }
-
-                size--;
-            }
-
-            even = !even;
         }
 
-        return true;
-    }
-}
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(final int val) {
-        this.val = val;
-    }
-
-    TreeNode(final int val, final TreeNode left, final TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+        return "1".repeat(one - 1) + "0".repeat(zero) + "1";
     }
 }
