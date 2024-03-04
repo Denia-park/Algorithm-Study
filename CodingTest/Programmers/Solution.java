c0e02a677fb7251941a048b1ccc0816a55422671
HyunggiPark <phg9587@naver.com>
2024-03-04 15:13:54 +0900

    프로그래머스 코딩테스트 연습 2019 카카오 개발자 겨울 인턴십 징검다리 건너기 - 정답 참고 - 이분탐색
    - 조건 다시 파악 후 풀이

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -13,6 +13,7 @@ class Solution {
 
             //m명의 인원이면 해당 징검다리를 통과할 수 있나?
             if (isPossible(stones, m, k)) {
+                //가능한 최대 인원 구하기
                 answer = Math.max(answer, m);
 
                 //가능하면 최소 인원 늘리기
@@ -30,13 +31,14 @@ class Solution {
         int count = 0;
 
         for (final int stone : stones) {
-            if (stone - m <= 0) {
+            if (stone - m < 0) { //못 밟는거
                 count++;
-            } else {
+            } else { //밟을 수 있는거
                 count = 0;
             }
 
-            if (count > limit) {
+            //못 밟는게 일정 수를 넘어가면, 해당 인원으로 못 건너감
+            if (count >= limit) {
                 return false;
             }
         }
