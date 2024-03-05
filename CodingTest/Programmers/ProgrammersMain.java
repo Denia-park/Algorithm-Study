e397ab3f9b1304940ffea83c8106359a5ec48ea7
HyunggiPark <phg9587@naver.com>
2024-03-05 22:55:45 +0900

    프로그래머스 2018 KAKAO BLIND RECRUITMENT [1차] 셔틀버스 - 정답 보고 풀이

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -4,14 +4,18 @@ public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(ts.solution(
-                new int[]{2, 4, 5, 3, 2, 1, 4, 2, 5, 1}, 3
-        ));
-        System.out.println(ts.solution(
-                new int[]{7, 2, 8, 7, 5, 2, 9}, 3
-        ));
+        System.out.println(ts.solution(1, 1, 5, new String[]{"08:00", "08:01", "08:02", "08:03"}));
+        System.out.println(ts.solution(2, 10, 2, new String[]{"09:10", "09:09", "08:00"}));
+        System.out.println(ts.solution(2, 1, 2, new String[]{"09:00", "09:00", "09:00", "09:00"}));
+        System.out.println(ts.solution(1, 1, 5, new String[]{"00:01", "00:01", "00:01", "00:01", "00:01"}));
+        System.out.println(ts.solution(1, 1, 1, new String[]{"23:59"}));
+        System.out.println(ts.solution(10, 60, 45, new String[]{"23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59"}));
     }
 }
+
+
+
+
 
 
 
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,46 +1,62 @@
 package CodingTest.Programmers;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.ArrayList;
+import java.util.List;
 
-//슬라이딩 윈도우로 풀어보기
 class Solution {
-    public int solution(final int[] stones, final int k) {
-        final int length = stones.length;
-        final Deque<Data> dq = new ArrayDeque<>();
+    public String solution(final int n, final int t, final int m, final String[] timetable) {
+        final List<Integer> startList = makeStartList(hourToMin("09:00"), n, t);
 
-        int answer = Integer.MAX_VALUE;
+        final List<Integer> crew = makeCrewList(timetable);
 
-        for (int i = 0; i < length; i++) {
-            final int curVal = stones[i];
-            final Data data = new Data(i, curVal);
+        int idx = 0;
+        int busLimit = m;
 
-            while (!dq.isEmpty() && dq.peekFirst().idx <= (i - k)) {
-                dq.pollFirst();
-            }
+        for (final Integer startTime : startList) {
+            busLimit = m;
 
-            while (!dq.isEmpty() && dq.peekLast().val <= curVal) {
-                dq.pollLast();
-            }
-
-            dq.addLast(data);
-
-            //k-1 번째부터 슬라이딩 윈도우의 크기가 Max이므로 해당 값 중에 최소 값을 구한다.
-            if (i >= (k - 1)) {
-                answer = Math.min(answer, dq.peekFirst().val);
+            while (idx < crew.size() && crew.get(idx) <= startTime && busLimit > 0) {
+                idx++;
+                busLimit--;
             }
         }
 
-        return answer;
+        if (busLimit > 0) {
+            return minToHour(startList.get(startList.size() - 1));
+        } else {
+            return minToHour(crew.get(idx - 1) - 1);
+        }
     }
 
-    class Data {
-        int idx;
-        int val;
+    private String minToHour(final Integer time) {
+        final int hour = time / 60;
+        final int min = time % 60;
 
-        public Data(final int idx, final int val) {
-            this.idx = idx;
-            this.val = val;
+        return String.format("%02d:%02d", hour, min);
+    }
+
+    private List<Integer> makeCrewList(final String[] timetable) {
+        final List<Integer> list = new ArrayList<>();
+        for (final String str : timetable) {
+            list.add(hourToMin(str));
         }
+        list.sort(null);
+
+        return list;
+    }
+
+    private int hourToMin(final String time) {
+        final String[] split = time.split(":");
+        return (Integer.parseInt(split[0]) * 60) + Integer.parseInt(split[1]);
+    }
+
+    private List<Integer> makeStartList(final int startMin, final int n, final int t) {
+        final List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(startMin + (t * i));
+        }
+
+        return list;
     }
 }
