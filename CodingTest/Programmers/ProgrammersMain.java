0d1193b7c6cc7453576e399c701817e88805aff9
HyunggiPark <phg9587@naver.com>
2024-03-08 16:18:23 +0900

    프로그래머스 연습문제 최고의 집합 - 정답 참고

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,12 +1,14 @@
 package CodingTest.Programmers;
 
+import java.util.Arrays;
+
 public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(ts.solution(4, new int[]{4, 3, 3}));
-        System.out.println(ts.solution(1, new int[]{2, 1, 2}));
-        System.out.println(ts.solution(3, new int[]{1, 1}));
+        System.out.println(Arrays.toString(ts.solution(2, 9)));
+        System.out.println(Arrays.toString(ts.solution(2, 1)));
+        System.out.println(Arrays.toString(ts.solution(2, 8)));
     }
 }
 
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,37 +1,27 @@
 package CodingTest.Programmers;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 
 class Solution {
-    public long solution(final int n, final int[] works) {
-        long answer = 0;
+    public int[] solution(int n, int s) {
+        final int[] answer = new int[n];
 
-        //제곱을 최소화하기 위해서는 제일 일이 많은 애들부터 줄여야 함
-        //우선순위큐에 넣어서 제일 높은 애들을 1씩 제거하고,
-        //남은 값들을 제곱해서 return하자
+        int idx = 0;
+        while (n > 0) {
+            final int temp = s / n;
 
-        final PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-
-        for (final int work : works) {
-            pq.add(work);
-        }
-
-        for (int i = 0; i < n; i++) {
-            final int top = pq.poll();
-
-            if (top == 0) {
-                pq.add(top);
-                continue;
+            if (temp == 0) {
+                return new int[]{-1};
             }
 
-            pq.add(top - 1);
+            answer[idx] = temp;
+            idx++;
+
+            s -= temp;
+            n--;
         }
 
-        while (!pq.isEmpty()) {
-            final int top = pq.poll();
-            answer += ((long) top * top);
-        }
+        Arrays.sort(answer);
 
         return answer;
     }
