f1475cfa99c2488c23c664b588f78ed2096d5bd3
HyunggiPark <phg9587@naver.com>
2024-03-08 16:19:26 +0900

    프로그래머스 연습문제 최고의 집합 - 정답 참고2

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int n, int s) {
+        if (n > s) {
+            return new int[]{-1};
+        }
+
         final int[] answer = new int[n];
 
         int idx = 0;
         while (n > 0) {
             final int temp = s / n;
-
-            if (temp == 0) {
-                return new int[]{-1};
-            }
 
             answer[idx] = temp;
             idx++;
