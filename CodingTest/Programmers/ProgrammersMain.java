9567c704d96576f59c7ee6aeb0fc278ad4d45edd
HyunggiPark <phg9587@naver.com>
2024-03-09 17:03:35 +0900

    프로그래머스 Summer/Winter Coding(~2018) 기지국 설치 - 1차 풀이 실패 - 2개 틀림

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -4,9 +4,8 @@ public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println((ts.solution(new int[]{3, 7, 2, 5, 4, 6, 1}, new boolean[]{false, true, true, true, true, false, false})));
-        System.out.println((ts.solution(new int[]{1, 2, 3}, new boolean[]{true, true, true})));
-        System.out.println((ts.solution(new int[]{6, 1, 5, 2, 3, 4}, new boolean[]{true, false, true, false, false, true})));
+        System.out.println((ts.solution(11, new int[]{4, 11}, 1)));
+        System.out.println((ts.solution(16, new int[]{9}, 2)));
     }
 }
 
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,41 +1,29 @@
 package CodingTest.Programmers;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-
 class Solution {
-    public int solution(final int[] rank, final boolean[] attendance) {
-        final int rankLen = rank.length;
+    public int solution(final int n, final int[] stations, final int w) {
+        int answer = 0;
 
-        final List<Student> list = new ArrayList<>();
+        int start = 1;
+        int end;
+        final int width = 2 * w + 1;
 
-        for (int i = 0; i < rankLen; i++) {
-            list.add(new Student(i, rank[i], attendance[i]));
+        for (final int station : stations) {
+            final int tempS = station - w;
+            final int tempE = station + w;
+
+            end = tempS;
+
+            answer += (int) Math.ceil((float) (end - start) / width);
+
+            start = tempE + 1;
         }
 
-        final List<Student> collect = list.stream()
-                .filter(s -> s.isAble)
-                .sorted(Comparator.comparingInt(s -> s.rank))
-                .collect(Collectors.toList());
-
-        final int a = collect.get(0).idx;
-        final int b = collect.get(1).idx;
-        final int c = collect.get(2).idx;
-
-        return 10000 * a + 100 * b + c;
-    }
-
-    class Student {
-        int idx;
-        int rank;
-        boolean isAble;
-
-        Student(final int idx, final int rank, final boolean isAble) {
-            this.idx = idx;
-            this.rank = rank;
-            this.isAble = isAble;
+        if (start < n) {
+            end = n + 1;
+            answer += (int) Math.ceil((float) (end - start) / width);
         }
+
+        return answer;
     }
 }
