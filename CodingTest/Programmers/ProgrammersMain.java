baa1f2b6285ba378d24456214600a1b61dc6f8df
HyunggiPark <phg9587@naver.com>
2024-03-09 12:01:19 +0900

    프로그래머스 코딩 기초 트레이닝 전국 대회 선발 고사 - 1차 풀이 완료

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,14 +1,12 @@
 package CodingTest.Programmers;
 
-import java.util.Arrays;
-
 public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(Arrays.toString(ts.solution(2, 9)));
-        System.out.println(Arrays.toString(ts.solution(2, 1)));
-        System.out.println(Arrays.toString(ts.solution(2, 8)));
+        System.out.println((ts.solution(new int[]{3, 7, 2, 5, 4, 6, 1}, new boolean[]{false, true, true, true, true, false, false})));
+        System.out.println((ts.solution(new int[]{1, 2, 3}, new boolean[]{true, true, true})));
+        System.out.println((ts.solution(new int[]{6, 1, 5, 2, 3, 4}, new boolean[]{true, false, true, false, false, true})));
     }
 }
 
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,28 +1,41 @@
 package CodingTest.Programmers;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class Solution {
-    public int[] solution(int n, int s) {
-        if (n > s) {
-            return new int[]{-1};
+    public int solution(final int[] rank, final boolean[] attendance) {
+        final int rankLen = rank.length;
+
+        final List<Student> list = new ArrayList<>();
+
+        for (int i = 0; i < rankLen; i++) {
+            list.add(new Student(i, rank[i], attendance[i]));
         }
 
-        final int[] answer = new int[n];
+        final List<Student> collect = list.stream()
+                .filter(s -> s.isAble)
+                .sorted(Comparator.comparingInt(s -> s.rank))
+                .collect(Collectors.toList());
 
-        int idx = 0;
-        while (n > 0) {
-            final int temp = s / n;
+        final int a = collect.get(0).idx;
+        final int b = collect.get(1).idx;
+        final int c = collect.get(2).idx;
 
-            answer[idx] = temp;
-            idx++;
+        return 10000 * a + 100 * b + c;
+    }
 
-            s -= temp;
-            n--;
+    class Student {
+        int idx;
+        int rank;
+        boolean isAble;
+
+        Student(final int idx, final int rank, final boolean isAble) {
+            this.idx = idx;
+            this.rank = rank;
+            this.isAble = isAble;
         }
-
-        Arrays.sort(answer);
-
-        return answer;
     }
 }
