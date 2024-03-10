4a83c675aa7af363e8058806ded5f7f883125e0b
HyunggiPark <phg9587@naver.com>
2024-03-10 16:34:02 +0900

    프로그래머스 2023 KAKAO BLIND RECRUITMENT 표 병합 = 1차 풀이 실패 - 7개 틀림

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,12 +1,13 @@
 package CodingTest.Programmers;
 
+import java.util.Arrays;
+
 public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println((ts.solution(11, new int[]{4, 11}, 1)));
-        System.out.println((ts.solution(16, new int[]{9}, 2)));
-        System.out.println((ts.solution(10, new int[]{1}, 1)));
+        System.out.println((Arrays.toString(ts.solution(new String[]{"UPDATE 1 1 menu", "UPDATE 1 2 category", "UPDATE 2 1 bibimbap", "UPDATE 2 2 korean", "UPDATE 2 3 rice", "UPDATE 3 1 ramyeon", "UPDATE 3 2 korean", "UPDATE 3 3 noodle", "UPDATE 3 4 instant", "UPDATE 4 1 pasta", "UPDATE 4 2 italian", "UPDATE 4 3 noodle", "MERGE 1 2 1 3", "MERGE 1 3 1 4", "UPDATE korean hansik", "UPDATE 1 3 group", "UNMERGE 1 4", "PRINT 1 3", "PRINT 1 4"}))));
+        System.out.println((Arrays.toString(ts.solution(new String[]{"UPDATE 1 1 a", "UPDATE 1 2 b", "UPDATE 2 1 c", "UPDATE 2 2 d", "MERGE 1 1 1 2", "MERGE 2 2 2 1", "MERGE 2 1 1 1", "PRINT 1 1", "UNMERGE 2 2", "PRINT 1 1"}))));
     }
 }
 
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,38 +1,117 @@
 package CodingTest.Programmers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    public int solution(final int n, final int[] stations, final int w) {
-        int answer = 0;
+    public String[] solution(final String[] commands) {
+        final List<String> answer = new ArrayList<>();
 
-        int start = 1;
-        int end;
-        final int width = 2 * w + 1;
-
-        for (final int station : stations) {
-            end = (station - w);
-
-            answer += ceil((end - start), width);
-
-            start = station + w + 1;
+        final String[] table = new String[51 * 51];
+        final int[] parents = new int[51 * 51];
+        for (int i = 0; i < 51 * 51; i++) {
+            parents[i] = i;
         }
 
-        if (start <= n) {
-            end = n + 1;
-            answer += ceil((end - start), width);
+        for (final String command : commands) {
+            final String[] split = command.split(" ");
+            final int len = split.length;
+
+            final String com = split[0];
+
+            if (com.equals("UPDATE")) {
+                //값 입력
+                if (len == 4) {
+                    final int row = Integer.parseInt(split[1]);
+                    final int col = Integer.parseInt(split[2]);
+                    final int parent = getParent(parents, convert(row, col));
+                    table[parent] = split[3];
+                }
+                //값 변경
+                else if (len == 3) {
+                    final String value1 = split[1];
+                    final String value2 = split[2];
+
+                    for (int row = 0; row < 51; row++) {
+                        for (int col = 0; col < 51; col++) {
+                            final int parent = getParent(parents, convert(row, col));
+
+                            if (table[parent] == null) {
+                                continue;
+                            }
+
+                            if (table[parent].equals(value1)) {
+                                table[parent] = value2;
+                            }
+                        }
+                    }
+                }
+            } else if (com.equals("MERGE")) {
+                final int row1 = Integer.parseInt(split[1]);
+                final int col1 = Integer.parseInt(split[2]);
+                final int convert1 = convert(row1, col1);
+                final int row2 = Integer.parseInt(split[3]);
+                final int col2 = Integer.parseInt(split[4]);
+                final int convert2 = convert(row2, col2);
+
+                if (convert1 == convert2) {
+                    continue;
+                }
+
+                String saveVal = table[convert1];
+                if (table[convert1] == null && table[convert2] != null) {
+                    saveVal = table[convert2];
+                }
+
+                union(parents, convert1, convert2);
+                final int parent = getParent(parents, convert1);
+                table[parent] = saveVal;
+            } else if (com.equals("UNMERGE")) {
+                final int row = Integer.parseInt(split[1]);
+                final int col = Integer.parseInt(split[2]);
+                final int convert = convert(row, col);
+                final int parent = getParent(parents, convert);
+                final String saveVal = table[parent];
+
+                for (int i = 0; i < 51 * 51; i++) {
+                    if (parents[i] == parent) {
+                        parents[i] = i;
+                        table[i] = null;
+                    }
+                }
+
+                table[convert] = saveVal;
+            } else if (com.equals("PRINT")) {
+                final int row = Integer.parseInt(split[1]);
+                final int col = Integer.parseInt(split[2]);
+                final int parent = getParent(parents, convert(row, col));
+                final String val = table[parent];
+
+                answer.add(val == null ? "EMPTY" : val);
+            }
         }
 
-        return answer;
+        return answer.toArray(String[]::new);
     }
 
-    int ceil(final int val, final int div) {
-        if (val <= 0) {
-            return 0;
+    private void union(final int[] parents, final int convert1, final int convert2) {
+        final int parent1 = getParent(parents, convert1);
+        final int parent2 = getParent(parents, convert2);
+
+        parents[Math.max(parent1, parent2)] = Math.min(parent1, parent2);
+    }
+
+    private int getParent(final int[] parents, final int idx) {
+        if (parents[idx] == idx) {
+            return idx;
         }
 
-        if (val % div == 0) {
-            return val / div;
-        } else {
-            return val / div + 1;
-        }
+        parents[idx] = getParent(parents, parents[idx]);
+
+        return parents[idx];
+    }
+
+    private int convert(final int row, final int col) {
+        return 50 * row + col;
     }
 }
