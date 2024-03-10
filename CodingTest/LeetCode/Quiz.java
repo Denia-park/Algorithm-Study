74ef18b35ea91fe569dfd2c75054aae3ba3dd4f3
HyunggiPark <phg9587@naver.com>
2024-03-10 11:35:15 +0900

    leetcode Intersection of Two Arrays - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,36 +1,46 @@
 package CodingTest.LeetCode;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.getCommon(new int[]{1, 2, 3}, new int[]{2, 4}));
-        System.out.println(solution.getCommon(new int[]{1, 2, 3, 6}, new int[]{2, 3, 4, 5}));
+        System.out.println(solution.intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2}));
+        System.out.println(solution.intersection(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4}));
     }
 }
 
 class Solution {
-    public int getCommon(final int[] nums1, final int[] nums2) {
-        final int answer = -1;
+    public int[] intersection(final int[] nums1, final int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
 
         int idx1 = 0;
         int idx2 = 0;
+        final int len1 = nums1.length;
+        final int len2 = nums2.length;
 
-        while (idx1 < nums1.length && idx2 < nums2.length) {
+        final Set<Integer> list = new HashSet<>();
+        while (idx1 < len1 && idx2 < len2) {
             final int val1 = nums1[idx1];
             final int val2 = nums2[idx2];
 
             if (val1 == val2) {
-                return val1;
-            }
-
-            if (val1 < val2) {
+                list.add(val1);
+                idx1++;
+                idx2++;
+            } else if (val1 < val2) {
                 idx1++;
             } else {
                 idx2++;
             }
         }
 
-        return answer;
+        return list.stream()
+                .mapToInt(i -> i)
+                .toArray();
     }
 }
