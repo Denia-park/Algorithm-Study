51f1ccd4fbe5e96ed55ef0cd5f77bde7efd32108
HyunggiPark <phg9587@naver.com>
2024-03-13 12:01:44 +0900

    프로그래머스 코딩 기초 트레이닝 문자열 정수의 합 - 1차 풀이 완료

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,17 +1,10 @@
 package CodingTest.Programmers;
 
-import java.util.Arrays;
-
 public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println((Arrays.toString(ts.solution(new String[]{"UPDATE 1 1 menu", "UPDATE 1 2 category", "UPDATE 2 1 bibimbap", "UPDATE 2 2 korean", "UPDATE 2 3 rice", "UPDATE 3 1 ramyeon", "UPDATE 3 2 korean", "UPDATE 3 3 noodle", "UPDATE 3 4 instant", "UPDATE 4 1 pasta", "UPDATE 4 2 italian", "UPDATE 4 3 noodle", "MERGE 1 2 1 3", "MERGE 1 3 1 4", "UPDATE korean hansik", "UPDATE 1 3 group", "UNMERGE 1 4", "PRINT 1 3", "PRINT 1 4"}))));
-        System.out.println((Arrays.toString(ts.solution(new String[]{"UPDATE 1 1 a", "UPDATE 1 2 b", "UPDATE 2 1 c", "UPDATE 2 2 d", "MERGE 1 1 1 2", "MERGE 2 2 2 1", "MERGE 2 1 1 1", "PRINT 1 1", "UNMERGE 2 2", "PRINT 1 1"}))));
-        System.out.println((Arrays.toString(ts.solution(new String[]{"UPDATE 1 1 apple", "MERGE 1 1 2 2", "MERGE 2 2 3 3", "UNMERGE 1 1", "UNMERGE 2 2", "PRINT 1 1", "PRINT 2 2", "PRINT 3 3"}))));
-        System.out.println((Arrays.toString(ts.solution(new String[]{"MERGE 1 1 2 2", "MERGE 1 1 3 3", "UPDATE 3 3 A", "PRINT 1 1", "PRINT 2 2", "PRINT 3 3"}))));
-        System.out.println((Arrays.toString(ts.solution(new String[]{"UPDATE 1 1 A", "UPDATE 2 2 B", "UPDATE 3 3 C", "UPDATE 4 4 D", "MERGE 1 1 2 2", "MERGE 3 3 4 4", "MERGE 1 1 3 3", "UNMERGE 1 1", "PRINT 1 1", "PRINT 2 2", "PRINT 3 3", "PRINT 4 4"}))));
-        System.out.println((Arrays.toString(ts.solution(new String[]{"MERGE 1 1 2 2", "UPDATE 1 1 A", "UNMERGE 1 1", "PRINT 1 1", "PRINT 2 2"}))));
+        System.out.println(ts.solution("123456789"));
     }
 }
 
--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,124 +1,14 @@
 package CodingTest.Programmers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    private static final int TOTAL_SIZE = 51 * 51;
+    public int solution(final String num_str) {
+        int answer = 0;
+        final int len = num_str.length();
 
-    public String[] solution(final String[] commands) {
-        final List<String> answer = new ArrayList<>();
-
-        final String[] table = new String[TOTAL_SIZE];
-        final int[] parents = new int[TOTAL_SIZE];
-        for (int i = 0; i < TOTAL_SIZE; i++) {
-            parents[i] = i;
+        for (int i = 0; i < len; i++) {
+            answer += num_str.charAt(i) - '0';
         }
 
-        for (final String command : commands) {
-            final String[] split = command.split(" ");
-            final int len = split.length;
-
-            final String com = split[0];
-
-            if (com.equals("UPDATE") && len == 4) {
-                //값 입력
-                final int parent = getParentByRowCol(parents, split[1], split[2]);
-                table[parent] = split[3];
-            } else if (com.equals("UPDATE") && len == 3) {
-                //값 수정
-                for (int idx = 0; idx < TOTAL_SIZE; idx++) {
-                    if (table[idx] != null && table[idx].equals(split[1])) {
-                        table[idx] = split[2];
-                    }
-                }
-            } else if (com.equals("MERGE")) {
-                if (split[1].equals(split[3]) && split[2].equals(split[4])) {
-                    continue;
-                }
-
-                final String saveVal = getSaveVal(parents, split, table);
-
-                final int idx1 = getIdxByRowCol(split[1], split[2]);
-                final int idx2 = getIdxByRowCol(split[3], split[4]);
-
-                final int unionParent = union(parents, idx1, idx2);
-                table[unionParent] = saveVal;
-            } else if (com.equals("UNMERGE")) {
-                final int parent = getParentByRowCol(parents, split[1], split[2]);
-                final String saveVal = table[parent];
-
-                for (int i = 0; i < TOTAL_SIZE; i++) {
-                    getParent(parents, i);
-                }
-
-                for (int i = 0; i < TOTAL_SIZE; i++) {
-                    if (parents[i] == parent) {
-                        parents[i] = i;
-                        table[i] = null;
-                    }
-                }
-
-                table[getIdxByRowCol(split[1], split[2])] = saveVal;
-            } else if (com.equals("PRINT")) {
-                final int parent = getParentByRowCol(parents, split[1], split[2]);
-                final String val = table[parent];
-
-                answer.add(val == null ? "EMPTY" : val);
-            }
-        }
-
-        return answer.toArray(String[]::new);
-    }
-
-    private String getSaveVal(final int[] parents, final String[] split, final String[] table) {
-        final int parent1 = getParentByRowCol(parents, split[1], split[2]);
-        final int parent2 = getParentByRowCol(parents, split[3], split[4]);
-
-        String saveVal = table[parent1];
-        if (table[parent1] == null && table[parent2] != null) {
-            saveVal = table[parent2];
-        }
-
-        return saveVal;
-    }
-
-    private int union(final int[] parents, final int idx1, final int idx2) {
-        final int parent1 = getParent(parents, idx1);
-        final int parent2 = getParent(parents, idx2);
-
-        final int min = Math.min(parent1, parent2);
-        parents[Math.max(parent1, parent2)] = min;
-
-        return min;
-    }
-
-
-    private int getParentByRowCol(final int[] parents, final String rowStr, final String colStr) {
-        final int row = Integer.parseInt(rowStr);
-        final int col = Integer.parseInt(colStr);
-
-        return getParent(parents, calculateIdx(row, col));
-    }
-
-    private int calculateIdx(final int row, final int col) {
-        return 50 * row + col;
-    }
-
-    private int getIdxByRowCol(final String rowStr, final String colStr) {
-        final int row = Integer.parseInt(rowStr);
-        final int col = Integer.parseInt(colStr);
-
-        return calculateIdx(row, col);
-    }
-
-    private int getParent(final int[] parents, final int idx) {
-        if (parents[idx] == idx) {
-            return idx;
-        }
-
-        parents[idx] = getParent(parents, parents[idx]);
-
-        return parents[idx];
+        return answer;
     }
 }
