bb9fd096610a25ce2bb3e1bb69c9da9f55c76fe8
HyunggiPark <phg9587@naver.com>
2024-03-14 11:56:51 +0900

    leetcode Binary Subarrays With Sum - 정답 참고 - 누적 합

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,5 +1,7 @@
 package CodingTest.LeetCode;
 
+import java.util.HashMap;
+
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
@@ -11,25 +13,16 @@ public class Quiz {
 
 class Solution {
     public int numSubarraysWithSum(final int[] nums, final int goal) {
-        return slidingWindowAtMost(nums, goal) - slidingWindowAtMost(nums, goal - 1);
-    }
+        int sum = 0, count = 0;
+        final HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); // 누적 합이 0인 경우를 위해 초기화
 
-    private int slidingWindowAtMost(final int[] nums, final int goal) {
-        int start = 0;
-        int currentSum = 0;
-        int totalCount = 0;
-
-        for (int end = 0; end < nums.length; end++) {
-            currentSum += nums[end];
-
-            while (start <= end && currentSum > goal) {
-                currentSum -= nums[start];
-                start++;
-            }
-
-            totalCount += (end - start + 1);
+        for (final int num : nums) {
+            sum += num; // 누적 합 계산
+            count += map.getOrDefault(sum - goal, 0); // 목표 합을 만족하는 경우 찾기
+            map.put(sum, map.getOrDefault(sum, 0) + 1); // 누적 합 업데이트
         }
 
-        return totalCount;
+        return count;
     }
 }
