abb3e7069a9323787beb3dbb725c04fa03a0fa9c
HyunggiPark <phg9587@naver.com>
2024-03-15 12:06:32 +0900

    leetcode Product of Array Except Self - 정답 참고 - 누적 곱

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,28 +1,41 @@
 package CodingTest.LeetCode;
 
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.numSubarraysWithSum(new int[]{1, 0, 1, 0, 1}, 2));
-        System.out.println(solution.numSubarraysWithSum(new int[]{0, 0, 0, 0, 0}, 0));
+        System.out.println(Arrays.toString(solution.productExceptSelf(new int[]{1, 2, 3, 4})));
+        System.out.println(Arrays.toString(solution.productExceptSelf(new int[]{-1, 1, 0, -3, 3})));
     }
 }
 
 class Solution {
-    public int numSubarraysWithSum(final int[] nums, final int goal) {
-        int sum = 0, count = 0;
-        final HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1); // 누적 합이 0인 경우를 위해 초기화
+    public int[] productExceptSelf(final int[] nums) {
+        final int len = nums.length;
 
-        for (final int num : nums) {
-            sum += num; // 누적 합 계산
-            count += map.getOrDefault(sum - goal, 0); // 목표 합을 만족하는 경우 찾기
-            map.put(sum, map.getOrDefault(sum, 0) + 1); // 누적 합 업데이트
+        final int[] answer = new int[len];
+        final int[] left = new int[len];
+        final int[] right = new int[len];
+
+        //left 초기화
+        left[0] = 1;
+        for (int i = 1; i < len; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
         }
 
-        return count;
+        //right 초기화
+        right[len - 1] = 1;
+        for (int i = len - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+
+        //answer 초기화
+        for (int i = 0; i < len; i++) {
+            answer[i] = left[i] * right[i];
+        }
+
+        return answer;
     }
 }
