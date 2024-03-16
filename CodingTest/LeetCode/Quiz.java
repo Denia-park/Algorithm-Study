9ca74df02a92cc0d06f0f9dba70cdba8daffed61
HyunggiPark <phg9587@naver.com>
2024-03-16 14:11:24 +0900

    leetcode Contiguous Array - 정답 참고 - 누적 합

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,41 +1,30 @@
 package CodingTest.LeetCode;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(Arrays.toString(solution.productExceptSelf(new int[]{1, 2, 3, 4})));
-        System.out.println(Arrays.toString(solution.productExceptSelf(new int[]{-1, 1, 0, -3, 3})));
+        System.out.println((solution.findMaxLength(new int[]{0, 1})));
+        System.out.println((solution.findMaxLength(new int[]{0, 1, 0})));
     }
 }
 
 class Solution {
-    public int[] productExceptSelf(final int[] nums) {
-        final int len = nums.length;
-
-        final int[] answer = new int[len];
-        final int[] left = new int[len];
-        final int[] right = new int[len];
-
-        //left 초기화
-        left[0] = 1;
-        for (int i = 1; i < len; i++) {
-            left[i] = left[i - 1] * nums[i - 1];
+    public int findMaxLength(final int[] nums) {
+        final Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int maxlen = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count = count + (nums[i] == 1 ? 1 : -1);
+            if (map.containsKey(count)) {
+                maxlen = Math.max(maxlen, i - map.get(count));
+            } else {
+                map.put(count, i);
+            }
         }
-
-        //right 초기화
-        right[len - 1] = 1;
-        for (int i = len - 2; i >= 0; i--) {
-            right[i] = right[i + 1] * nums[i + 1];
-        }
-
-        //answer 초기화
-        for (int i = 0; i < len; i++) {
-            answer[i] = left[i] * right[i];
-        }
-
-        return answer;
+        return maxlen;
     }
 }
