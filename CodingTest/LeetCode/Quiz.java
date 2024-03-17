f9cbb5241f7faed5e468eb621c4c327782f86a95
HyunggiPark <phg9587@naver.com>
2024-03-17 16:41:30 +0900

    leetcode Koko Eating Bananas - 1차 풀이 틀림

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,45 +1,57 @@
 package CodingTest.LeetCode;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println((Arrays.deepToString(solution.insert(new int[][]{{1, 3}, {6, 9}}, new int[]{2, 5}))));
-        System.out.println((Arrays.deepToString(solution.insert(new int[][]{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}}, new int[]{4, 8}))));
+        System.out.println(solution.minEatingSpeed(new int[]{3, 6, 7, 11}, 8));
+        System.out.println(solution.minEatingSpeed(new int[]{30, 11, 23, 4, 20}, 5));
+        System.out.println(solution.minEatingSpeed(new int[]{30, 11, 23, 4, 20}, 6));
     }
 }
 
 class Solution {
-    public int[][] insert(final int[][] intervals, final int[] newInterval) {
-        final int n = intervals.length;
-        int i = 0;
-        final List<int[]> res = new ArrayList<>();
+    public int minEatingSpeed(final int[] piles, final int h) {
+        //먹을 수 있는 값 중에 제일 작은 값이니까, 맥스로 값을 설정한다.
+        int answer = Integer.MAX_VALUE;
 
-        // Case 1: No overlapping before merging intervals
-        while (i < n && intervals[i][1] < newInterval[0]) {
-            res.add(intervals[i]);
-            i++;
+        //최대 바나나 개수를 찾는다
+        final int maxBanana = Arrays.stream(piles).max().getAsInt();
+
+        int left = 1;
+        int right = maxBanana;
+
+        //max값을 최대 바나나 개수로 두고, 이진 탐색을 통해 개수를 바꿔가면서
+        //다 먹을 수 있는 개수를 찾는다.
+        while (left <= right) {
+            final int mid = left + ((right - left) / 2);
+
+            if (isAble(piles, h, mid)) {
+                answer = Math.min(answer, mid);
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
 
-        // Case 2: Overlapping and merging intervals
-        while (i < n && newInterval[1] >= intervals[i][0]) {
-            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-            i++;
-        }
-        res.add(newInterval);
+        return answer;
+    }
 
-        // Case 3: No overlapping after merging newInterval
-        while (i < n) {
-            res.add(intervals[i]);
-            i++;
+    private boolean isAble(final int[] piles, final int h, final int mid) {
+        int tempHour = 0;
+
+        for (final int pile : piles) {
+            final int divideHour = pile / mid;
+
+            if (pile % mid == 0) {
+                tempHour += divideHour;
+            } else {
+                tempHour += (divideHour + 1);
+            }
         }
 
-        // Convert List to array
-        return res.toArray(new int[res.size()][]);
+        return tempHour <= h;
     }
 }
