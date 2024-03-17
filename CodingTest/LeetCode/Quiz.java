062cad28be3752c8daa2a8ab177128b90be8a7f8
HyunggiPark <phg9587@naver.com>
2024-03-17 11:00:25 +0900

    leetcode Insert Interval - 1차 풀이 완료 - 시간이 오래 걸림

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,30 +1,40 @@
 package CodingTest.LeetCode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println((solution.findMaxLength(new int[]{0, 1})));
-        System.out.println((solution.findMaxLength(new int[]{0, 1, 0})));
+        System.out.println((Arrays.deepToString(solution.insert(new int[][]{{1, 3}, {6, 9}}, new int[]{2, 5}))));
+        System.out.println((Arrays.deepToString(solution.insert(new int[][]{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}}, new int[]{4, 8}))));
     }
 }
 
 class Solution {
-    public int findMaxLength(final int[] nums) {
-        final Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, -1);
-        int maxlen = 0, count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            count = count + (nums[i] == 1 ? 1 : -1);
-            if (map.containsKey(count)) {
-                maxlen = Math.max(maxlen, i - map.get(count));
-            } else {
-                map.put(count, i);
+    public int[][] insert(final int[][] intervals, final int[] newInterval) {
+        final List<int[]> ints = new ArrayList<>();
+
+        Collections.addAll(ints, intervals);
+        ints.add(newInterval);
+
+        ints.sort(Comparator.comparingInt(arr -> arr[0]));
+
+        final Deque<int[]> answer = new ArrayDeque<>();
+        for (int i = 0; i < ints.size(); i++) {
+            final int[] interval = ints.get(i);
+
+            //마지막에 등록된 인터벌의 종료시간보다 현재 인터벌의 시작시간이 빠르면 합쳐준다.
+            if (!answer.isEmpty() && answer.peekLast()[1] >= interval[0]) {
+                final int[] last = answer.pollLast();
+
+                answer.addLast(new int[]{Math.min(last[0], interval[0]), Math.max(last[1], interval[1])});
+                continue;
             }
+
+            answer.add(interval);
         }
-        return maxlen;
+
+        return answer.toArray(int[][]::new);
     }
 }
