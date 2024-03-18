b09162490472b43ff6ae5c99b07f154c2d3e887b
HyunggiPark <phg9587@naver.com>
2024-03-18 09:44:59 +0900

    leetcode Minimum Number of Arrows to Burst Balloons - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,52 +1,62 @@
 package CodingTest.LeetCode;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(solution.minEatingSpeed(new int[]{3, 6, 7, 11}, 8));
-        System.out.println(solution.minEatingSpeed(new int[]{30, 11, 23, 4, 20}, 5));
-        System.out.println(solution.minEatingSpeed(new int[]{30, 11, 23, 4, 20}, 6));
+        System.out.println(solution.findMinArrowShots(
+                new int[][]{
+                        new int[]{10, 16},
+                        new int[]{2, 8},
+                        new int[]{1, 6},
+                        new int[]{7, 12}
+                }
+        ));
+        System.out.println(solution.findMinArrowShots(
+                new int[][]{
+                        new int[]{1, 2},
+                        new int[]{3, 4},
+                        new int[]{5, 6},
+                        new int[]{7, 8}
+                }
+        ));
+        System.out.println(solution.findMinArrowShots(
+                new int[][]{
+                        new int[]{1, 2},
+                        new int[]{2, 3},
+                        new int[]{3, 4},
+                        new int[]{4, 5}
+                }
+        ));
     }
 }
 
 class Solution {
-    public int minEatingSpeed(final int[] piles, final int h) {
-        //먹을 수 있는 값 중에 제일 작은 값이니까, 맥스로 값을 설정한다.
-        int answer = Integer.MAX_VALUE;
+    public int findMinArrowShots(final int[][] points) {
+        int count = 0;
 
-        int left = 1;
-        int right = 1_000_000_000;
+        final PriorityQueue<int[]> pq = new PriorityQueue<>(
+                Comparator.comparingInt((int[] arr) -> arr[1])
+                        .thenComparingInt(arr -> arr[0])
+        );
 
-        //max값을 최대 바나나 개수로 두고, 이진 탐색을 통해 개수를 바꿔가면서
-        //다 먹을 수 있는 개수를 찾는다.
-        while (left <= right) {
-            final int mid = left + ((right - left) / 2);
+        Collections.addAll(pq, points);
 
-            if (isAble(piles, h, mid)) {
-                answer = Math.min(answer, mid);
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+        while (!pq.isEmpty()) {
+            final int[] cur = pq.poll();
+
+            //현재의 끝나는 부분이 다음의 시작보다 크면 서로 겹치므로 해당 풍선은 제거한다.
+            while (!pq.isEmpty() && cur[1] >= pq.peek()[0]) {
+                pq.poll();
             }
+
+            count++;
         }
 
-        return answer;
-    }
-
-    private boolean isAble(final int[] piles, final int h, final int mid) {
-        long tempHour = 0L;
-
-        for (final int pile : piles) {
-            final int divideHour = pile / mid;
-
-            if (pile % mid == 0) {
-                tempHour += divideHour;
-            } else {
-                tempHour += (divideHour + 1);
-            }
-        }
-
-        return tempHour <= h;
+        return count;
     }
 }
