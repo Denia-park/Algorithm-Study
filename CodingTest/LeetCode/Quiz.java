9e8458bdcda74545e43121c73fde218f12b73c1f
HyunggiPark <phg9587@naver.com>
2024-03-19 12:28:01 +0900

    leetcode Task Scheduler - 정답참고

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,8 +1,8 @@
 package CodingTest.LeetCode;
 
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class Quiz {
@@ -38,66 +38,41 @@ public class Quiz {
 
 class Solution {
     public int leastInterval(final char[] tasks, final int n) {
-        int curTaskIdx = 0;
-
-        final Map<Character, Task> taskMap = new HashMap<>();
-
-        for (final char task : tasks) {
-            taskMap.computeIfAbsent(task, Task::new).count++;
+        final int[] freq = new int[26];
+        for (final char ch : tasks) {
+            freq[ch - 'A']++;
         }
 
-        final PriorityQueue<Task> workQue = new PriorityQueue<>(
-                Comparator.comparingInt(t -> t.count * -1)
-        );
-        final PriorityQueue<Task> waitQue = new PriorityQueue<>(
-                Comparator.comparingInt(t -> t.lastIdx)
-        );
-
-        workQue.addAll(taskMap.values());
-
-        while (!workQue.isEmpty() || !waitQue.isEmpty()) {
-            //waitQue 확인하고, 작업 처리가 가능한 애들을 workQue에 넣자
-            while (!waitQue.isEmpty() && (curTaskIdx - waitQue.peek().lastIdx > n)) {
-                workQue.add(waitQue.poll());
+        final PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for (final int val : freq) {
+            if (val > 0) {
+                pq.add(val);
             }
+        }
 
-            //가능한 작업이 없음
-            if (workQue.isEmpty()) {
-                //IDLE 작업
-                curTaskIdx++;
-                continue;
+        int time = 0;
+        final int cycle = n + 1;
+        while (!pq.isEmpty()) {
+            int tempCycle = cycle;
+            final List<Integer> store = new ArrayList<>();
+            int taskCount = 0;
+
+            while (tempCycle > 0 && !pq.isEmpty()) {
+                tempCycle--;
+
+                final int curFreq = pq.poll();
+
+                if (curFreq > 1) {
+                    store.add(curFreq - 1);
+                }
+
+                taskCount++;
             }
+            pq.addAll(store);
 
-            final Task curTask = workQue.poll();
-
-            //바로 업무 처리 가능
-            proceedWork(curTask, curTaskIdx, waitQue);
-            curTaskIdx++;
+            time += pq.isEmpty() ? taskCount : cycle;
         }
 
-        return curTaskIdx;
-    }
-
-    private void proceedWork(final Task curTask, final int curTaskIdx, final PriorityQueue<Task> waitQue) {
-        curTask.lastIdx = curTaskIdx;
-
-        //업무 진행, 남은 업무가 없으면 waitQue에 추가하지 않음
-        if (curTask.count > 1) {
-            curTask.count--;
-
-            //업무 남아 있음
-            waitQue.add(curTask);
-        }
-    }
-
-    static class Task {
-        int lastIdx;
-        char ch;
-        int count = 0;
-
-        public Task(final char ch) {
-            this.ch = ch;
-            this.lastIdx = -1;
-        }
+        return time;
     }
 }
