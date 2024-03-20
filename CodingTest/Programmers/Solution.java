ec4543666ed11ed043ce914d84a0795e9c659240
HyunggiPark <phg9587@naver.com>
2024-03-20 14:38:28 +0900

    프로그래머스 Summer/Winter Coding(~2018) 스티커 모으기(2) - 정답 참고2

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,39 +1,37 @@
 package CodingTest.Programmers;
 
-// https://blog.naver.com/gkswlcjs2/223198277102
-
 class Solution {
     public int solution(final int[] sticker) {
-        final int length = sticker.length;
+        final int n = sticker.length;
+        final int[] dp1 = new int[n];
+        final int[] dp2 = new int[n];
 
-        if (length == 1) return sticker[0];
-        if (length == 2) return Math.max(sticker[0], sticker[1]);
+        if (n == 1)
+            return sticker[0];
 
-        final int[] tempSticker = new int[length];
-        System.arraycopy(sticker, 0, tempSticker, 0, length);
-        tempSticker[length - 1] = 0;
+        //첫번째 스티커를 뜯었을 경우
+        dp1[0] = sticker[0];
+        dp1[1] = sticker[0];
 
-        final int[] tempSticker2 = new int[length];
-        System.arraycopy(sticker, 1, tempSticker2, 0, length - 1);
+        for (int i = 2; i < n; i++) {
+            int stickerVal = sticker[i];
 
-        final int[] dp = new int[length];
+            //첫번째 스티커를 뜯으면, 마지막 sticker는 0이다. (뜯을 수 없으므로)
+            if (i == (n - 1)) {
+                stickerVal = 0;
+            }
 
-        dp[0] = tempSticker[0];
-        dp[1] = Math.max(dp[0], tempSticker[1]);
-
-        for (int i = 0; i < length - 2; i++) {
-            dp[i + 2] = Math.max(dp[i] + tempSticker[i + 2], dp[i + 1]);
+            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + stickerVal);
         }
 
-        final int[] dp2 = new int[length];
+        //첫번째 스티커를 뜯지 않았을 경우
+        dp2[0] = 0;
+        dp2[1] = sticker[1];
 
-        dp2[0] = tempSticker2[0];
-        dp2[1] = Math.max(dp2[0], tempSticker2[1]);
-
-        for (int i = 0; i < length - 2; i++) {
-            dp2[i + 2] = Math.max(dp2[i] + tempSticker2[i + 2], dp2[i + 1]);
+        for (int i = 2; i < n; i++) {
+            dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + sticker[i]);
         }
 
-        return Math.max(dp[length - 1], dp2[length - 1]);
+        return Math.max(dp1[n - 1], dp2[n - 1]);
     }
 }
