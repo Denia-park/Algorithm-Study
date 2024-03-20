1a8538746aed85fbc6d1fe98ef8fd77f3466ef12
HyunggiPark <phg9587@naver.com>
2024-03-20 14:27:11 +0900

    프로그래머스 Summer/Winter Coding(~2018) 스티커 모으기(2) - 정답 참고

--- CodingTest/Programmers/Solution.java
+++ CodingTest/Programmers/Solution.java
@@ -1,66 +1,39 @@
 package CodingTest.Programmers;
 
-/*
-1. 많이 재생된 장르 2가지를 고른다.
-2. 해당 장르에서 제일 많이 재생된 노래를 고른다 (재생수가 같으면 고유번호 순)
- */
-
-import java.util.*;
+// https://blog.naver.com/gkswlcjs2/223198277102
 
 class Solution {
-    public int[] solution(final String[] genres, final int[] plays) {
-        //장르와 노래 재생 수를 저장
-        final Map<String, Integer> genreMap = new HashMap<>();
-        //장르와 노래를 저장
-        final Map<String, List<Song>> genreSongMap = new HashMap<>();
+    public int solution(final int[] sticker) {
+        final int length = sticker.length;
 
-        for (int i = 0; i < plays.length; i++) {
-            final String genre = genres[i];
-            final int play = plays[i];
+        if (length == 1) return sticker[0];
+        if (length == 2) return Math.max(sticker[0], sticker[1]);
 
-            genreMap.put(genre, genreMap.getOrDefault(genre, 0) + play);
+        final int[] tempSticker = new int[length];
+        System.arraycopy(sticker, 0, tempSticker, 0, length);
+        tempSticker[length - 1] = 0;
 
-            genreSongMap.computeIfAbsent(genre, k -> new ArrayList<>()).add(new Song(i, play));
+        final int[] tempSticker2 = new int[length];
+        System.arraycopy(sticker, 1, tempSticker2, 0, length - 1);
+
+        final int[] dp = new int[length];
+
+        dp[0] = tempSticker[0];
+        dp[1] = Math.max(dp[0], tempSticker[1]);
+
+        for (int i = 0; i < length - 2; i++) {
+            dp[i + 2] = Math.max(dp[i] + tempSticker[i + 2], dp[i + 1]);
         }
 
-        final List<Integer> answer = new ArrayList<>();
+        final int[] dp2 = new int[length];
 
-        //장르, 노래 Map을 정렬
-        genreMap.entrySet().stream()
-                .sorted(Comparator.comparingInt(e -> -1 * e.getValue()))
-                .map(Map.Entry::getKey)
-                .forEach(
-                        genre -> {
-                            final List<Song> songs = genreSongMap.get(genre);
-                            songs.sort(null);
+        dp2[0] = tempSticker2[0];
+        dp2[1] = Math.max(dp2[0], tempSticker2[1]);
 
-                            int idx = 0;
-                            while (idx < songs.size() && idx < 2) {
-                                answer.add(songs.get(idx).idx);
-                                idx++;
-                            }
-                        }
-                );
-
-        return answer.stream().mapToInt(i -> i).toArray();
-    }
-
-    static class Song implements Comparable<Song> {
-        int idx;
-        int playCount;
-
-        public Song(final int idx, final int playCount) {
-            this.idx = idx;
-            this.playCount = playCount;
+        for (int i = 0; i < length - 2; i++) {
+            dp2[i + 2] = Math.max(dp2[i] + tempSticker2[i + 2], dp2[i + 1]);
         }
 
-        @Override
-        public int compareTo(final Song o) {
-            if (this.playCount == o.playCount) {
-                return Integer.compare(this.idx, o.idx);
-            } else {
-                return -1 * Integer.compare(this.playCount, o.playCount);
-            }
-        }
+        return Math.max(dp[length - 1], dp2[length - 1]);
     }
 }
