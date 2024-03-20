2b4d5c2bda3e13dd3c409646a7abf1beab226ca5
HyunggiPark <phg9587@naver.com>
2024-03-20 10:13:17 +0900

    leetcode Merge In Between Linked Lists - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,78 +1,63 @@
 package CodingTest.LeetCode;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
-
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-        System.out.println(
-                solution.leastInterval(
-                        new char[]{'A', 'A', 'A', 'B', 'B', 'B'},
-                        2
-                )
-        );
-        System.out.println(
-                solution.leastInterval(
-                        new char[]{'A', 'C', 'A', 'B', 'D', 'B'},
-                        2
-                )
-        );
-        System.out.println(
-                solution.leastInterval(
-                        new char[]{'A', 'A', 'A', 'B', 'B', 'B'},
-                        3
-                )
-        );
-        System.out.println(
-                solution.leastInterval(
-                        new char[]{'A', 'A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F', 'G'},
-                        1
-                )
-        );
+//        System.out.println(
+//                solution.mergeInBetween(
+//                        new char[]{'A', 'A', 'A', 'B', 'B', 'B'},
+//                        2
+//                )
+//        );
     }
 }
 
 class Solution {
-    public int leastInterval(final char[] tasks, final int n) {
-        final int[] freq = new int[26];
-        for (final char ch : tasks) {
-            freq[ch - 'A']++;
-        }
+    public ListNode mergeInBetween(final ListNode list1, final int a, final int b, final ListNode list2) {
+        int idx = 0;
 
-        final PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        for (final int val : freq) {
-            if (val > 0) {
-                pq.add(val);
+        ListNode putStart = null;
+        ListNode putEnd = null;
+
+        ListNode search = list1;
+        while (search != null) {
+            if (idx == a - 1) {
+                putStart = search;
+            } else if (idx == b + 1) {
+                putEnd = search;
             }
+
+            search = search.next;
+            idx++;
         }
 
-        int time = 0;
-        final int cycle = n + 1;
-        while (!pq.isEmpty()) {
-            int tempCycle = cycle;
-            final List<Integer> store = new ArrayList<>();
-            int taskCount = 0;
+        putStart.next = list2;
 
-            while (tempCycle > 0 && !pq.isEmpty()) {
-                tempCycle--;
-
-                final int curFreq = pq.poll();
-
-                if (curFreq > 1) {
-                    store.add(curFreq - 1);
-                }
-
-                taskCount++;
-            }
-            pq.addAll(store);
-
-            time += pq.isEmpty() ? taskCount : cycle;
+        search = list2;
+        while (search.next != null) {
+            search = search.next;
         }
 
-        return time;
+        search.next = putEnd;
+
+        return list1;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(final int val) {
+        this.val = val;
+    }
+
+    ListNode(final int val, final ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
