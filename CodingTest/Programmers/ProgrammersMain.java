497dfa6e19d470fb35f2255a83130e301b9aa39d
HyunggiPark <phg9587@naver.com>
2024-03-21 11:46:00 +0900

    소프티어 금고털이 - 1차 풀이 완료

--- CodingTest/Programmers/ProgrammersMain.java
+++ CodingTest/Programmers/ProgrammersMain.java
@@ -1,35 +1,29 @@
 package CodingTest.Programmers;
 
-import java.util.Arrays;
-
 public class ProgrammersMain {
     public static void main(final String[] args) {
         final Solution ts = new Solution();
 
-        System.out.println(Arrays.toString(
-                ts.solution(
-                        new String[]{"classic", "pop", "classic", "classic", "pop"}
-                        , new int[]{500, 600, 150, 800, 2500}
-                )
-        ));
-        System.out.println(Arrays.toString(
-                ts.solution(
-                        new String[]{"classic", "pop", "classic", "classic"}
-                        , new int[]{800, 600, 150, 800}
-                )
-        ));
-        System.out.println(Arrays.toString(
-                ts.solution(
-                        new String[]{"A", "A", "B", "A", "B", "B", "A", "A", "A", "A"}
-                        , new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-                )
-        ));
-        System.out.println(Arrays.toString(
-                ts.solution(
-                        new String[]{"classic", "pop", "classic", "classic", "jazz", "pop", "Rock", "jazz"}
-                        , new int[]{500, 600, 150, 800, 1100, 2500, 100, 1000}
-                )
-        ));
+//        System.out.println(Arrays.toString(
+//                ts.solution(
+//                        new String[]{"classic", "pop", "classic", "classic", "pop"}
+//                )
+//        ));
+//        System.out.println(Arrays.toString(
+//                ts.solution(
+//                        new String[]{"classic", "pop", "classic", "classic"}
+//                )
+//        ));
+//        System.out.println(Arrays.toString(
+//                ts.solution(
+//                        new String[]{"A", "A", "B", "A", "B", "B", "A", "A", "A", "A"}
+//                )
+//        ));
+//        System.out.println(Arrays.toString(
+//                ts.solution(
+//                        new String[]{"classic", "pop", "classic", "classic", "jazz", "pop", "Rock", "jazz"}
+//                )
+//        ));
     }
 }
 
--- CodingTest/softeer/Main.java
+++ CodingTest/softeer/Main.java
@@ -1,71 +1,55 @@
 package CodingTest.softeer;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class Main {
-    static int size, manCount;
-    static int[][] board = new int[22][22];
-    static int[][] visited = new int[22][22];
-    static long sum = 0;
-    static long ans = 0;
-    static int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    static ArrayList<Man> manList = new ArrayList<>();
+    public static void main(final String[] args) throws Exception {
+        final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(final String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        size = scanner.nextInt();
-        manCount = scanner.nextInt();
-        for (int i = 1; i <= size; i++) {
-            for (int j = 1; j <= size; j++) {
-                board[i][j] = scanner.nextInt();
+        final int[] split = Arrays.stream(bf.readLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+
+        int totalWeight = split[0];
+        final int typeNumber = split[1];
+
+        final List<Gem> gems = new ArrayList<>();
+        for (int i = 0; i < typeNumber; i++) {
+            final String[] temp = bf.readLine().split(" ");
+            gems.add(new Gem(temp[0], temp[1]));
+        }
+
+        gems.sort(Comparator.comparingInt(g -> (-1 * g.pricePerWeight)));
+
+
+        int answer = 0;
+        for (final Gem gem : gems) {
+            final int weight = gem.weight;
+            final int pricePerWeight = gem.pricePerWeight;
+
+            if (weight > totalWeight) {
+                answer += (totalWeight * pricePerWeight);
+                break;
             }
-        }
-        for (int i = 0; i < manCount; i++) {
-            final int manRow = scanner.nextInt();
-            final int manCol = scanner.nextInt();
-            sum += board[manRow][manCol];
-            visited[manRow][manCol] = 1;
-            manList.add(new Man(manRow, manCol));
-        }
-        dfs(manList.get(0).row, manList.get(0).col, 0, 0);
-        System.out.println(ans);
-        scanner.close();
-    }
 
-    static boolean canGo(final int nx, final int ny) {
-        return nx >= 1 && ny >= 1 && nx <= size && ny <= size;
-    }
-
-    static void dfs(final int row, final int col, final int depth, final int id) {
-        if (visited[row][col] == 0) sum += board[row][col];
-        visited[row][col]++;
-
-        if (depth == 3 && id < manCount - 1) {
-            final Man man = manList.get(id + 1);
-            dfs(man.row, man.col, 0, id + 1);
-        } else if (depth == 3 && id == manCount - 1) {
-            ans = Math.max(ans, sum);
-        } else {
-            for (final int[] offset : directions) {
-                final int nextRow = row + offset[0];
-                final int nextCol = col + offset[1];
-                if (canGo(nextRow, nextCol)) {
-                    dfs(nextRow, nextCol, depth + 1, id);
-                }
-            }
+            totalWeight -= weight;
+            answer += (weight * pricePerWeight);
         }
 
-        visited[row][col]--;
-        if (visited[row][col] == 0) sum -= board[row][col];
+        System.out.println(answer);
     }
 
-    static class Man {
-        int row, col;
+    static class Gem {
+        int weight;
+        int pricePerWeight;
 
-        Man(final int row, final int col) {
-            this.row = row;
-            this.col = col;
+        Gem(final String weight, final String price) {
+            this.weight = Integer.parseInt(weight);
+            this.pricePerWeight = Integer.parseInt(price);
         }
     }
 }
