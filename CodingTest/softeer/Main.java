c80c19361d417ea6ffb22c81512a797b4401793e
HyunggiPark <phg9587@naver.com>
2024-03-22 13:03:00 +0900

    소프티어 나무 섭지 - 리팩터링

--- CodingTest/softeer/Main.java
+++ CodingTest/softeer/Main.java
@@ -25,41 +25,41 @@ public class Main {
     public static void main(final String[] args) throws Exception {
         final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        final int[] split = convertInts(br.readLine().split(" "));
+        //Map 사이즈 체크
+        final int[] split = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
         final int row = split[0];
         final int col = split[1];
 
-        final String[] map = new String[row];
-        for (int i = 0; i < row; i++) {
-            map[i] = br.readLine();
-        }
-
-        //좌표
+        //남우, 유령, 목표의 좌표를 저장
         final List<int[]> ghosts = new ArrayList<>();
         int[] namwoo = new int[2];
         int[] goal = new int[2];
 
-        for (int r = 0; r < map.length; r++) {
-            final String line = map[r];
+        final String[][] map = new String[row][col];
+        for (int r = 0; r < row; r++) {
+            final String[] tempRow = br.readLine().split("");
 
-            final char[] charArray = line.toCharArray();
+            for (int c = 0; c < tempRow.length; c++) {
+                final String ch = tempRow[c];
+                map[r][c] = ch;
 
-            for (int c = 0; c < charArray.length; c++) {
-                final char ch = charArray[c];
-                if (ch == 'N') { //남우
+                if (ch.equals("N")) { //남우
                     namwoo = new int[]{r, c};
-                } else if (ch == 'G') {
+                } else if (ch.equals("G")) { //유령
                     ghosts.add(new int[]{r, c});
-                } else if (ch == 'D') {
+                } else if (ch.equals("D")) { //목표
                     goal = new int[]{r, c};
                 }
             }
         }
 
-        //namwoo
+        //namwoo가 목표까지 도착하는데 몇초가 걸리는지 계산한다.
         final int namwooTime = bfs(map, namwoo, false);
 
+        //Comparator에서 goal을 쓰기 위해 final로 지정 (final 변수만이 람다식에서 사용이 가능)
         final int[] finalGoal = goal;
+        //모든 유령중에서 가장 목표와 가까운 순으로 유령 좌표를 정렬한다.
         ghosts.sort(
                 Comparator.comparingInt(
                         gh -> {
@@ -71,17 +71,21 @@ public class Main {
                 )
         );
 
+        //가장 가까운 유령이 목표에 몇초만에 도착하는지 계산한다.
         final int ghostTime = bfs(map, ghosts.get(0), true);
 
+        //남우가 목표에 도착할 수 없으면 No를 출력
         if (namwooTime == -1) {
             System.out.println("No");
             return;
         }
 
+        //남우보다 유령이 먼저 목표에 도착하면, 남우는 탈출이 불가능하다.
         System.out.println(ghostTime <= namwooTime ? "No" : "Yes");
     }
 
-    private static int bfs(final String[] map, final int[] obj, final boolean isWallPass) {
+    private static int bfs(final String[][] map, final int[] obj, final boolean isWallPass) {
+        //BFS를 쓰기 위해, 옮길 방향을 정의
         final int[][] directions = new int[][]{
                 {-1, 0},
                 {0, 1},
@@ -89,23 +93,29 @@ public class Main {
                 {0, -1},
         };
 
-        final boolean[][] isVisited = new boolean[map.length][map[0].length()];
+        //방문 좌표 체크용 boolean 배열
+        final boolean[][] isVisited = new boolean[map.length][map[0].length];
+        //BFS 쓰기 위해 Deque를 정의
         final Deque<int[]> dq = new ArrayDeque<>();
+        //초기값 (즉, 유령 혹은 남우의 초기 좌표를 입력)
         dq.addLast(new int[]{obj[0], obj[1], 0});
+        //방문 처리
         isVisited[obj[0]][obj[1]] = true;
 
+        //Deque가 빌 때까지, BSF를 수행
         while (!dq.isEmpty()) {
             final int[] cur = dq.pollFirst();
-            if (map[cur[0]].charAt(cur[1]) == 'D') {
+            if (map[cur[0]][cur[1]].equals("D")) { //목표에 도착하면 종료
                 return cur[2];
             }
 
+            //4방향을 탐색
             for (final int[] direction : directions) {
                 final int nextRow = cur[0] + direction[0];
                 final int nextCol = cur[1] + direction[1];
 
-                //범위를 벗어나는지,
-                if (isOut(map, nextRow, nextCol)) {
+                //범위를 벗어나는지 검사
+                if (isOutMap(map, nextRow, nextCol)) {
                     continue;
                 }
                 //이미 방문했는지 검사
@@ -113,24 +123,23 @@ public class Main {
                     continue;
                 }
                 //벽에 부딪히는지 검사
-                if (!isWallPass && map[nextRow].charAt(nextCol) == '#') {
+                if (!isWallPass && map[nextRow][nextCol].equals("#")) {
                     continue;
                 }
 
+                //방문 체크
                 isVisited[nextRow][nextCol] = true;
+                //Deque에 추가
                 dq.addLast(new int[]{nextRow, nextCol, cur[2] + 1});
             }
         }
 
+        //탈출하지 못했으면 -1를 리턴
         return -1;
     }
 
-    private static boolean isOut(final String[] map, final int row, final int col) {
-        return row < 0 || row >= map.length || col < 0 || col >= map[0].length();
-    }
-
-    private static int[] convertInts(final String[] s) {
-        return Arrays.stream(s).mapToInt(Integer::parseInt).toArray();
+    //Map을 벗어나는지, 아닌지 확인
+    private static boolean isOutMap(final String[][] map, final int row, final int col) {
+        return row < 0 || row >= map.length || col < 0 || col >= map[0].length;
     }
 }
-
