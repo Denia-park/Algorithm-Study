364f41ee3e9e295772920d180d12985172da559f
HyunggiPark <phg9587@naver.com>
2024-03-24 17:14:58 +0900

    leetcode Find the Duplicate Number - 1차 풀이 완료

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -1,72 +1,44 @@
 package CodingTest.LeetCode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Quiz {
     public static void main(final String[] args) {
         final Solution solution = new Solution();
 
-//        System.out.println(
-//                solution.mergeInBetween(
-//                        new char[]{'A', 'A', 'A', 'B', 'B', 'B'},
-//                        2
-//                )
-//        );
+        System.out.println(
+                solution.findDuplicate(
+                        new int[]{1, 3, 4, 2, 2}
+                )
+        );
+
+        System.out.println(
+                solution.findDuplicate(
+                        new int[]{3, 1, 3, 4, 2}
+                )
+        );
+
+        System.out.println(
+                solution.findDuplicate(
+                        new int[]{3, 3, 3, 3, 3}
+                )
+        );
     }
 }
 
 class Solution {
-    public ListNode reverse(final ListNode head) {
-        if (head == null) return null;
-        ListNode prev = null;
-        ListNode curr = head;
-        ListNode nextNode = null;
-        while (curr != null) {
-            nextNode = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nextNode;
+    public int findDuplicate(final int[] nums) {
+        final Set<Integer> set = new HashSet<>();
+
+        for (final int num : nums) {
+            if (set.contains(num)) {
+                return num;
+            } else {
+                set.add(num);
+            }
         }
-        return prev;
-    }
 
-    public void merge(ListNode list1, ListNode list2) {
-        while (list2 != null) {
-            final ListNode nextNode = list1.next;
-            list1.next = list2;
-            list1 = list2;
-            list2 = nextNode;
-        }
-    }
-
-    public void reorderList(final ListNode head) {
-        if (head == null || head.next == null) return;
-        ListNode slow = head;
-        ListNode fast = head;
-        ListNode prev = head;
-        while (fast != null && fast.next != null) {
-            prev = slow;
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        prev.next = null;
-        final ListNode list1 = head;
-        final ListNode list2 = reverse(slow);
-        merge(list1, list2);
-    }
-}
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(final int val) {
-        this.val = val;
-    }
-
-    ListNode(final int val, final ListNode next) {
-        this.val = val;
-        this.next = next;
+        return -1;
     }
 }
