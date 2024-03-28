31eb63d7614ff5393eba8650428fa9988d744808
HyunggiPark <phg9587@naver.com>
2024-03-28 21:02:04 +0900

    leetcode Length of Longest Subarray With at Most K Frequency - 정답 참고 - 수정

--- CodingTest/LeetCode/Quiz.java
+++ CodingTest/LeetCode/Quiz.java
@@ -29,16 +29,16 @@ public class Quiz {
 
 class Solution {
     public int maxSubarrayLength(final int[] nums, final int k) {
-        int ans = 0, start = -1;
+        int ans = 0, start = 0;
         final Map<Integer, Integer> frequency = new HashMap();
 
         for (int end = 0; end < nums.length; end++) {
             frequency.put(nums[end], frequency.getOrDefault(nums[end], 0) + 1);
             while (frequency.get(nums[end]) > k) {
-                start++;
                 frequency.put(nums[start], frequency.get(nums[start]) - 1);
+                start++;
             }
-            ans = Math.max(ans, end - start);
+            ans = Math.max(ans, end - start + 1);
         }
 
         return ans;
